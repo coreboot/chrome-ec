5301485a9db06887428c31f89d7e3bd59e79603f
David Hendricks <dhendrix@chromium.org>
2012-09-06 09:12:51 -0700

    Revert "Save panic data across reboots, and add panicinfo command"
    This reverts commit 5f92fdc0b6ec36beeb5c1d447f052e5f77d29e39
    
    Reverting based on Vincent's comment. Tested locally that this doesn't break compilation for Snow.
    
    Change-Id: I9d05ff300156448096af41b5cb2af972425dccf8
    Reviewed-on: https://gerrit.chromium.org/gerrit/32396
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Tested-by: David Hendricks <dhendrix@chromium.org>

--- common/system_common.c
+++ common/system_common.c
@@ -15,7 +15,6 @@
 #include "hooks.h"
 #include "host_command.h"
 #include "lpc.h"
-#include "panic.h"
 #include "system.h"
 #include "task.h"
 #include "uart.h"
@@ -61,8 +60,10 @@ struct jump_data {
 			       * data from the previous image. */
 };
 
-/* Jump data (at end of RAM, or preceding panic data) */
-static struct jump_data *jdata;
+/* Jump data goes at the end of RAM */
+static struct jump_data * const jdata =
+	(struct jump_data *)(CONFIG_RAM_BASE + CONFIG_RAM_SIZE
+			     - sizeof(struct jump_data));
 
 /*
  * Reset flag descriptions.  Must be in same order as bits of RESET_FLAG_
@@ -164,7 +165,7 @@ int system_add_jump_tag(uint16_t tag, int version, int size, const void *data)
 	struct jump_tag *t;
 
 	/* Only allowed during a sysjump */
-	if (!jdata || jdata->magic != JUMP_DATA_MAGIC)
+	if (jdata->magic != JUMP_DATA_MAGIC)
 		return EC_ERROR_UNKNOWN;
 
 	/* Make room for the new tag */
@@ -186,9 +187,6 @@ const uint8_t *system_get_jump_tag(uint16_t tag, int *version, int *size)
 {
 	const struct jump_tag *t;
 	int used = 0;
-
-	if (!jdata)
-		return NULL;
 
 	/* Search through tag data for a match */
 	while (used < jdata->jump_tag_total) {
@@ -458,18 +456,6 @@ const char *system_get_build_info(void)
 
 int system_common_pre_init(void)
 {
-	uint32_t addr;
-
-	/*
-	 * Put the jump data before the panic data, or at the end of RAM if
-	 * panic data is not present.
-	 */
-	addr = (uint32_t)panic_get_data();
-	if (!addr)
-		addr = CONFIG_RAM_BASE + CONFIG_RAM_SIZE;
-
-	jdata = (struct jump_data *)(addr - sizeof(struct jump_data));
-
 	/*
 	 * Check jump data if this is a jump between images.  Jumps all show up
 	 * as an unknown reset reason, because we jumped directly from one
