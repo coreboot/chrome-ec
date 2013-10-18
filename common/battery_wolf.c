ca7282a9df5128ab50ce2327f862129cdf78edc1
Henry Hsu <Henry.Hsu@quantatw.com>
2013-10-18 19:32:40 +0800

    Support battery ship mode command
    Support wolf enter ship mode.
    
    BUG=chrome-os-partner:23431
    BRANCH=Wolf
    TEST=manual
    triger command 'ectool batterycutoff', then system shut down
    after 10 seconds.
    
    Change-Id: If3c23ebb33b83ebe4c98726ed6309696b94fb795
    Signed-off-by: Henry Hsu <Henry.Hsu@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/173666
    Reviewed-by: Dave Parker <dparker@chromium.org>

--- common/battery_wolf.c
+++ common/battery_wolf.c
@@ -7,6 +7,11 @@
 
 #include "battery_pack.h"
 #include "gpio.h"
+#include "host_command.h"
+#include "smart_battery.h"
+
+#define SB_SHIP_MODE_ADDR	0x00
+#define SB_SHIP_MODE_DATA	0x0010
 
 const struct battery_temperature_ranges bat_temp_ranges = {
 	.start_charging_min_c = 0,
@@ -32,6 +37,20 @@ const struct battery_info *battery_get_info(void)
 {
 	return &info;
 }
+
+/* Triger the command twice to make sure the battery can be cut-off.
+ * Need to confirm with vendor why the command should be trigered twice.
+ * By the result of experiment, it's ok and can be cut-off. */
+int battery_command_cut_off(struct host_cmd_handler_args *args)
+{
+	int rv;
+	rv = sb_write(SB_SHIP_MODE_ADDR, SB_SHIP_MODE_DATA);
+	if (!rv)
+		rv = sb_write(SB_SHIP_MODE_ADDR, SB_SHIP_MODE_DATA);
+	return rv;
+}
+DECLARE_HOST_COMMAND(EC_CMD_BATTERY_CUT_OFF, battery_command_cut_off,
+		     EC_VER_MASK(0));
 
 /* FIXME: The smart battery should do the right thing - that's why it's
  * called "smart". Do we really want to second-guess it? For now, let's not. */
