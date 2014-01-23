2e0d4a0a8e6667dd78b6cb950333c3fc76b9d059
Henry Hsu <Henry.Hsu@quantatw.com>
2014-01-23 18:53:10 +0800

    Add battery_detect support.
    Glimmer board can detect battery by Hardware circuit. Add this
    function will skip 30s when boot with AC only.
    
    BUG=chrome-os-partner:24649
    BRANCH=glimmer
    TEST=Remove battery then plug AC, it boot quickly. Use ec console
    to check BAT_DETECT_L pin, I got high when batteryless and vice
    versa.
    
    Change-Id: Iee0bc0346c2574f3ec3ba971eca72730500c1c7a
    Signed-off-by: Henry Hsu <Henry.Hsu@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/183607
    Reviewed-by: Dave Parker <dparker@chromium.org>

--- board/glimmer/board.h
+++ board/glimmer/board.h
@@ -11,6 +11,7 @@
 /* Optional features */
 #define CONFIG_AP_HANG_DETECT
 #define CONFIG_BACKLIGHT_LID
+#define CONFIG_BATTERY_CHECK_CONNECTED
 #define CONFIG_BATTERY_SMART
 #define CONFIG_BOARD_VERSION
 #define CONFIG_CHARGER
@@ -92,6 +93,7 @@ enum gpio_signal {
 	GPIO_PP1000_S0IX_PGOOD,    /* Power good on 1.00V (S0iX supplies) */
 	GPIO_USB1_OC_L,            /* USB port overcurrent warning */
 	GPIO_USB2_OC_L,            /* USB port overcurrent warning */
+	GPIO_BAT_DETECT_L,         /* Battery detect. Repurposed BAT_TEMP */
 
 	/* Outputs */
 	GPIO_CPU_PROCHOT,          /* Force CPU to think it's overheated */
