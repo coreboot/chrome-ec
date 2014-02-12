7caca84303d731230862c6298027a5231889636f
Jaehoon Kim <jh228.kim@samsung.com>
2014-02-12 10:39:07 +0900

    Pit: Change the temperature range in discharging state.
    Samsung change the temperature range in battery discharing state from (0 <=
    temp < 100) to (-20 <= temp < 70) from this year. That's why we have to
    change the battery_temperature_range structure value as soon as possible
    so that our reliability team check the battery.
    
    BUG=chrome-os-partner:25731
    TEST=Boot on the pit / pi and then check the battery is working or
    not in the oven.
    BRANCH=pit
    
    Change-Id: Idc41bda7f97dfeaaac7857002d828b4a60f5f4d3
    Signed-off-by: Jaehoon Kim <jh228.kim@samsung.com>
    Reviewed-on: https://chromium-review.googlesource.com/185817
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

--- board/pit/board.c
+++ board/pit/board.c
@@ -98,8 +98,8 @@ const struct battery_temperature_ranges bat_temp_ranges = {
 	.start_charging_max_c = 45,
 	.charging_min_c       = 0,
 	.charging_max_c       = 60,
-	.discharging_min_c    = 0,
-	.discharging_max_c    = 100,
+	.discharging_min_c    = -20,
+	.discharging_max_c    = 70,
 };
 
 /* I2C ports */
