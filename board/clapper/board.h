0fe4d0e12116448acfa55dea6277ba65229abf36
git-CarryBK_Chen.compal.com <CarryBK_Chen@compal.com>
2014-02-20 20:21:04 +0800

    Clapper: Modify input current
    Change to 2.048A
    
    BRANCH=clapper
    BUG=chrome-os-partner:24373
    TEST=manually
    Signed-off-by: Carry Chen <CarryBK_Chen@compal.com>
    
    Change-Id: I26b4026ba12355670f376148456c512a9bd12c0f
    Reviewed-on: https://chromium-review.googlesource.com/187232
    Reviewed-by: Dave Parker <dparker@chromium.org>
    Commit-Queue: Dave Parker <dparker@chromium.org>
    Tested-by: Dave Parker <dparker@chromium.org>

--- board/clapper/board.h
+++ board/clapper/board.h
@@ -17,9 +17,8 @@
 #define CONFIG_BUTTON_COUNT 2
 #define CONFIG_CHARGER
 #define CONFIG_CHARGER_BQ24715
-/* TODO(crosbug.com/p/24374): Set input current to match adapter. */
 #define CONFIG_CHARGER_DISCHARGE_ON_AC
-#define CONFIG_CHARGER_INPUT_CURRENT 1700   /* 33 W adapter, 19 V, 1.75 A */
+#define CONFIG_CHARGER_INPUT_CURRENT 2048   /* 45 W adapter, 20 V, 2.048 A */
 #define CONFIG_CHARGER_SENSE_RESISTOR 10    /* Charge sense resistor, mOhm */
 #define CONFIG_CHARGER_SENSE_RESISTOR_AC 10 /* Input senso resistor, mOhm */
 #define CONFIG_CHIPSET_BAYTRAIL
