jackadam1981/ChromeEc

Add 'at-shutdown' option to batterycutoff host command
If at-shutdown is specified, the battery is cut off
5 seconds after the host has shutdown.

BUG=chrome-os-partner:28887
BRANCH=None
TEST=Run batterycutoff ectool command and cutoff console
command with and without 'at-shutdown' option. Verify
the battery is cut off immediately without the option
specified and 5 seconds after shutdown with. View the
console log to see the deferred cutoff occur.

Change-Id: Id4bacf79ad3add885260655f80cb8127bafe1ad6
Signed-off-by: Dave Parker <dparker@google.com>

common/battery.c
@@ -17,8 +17,14 @@
 #include "util.h"
 #include "watchdog.h"
 
-#ifdef CONFIG_BATTERY_CUT_OFF
-static int is_cut_off;
+#define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
+
+#ifdef CONFIG_BATTERY_CUTOFF
+#define BATTERY_CUTOFF_DELAY_US (5 * SECOND)
+
+enum battery_cutoff_states battery_cutoff_state =
+	BATTERY_CUTOFF_STATE_NORMAL;
+
 #endif
 
 #ifdef CONFIG_BATTERY_PRESENT_GPIO
@@ -261,41 +267,88 @@ DECLARE_CONSOLE_COMMAND(battery, command_battery,
 			"Print battery info",
 			NULL);
 
-
-#ifdef CONFIG_BATTERY_CUT_OFF
+#ifdef CONFIG_BATTERY_CUTOFF
 int battery_is_cut_off(void)
 {
-	return is_cut_off;
+	return (battery_cutoff_state == BATTERY_CUTOFF_STATE_CUTOFF);
 }
 
-static void clean_cut_off(void)
+static void clear_pending_cutoff(void)
 {
 	if (extpower_is_present())
-		is_cut_off = 0;
+		battery_cutoff_state = BATTERY_CUTOFF_STATE_NORMAL;
 }
-DECLARE_HOOK(HOOK_AC_CHANGE, clean_cut_off, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_AC_CHANGE, clear_pending_cutoff, HOOK_PRIO_DEFAULT);
 
-static int battery_command_cut_off(struct host_cmd_handler_args *args)
+static int battery_command_cutoff(struct host_cmd_handler_args *args)
 {
-	int rv = board_cut_off_battery();
+	const struct ec_params_battery_cutoff *p;
+	int rv;
+
+	if (args->version == 1) {
+		p = args->params;
+		if (p->flags & EC_BATTERY_CUTOFF_FLAG_AT_SHUTDOWN) {
+			battery_cutoff_state = BATTERY_CUTOFF_STATE_PENDING;
+			return EC_RES_SUCCESS;
+		}
+	}
+
+	rv = board_cutoff_battery();
 	if (!rv)
-		is_cut_off = 1;
+		battery_cutoff_state = BATTERY_CUTOFF_STATE_CUTOFF;
 
 	return rv;
 }
-DECLARE_HOST_COMMAND(EC_CMD_BATTERY_CUT_OFF, battery_command_cut_off,
-		EC_VER_MASK(0));
+DECLARE_HOST_COMMAND(EC_CMD_BATTERY_CUT_OFF, battery_command_cutoff,
+		EC_VER_MASK(0) | EC_VER_MASK(1));
+
+static void pending_cutoff_deferred(void)
+{
+	int rv;
+
+	rv = board_cutoff_battery();
+
+	if (rv == EC_SUCCESS)
+		CPRINTF("[%T Battery cut off succeeded.]\n");
+	else
+		CPRINTF("[%T Battery cut off failed!]\n");
+}
+DECLARE_DEFERRED(pending_cutoff_deferred);
+
+static void check_pending_cutoff(void)
+{
+	if (battery_cutoff_state == BATTERY_CUTOFF_STATE_PENDING) {
+		CPRINTF("[%T Cutting off battery in %d second(s)]\n",
+		BATTERY_CUTOFF_DELAY_US / SECOND);
+		hook_call_deferred(pending_cutoff_deferred,
+				   BATTERY_CUTOFF_DELAY_US);
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, check_pending_cutoff, HOOK_PRIO_LAST);
 
 static int command_cutoff(int argc, char **argv)
 {
-	int rv = board_cut_off_battery();
-	if (!rv)
-		is_cut_off = 1;
+	int rv;
+
+	if (argc > 1) {
+		if (!strcasecmp(argv[1], "at-shutdown")) {
+			battery_cutoff_state = BATTERY_CUTOFF_STATE_PENDING;
+			return EC_SUCCESS;
+		} else {
+			return EC_ERROR_INVAL;
+		}
+	}
+
+	rv = board_cutoff_battery();
+	if (!rv) {
+		ccprintf("[%T Battery cut off]\n");
+		battery_cutoff_state = BATTERY_CUTOFF_STATE_CUTOFF;
+	}
 
 	return rv;
 }
 DECLARE_CONSOLE_COMMAND(cutoff, command_cutoff,
-		"",
+		"[at-shutdown]",
 		"Cut off the battery output",
 		NULL);
 #else
@@ -303,7 +356,7 @@ int battery_is_cut_off(void)
 {
 	return 0;  /* Always return NOT cut off */
 }
-#endif  /* CONFIG_BATTERY_CUT_OFF */
+#endif  /* CONFIG_BATTERY_CUTOFF */
 
 #ifdef CONFIG_BATTERY_VENDOR_PARAM
 static int console_command_battery_vendor_param(int argc, char **argv)

coreboot/chrome-ec

1 seconds after the host has shutdown.
BUG=chrome-os-partner:29292,chrome-os-partner:28887
BRANCH=tot,nyan
specified and 1 seconds after shutdown with. View the
The following tests are verified on big.
console:
  cutoff, AC on: system is off after removing AC.
  cutoff, AC off: system is off immediately.
  at-shutdown, AC on: system is off after "power off" and removing AC.
  at-shutdown, AC off: system is off after "power off".
ectool:
  batterycutoff, AC on: system is off after removing AC.
  batterycutoff, AC off: system is off immediately.
  at-shutdown, AC on: battery is cut off after 1s of shutdown.
                      system is off right after removing AC power.
  at-shutdown, AC off: system is off after 1s of shutdown.
      [84.058416 power state 3 = S0, in 0x0000]
      [84.058803 power lost input; wanted 0x0001, got 0x0000]
      [84.059120 power off 3]
      [84.072148 Cutting off battery in 1 second(s)]
      [84.123896 power shutdown complete]
      [84.128790 power state 7 = S0->S3, in 0x0002]
      [84.139694 power state 2 = S3, in 0x0002]
      [84.150857 power state 8 = S3->S5, in 0x0002]
      [84.166975 power state 1 = S5, in 0x0002]
      [84.177972 power state 1 = S5, in 0x0002]
      [85.080012 Battery cut off succeeded.]
Change-Id: I56236187fb41c3b9f8cb0c4d0e48080922f075a2
Original-Change-Id: Id4bacf79ad3add885260655f80cb8127bafe1ad6
Signed-off-by: Louis Yung-Chieh Lo <yjlou@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/203849
Reviewed-by: David Hendricks <dhendrix@chromium.org>

common/battery.c
@@ -17,8 +17,17 @@
 #include "util.h"
 #include "watchdog.h"
 
+#define CPRINTF(format, args...) cprintf(CC_CHARGER, format, ## args)
+
 #ifdef CONFIG_BATTERY_CUT_OFF
-static int is_cut_off;
+
+#ifndef CONFIG_BATTERY_CUTOFF_DELAY_US
+#define CONFIG_BATTERY_CUTOFF_DELAY_US (1 * SECOND)
+#endif
+
+static enum battery_cutoff_states battery_cutoff_state =
+	BATTERY_CUTOFF_STATE_NORMAL;
+
 #endif
 
 #ifdef CONFIG_BATTERY_PRESENT_GPIO
@@ -256,41 +265,95 @@ DECLARE_CONSOLE_COMMAND(battery, command_battery,
 			"Print battery info",
 			NULL);
 
-
 #ifdef CONFIG_BATTERY_CUT_OFF
 int battery_is_cut_off(void)
 {
-	return is_cut_off;
+	return (battery_cutoff_state == BATTERY_CUTOFF_STATE_CUT_OFF);
 }
 
-static void clean_cut_off(void)
+static void pending_cutoff_deferred(void)
 {
-	if (extpower_is_present())
-		is_cut_off = 0;
-}
-DECLARE_HOOK(HOOK_AC_CHANGE, clean_cut_off, HOOK_PRIO_DEFAULT);
+	int rv;
 
-static int battery_command_cut_off(struct host_cmd_handler_args *args)
+	rv = board_cut_off_battery();
+
+	if (rv == EC_SUCCESS)
+		CPRINTF("[%T Battery cut off succeeded.]\n");
+	else
+		CPRINTF("[%T Battery cut off failed!]\n");
+}
+DECLARE_DEFERRED(pending_cutoff_deferred);
+
+static void clear_pending_cutoff(void)
 {
-	int rv = board_cut_off_battery();
-	if (!rv)
-		is_cut_off = 1;
+	if (extpower_is_present()) {
+		battery_cutoff_state = BATTERY_CUTOFF_STATE_NORMAL;
+		hook_call_deferred(pending_cutoff_deferred, -1);
+	}
+}
+DECLARE_HOOK(HOOK_AC_CHANGE, clear_pending_cutoff, HOOK_PRIO_DEFAULT);
+
+static int battery_command_cutoff(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_battery_cutoff *p;
+	int rv;
+
+	if (args->version == 1) {
+		p = args->params;
+		if (p->flags & EC_BATTERY_CUTOFF_FLAG_AT_SHUTDOWN) {
+			battery_cutoff_state = BATTERY_CUTOFF_STATE_PENDING;
+			CPRINTF("[%T BAT cut off at-shutdown is scheduled]\n");
+			return EC_RES_SUCCESS;
+		}
+	}
+
+	rv = board_cut_off_battery();
+	if (!rv) {
+		CPRINTF("[%T Battery cut off is successful.]\n");
+		battery_cutoff_state = BATTERY_CUTOFF_STATE_CUT_OFF;
+	} else {
+		CPRINTF("[%T Battery cut off has failed.]\n");
+	}
 
 	return rv;
 }
-DECLARE_HOST_COMMAND(EC_CMD_BATTERY_CUT_OFF, battery_command_cut_off,
-		EC_VER_MASK(0));
+DECLARE_HOST_COMMAND(EC_CMD_BATTERY_CUT_OFF, battery_command_cutoff,
+		EC_VER_MASK(0) | EC_VER_MASK(1));
+
+static void check_pending_cutoff(void)
+{
+	if (battery_cutoff_state == BATTERY_CUTOFF_STATE_PENDING) {
+		CPRINTF("[%T Cutting off battery in %d second(s)]\n",
+			CONFIG_BATTERY_CUTOFF_DELAY_US / SECOND);
+		hook_call_deferred(pending_cutoff_deferred,
+				   CONFIG_BATTERY_CUTOFF_DELAY_US);
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_SHUTDOWN, check_pending_cutoff, HOOK_PRIO_LAST);
 
 static int command_cutoff(int argc, char **argv)
 {
-	int rv = board_cut_off_battery();
-	if (!rv)
-		is_cut_off = 1;
+	int rv;
+
+	if (argc > 1) {
+		if (!strcasecmp(argv[1], "at-shutdown")) {
+			battery_cutoff_state = BATTERY_CUTOFF_STATE_PENDING;
+			return EC_SUCCESS;
+		} else {
+			return EC_ERROR_INVAL;
+		}
+	}
+
+	rv = board_cut_off_battery();
+	if (!rv) {
+		ccprintf("[%T Battery cut off]\n");
+		battery_cutoff_state = BATTERY_CUTOFF_STATE_CUT_OFF;
+	}
 
 	return rv;
 }
 DECLARE_CONSOLE_COMMAND(cutoff, command_cutoff,
-		"",
+		"[at-shutdown]",
 		"Cut off the battery output",
 		NULL);
 #else