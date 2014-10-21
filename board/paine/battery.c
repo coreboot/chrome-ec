a28e8bdaf614a1f6405488a5b4dba9510f511380
Hsu Henry <Henry.Hsu@quantatw.com>
2014-10-21 16:38:48 +0800

    Paine: Update battery spec
    Update the parameter from the spec.
    
    BUG=chrome-os-partner:33083
    BRANCH=paine
    TEST=flash ec, the "ectool batterycutoff" works with the two battery.
    
    Change-Id: Ifcdde2d9e2dd4fdaf3ffa2b5bb4c007fcdc3826e
    Signed-off-by: Henry Hsu <Henry.Hsu@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/224598
    Reviewed-by: Mohammed Habibulla <moch@chromium.org>

--- board/paine/battery.c
+++ board/paine/battery.c
@@ -13,22 +13,22 @@
 #define SB_SHIP_MODE_ADDR	0x3a
 #define SB_SHIP_MODE_DATA	0xc574
 
-/* Values for 54Wh 3UPF656790-1-T1001 battery */
+/* Values for 45W 3UAF576790-1-T1183 & LIS3105ACPC(SY6) batteries */
 static const struct battery_info info = {
 
-	.voltage_max    = 12600,
-	.voltage_normal = 11100, /* Average of max & min */
+	.voltage_max    = 13050,
+	.voltage_normal = 11025, /* Average of max & min */
 	.voltage_min    =  9000,
 
 	/* Pre-charge values. */
-	.precharge_current  = 392,	/* mA */
+	.precharge_current  = 256,	/* mA */
 
 	.start_charging_min_c = 0,
-	.start_charging_max_c = 60,
+	.start_charging_max_c = 50,
 	.charging_min_c       = 0,
 	.charging_max_c       = 60,
 	.discharging_min_c    = 0,
-	.discharging_max_c    = 50,
+	.discharging_max_c    = 60,
 };
 
 const struct battery_info *battery_get_info(void)
