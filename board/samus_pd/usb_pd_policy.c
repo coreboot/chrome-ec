jackadam1981/ChromeEc

samus_pd: Open USB data switches in UFP mode
Samus USB ports can't actually act as UFPs, so open switches when in
UFP mode.

BUG=chrome-os-partner:32003
TEST=Manual on Samus. Connect two Samus units, run `pd 1 swap data`,
verify that switches are opened on switch to UFP. Unplug samus and
connect a USB 2.0 device instead, verify that ports are again closed.
BRANCH=samus

Change-Id: I9e1ca58089caf29e419698c8426bf8b72500833a
Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>

board/samus_pd/usb_pd_policy.c
@@ -10,6 +10,7 @@
 #include "gpio.h"
 #include "hooks.h"
 #include "host_command.h"
+#include "pi3usb9281.h"
 #include "registers.h"
 #include "task.h"
 #include "timer.h"
@@ -226,7 +227,8 @@ int pd_check_data_swap(int port, int data_role)
 
 void pd_execute_data_swap(int port, int data_role)
 {
-	/* TODO: when switching to UFP need to open D+/D- switches */
+	/* Open USB switches when taking UFP role */
+	pi3usb9281_open_switches(port, (data_role == PD_ROLE_UFP));
 }
 
 void pd_new_contract(int port, int pr_role, int dr_role,

driver/usb_switch_pi3usb9281.c
@@ -185,6 +185,17 @@ int pi3usb9281_set_pins(uint8_t chip_idx, uint8_t val)
 	return pi3usb9281_write(chip_idx, PI3USB9281_REG_MANUAL, val);
 }
 
+int pi3usb9281_open_switches(uint8_t chip_idx, int open)
+{
+	uint8_t ctrl = pi3usb9281_read(chip_idx, PI3USB9281_REG_CONTROL) & 0x15;
+	if (open)
+		ctrl &= ~PI3USB9281_CTRL_SWITCH_AUTO;
+	else
+		ctrl |= PI3USB9281_CTRL_SWITCH_AUTO;
+
+	return pi3usb9281_write(chip_idx, PI3USB9281_REG_CONTROL, ctrl);
+}
+
 static void pi3usb9281_init(void)
 {
 	uint8_t dev_id;

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/233711
Reviewed-by: Alec Berg <alecaberg@chromium.org>
(cherry picked from commit c8f98e80fdbe85fc0bc0f892d4b22dad19186aef)
Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/235743
Tested-by: Duncan Laurie <dlaurie@chromium.org>
Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
Commit-Queue: Duncan Laurie <dlaurie@chromium.org>
Trybot-Ready: Duncan Laurie <dlaurie@chromium.org>

board/samus_pd/usb_pd_policy.c
@@ -10,6 +10,7 @@
 #include "gpio.h"
 #include "hooks.h"
 #include "host_command.h"
+#include "pi3usb9281.h"
 #include "registers.h"
 #include "task.h"
 #include "timer.h"
@@ -226,7 +227,8 @@ int pd_check_data_swap(int port, int data_role)
 
 void pd_execute_data_swap(int port, int data_role)
 {
-	/* TODO: when switching to UFP need to open D+/D- switches */
+	/* Open USB switches when taking UFP role */
+	pi3usb9281_set_switches(port, (data_role == PD_ROLE_UFP));
 }
 
 void pd_new_contract(int port, int pr_role, int dr_role,

driver/usb_switch_pi3usb9281.c
@@ -185,6 +185,17 @@ int pi3usb9281_set_pins(uint8_t chip_idx, uint8_t val)
 	return pi3usb9281_write(chip_idx, PI3USB9281_REG_MANUAL, val);
 }
 
+int pi3usb9281_set_switches(uint8_t chip_idx, int open)
+{
+	uint8_t ctrl = pi3usb9281_read(chip_idx, PI3USB9281_REG_CONTROL) & 0x15;
+	if (open)
+		ctrl &= ~PI3USB9281_CTRL_SWITCH_AUTO;
+	else
+		ctrl |= PI3USB9281_CTRL_SWITCH_AUTO;
+
+	return pi3usb9281_write(chip_idx, PI3USB9281_REG_CONTROL, ctrl);
+}
+
 static void pi3usb9281_init(void)
 {
 	uint8_t dev_id;