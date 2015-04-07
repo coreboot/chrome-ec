af19b81dbbc1d5d974618f788d5b1b0df19958cd
David Huang <David.Huang@quantatw.com>
2015-04-07 14:55:50 +0800

    Gandof: Init board
    Copy Yuna setting to init board.
    
    BUG=none
    BRANCH=gandof
    TEST=Run "make BOARD=gandof" and "make buildall -j" to build code
     and ec.bin can be generated.
    
    Change-Id: I5f6332a0af60fddce068fb8d59cd4a3420869da7
    Signed-off-by: David Huang <David.Huang@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/264247
    Reviewed-by: Mohammed Habibulla <moch@google.com>
    Commit-Queue: David Wu <david_wu@quantatw.com>
    Tested-by: David Wu <david_wu@quantatw.com>

---
+++ board/gandof/battery.c
@@ -0,0 +1,63 @@
+/* Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Battery pack vendor provided charging profile
+ */
+
+#include "battery.h"
+#include "battery_smart.h"
+#include "gpio.h"
+#include "host_command.h"
+
+#define SB_SHIP_MODE_ADDR	0x3a
+#define SB_SHIP_MODE_DATA	0xc574
+
+/* Values for 48Wh 4UAF495780-1-T1186/AC011353-PRR14G01 battery */
+static const struct battery_info info_4s1p = {
+
+	.voltage_max    = 17200,
+	.voltage_normal = 15200, /* Average of max & min */
+	.voltage_min    = 12000,
+
+	/* Pre-charge values. */
+	.precharge_current  = 256,	/* mA */
+
+	.start_charging_min_c = 0,
+	.start_charging_max_c = 50,
+	.charging_min_c       = 0,
+	.charging_max_c       = 60,
+	.discharging_min_c    = 0,
+	.discharging_max_c    = 40,
+};
+
+/* Values for 54Wh LIS3091ACPC(SYS6)/AC011401-PRR13G01 battery */
+static const struct battery_info info_3s1p = {
+
+	.voltage_max    = 12600,
+	.voltage_normal = 11100, /* Average of max & min */
+	.voltage_min    = 9000,
+
+	 /* Pre-charge values. */
+	.precharge_current  = 256,      /* mA */
+
+	.start_charging_min_c = 0,
+	.start_charging_max_c = 50,
+	.charging_min_c       = 0,
+	.charging_max_c       = 60,
+	.discharging_min_c    = 0,
+	.discharging_max_c    = 75,
+};
+
+const struct battery_info *battery_get_info(void)
+{
+	if (gpio_get_level(GPIO_BAT_ID))
+		return &info_3s1p;
+	else
+		return &info_4s1p;
+}
+
+int board_cut_off_battery(void)
+{
+	return sb_write(SB_SHIP_MODE_ADDR, SB_SHIP_MODE_DATA);
+}
---
+++ board/gandof/board.c
@@ -0,0 +1,207 @@
+/* Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* EC for Gandof board configuration */
+
+#include "adc.h"
+#include "adc_chip.h"
+#include "backlight.h"
+#include "chipset.h"
+#include "common.h"
+#include "console.h"
+#include "driver/temp_sensor/g781.h"
+#include "extpower.h"
+#include "fan.h"
+#include "gpio.h"
+#include "host_command.h"
+#include "i2c.h"
+#include "jtag.h"
+#include "keyboard_scan.h"
+#include "lid_switch.h"
+#include "peci.h"
+#include "power.h"
+#include "power_button.h"
+#include "pwm.h"
+#include "pwm_chip.h"
+#include "registers.h"
+#include "switch.h"
+#include "temp_sensor.h"
+#include "temp_sensor_chip.h"
+#include "thermal.h"
+#include "timer.h"
+#include "uart.h"
+#include "util.h"
+
+#include "gpio_list.h"
+
+#ifdef CONFIG_FAN_RPM_CUSTOM
+#define NUM_FAN_LEVELS 8
+
+struct fan_step {
+	int on;
+	int off;
+	int rpm;
+};
+
+/* Do not make the fan on/off point equal to 0 or 100 */
+const struct fan_step fan_table[NUM_FAN_LEVELS] = {
+	{.rpm = 0},
+	{.on = 9, .off = 1, .rpm = 3200},
+	{.on = 20, .off = 12, .rpm = 3700},
+	{.on = 31, .off = 23, .rpm = 4000},
+	{.on = 37, .off = 29, .rpm = 4400},
+	{.on = 40, .off = 32, .rpm = 4900},
+	{.on = 59, .off = 51, .rpm = 5500},
+	{.on = 98, .off = 90, .rpm = 6500},
+};
+
+int fan_percent_to_rpm(int fan, int pct)
+{
+	static int index;
+	static int previous_pct;
+	int i;
+	int temp_index;
+
+	/*
+	 * Compare the pct and previous pct, we have the three paths :
+	 *  1. decreasing path. (check the off point)
+	 *  2. increasing path. (check the on point)
+	 *  3. invariant path. (return the current RPM)
+	 */
+	if (pct == previous_pct) {
+		temp_index = index;
+	} else if (pct < previous_pct) {
+		temp_index = 0;
+		for (i = 1; i <= index; i++) {
+			if (pct > fan_table[i].off)
+				temp_index = i;
+			else
+				break;
+		}
+	} else {
+		temp_index = NUM_FAN_LEVELS - 1;
+		for (i = NUM_FAN_LEVELS - 1; i > index; i--) {
+			if (pct < fan_table[i].on)
+				temp_index = i - 1;
+			else
+				break;
+		}
+	}
+	index = temp_index;
+
+	previous_pct = pct;
+
+	if (fan_table[index].rpm != fan_get_rpm_target(fans[fan].ch))
+		cprintf(CC_THERMAL, "[%T Setting fan RPM to %d]\n",
+			fan_table[index].rpm);
+
+	return fan_table[index].rpm;
+}
+#endif /* CONFIG_FAN_RPM_CUSTOM */
+
+/* power signal list.  Must match order of enum power_signal. */
+const struct power_signal_info power_signal_list[] = {
+	{GPIO_PP5000_PGOOD,  1, "PGOOD_PP5000"},
+	{GPIO_PP1350_PGOOD,  1, "PGOOD_PP1350"},
+	{GPIO_PP1050_PGOOD,  1, "PGOOD_PP1050"},
+	{GPIO_VCORE_PGOOD,   1, "PGOOD_VCORE"},
+	{GPIO_PCH_SLP_S0_L,  1, "SLP_S0#_DEASSERTED"},
+	{GPIO_PCH_SLP_S3_L,  1, "SLP_S3#_DEASSERTED"},
+	{GPIO_PCH_SLP_S5_L,  1, "SLP_S5#_DEASSERTED"},
+	{GPIO_PCH_SLP_SUS_L, 1, "SLP_SUS#_DEASSERTED"},
+};
+BUILD_ASSERT(ARRAY_SIZE(power_signal_list) == POWER_SIGNAL_COUNT);
+
+/* ADC channels. Must be in the exactly same order as in enum adc_channel. */
+const struct adc_t adc_channels[] = {
+	/* EC internal temperature is calculated by
+	 * 273 + (295 - 450 * ADC_VALUE / ADC_READ_MAX) / 2
+	 * = -225 * ADC_VALUE / ADC_READ_MAX + 420.5
+	 */
+	{"ECTemp", LM4_ADC_SEQ0, -225, ADC_READ_MAX, 420,
+	 LM4_AIN_NONE, 0x0e /* TS0 | IE0 | END0 */, 0, 0},
+
+	/* IOUT == ICMNT is on PE3/AIN0 */
+	/* We have 0.01-ohm resistors, and IOUT is 20X the differential
+	 * voltage, so 1000mA ==> 200mV.
+	 * ADC returns 0x000-0xFFF, which maps to 0.0-3.3V (as configured).
+	 * mA = 1000 * ADC_VALUE / ADC_READ_MAX * 3300 / 200
+	 */
+	{"ChargerCurrent", LM4_ADC_SEQ1, 33000, ADC_READ_MAX * 2, 0,
+	 LM4_AIN(0), 0x06 /* IE0 | END0 */, LM4_GPIO_E, (1<<3)},
+};
+BUILD_ASSERT(ARRAY_SIZE(adc_channels) == ADC_CH_COUNT);
+
+/* PWM channels. Must be in the exactly same order as in enum pwm_channel. */
+const struct pwm_t pwm_channels[] = {
+	{0, 0},
+};
+BUILD_ASSERT(ARRAY_SIZE(pwm_channels) == PWM_CH_COUNT);
+
+/* Physical fans. These are logically separate from pwm_channels. */
+const struct fan_t fans[] = {
+	{.flags = FAN_USE_RPM_MODE,
+	 .rpm_min = 3200,
+	 .rpm_max = 6500,
+	 .ch = 2,
+	 .pgood_gpio = GPIO_PP5000_PGOOD,
+	 .enable_gpio = GPIO_PP5000_FAN_EN,
+	},
+};
+BUILD_ASSERT(ARRAY_SIZE(fans) == CONFIG_FANS);
+
+/* I2C ports */
+const struct i2c_port_t i2c_ports[] = {
+	{"batt_chg", 0, 100},
+	{"thermal",  5, 100},
+};
+const unsigned int i2c_ports_used = ARRAY_SIZE(i2c_ports);
+
+/* Temperature sensors data; must be in same order as enum temp_sensor_id. */
+const struct temp_sensor_t temp_sensors[] = {
+	{"PECI", TEMP_SENSOR_TYPE_CPU, peci_temp_sensor_get_val, 0, 2},
+	{"ECInternal", TEMP_SENSOR_TYPE_BOARD, chip_temp_sensor_get_val, 0, 4},
+	{"G781Internal", TEMP_SENSOR_TYPE_BOARD, g781_get_val,
+		G781_IDX_INTERNAL, 4},
+	{"G781External", TEMP_SENSOR_TYPE_BOARD, g781_get_val,
+		G781_IDX_EXTERNAL, 4},
+};
+BUILD_ASSERT(ARRAY_SIZE(temp_sensors) == TEMP_SENSOR_COUNT);
+
+/* Thermal limits for each temp sensor. All temps are in degrees K. Must be in
+ * same order as enum temp_sensor_id. To always ignore any temp, use 0.
+ */
+struct ec_thermal_config thermal_params[] = {
+	/* Only the AP affects the thermal limits and fan speed. */
+	{{C_TO_K(95), C_TO_K(97), C_TO_K(105)}, C_TO_K(32), C_TO_K(96)},
+	{{0, 0, 0}, 0, 0},
+	{{0, 0, 0}, 0, 0},
+	{{0, 0, 0}, 0, 0},
+};
+BUILD_ASSERT(ARRAY_SIZE(thermal_params) == TEMP_SENSOR_COUNT);
+
+struct keyboard_scan_config keyscan_config = {
+	.output_settle_us = 40,
+	.debounce_down_us = 6 * MSEC,
+	.debounce_up_us = 30 * MSEC,
+	.scan_period_us = 1500,
+	.min_post_scan_delay_us = 1000,
+	.poll_timeout_us = SECOND,
+	.actual_key_mask = {
+		0x14, 0xff, 0xff, 0xff, 0xff, 0xf5, 0xff,
+		0xa4, 0xff, 0xf6, 0x55, 0xfa, 0xca  /* full set */
+	},
+};
+
+/**
+ * Discharge battery when on AC power for factory test.
+ */
+int board_discharge_on_ac(int enable)
+{
+	if (enable)
+		gpio_set_level(GPIO_CHARGE_L, 1);
+	else
+		gpio_set_level(GPIO_CHARGE_L, 0);
+	return EC_SUCCESS;
+}
