ed4b2ec29572d999d713c60c594d59caf1dc2d06
Chris Zhong <zyw@rock-chips.com>
2015-05-04 15:11:02 +0800

    Thea: Add g-sensor kx022 to thea board.
    Thea has two kx002 g-sensors, modify to support them.
    
    BUG=chrome-os-partner:39765
    TEST=ectool motionsense
    Rotate the main board and sensor board, make sure meet the spec:
    "Sysfs Interface to EC Sensors: Accelerometer & Gyro"
    BRANCH=firmware-veyron-6588.B
    
    Change-Id: I1a2037907b88bef1080ad78b0c74448f4fb8f41a
    Signed-off-by: Chris Zhong <zyw@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/268943
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-by: Alexandru Stan <amstan@chromium.org>
    Commit-Queue: Alexandru Stan <amstan@chromium.org>
    Tested-by: Alexandru Stan <amstan@chromium.org>

--- board/thea/board.c
+++ board/thea/board.c
@@ -7,11 +7,15 @@
 #include "battery.h"
 #include "chipset.h"
 #include "common.h"
+#include "driver/accel_kx022.h"
 #include "extpower.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "keyboard_raw.h"
 #include "lid_switch.h"
+#include "math_util.h"
+#include "motion_lid.h"
+#include "motion_sense.h"
 #include "power.h"
 #include "power_button.h"
 #include "power.h"
@@ -73,3 +77,63 @@ void board_config_pre_init(void)
 	 */
 	STM32_SYSCFG_CFGR1 |= (1 << 9) | (1 << 10); /* Remap USART1 RX/TX DMA */
 }
+
+/* Base Sensor mutex */
+static struct mutex g_base_mutex;
+
+/* Lid Sensor mutex */
+static struct mutex g_lid_mutex;
+
+/* kxcj9 local/private data */
+struct kx022_data g_kx022_data0;
+
+struct kx022_data g_kx022_data1;
+
+/* Four Motion sensors */
+/* Matrix to rotate accelrator into standard reference frame */
+const matrix_3x3_t base_standard_ref = {
+	{ 0, FLOAT_TO_FP(-1), 0},
+	{ FLOAT_TO_FP(1), 0,  0},
+	{ 0, 0, FLOAT_TO_FP(-1)}
+};
+
+const matrix_3x3_t lid_standard_ref = {
+	{ 0, FLOAT_TO_FP(1), 0},
+	{ FLOAT_TO_FP(1), 0, 0},
+	{ 0, 0, FLOAT_TO_FP(1)}
+};
+
+struct motion_sensor_t motion_sensors[] = {
+
+	/*
+	 * Note: lsm6ds0: supports accelerometer and gyro sensor
+	 * Requriement: accelerometer sensor must init before gyro sensor
+	 * DO NOT change the order of the following table.
+	 */
+	{SENSOR_ACTIVE_S0_S3, "Base", MOTIONSENSE_CHIP_KX022,
+		MOTIONSENSE_TYPE_ACCEL, MOTIONSENSE_LOC_BASE,
+		&kx022_drv, &g_base_mutex, &g_kx022_data0,
+		KX022_ADDR1, &base_standard_ref, 119000, 2},
+
+	{SENSOR_ACTIVE_S0_S3, "Lid",  MOTIONSENSE_CHIP_KX022,
+		MOTIONSENSE_TYPE_ACCEL, MOTIONSENSE_LOC_LID,
+		&kx022_drv, &g_lid_mutex, &g_kx022_data1,
+		KX022_ADDR0, &lid_standard_ref, 100000, 2},
+};
+const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
+
+/* Define the accelerometer orientation matrices. */
+const struct accel_orientation acc_orient = {
+	/* Hinge aligns with y axis. */
+	.rot_hinge_90 = {
+		{  0, 0, FLOAT_TO_FP(1)},
+		{  0, FLOAT_TO_FP(1), 0},
+		{ FLOAT_TO_FP(-1), 0, 0}
+	},
+	.rot_hinge_180 = {
+		{ FLOAT_TO_FP(-1), 0, 0},
+		{  0, FLOAT_TO_FP(1), 0},
+		{  0, 0, FLOAT_TO_FP(-1)}
+	},
+	.hinge_axis = {0, 1, 0},
+};
