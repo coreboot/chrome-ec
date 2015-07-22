jackadam1981/ChromeEc

ryu: Fix orientation of accel and compass
Add matrices to align sensors vectors with the device reference.
Move rotation in read routines, to allow fifo to contains processed
information.

BRANCH=smaug
TEST=Worsk on smaug
BUG=chrome-os-partner:39900

Change-Id: I009e7f24ef6ee0574ed664aeb5fd649fcd7039fd
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

driver/accel_kxcj9.c
@@ -385,12 +385,14 @@ error_enable_sensor:
 }
 #endif
 
-static int read(const struct motion_sensor_t *s, vector_3_t v)
+static int read(const struct motion_sensor_t *s, vector_3_t xyz)
 {
 	uint8_t acc[6];
 	uint8_t reg = KXCJ9_XOUT_L;
 	int ret, i, range, resolution;
 	struct kxcj9_data *data = s->drv_data;
+	vector_3_t temp_v;
+	int *v = (*s->rot_standard_ref == NULL ? xyz : temp_v);
 
 	/* Read 6 bytes starting at KXCJ9_XOUT_L. */
 	mutex_lock(s->mutex);
@@ -424,6 +426,8 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 		v[i] <<= (16 - resolution);
 		v[i] += (data->offset[i] << 5) / range;
 	}
+	if (*s->rot_standard_ref != NULL)
+		rotate(v, *s->rot_standard_ref, xyz);
 	return EC_SUCCESS;
 }
 

driver/accelgyro_bmi160.c
@@ -502,8 +502,11 @@ end_perform_calib:
 	return ret;
 }
 
-void normalize(const struct motion_sensor_t *s, vector_3_t v, uint8_t *data)
+void normalize(const struct motion_sensor_t *s, vector_3_t xyz, uint8_t *data)
 {
+	vector_3_t temp_v;
+	int *v = (*s->rot_standard_ref == NULL ? xyz : temp_v);
+
 #ifdef CONFIG_MAG_BMI160_BMM150
 	if (s->type == MOTIONSENSE_TYPE_MAG)
 		bmm150_normalize(s, v, data);
@@ -514,6 +517,8 @@ void normalize(const struct motion_sensor_t *s, vector_3_t v, uint8_t *data)
 		v[1] = ((int16_t)((data[3] << 8) | data[2]));
 		v[2] = ((int16_t)((data[5] << 8) | data[4]));
 	}
+	if (*s->rot_standard_ref != NULL)
+		rotate(v, *s->rot_standard_ref, xyz);
 }
 
 #ifdef CONFIG_ACCEL_INTERRUPTS
@@ -782,9 +787,8 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 	 * to get the latest updated sensor data quickly.
 	 */
 	if (status & BMI160_DRDY_MASK(s->type)) {
-		v[0] = s->raw_xyz[0];
-		v[1] = s->raw_xyz[1];
-		v[2] = s->raw_xyz[2];
+		if (v != s->raw_xyz)
+			memcpy(v, s->raw_xyz, sizeof(s->raw_xyz));
 		return EC_SUCCESS;
 	}
 

driver/accelgyro_lsm6ds0.c
@@ -338,12 +338,14 @@ static int is_data_ready(const struct motion_sensor_t *s, int *ready)
 	return EC_SUCCESS;
 }
 
-static int read(const struct motion_sensor_t *s, vector_3_t v)
+static int read(const struct motion_sensor_t *s, vector_3_t xyz)
 {
 	uint8_t raw[6];
 	uint8_t xyz_reg;
 	int ret, range, i, tmp = 0;
 	struct lsm6ds0_data *data = s->drv_data;
+	vector_3_t temp_v;
+	int *v = (*s->rot_standard_ref == NULL ? xyz : temp_v);
 
 	ret = is_data_ready(s, &tmp);
 	if (ret != EC_SUCCESS)
@@ -355,9 +357,8 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 	 * to get the latest updated sensor data quickly.
 	 */
 	if (!tmp) {
-		v[0] = s->raw_xyz[0];
-		v[1] = s->raw_xyz[1];
-		v[2] = s->raw_xyz[2];
+		if (v != s->raw_xyz)
+			memcpy(v, s->raw_xyz, sizeof(s->raw_xyz));
 		return EC_SUCCESS;
 	}
 
@@ -380,6 +381,8 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 		v[i] = ((int16_t)((raw[i * 2 + 1] << 8) | raw[i * 2]));
 		v[i] += (data->offset[i] << 5) / range;
 	}
+	if (*s->rot_standard_ref != NULL)
+		rotate(v, *s->rot_standard_ref, xyz);
 
 	return EC_SUCCESS;
 }

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/286659
Reviewed-by: Alec Berg <alecaberg@chromium.org>
(cherry picked from commit 57ec5805fe302b8da22e4141a3e5620812fd7663)
Reviewed-on: https://chromium-review.googlesource.com/287492

driver/accel_kxcj9.c
@@ -425,6 +425,8 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 		v[i] <<= (16 - resolution);
 		v[i] += (data->offset[i] << 5) / range;
 	}
+	if (*s->rot_standard_ref != NULL)
+		rotate(v, *s->rot_standard_ref, v);
 	return EC_SUCCESS;
 }
 

driver/accelgyro_bmi160.c
@@ -508,6 +508,8 @@ void normalize(const struct motion_sensor_t *s, vector_3_t v, uint8_t *data)
 	v[0] = ((int16_t)((data[1] << 8) | data[0]));
 	v[1] = ((int16_t)((data[3] << 8) | data[2]));
 	v[2] = ((int16_t)((data[5] << 8) | data[4]));
+	if (*s->rot_standard_ref != NULL)
+		rotate(v, *s->rot_standard_ref, v);
 }
 
 #ifdef CONFIG_ACCEL_INTERRUPTS
@@ -776,9 +778,8 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 	 * to get the latest updated sensor data quickly.
 	 */
 	if (status & BMI160_DRDY_MASK(s->type)) {
-		v[0] = s->raw_xyz[0];
-		v[1] = s->raw_xyz[1];
-		v[2] = s->raw_xyz[2];
+		if (v != s->raw_xyz)
+			memcpy(v, s->raw_xyz, sizeof(s->raw_xyz));
 		return EC_SUCCESS;
 	}
 

driver/accelgyro_lsm6ds0.c
@@ -356,9 +356,8 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 	 * to get the latest updated sensor data quickly.
 	 */
 	if (!tmp) {
-		v[0] = s->raw_xyz[0];
-		v[1] = s->raw_xyz[1];
-		v[2] = s->raw_xyz[2];
+		if (v != s->raw_xyz)
+			memcpy(v, s->raw_xyz, sizeof(s->raw_xyz));
 		return EC_SUCCESS;
 	}
 
@@ -381,6 +380,8 @@ static int read(const struct motion_sensor_t *s, vector_3_t v)
 		v[i] = ((int16_t)((raw[i * 2 + 1] << 8) | raw[i * 2]));
 		v[i] += (data->offset[i] << 5) / range;
 	}
+	if (*s->rot_standard_ref != NULL)
+		rotate(v, *s->rot_standard_ref, v);
 
 	return EC_SUCCESS;
 }