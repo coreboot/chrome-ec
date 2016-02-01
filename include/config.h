jackadam1981/ChromeEc

tcpc: add support for TCPC with integrated high-speed muxes
The TCPCI specification defines ane optional register
18h 'CONFIG_STANDARD_OUTPUT' providing a standardized way
of steering the high-speed muxes.

Implement the feature as a usb_mux_driver, under the conditional flag
CONFIG_USB_PD_TCPM_CONFIG_OUTPUT.
The USB PD port index should be set in the port_addr field of the
'usb_mux' structure.

Signed-off-by: Vincent Palatin <vpalatin@chromium.org>

BRANCH=none
BUG=chrome-os-partner:49605
TEST=run pdeval-stm32f072 connected to a Parade PS8751 board and test USB/DP
muxing.

Change-Id: I7e5f0b8ec70b1910b2cff9d106514baca8c899e5

include/config.h
@@ -1833,6 +1833,12 @@
 #undef CONFIG_USB_PD_TCPM_FUSB302
 
 /*
+ * Use this option if the TCPC port controller supports the optional register
+ * 18h CONFIG_STANDARD_OUTPUT to steer the high-speed muxes.
+ */
+#undef CONFIG_USB_PD_TCPM_CONFIG_OUTPUT
+
+/*
  * Use this option if the TCPC port controller is on a seperate chip from
  * the TCPM layer and if VUBS detect GPIO is not available on the TCPM
  * mcu.

coreboot/chrome-ec

CONFIG_USB_PD_TCPM_MUX.
Previous-Reviewed-on: https://chromium-review.googlesource.com/322956
(cherry picked from commit d86bf8f30683c7d26d8bb51ee73c15f323674985)
Reviewed-on: https://chromium-review.googlesource.com/325012
Tested-by: Duncan Laurie <dlaurie@chromium.org>
Reviewed-by: Duncan Laurie <dlaurie@chromium.org>
Commit-Queue: Duncan Laurie <dlaurie@chromium.org>

include/config.h
@@ -1830,6 +1830,12 @@
 #undef CONFIG_USB_PD_TCPM_FUSB302
 
 /*
+ * Use this option if the TCPC port controller supports the optional register
+ * 18h CONFIG_STANDARD_OUTPUT to steer the high-speed muxes.
+ */
+#undef CONFIG_USB_PD_TCPM_MUX
+
+/*
  * Use this option if the TCPC port controller is on a seperate chip from
  * the TCPM layer and if VUBS detect GPIO is not available on the TCPM
  * mcu.