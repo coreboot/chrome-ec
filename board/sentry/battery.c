43720b1411e739611a82225ca73e9892f9ee732e
Keith Tzeng <keith.tzeng@quantatw.com>
2016-03-20 22:39:14 +0800

    Sentry: moved some data to __bss_slow area
    BUG=none
    BRANCH=glados
    TEST=make buildall
    Signed-off-by: Keith Tzeng <Keith.Tzeng@quantatw.com>
    
    Change-Id: I907b8a4a49b292cd956e75004c17574a7746309b
    Reviewed-on: https://chromium-review.googlesource.com/333794
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: Keith Tzeng <keith.tzeng@quantatw.com>
    Tested-by: Keith Tzeng <keith.tzeng@quantatw.com>

--- board/sentry/battery.c
+++ board/sentry/battery.c
@@ -50,7 +50,7 @@ static const struct battery_info info = {
 
 const struct battery_info *battery_get_info(void)
 {
-	static struct battery_info batt_info;
+	static struct battery_info __bss_slow batt_info;
 
 	if (battery_is_present() == BP_NO) {
 		batt_info = info;
