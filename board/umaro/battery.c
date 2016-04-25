998463fec199268264254fd9506c3ccaed3866d2
SolonFan <solonfan@ami.com.cn>
2016-04-25 14:46:19 +0800

    Umaro: Update the battery info as the umaro battery design.
    BUG=None
    TEST=Compiled
    BRANCH=firmware-strago-7287.B
    Signed-off-by: Solon Fan <solonfan@ami.com.cn>
    
    Change-Id: I80ad70af1fee9e4d271814f589320b0142d7f3c1
    Reviewed-on: https://chromium-review.googlesource.com/340288
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Tested-by: fan solon <solonfan@ami.com.cn>
    Commit-Queue: fan solon <solonfan@ami.com.cn>

--- board/umaro/battery.c
+++ board/umaro/battery.c
@@ -12,10 +12,10 @@
 #define SB_SHUTDOWN_DATA	0x0010
 
 static const struct battery_info info = {
-	.voltage_max = 8700,/* mV */
-	.voltage_normal = 7600,
+	.voltage_max = 8400,/* mV */
+	.voltage_normal = 7400,
 	.voltage_min = 6000,
-	.precharge_current = 150,/* mA */
+	.precharge_current = 500,/* mA */
 	.start_charging_min_c = 0,
 	.start_charging_max_c = 45,
 	.charging_min_c = 0,
