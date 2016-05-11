dd54644e3b2eed4bb161998fc8016494f2aa78e7
Bruce <Bruce.Wan@quantatw.com>
2016-05-11 11:00:08 +0800

    Relm: modify battery setting
    modify battery setting for 3 cells
    
    BUG=None
    BRANCH=firmware-strago-7287.B
    TEST=make buildall
    
    Change-Id: I2c8bc1b9cc3410b1cb2dd42ef9096bd61f097d55
    Signed-off-by: Bruce.Wan <Bruce.Wan@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/343982
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Keith Tzeng <keith.tzeng@quantatw.com>
    Tested-by: Keith Tzeng <keith.tzeng@quantatw.com>

--- board/relm/battery.c
+++ board/relm/battery.c
@@ -12,14 +12,14 @@
 #define SB_SHUTDOWN_DATA	0x0010
 
 static const struct battery_info info = {
-	.voltage_max = 8700,/* mV */
-	.voltage_normal = 7600,
-	.voltage_min = 6000,
-	.precharge_current = 150,/* mA */
+	.voltage_max = 12600,/* mV */
+	.voltage_normal = 11100,
+	.voltage_min = 9000,
+	.precharge_current = 204,/* mA */
 	.start_charging_min_c = 0,
-	.start_charging_max_c = 45,
+	.start_charging_max_c = 60,
 	.charging_min_c = 0,
-	.charging_max_c = 45,
+	.charging_max_c = 60,
 	.discharging_min_c = -20,
 	.discharging_max_c = 60,
 };
