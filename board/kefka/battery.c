1764b19741e01d99ed691331c83cf5388c07c68e
Peggy Chuang <PeggyChuang@ami.com.tw>
2016-07-12 19:10:35 +0800

    Kefka: modify battery parameters follow battery spec
    Kefka will use different battery, they are LGC and BYD battery.
    We can know battery type from battery manufacture name.
    
    BUG=None
    BRANCH=firmware-strago-7287.B
    TEST=make buildall -j, verified
    Signed-off-by: PeggyChuang <peggychuang@ami.com.tw>
    
    Change-Id: I681db4c6544316c99a0d074af1af4571cb8c62d7
    Reviewed-on: https://chromium-review.googlesource.com/360085
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: Peggy Chuang <PeggyChuang@ami.com.tw>
    Tested-by: Peggy Chuang <PeggyChuang@ami.com.tw>

--- board/kefka/battery.c
+++ board/kefka/battery.c
@@ -7,26 +7,49 @@
 
 #include "battery.h"
 #include "battery_smart.h"
+#include "util.h"
 
 /* Shutdown mode parameter to write to manufacturer access register */
 #define SB_SHUTDOWN_DATA	0x0010
 
-static const struct battery_info info = {
-	.voltage_max = 8700,/* mV */
-	.voltage_normal = 7600,
-	.voltage_min = 6000,
-	.precharge_current = 150,/* mA */
+static const struct battery_info BYD_info = {
+	.voltage_max = 13200,/* mV */
+	.voltage_normal = 11400,
+	.voltage_min = 9000,
+	.precharge_current = 128,/* mA */
 	.start_charging_min_c = 0,
-	.start_charging_max_c = 45,
+	.start_charging_max_c = 60,
 	.charging_min_c = 0,
-	.charging_max_c = 45,
+	.charging_max_c = 60,
+	.discharging_min_c = 0,
+	.discharging_max_c = 70,
+};
+
+static const struct battery_info LGC_info = {
+	.voltage_max = 13200,/* mV */
+	.voltage_normal = 11400,
+	.voltage_min = 9000,
+	.precharge_current = 256,/* mA */
+	.start_charging_min_c = -3,
+	.start_charging_max_c = 50,
+	.charging_min_c = -3,
+	.charging_max_c = 60,
 	.discharging_min_c = -20,
-	.discharging_max_c = 60,
+	.discharging_max_c = 70,
 };
 
 const struct battery_info *battery_get_info(void)
 {
-	return &info;
+	const struct battery_info *ref = &BYD_info;
+	char manuf[4];
+
+	battery_manufacturer_name(manuf, sizeof(manuf));
+
+	if (!strcasecmp(manuf, "LGC")) {
+		ref = &LGC_info;
+	}
+
+	return ref;
 }
 
 int board_cut_off_battery(void)
