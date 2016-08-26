8a37319f6a4481327858e8f0ea86668c09535c4d
Gwendal Grignou <gwendal@chromium.org>
2016-08-26 13:49:56 -0700

    Revert "acc: kx022: Increase init delay to 20ms"
    Revert kx022 driver from minnie tree to use main tree one.
    
    BUG=b:27849483
    BRANCH=minnie
    TEST=compile
    
    This reverts commit f258db7ccc37b46ebaf2831d50bc135f37ff30f4.
    
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Change-Id: I9e757e0bcd892b9733137eefdec03cd4aadac99d
    Reviewed-on: https://chromium-review.googlesource.com/378199

--- driver/accel_kx022.c
+++ driver/accel_kx022.c
@@ -496,7 +496,7 @@ static int init(const struct motion_sensor_t *s)
 			break;
 
 		/* Check for timeout. */
-		if (cnt++ > 20) {
+		if (cnt++ > 5) {
 			ret = EC_ERROR_TIMEOUT;
 			CPRINTF("%s: SRST Error.\n", s->name);
 			return ret;
