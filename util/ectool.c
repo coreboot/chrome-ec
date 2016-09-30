jackadam1981/ChromeEc

flash: Add command to get SPI flash chip info
Previously, there was no way to identify which flash chip was used by
the EC, for ECs using an external SPI flash.  Now, 'ectool flashinfo'
will print more information about the SPI flash chip in these cases.

BUG=chrome-os-partner:56765
BRANCH=any EC with MEC1322 or NPCX still going through factory
TEST=ectool flashinfo works on samus (with no additional info)
     and prints additional info on chell and kevin

Change-Id: I3c162f7ad12ed4b30ab951c03f24476683382114
Signed-off-by: Randall Spangler <rspangler@chromium.org>

util/ectool.c
@@ -721,6 +721,7 @@ int cmd_reboot_ec(int argc, char *argv[])
 int cmd_flash_info(int argc, char *argv[])
 {
 	struct ec_response_flash_info_1 r;
+	struct ec_response_flash_spi_info s;
 	int cmdver = 1;
 	int rsize = sizeof(r);
 	int rv;
@@ -745,6 +746,21 @@ int cmd_flash_info(int argc, char *argv[])
 		/* Fields added in ver.1 available */
 		printf("WriteIdealSize %d\nFlags 0x%x\n",
 		       r.write_ideal_size, r.flags);
+	}
+
+	/* Print SPI flash info if available */
+	rv = ec_command(EC_CMD_FLASH_SPI_INFO, 0, NULL, 0, &s, sizeof(s));
+	if (rv == EC_RES_SUCCESS) {
+		printf("JEDECManufacturerID 0x%02x\n", s.jedec[0]);
+		printf("JEDECDeviceID 0x%02x 0x%02x\n", s.jedec[1], s.jedec[2]);
+		printf("JEDECCapacity %d\n", 1 << s.jedec[2]);
+		printf("ManufacturerID 0x%02x\n", s.mfr_dev_id[0]);
+		printf("DeviceID 0x%02x\n", s.mfr_dev_id[1]);
+		printf("StatusRegister1 0x%02x\n", s.sr1);
+		printf("StatusRegister2 0x%02x\n", s.sr2);
+	} else if (rv != -EC_RES_INVALID_COMMAND) {
+		/* Command was supported, but failed */
+		return rv;
 	}
 
 	return 0;

coreboot/chrome-ec

TEST=define CONFIG_HOSTCMD_FLASH_SPI_INFO, then
     'ectool flashspiinfo' on samus indicates no SPI flash info,
     and prints additional info on chell and kevin.  Without
     the config defined, all platforms report no spi flash info.
CQ-DEPEND=CL:386368
Original-Change-Id: I3c162f7ad12ed4b30ab951c03f24476683382114
Reviewed-on: https://chromium-review.googlesource.com/385702
Reviewed-by: Shawn N <shawnn@chromium.org>
Change-Id: Iad9534763c52265f3efa42cd8bc4fcafc796c9df
Reviewed-on: https://chromium-review.googlesource.com/389874
Tested-by: Ivy Jian <ivy_jian@compal.com>

util/ectool.c
@@ -99,6 +99,8 @@ const char help_str[] =
 	"      Erases EC flash\n"
 	"  flashinfo\n"
 	"      Prints information on the EC flash\n"
+	"  flashspiinfo\n"
+	"      Prints information on EC SPI flash, if present\n"
 	"  flashpd <dev_id> <port> <filename>\n"
 	"      Flash commands over PD\n"
 	"  flashprotect [now] [enable | disable]\n"
@@ -666,6 +668,33 @@ int cmd_flash_info(int argc, char *argv[])
 		       r.write_ideal_size, r.flags);
 	}
 
+	return 0;
+}
+
+int cmd_flash_spi_info(int argc, char *argv[])
+{
+	struct ec_response_flash_spi_info r;
+	int rv;
+
+	memset(&r, 0, sizeof(r));
+
+	/* Print SPI flash info if available */
+	if (!ec_cmd_version_supported(EC_CMD_FLASH_SPI_INFO, 0)) {
+		printf("EC has no info (does not use SPI flash?)\n");
+		return -1;
+	}
+
+	rv = ec_command(EC_CMD_FLASH_SPI_INFO, 0, NULL, 0, &r, sizeof(r));
+	if (rv < 0)
+		return rv;
+
+	printf("JEDECManufacturerID 0x%02x\n", r.jedec[0]);
+	printf("JEDECDeviceID 0x%02x 0x%02x\n", r.jedec[1], r.jedec[2]);
+	printf("JEDECCapacity %d\n", 1 << r.jedec[2]);
+	printf("ManufacturerID 0x%02x\n", r.mfr_dev_id[0]);
+	printf("DeviceID 0x%02x\n", r.mfr_dev_id[1]);
+	printf("StatusRegister1 0x%02x\n", r.sr1);
+	printf("StatusRegister2 0x%02x\n", r.sr2);
 	return 0;
 }
 
@@ -6366,6 +6395,7 @@ const struct command commands[] = {
 	{"flashread", cmd_flash_read},
 	{"flashwrite", cmd_flash_write},
 	{"flashinfo", cmd_flash_info},
+	{"flashspiinfo", cmd_flash_spi_info},
 	{"flashpd", cmd_flash_pd},
 	{"forcelidopen", cmd_force_lid_open},
 	{"gpioget", cmd_gpio_get},