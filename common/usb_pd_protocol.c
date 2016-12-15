jackadam1981/ChromeEc

usb_pd_policy: Automatically swap vconn if adapter requests it
During discovery, if adapter requests vconn power in the AMA flags,
make sure that we provide vconn.

This, for example, is necessary for the Apple HDMI adapter to work
on boot, when connected in S5. In that case, adapter does request
vconn swap, but we reject that as the system is off, and, therefore
5V supply is off. On boot, we send another discovery request, which
will detect this case and swap the power.

BRANCH=none
BUG=chromium:644663
TEST=On elm, S5. Plug adapter with power+HDMI. Switch on elm,
     type "pd 0 vdm ident" in console, display works.

Change-Id: I55b6658c2bc0574b8427ae086f61daf03730a725

common/usb_pd_protocol.c
@@ -837,6 +837,18 @@ static void pd_request_vconn_swap(int port)
 		set_state(port, PD_STATE_VCONN_SWAP_SEND);
 	task_wake(PD_PORT_TO_TASK_ID(port));
 }
+
+void pd_request_vconn_src(int port)
+{
+	/*
+	 * If we don't currently provide vconn, and we can supply it, send
+	 * a vconn swap request.
+	 */
+	if (!(pd[port].flags & PD_FLAGS_VCONN_ON)) {
+		if (pd_check_vconn_swap(port))
+			pd_request_vconn_swap(port);
+	}
+}
 #endif
 #endif /* CONFIG_USB_PD_DUAL_ROLE */
 

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/415697
Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
Tested-by: Nicolas Boichat <drinkcat@chromium.org>
Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
(cherry picked from commit 87e8cd610313fa7fe902c2fd359ade18e6b22ef0)
Signed-off-by: Nicolas Boichat <drinkcat@google.com>
Reviewed-on: https://chromium-review.googlesource.com/420425
Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>
Reviewed-by: Rong Chang <rongchang@chromium.org>

common/usb_pd_protocol.c
@@ -795,6 +795,18 @@ static void pd_request_vconn_swap(int port)
 		set_state(port, PD_STATE_VCONN_SWAP_SEND);
 	task_wake(PD_PORT_TO_TASK_ID(port));
 }
+
+void pd_try_vconn_src(int port)
+{
+	/*
+	 * If we don't currently provide vconn, and we can supply it, send
+	 * a vconn swap request.
+	 */
+	if (!(pd[port].flags & PD_FLAGS_VCONN_ON)) {
+		if (pd_check_vconn_swap(port))
+			pd_request_vconn_swap(port);
+	}
+}
 #endif
 #endif /* CONFIG_USB_PD_DUAL_ROLE */
 