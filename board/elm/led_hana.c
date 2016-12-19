ea65ceaf7fe3af9f76b3f8acb78d939bd33ad2aa
geap <geap@bitland.com.cn>
2016-12-19 17:39:51 +0800

    hana: Update the led behavior to follow the latest specification For power led:   Use single led with white color. Hardware also need to be changed.   S0: White led always on   S5: White led always off   S3: off for AC out, 500ms off/3s on for AC in
    For battery led:
      Still use green and red color.
      For discharge: off
      For charge:
        Battery capacity 0%~4%	Red
        Battery capacity 5%~96%	Orange (Red+Green)
        Battery capacity 97%~100%	Green
    
    BUG=chrome-os-partner:60239
    BRANCH=oak
    TEST=load and test on hana-rev1
    
    Change-Id: Idaed29f47b8be2d64ff1bf67b8a95dddde62befb
    Reviewed-on: https://chromium-review.googlesource.com/421382
    Tested-by: Barry Ge <geap@bitland.com.cn>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    Commit-Queue: Barry Ge <geap@bitland.com.cn>

--- board/elm/led_hana.c
+++ board/elm/led_hana.c
@@ -8,6 +8,7 @@
 #include "battery.h"
 #include "charge_state.h"
 #include "chipset.h"
+#include "extpower.h"
 #include "gpio.h"
 #include "hooks.h"
 #include "host_command.h"
@@ -15,9 +16,8 @@
 #include "util.h"
 #include "system.h"
 
-#define CRITICAL_LOW_BATTERY_PERMILLAGE 71
-#define LOW_BATTERY_PERMILLAGE 137
-#define FULL_BATTERY_PERMILLAGE 937
+#define CRITICAL_LOW_BATTERY_PERMILLAGE 86		/* 5*(95-4)/100+4   to meet UI display 5% */
+#define FULL_BATTERY_PERMILLAGE 923		/* 97*(95-4)/100+4   to meet UI display 97% */
 
 const enum ec_led_id supported_led_ids[] = {
 	EC_LED_ID_BATTERY_LED,
@@ -29,8 +29,7 @@ const int supported_led_ids_count = ARRAY_SIZE(supported_led_ids);
 enum led_color {
 	BAT_LED_GREEN = 0,
 	BAT_LED_RED,
-	PWR_LED_GREEN,
-	PWR_LED_RED,
+	PWR_LED_WHITE,
 	LED_COLOR_COUNT		/* Number of colors, not a color itself */
 };
 
@@ -43,11 +42,8 @@ static int bat_led_set(enum led_color color, int on)
 	case BAT_LED_RED:
 		gpio_set_level(GPIO_BAT_LED1, on); /* BAT_LED_RED */
 		break;
-	case PWR_LED_GREEN:
-		gpio_set_level(GPIO_PWR_LED0, on); /* PWR_LED_GREEN */
-		break;
-	case PWR_LED_RED:
-		gpio_set_level(GPIO_PWR_LED1, on); /* PWR_LED_RED */
+	case PWR_LED_WHITE:
+		gpio_set_level(GPIO_PWR_LED0, on); /* PWR_LED_WHITE */
 		break;
 	default:
 		return EC_ERROR_UNKNOWN;
@@ -57,9 +53,18 @@ static int bat_led_set(enum led_color color, int on)
 
 void led_get_brightness_range(enum ec_led_id led_id, uint8_t *brightness_range)
 {
-	/* Ignoring led_id as both leds support the same colors */
-	brightness_range[EC_LED_COLOR_GREEN] = 1;
-	brightness_range[EC_LED_COLOR_RED] = 1;
+	switch (led_id) {
+	case EC_LED_ID_BATTERY_LED:
+		brightness_range[EC_LED_COLOR_GREEN] = 1;
+		brightness_range[EC_LED_COLOR_RED] = 1;
+		brightness_range[EC_LED_COLOR_AMBER] = 1;
+		break;
+	case EC_LED_ID_POWER_LED:
+		brightness_range[EC_LED_COLOR_WHITE] = 1;
+		break;
+	default:
+		break;
+	}
 }
 
 int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
@@ -71,21 +76,19 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 		} else if (brightness[EC_LED_COLOR_RED] != 0) {
 			bat_led_set(BAT_LED_GREEN, 0);
 			bat_led_set(BAT_LED_RED, 1);
+		} else if (brightness[EC_LED_COLOR_AMBER] != 0) {
+			bat_led_set(BAT_LED_GREEN, 1);
+			bat_led_set(BAT_LED_RED, 1);
 		} else {
 			bat_led_set(BAT_LED_GREEN, 0);
 			bat_led_set(BAT_LED_RED, 0);
 		}
 		return EC_SUCCESS;
 	} else if (EC_LED_ID_POWER_LED == led_id) {
-		if (brightness[EC_LED_COLOR_GREEN] != 0) {
-			bat_led_set(PWR_LED_GREEN, 1);
-			bat_led_set(PWR_LED_RED, 0);
-		} else if (brightness[EC_LED_COLOR_RED] != 0) {
-			bat_led_set(PWR_LED_GREEN, 0);
-			bat_led_set(PWR_LED_RED, 1);
+		if (brightness[EC_LED_COLOR_WHITE] != 0) {
+			bat_led_set(PWR_LED_WHITE, 1);
 		} else {
-			bat_led_set(PWR_LED_GREEN, 0);
-			bat_led_set(PWR_LED_RED, 0);
+			bat_led_set(PWR_LED_WHITE, 0);
 		}
 		return EC_SUCCESS;
 	} else {
@@ -93,40 +96,42 @@ int led_set_brightness(enum ec_led_id led_id, const uint8_t *brightness)
 	}
 }
 
-static unsigned blink_second;
+static unsigned blink_500ms;
 
+/*
+ * Power Led Control
+ * S0: White led always on
+ * S5: White led always off
+ * S3: off for AC out, 500ms off/3s on for AC in
+ */
 static void hana_led_set_power(void)
 {
-	/*
-	 * PWR LED behavior:
-	 * Power on: Blue ON
-	 * Suspend: Orange in breeze mode ( 1 sec on/ 3 sec off)
-	 * Power off: OFF
-	 */
 	if (chipset_in_state(CHIPSET_STATE_ANY_OFF)) {
-		bat_led_set(PWR_LED_GREEN, 0);
-		bat_led_set(PWR_LED_RED, 0);
+		bat_led_set(PWR_LED_WHITE, 0);
 	} else if (chipset_in_state(CHIPSET_STATE_ON)) {
-		bat_led_set(PWR_LED_GREEN, 1);
-		bat_led_set(PWR_LED_RED, 0);
+		bat_led_set(PWR_LED_WHITE, 1);
 	} else if (chipset_in_state(CHIPSET_STATE_SUSPEND)) {
-		bat_led_set(PWR_LED_GREEN, 0);
-		bat_led_set(PWR_LED_RED,
-			    (blink_second & 3) ? 0 : 1);
+		if (extpower_is_present()) {
+			bat_led_set(PWR_LED_WHITE,
+				(blink_500ms % 7) ? 1 : 0);
+		} else {
+			bat_led_set(PWR_LED_WHITE, 0);
+		}
 	}
 }
 
+/*
+ * Battery Led Control
+ * Discharge: OFF
+ * Charge:
+ * 	Battery_Capacity 0%~4%		Red
+ * 	Battery_Capacity 5%~96%		Orange (Red+Green)
+ * 	Battery_Capacity 97%~100%	Green
+ * Battery error: Red blink(1:1)
+ * Factory force idle: Green 2 sec, Red 2 sec
+ */
 static void hana_led_set_battery(void)
 {
-	/*
-	 * BAT LED behavior:
-	 * - Fully charged / normal idle: Blue ON
-	 * - Charging: Orange ON
-	 * - Battery discharging capacity<10%, Orange blink(1:3)
-	 *				 < 3%, Orange blink(1:1)
-	 * - Battery error: Orange blink(1:1)
-	 * - Factory force idle: Blue 2 sec, Orange 2 sec
-	 */
 	uint32_t charge_flags = charge_get_flags();
 	int remaining_capacity;
 	int full_charge_capacity;
@@ -140,39 +145,30 @@ static void hana_led_set_battery(void)
 
 	switch (charge_get_state()) {
 	case PWR_STATE_CHARGE:
-		if (permillage < FULL_BATTERY_PERMILLAGE) {
+	case PWR_STATE_CHARGE_NEAR_FULL:
+		if (permillage < CRITICAL_LOW_BATTERY_PERMILLAGE) {
 			bat_led_set(BAT_LED_GREEN, 0);
+			bat_led_set(BAT_LED_RED, 1);
+		} else if (permillage < FULL_BATTERY_PERMILLAGE) {
+			bat_led_set(BAT_LED_GREEN, 1);
 			bat_led_set(BAT_LED_RED, 1);
 		} else {
 			bat_led_set(BAT_LED_GREEN, 1);
 			bat_led_set(BAT_LED_RED, 0);
 		}
 		break;
-	case PWR_STATE_CHARGE_NEAR_FULL:
-		bat_led_set(BAT_LED_GREEN, 1);
-		bat_led_set(BAT_LED_RED, 0);
-		break;
 	case PWR_STATE_DISCHARGE:
 		bat_led_set(BAT_LED_GREEN, 0);
-		if (!chipset_in_state(CHIPSET_STATE_ANY_OFF) &&
-		    permillage <= CRITICAL_LOW_BATTERY_PERMILLAGE)
-			bat_led_set(BAT_LED_RED,
-				    (blink_second & 1) ? 0 : 1);
-		else if (!chipset_in_state(CHIPSET_STATE_ANY_OFF) &&
-			 permillage <= LOW_BATTERY_PERMILLAGE)
-			bat_led_set(BAT_LED_RED,
-				    (blink_second & 3) ? 0 : 1);
-		else
-			bat_led_set(BAT_LED_RED, 0);
+		bat_led_set(BAT_LED_RED, 0);
 		break;
 	case PWR_STATE_ERROR:
 		bat_led_set(BAT_LED_GREEN, 0);
-		bat_led_set(BAT_LED_RED, (blink_second & 1) ? 0 : 1);
+		bat_led_set(BAT_LED_RED, (blink_500ms & 2) ? 0 : 1);
 		break;
 	case PWR_STATE_IDLE: /* Ext. power connected in IDLE. */
 		if (charge_flags & CHARGE_FLAG_FORCE_IDLE) {
-			bat_led_set(BAT_LED_GREEN, (blink_second & 2) ? 0 : 1);
-			bat_led_set(BAT_LED_RED, (blink_second & 2) ? 1 : 0);
+			bat_led_set(BAT_LED_GREEN, (blink_500ms & 4) ? 0 : 1);
+			bat_led_set(BAT_LED_RED, (blink_500ms & 4) ? 1 : 0);
 		} else {
 			bat_led_set(BAT_LED_GREEN, 1);
 			bat_led_set(BAT_LED_RED, 0);
@@ -185,15 +181,15 @@ static void hana_led_set_battery(void)
 }
 
 /**
- * Called by hook task every 1 sec
+ * Called by hook task every 500 milliseconds
  */
-static void led_second(void)
+static void led_500ms(void)
 {
-	blink_second++;
+	blink_500ms++;
 
 	if (led_auto_control_is_enabled(EC_LED_ID_POWER_LED))
 		hana_led_set_power();
 	if (led_auto_control_is_enabled(EC_LED_ID_BATTERY_LED))
 		hana_led_set_battery();
 }
-DECLARE_HOOK(HOOK_SECOND, led_second, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_TICK, led_500ms, HOOK_PRIO_DEFAULT);
