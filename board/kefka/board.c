16d79f343b631a64f9d539f20d075f57a5e412ed
Chris Wang <chriswang@ami.com.tw>
2016-12-21 20:39:42 +0800

    Kefka: Enable motion sensor when system in S3
    Enable base/lid motion sensor when system in S3.
    
    BUG=chrome-os-partner:61201
    BRANCH=firmware-strago-7287.B
    TEST=Manual on Sabin sku. Boot system with lid open in clamshell
    mode. Open a shell window and type powerd_dbus_suspend to bring
    the system to suspend state. Now fold the system to tablet mode.
    Verify that pressing a key or trackpad does not cause system to wake up
    Signed-off-by: Chris Wang <chriswang@ami.com.tw>
    
    Change-Id: Ie3e7ccc99d0cf6e0bcfbfe591c68a46c8c381660
    Reviewed-on: https://chromium-review.googlesource.com/422828
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Commit-Queue: Chris Wang <chriswang@ami.com.tw>
    Tested-by: Chris Wang <chriswang@ami.com.tw>

--- board/kefka/board.c
+++ board/kefka/board.c
@@ -152,12 +152,12 @@ struct motion_sensor_t motion_sensors[] = {
 			 .odr = 10000 | ROUND_UP_FLAG,
 			 .ec_rate = 0,
 		 },
-		 /* Sensor off in S3/S5 */
+		 /* EC use accel for angle detection */
 		 [SENSOR_CONFIG_EC_S3] = {
-			 .odr = 0,
+			 .odr = 10000 | ROUND_UP_FLAG,
 			 .ec_rate = 0
 		 },
-		 /* Sensor off in S3/S5 */
+		 /* Sensor off in S5 */
 		 [SENSOR_CONFIG_EC_S5] = {
 			 .odr = 0,
 			 .ec_rate = 0
@@ -188,12 +188,12 @@ struct motion_sensor_t motion_sensors[] = {
 			 .odr = 10000 | ROUND_UP_FLAG,
 			 .ec_rate = 0,
 		 },
-		 /* Sensor off in S3/S5 */
+		 /* EC use accel for angle detection */
 		 [SENSOR_CONFIG_EC_S3] = {
-			 .odr = 0,
+			 .odr = 10000 | ROUND_UP_FLAG,
 			 .ec_rate = 0
 		 },
-		 /* Sensor off in S3/S5 */
+		 /* Sensor off in S5 */
 		 [SENSOR_CONFIG_EC_S5] = {
 			 .odr = 0,
 			 .ec_rate = 0
