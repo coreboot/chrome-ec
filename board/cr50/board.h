2323a9fd3c3440afe8d0d5e3c64c80fafbf83062
Vincent Palatin <vpalatin@chromium.org>
2017-01-19 15:22:15 +0100

    cr50: add U2F support
    Implement U2F (universal second factor authentication) feature
    over TPM vendor commands.
    The raw U2F APDU as defined by the FIDO Alliance 'U2F Raw Message Formats'
    specification can be sent using the VENDOR_CC_U2F_APDU command.
    So the vendor command is taking a ISO7816-4:2005 APDU format frame as input
    as defined by the spec and returns another APDU using ISO7816-4 status
    code.
    
    The APDU is processed by the common U2F code using u2f_apdu_rcv(),
    this hardware specific code provides:
    - the user physical presence detection (done by the power button press)
      returned by the pop_check_presence() callback.
    - the connection to the cryptographic hardware to generate/derive the
      keys used by the U2F and individual attestation functions.
    
    This feature/vendor command has 3 modes:
    - disabled
    - U2F (only the commands/flags defined by the U2F specification)
    - G2F (the U2F commands plus some extensions for individual attestation)
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=cr50
    BUG=b:35545754
    TEST=pass U2FTest and HIDTest.
    
    Change-Id: Ic2591f369763fb4ba67926e2b4a0c2cd35330a18
    Reviewed-on: https://chromium-review.googlesource.com/518139
    Commit-Ready: Vincent Palatin <vpalatin@chromium.org>
    Tested-by: Vincent Palatin <vpalatin@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    (cherry picked from commit 28e76b309d595923121e70cbd9e3228affad3a09)
    Reviewed-on: https://chromium-review.googlesource.com/571643
    Tested-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    (cherry picked from commit 49ee45008bf836d0017bf8aa5bcb1ae728104f71)

--- board/cr50/board.h
+++ board/cr50/board.h
@@ -82,6 +82,9 @@
 /* Enable debug cable detection */
 #define CONFIG_RDD
 
+/* Also use the cr50 as a second factor authentication */
+#define CONFIG_U2F
+
 /* USB configuration */
 #define CONFIG_USB
 #define CONFIG_USB_CONSOLE
@@ -171,6 +174,7 @@ enum device_type {
 enum nvmem_vars {
 	NVMEM_VAR_CONSOLE_LOCKED = 0,
 	NVMEM_VAR_TEST_VAR,
+	NVMEM_VAR_U2F_SALT,
 
 	NVMEM_VARS_COUNT
 };
@@ -199,6 +203,7 @@ int board_id_is_mismatched(void);
 
 /* Returns True if chip is brought up in a factory test harness. */
 int chip_factory_mode(void);
+void power_button_record(void);
 
 #endif /* !__ASSEMBLER__ */
 
