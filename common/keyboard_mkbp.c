jackadam1981/ChromeEc

mkbp_event: Properly queue events during host sleep
Don't queue non-wake events, and ensure wake events (and all subsequent
events) always get queued.

BUG=chrome-os-partner:59248, chrome-os-partner:59336
BRANCH=gru
TEST=Manual on kevin, go to suspend, press volume keys dozens of times,
press 'shift', verify device wakes. Place cursor on URL bar, go to
suspend, type "google" quickly, verify device wakes and "google" appears
on URL bar. Go to suspend, press 'VolUp' key 5 times, press keyboard,
verify device wakes and no volume meter is seen on display.

Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
Change-Id: Ibe761187fbcefd686776a512786550970a6fc067

common/keyboard_mkbp.c
@@ -161,7 +161,6 @@ test_mockable int keyboard_fifo_add(const uint8_t *buffp)
 
 test_mockable int mkbp_fifo_add(uint8_t event_type, const uint8_t *buffp)
 {
-	int ret = EC_SUCCESS;
 	uint8_t size;
 
 	/*
@@ -175,8 +174,8 @@ test_mockable int mkbp_fifo_add(uint8_t event_type, const uint8_t *buffp)
 	if (fifo_entries >= config.fifo_max_depth) {
 		CPRINTS("MKBP common FIFO depth %d reached",
 			config.fifo_max_depth);
-		ret = EC_ERROR_OVERFLOW;
-		goto fifo_push_done;
+
+		return EC_ERROR_OVERFLOW;
 	}
 
 	size = get_data_size(event_type);
@@ -185,14 +184,17 @@ test_mockable int mkbp_fifo_add(uint8_t event_type, const uint8_t *buffp)
 	memcpy(&fifo[fifo_end].data, buffp, size);
 	fifo_end = (fifo_end + 1) % FIFO_DEPTH;
 	atomic_add(&fifo_entries, 1);
+
+	/*
+	 * If our event didn't generate an interrupt then the host is still
+	 * asleep. In this case, we don't want to queue our event, except if
+	 * another event just woke the host (and wake is already in progress).
+	 */
+	if (!mkbp_send_event(event_type) && fifo_entries == 1)
+		mkbp_clear_fifo();
+
 	mutex_unlock(&fifo_mutex);
-
-fifo_push_done:
-
-	if (ret == EC_SUCCESS)
-		mkbp_send_event(event_type);
-
-	return ret;
+	return EC_SUCCESS;
 }
 
 void mkbp_update_switches(uint32_t sw, int state)

coreboot/chrome-ec

BUG=chrome-os-partner:59248, chrome-os-partner:59336,chrome-os-partner:60882
BRANCH=gru,veyron
Original-Change-Id: Ibe761187fbcefd686776a512786550970a6fc067
Reviewed-on: https://chromium-review.googlesource.com/405717
Commit-Queue: Douglas Anderson <dianders@chromium.org>
Tested-by: Douglas Anderson <dianders@chromium.org>
Reviewed-by: Douglas Anderson <dianders@chromium.org>
(cherry picked from commit aa2f01566314604404e104d7975c6c755c22a601)
Reviewed-on: https://chromium-review.googlesource.com/407958
Commit-Ready: Douglas Anderson <dianders@chromium.org>
Reviewed-by: Stefan Reinauer <reinauer@chromium.org>
(cherry picked from commit 1f6600fd344bb5eb5476206a05c2bc773252d788)
Conflicts:
	common/keyboard_mkbp.c
Change-Id: Id17caf30fd8b3d609db527f254c5e4fdc09601eb
Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
Reviewed-on: https://chromium-review.googlesource.com/438070
Reviewed-by: Alexandru Stan <amstan@chromium.org>
Reviewed-by: Simon Glass <sjg@google.com>

common/keyboard_mkbp.c
@@ -123,8 +123,6 @@ void keyboard_clear_buffer(void)
 
 test_mockable int keyboard_fifo_add(const uint8_t *buffp)
 {
-	int ret = EC_SUCCESS;
-
 	/*
 	 * If keyboard protocol is not enabled, don't save the state to the
 	 * FIFO or trigger an interrupt.
@@ -135,27 +133,29 @@ test_mockable int keyboard_fifo_add(const uint8_t *buffp)
 	if (kb_fifo_entries >= config.fifo_max_depth) {
 		CPRINTS("KB FIFO depth %d reached",
 			config.fifo_max_depth);
-		ret = EC_ERROR_OVERFLOW;
-		goto kb_fifo_push_done;
+
+		return EC_ERROR_OVERFLOW;
 	}
 
 	mutex_lock(&fifo_mutex);
 	memcpy(kb_fifo[kb_fifo_end], buffp, KEYBOARD_COLS);
 	kb_fifo_end = (kb_fifo_end + 1) % KB_FIFO_DEPTH;
 	atomic_add(&kb_fifo_entries, 1);
-	mutex_unlock(&fifo_mutex);
 
-kb_fifo_push_done:
-
-	if (ret == EC_SUCCESS) {
 #ifdef CONFIG_MKBP_EVENT
-		mkbp_send_event(EC_MKBP_EVENT_KEY_MATRIX);
+	/*
+	 * If our event didn't generate an interrupt then the host is still
+	 * asleep. In this case, we don't want to queue our event, except if
+	 * another event just woke the host (and wake is already in progress).
+	 */
+	if (!mkbp_send_event(EC_MKBP_EVENT_KEY_MATRIX) && kb_fifo_entries == 1)
+		keyboard_clear_buffer();
 #else
 		set_host_interrupt(1);
 #endif
-	}
 
-	return ret;
+	mutex_unlock(&fifo_mutex);
+	return EC_SUCCESS;
 }
 
 #ifdef CONFIG_MKBP_EVENT