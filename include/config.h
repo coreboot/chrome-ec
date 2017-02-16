jackadam1981/ChromeEc

button: Check volume up/down status and set recovery mode
Add support for entering recovery mode using volume up/down keys.

BRANCH=none
BUG=chrome-os-partner:61930
TEST=Press Power+Volume Up+Volume Down, poppy enters recovery

Change-Id: Id40a144e9b430cfb9dfd47048e9e96d598bc3db8

include/config.h
@@ -2421,6 +2421,9 @@
 /* Number of supported slots for secure temporary storage */
 #undef CONFIG_VSTORE_SLOT_COUNT
 
+/* Support for entering recovery mode using volume buttons. */
+#undef CONFIG_VOLUME_BUTTON_RECOVERY
+
 /*****************************************************************************/
 /* Watchdog config */
 

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/428530
Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
Tested-by: Nicolas Boichat <drinkcat@chromium.org>
Reviewed-by: Randall Spangler <rspangler@chromium.org>
(cherry picked from commit 90ae18655c01e880563add1c761193316a633034)
Reviewed-on: https://chromium-review.googlesource.com/442926
Reviewed-by: Shawn N <shawnn@chromium.org>
Commit-Queue: Philip Chen <philipchen@chromium.org>
Tested-by: Philip Chen <philipchen@chromium.org>

include/config.h
@@ -328,6 +328,9 @@
  */
 #undef CONFIG_BUTTON_COUNT
 
+/* Support for entering recovery mode using volume buttons. */
+#undef CONFIG_BUTTON_RECOVERY
+
 /*
  * Enable case close debug (CCD) mode in the EC.
  */