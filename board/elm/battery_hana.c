e4e8faec5ec0062fac2cd94faed3f1beea08d5da
Yidi Lin <yidi.lin@mediatek.com>
2017-03-21 17:51:41 +0800

    hana/birch: Add support for Sunwoda battery
    Hana and Birch use same board files but have different batteries.
    Refactor battery_hana.c to support multiple battery profiles.
    
    BUG=b:35586837
    BRANCH=oak
    TEST=verified with Simplo and Sunwoda battery
    
    Change-Id: Ib5050798c583aa8f358431888f8f6b028c9219aa
    Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/428526
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Rong Chang <rongchang@chromium.org>

--- board/elm/battery_hana.c
+++ board/elm/battery_hana.c
@@ -7,14 +7,36 @@
 
 #include "battery.h"
 #include "battery_smart.h"
+#include "console.h"
 #include "util.h"
 
-/* Shutdown mode parameter to write to manufacturer access register */
-#define SB_SHIP_MODE_REG	0x34
-#define SB_SHUTDOWN_DATA1	0x0000
-#define SB_SHUTDOWN_DATA2       0x1000
+/* Console output macros */
+#define CPRINTS(format, args...) cprints(CC_CHARGER, format, ## args)
 
-static const struct battery_info info = {
+enum battery_type {
+	INIT = -1,
+	SIMPLO = 0,
+	SUNWODA,
+	BATTERY_TYPE_COUNT,
+	DEFAULT_BATTERY_TYPE = SIMPLO,
+};
+
+static enum battery_type board_battery_type = INIT;
+
+struct ship_mode_info {
+	int ship_mode_reg;
+	int ship_mode_data[2];
+};
+
+struct battery_device {
+	const char			*manuf;
+	const char			*device;
+	int				design_mv;
+	const struct battery_info	*battery_info;
+	const struct ship_mode_info	*ship_mode_info;
+};
+
+static const struct battery_info info_simplo = {
 	.voltage_max = 12600,
 	.voltage_normal = 11100,
 	.voltage_min = 9100,
@@ -29,20 +51,104 @@ static const struct battery_info info = {
 	.discharging_max_c = 60,
 };
 
+static const struct ship_mode_info ship_mode_info_simplo = {
+	.ship_mode_reg = 0x34,
+	.ship_mode_data = { 0x0000, 0x1000 },
+};
+
+static const struct battery_info info_sunwoda = {
+	.voltage_max = 13050,
+	.voltage_normal = 11400,
+	.voltage_min = 9000,
+	/* Pre-charge values. */
+	.precharge_current = 300, /* mA */
+
+	.start_charging_min_c = 0,
+	.start_charging_max_c = 45,
+	.charging_min_c = 0,
+	.charging_max_c = 60,
+	.discharging_min_c = 0,
+	.discharging_max_c = 60,
+};
+
+static const struct ship_mode_info ship_mode_info_sunwoda = {
+	.ship_mode_reg = 0x00,
+	.ship_mode_data = { 0x0010, 0x0010 },
+};
+
+static const struct battery_device support_batteries[BATTERY_TYPE_COUNT] = {
+	[SIMPLO] = {
+		.manuf		= "SMP",
+		.device		= "L15M3PB1",
+		.design_mv	= 11100,
+		.battery_info	= &info_simplo,
+		.ship_mode_info	= &ship_mode_info_simplo,
+	},
+	[SUNWODA] = {
+		.manuf		= "sunwoda",
+		.device		= "BBLD485595",
+		.design_mv	= 11400,
+		.battery_info	= &info_sunwoda,
+		.ship_mode_info	= &ship_mode_info_sunwoda,
+	},
+};
+
+static inline const struct battery_device *get_battery_device(void)
+{
+	int i;
+	char manuf[32], device[32];
+	int design_mv;
+
+	if (board_battery_type != INIT)
+		return &support_batteries[board_battery_type];
+
+	if (battery_manufacturer_name(manuf, sizeof(manuf)))
+		goto err_unknown;
+
+	if (battery_device_name(device, sizeof(device)))
+		goto err_unknown;
+
+	if (battery_design_voltage((int *)&design_mv))
+		goto err_unknown;
+
+	for (i = 0; i < BATTERY_TYPE_COUNT; ++i) {
+		if ((strcasecmp(support_batteries[i].manuf, manuf) == 0) &&
+		    (strcasecmp(support_batteries[i].device, device) == 0) &&
+		    (support_batteries[i].design_mv == design_mv)) {
+			board_battery_type = i;
+
+			return &support_batteries[board_battery_type];
+		}
+	}
+
+	CPRINTS("un-recognized battery Manuf:%s, Device:%s",
+		manuf, device);
+
+err_unknown:
+	CPRINTS("Use default battery profile");
+	board_battery_type = DEFAULT_BATTERY_TYPE;
+
+	return &support_batteries[board_battery_type];
+}
+
 const struct battery_info *battery_get_info(void)
 {
-	return &info;
+	return get_battery_device()->battery_info;
 }
 
 int board_cut_off_battery(void)
 {
 	int rv;
+	const struct ship_mode_info *ship_mode_inf =
+				get_battery_device()->ship_mode_info;
 
 	/* Ship mode command must be sent twice to take effect */
-	rv = sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA1);
+	rv = sb_write(ship_mode_inf->ship_mode_reg,
+			ship_mode_inf->ship_mode_data[0]);
 
 	if (rv != EC_SUCCESS)
 		return rv;
 
-	return sb_write(SB_SHIP_MODE_REG, SB_SHUTDOWN_DATA2);
+	return sb_write(ship_mode_inf->ship_mode_reg,
+			ship_mode_inf->ship_mode_data[1]);
 }
