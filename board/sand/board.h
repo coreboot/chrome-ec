8ed5cf2dd61c8f9140f560b4f61be1b62289b201
Ryan Zhang <Ryan.Zhang@quanta.corp-partner.google.com>
2017-04-10 20:11:49 +0800

    Sand: Add Keyboard backlight config.
    + add PWM for keyboard backlight for new feature.
    + remove PWM LED config (clean up)
    
    BUG=None
    BRANCH=firmware-reef-9042.B
    TEST=`make -j BOARD=sand`, `console.cmd.kblight 0% ~ 100%`
    Signed-off-by: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>
    
    Change-Id: If23c0ab54ff8c18700ebb51a2d43878e4e105170
    Reviewed-on: https://chromium-review.googlesource.com/472291
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Commit-Queue: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>
    Tested-by: Ryan Zhang <ryan.zhang@quanta.corp-partner.google.com>

--- board/sand/board.h
+++ board/sand/board.h
@@ -128,6 +128,7 @@
 #define CONFIG_LTO
 #define CONFIG_POWER_SIGNAL_INTERRUPT_STORM_DETECT_THRESHOLD 30
 #define CONFIG_PWM
+#define CONFIG_PWM_KBLIGHT
 #define CONFIG_TEMP_SENSOR
 #define CONFIG_THERMISTOR_NCP15WB
 #define CONFIG_DPTF
@@ -180,8 +181,7 @@ enum adc_channel {
 };
 
 enum pwm_channel {
-	PWM_CH_LED_GREEN = 0,
-	PWM_CH_LED_RED,
+	PWM_CH_KBLIGHT,
 	/* Number of PWM channels */
 	PWM_CH_COUNT
 };
