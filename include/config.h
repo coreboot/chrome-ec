1aab6c4f3dce338ab80d27774425b23e51f23543
Vincent Palatin <vpalatin@chromium.org>
2017-05-30 18:19:26 +0200

    Add U2F implementation
    Add the common code to support FIDO U2F (Universal second factor
    authentication) protocol implementation: the APDU parsing and standard
    commands execution, plus a few non-standard flags and hooks.
    
    The u2f.h header is the unmodified copy from the U2F v1.1
    Specifications archive.
    
    Mostly copied over from the cr52 code-base.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=cr50
    BUG=b:35545754
    TEST=with follow-up CLs, run U2FTest on Eve.
    CQ-DEPEND=CL:*390230
    
    Change-Id: I636d4a77ea69d69b5ab18a958e58ee6fcb2476bc
    Reviewed-on: https://chromium-review.googlesource.com/518136
    Commit-Ready: Vincent Palatin <vpalatin@chromium.org>
    Tested-by: Vincent Palatin <vpalatin@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    (cherry picked from commit 3fdda8b6f1a127bff83ed7e30316a837b941875c)
    Reviewed-on: https://chromium-review.googlesource.com/571642
    Tested-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    (cherry picked from commit 605fbc270a0c575daf869281818ac05e454755c3)

--- include/config.h
+++ include/config.h
@@ -2135,6 +2135,10 @@
 #undef CONFIG_STREAM_USART4
 
 /*****************************************************************************/
+/* U2F config: second factor authentication */
+#undef CONFIG_U2F
+
+/*****************************************************************************/
 /* USB stream config */
 #undef CONFIG_STREAM_USB
 
