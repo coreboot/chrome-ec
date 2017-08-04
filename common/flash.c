jackadam1981/ChromeEc

flash: add flash selection support
Some chips require special operations before flash can be accessed
(read, write, erase), without it the flash operations could be
corrupted.  The chip that requires this should enable the
CONFIG_FLASH_INFO_SELECT_REQUIRED config, which exposes
EC_FLASH_INFO_SELECT_REQUIRED in flashinfo flags. Before any flash
operations is executed on the chip, EC_CMD_FLASH_SELECT should be issued
to notify the chip to prepare for the flash operations.

BRANCH=none
BUG=b:63685022
TEST=with depended CLs, touchpad interrupt should be disabled when
     flashrom is in progress.
CQ-DEPEND=CL:

Change-Id: I96455adbe739d5f924edf382a2752404a7c5ad04
Signed-off-by: Wei-Ning Huang <wnhuang@google.com>

common/flash.c
@@ -1092,6 +1092,9 @@ static int flash_command_get_info(struct host_cmd_handler_args *args)
 #else
 		r_2->flags = 0;
 #endif
+#ifdef CONFIG_FLASH_INFO_SELECT_REQUIRED
+		r_2->flags |= EC_FLASH_INFO_SELECT_REQUIRED;
+#endif
 		r_2->write_ideal_size = ideal_size;
 		r_2->num_banks_total = banks_size;
 		r_2->num_banks_desc = MIN(banks_size, p_2->num_banks_desc);
@@ -1117,6 +1120,9 @@ static int flash_command_get_info(struct host_cmd_handler_args *args)
 		r_1->write_ideal_size = ideal_size;
 #if (CONFIG_FLASH_ERASED_VALUE32 == 0)
 		r_1->flags |= EC_FLASH_INFO_ERASE_TO_0;
+#endif
+#ifdef CONFIG_FLASH_INFO_SELECT_REQUIRED
+		r_1->flags |= EC_FLASH_INFO_SELECT_REQUIRED;
 #endif
 	}
 	return EC_RES_SUCCESS;
@@ -1332,3 +1338,18 @@ static int flash_command_region_info(struct host_cmd_handler_args *args)
 DECLARE_HOST_COMMAND(EC_CMD_FLASH_REGION_INFO,
 		     flash_command_region_info,
 		     EC_VER_MASK(EC_VER_FLASH_REGION_INFO));
+
+
+#ifdef CONFIG_FLASH_INFO_SELECT_REQUIRED
+
+static int flash_command_select(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_flash_select *p = args->params;
+
+	return board_flash_select(p->selected);
+}
+DECLARE_HOST_COMMAND(EC_CMD_FLASH_SELECT,
+		     flash_command_select,
+		     EC_VER_MASK(0));
+
+#endif /* CONFIG_FLASH_INFO_SELECT_REQUIRED */

include/ec_commands.h
@@ -1129,6 +1129,13 @@ struct __ec_align4 ec_response_flash_info {
 /* EC flash erases bits to 0 instead of 1 */
 #define EC_FLASH_INFO_ERASE_TO_0 (1 << 0)
 
+/* Flash must be selected for read/write/erase operations to succeed.  This may
+ * be necessary on a chip where write/erase can be corrupted by other board
+ * activity, or where the chip needs to enable some sort of programming voltage,
+ * or where the read/write/erase operations require cleanly suspending other
+ * chip functionality. */
+#define EC_FLASH_INFO_SELECT_REQUIRED (1 << 1)
+
 /*
  * Version 1 returns the same initial fields as version 0, with additional
  * fields following.
@@ -1398,6 +1405,15 @@ struct __ec_align1 ec_response_flash_spi_info {
 	/* Status registers from command 0x05 and 0x35 */
 	uint8_t sr1, sr2;
 };
+
+
+/* Select flash during flash operations */
+#define EC_CMD_FLASH_SELECT 0x0019
+
+struct __ec_align4 ec_params_flash_select {
+	uint8_t selected;
+};
+
 
 /*****************************************************************************/
 /* PWM commands */

coreboot/chrome-ec

CONFIG_FLASH_SELECT_REQUIRED config, which exposes
 Conflicts:
    (checked out from master)
	include/config.h
CQ-DEPEND=CL:*416548
Reviewed-on: https://chromium-review.googlesource.com/582374
Commit-Ready: Wei-Ning Huang <wnhuang@chromium.org>
Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>
(cherry picked from commit b9c8dcb9f6b113d35f9da7a218df0507d4a492c2)
Reviewed-on: https://chromium-review.googlesource.com/598521
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
Tested-by: Vadim Bendebury <vbendeb@chromium.org>
Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>

common/flash.c
@@ -1072,6 +1072,9 @@ static int flash_command_get_info(struct host_cmd_handler_args *args)
 #else
 		r_2->flags = 0;
 #endif
+#ifdef CONFIG_FLASH_SELECT_REQUIRED
+		r_2->flags |= EC_FLASH_INFO_SELECT_REQUIRED;
+#endif
 		r_2->write_ideal_size = ideal_size;
 		r_2->num_banks_total = banks_size;
 		r_2->num_banks_desc = MIN(banks_size, p_2->num_banks_desc);
@@ -1097,6 +1100,9 @@ static int flash_command_get_info(struct host_cmd_handler_args *args)
 		r_1->write_ideal_size = ideal_size;
 #if (CONFIG_FLASH_ERASED_VALUE32 == 0)
 		r_1->flags |= EC_FLASH_INFO_ERASE_TO_0;
+#endif
+#ifdef CONFIG_FLASH_SELECT_REQUIRED
+		r_1->flags |= EC_FLASH_INFO_SELECT_REQUIRED;
 #endif
 	}
 	return EC_RES_SUCCESS;
@@ -1268,3 +1274,18 @@ static int flash_command_region_info(struct host_cmd_handler_args *args)
 DECLARE_HOST_COMMAND(EC_CMD_FLASH_REGION_INFO,
 		     flash_command_region_info,
 		     EC_VER_MASK(EC_VER_FLASH_REGION_INFO));
+
+
+#ifdef CONFIG_FLASH_SELECT_REQUIRED
+
+static int flash_command_select(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_flash_select *p = args->params;
+
+	return board_flash_select(p->select);
+}
+DECLARE_HOST_COMMAND(EC_CMD_FLASH_SELECT,
+		     flash_command_select,
+		     EC_VER_MASK(0));
+
+#endif /* CONFIG_FLASH_SELECT_REQUIRED */

include/ec_commands.h
@@ -1114,6 +1114,13 @@ struct __ec_align4 ec_response_flash_info {
 /* EC flash erases bits to 0 instead of 1 */
 #define EC_FLASH_INFO_ERASE_TO_0 (1 << 0)
 
+/* Flash must be selected for read/write/erase operations to succeed.  This may
+ * be necessary on a chip where write/erase can be corrupted by other board
+ * activity, or where the chip needs to enable some sort of programming voltage,
+ * or where the read/write/erase operations require cleanly suspending other
+ * chip functionality. */
+#define EC_FLASH_INFO_SELECT_REQUIRED (1 << 1)
+
 /*
  * Version 1 returns the same initial fields as version 0, with additional
  * fields following.
@@ -1347,6 +1354,16 @@ struct __ec_align1 ec_response_flash_spi_info {
 	/* Status registers from command 0x05 and 0x35 */
 	uint8_t sr1, sr2;
 };
+
+
+/* Select flash during flash operations */
+#define EC_CMD_FLASH_SELECT 0x0019
+
+struct __ec_align4 ec_params_flash_select {
+	/* 1 to select flash, 0 to deselect flash */
+	uint8_t select;
+};
+
 
 /*****************************************************************************/
 /* PWM commands */