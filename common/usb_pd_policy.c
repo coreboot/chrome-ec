11c265c5e06be301f67bbe30ea57ae3176969d38
Aseda Aboagye <aaboagye@google.com>
2017-08-10 13:12:30 -0700

    charge_manager: Consider port in source PDO.
    When CONFIG_USB_PD_MAX_SINGLE_SOURCE_CURRENT is defined for a board, as
    its name implies, the board can source a higher current if there is
    only one port acting as a source.
    
    This commit fixes an issue with selecting the right source capability
    message to advertise.  charge_manager_get_source_pdo() was simply
    checking if there was more than one sink connected, instead of checking
    if there were any *other* sinks connected.  In the event that a sink
    was connected to a different port, we would advertise the max source
    PDO.
    
    BUG=b:64037926, b:35577509
    BRANCH=gru,eve,reef
    TEST=Connect sink to port 1.  Connect a AMA to port 0 that claims that
    VBUS isn't necessary.  Start sending source caps, verify that the max
    PDO is not being advertised in the source caps.
    
    Change-Id: I83c4ac56a0db2ec2fbb41ee90c48c5c9cad90b97
    Signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Original-Change-Id: Ie4145ecaf98d5b9070ad3e8b139e5653685fa801
    Original-signed-off-by: Aseda Aboagye <aaboagye@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/610479
    Original-Commit-Ready: Aseda Aboagye <aaboagye@chromium.org>
    Original-tested-by: Aseda Aboagye <aaboagye@chromium.org>
    Original-reviewed-by: Shawn N <shawnn@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/614651
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Commit-Queue: Aseda Aboagye <aaboagye@chromium.org>
    Tested-by: Aseda Aboagye <aaboagye@chromium.org>

--- common/usb_pd_policy.c
+++ common/usb_pd_policy.c
@@ -32,7 +32,7 @@
 
 static int rw_flash_changed = 1;
 
-int pd_check_requested_voltage(uint32_t rdo)
+int pd_check_requested_voltage(uint32_t rdo, const int port)
 {
 	int max_ma = rdo & 0x3FF;
 	int op_ma = (rdo >> 10) & 0x3FF;
@@ -42,7 +42,7 @@ int pd_check_requested_voltage(uint32_t rdo)
 #if defined(CONFIG_USB_PD_DYNAMIC_SRC_CAP) || \
 		defined(CONFIG_USB_PD_MAX_SINGLE_SOURCE_CURRENT)
 	const uint32_t *src_pdo;
-	const int pdo_cnt = charge_manager_get_source_pdo(&src_pdo);
+	const int pdo_cnt = charge_manager_get_source_pdo(&src_pdo, port);
 #else
 	const uint32_t *src_pdo = pd_src_pdo;
 	const int pdo_cnt = pd_src_pdo_cnt;
