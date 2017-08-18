jackadam1981/ChromeEc

g: show RW headers' Board ID fields in 'version' output
The contents of the board ID fields of the Ct50 headers is an
important piece of information which determines if an image can run on
a particular H1 chip.

This patch adds this information to the output of the 'version'
command, printing both the contents of the fields of the RW images and
if the image would run with the current INFO1 board ID contents (Yes
or NO).

The board_id feature is in fact g chipset specific, this is why
board_id support files are being moved from the cr50 board scope to
the g chip scope.

BRANCH=cr50
BUG=b:35587387,b:35587053
TEST=observed expected output in the version command:
  > bid
  Board ID: 000000fa, flags 000000ff
  > vers
  Chip:    g cr50 B2-C
  Board:   0
  RO_A:  * 0.0.10/29d77172
  RO_B:    0.0.10/c2a3f8f9
  RW_A:  * 0.0.20/DBG/cr50_v1.1.6542-856c3aff4
  RW_B:    0.0.20/DBG/cr50_v1.1.6543-2c68a2630+
  BID A:   00000000:00000000:00000000 Yes
  BID B:   000000ea:0000fffc:000000ff  No
  Build:   0.0.20/DBG/cr50_v1.1.6542-856c3aff4
           tpm2:v0.0.289-cb2de5a
           cryptoc:v0.0.8-6283eee
           2017-06-09 15:34:19 vbendeb@eskimo.mtv.corp.google.com
  >

Change-Id: I5b283abf304a7408ca8f424407044fca238185e1
Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

chip/g/system.c
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-#include "config.h"
+#include "board_id.h"
 #include "console.h"
 #include "cpu.h"
 #include "cpu.h"
@@ -687,3 +687,32 @@ void system_get_rollback_bits(char *value, size_t value_size)
 	snprintf(value, value_size, "%d/%d/%d", info_count,
 		 headers[0].count, headers[1].count);
 }
+
+#ifdef CONFIG_CHIP_VERSION_EXTENSION
+
+void system_print_chip_version_info(void)
+{
+	int i;
+	struct board_id bid;
+	enum system_image_copy_t rw_images[] = {
+		SYSTEM_IMAGE_RW, SYSTEM_IMAGE_RW_B
+	};
+
+	if (read_board_id(&bid) != EC_SUCCESS) {
+		ccprintf("Board ID read failure!\n");
+		return;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(rw_images); i++) {
+		struct SignedHeader *ss = (struct SignedHeader *)
+			get_program_memory_addr(rw_images[i]);
+
+		ccprintf("BID %c:   %08x:%08x:%08x %s\n", 'A' + i,
+			 ss->board_id_type ^ SIGNED_HEADER_PADDING,
+			 ss->board_id_type_mask ^ SIGNED_HEADER_PADDING,
+			 ss->board_id_flags ^ SIGNED_HEADER_PADDING,
+			 check_board_id_vs_header(&bid, ss) ? " No" : "Yes");
+	}
+}
+
+#endif /* CONFIG_CHIP_VERSION_EXTENSION */

coreboot/chrome-ec

The contents of the board ID fields of the Cr50 image headers is an
 Conflicts:
	include/config.h
Reviewed-on: https://chromium-review.googlesource.com/530033
Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
Reviewed-by: Randall Spangler <rspangler@chromium.org>
(cherry picked from commit 68079d94a66830ae25bb0c6d6b5ac53555e05fd9)
Reviewed-on: https://chromium-review.googlesource.com/622235

chip/g/system.c
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-#include "config.h"
+#include "board_id.h"
 #include "console.h"
 #include "cpu.h"
 #include "cpu.h"
@@ -679,3 +679,32 @@ void system_get_rollback_bits(char *value, size_t value_size)
 	snprintf(value, value_size, "%d/%d/%d", info_count,
 		 headers[0].count, headers[1].count);
 }
+
+#ifdef CONFIG_EXTENDED_VERSION_INFO
+
+void system_print_extended_version_info(void)
+{
+	int i;
+	struct board_id bid;
+	enum system_image_copy_t rw_images[] = {
+		SYSTEM_IMAGE_RW, SYSTEM_IMAGE_RW_B
+	};
+
+	if (read_board_id(&bid) != EC_SUCCESS) {
+		ccprintf("Board ID read failure!\n");
+		return;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(rw_images); i++) {
+		struct SignedHeader *ss = (struct SignedHeader *)
+			get_program_memory_addr(rw_images[i]);
+
+		ccprintf("BID %c:   %08x:%08x:%08x %s\n", 'A' + i,
+			 ss->board_id_type ^ SIGNED_HEADER_PADDING,
+			 ss->board_id_type_mask ^ SIGNED_HEADER_PADDING,
+			 ss->board_id_flags ^ SIGNED_HEADER_PADDING,
+			 check_board_id_vs_header(&bid, ss) ? " No" : "Yes");
+	}
+}
+
+#endif /* CONFIG_EXTENDED_VERSION_INFO */