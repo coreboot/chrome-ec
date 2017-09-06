6644e880ade627300aa1bc80aa7d74100776d955
Nicolas Boichat <drinkcat@chromium.org>
2017-09-06 18:04:19 +0800

    common/sha256: agressive SHA-256 unrolling as an option
    Reduces "hash done" time from ~1.30 to ~1.15s on soraka.
    
    BRANCH=none
    BUG=chromium:702378
    BUG=b:64196191
    TEST=Boot soraka, looks at hash done time.
    TEST=make run-sha256 run-sha256_unrolled passes.
    
    Change-Id: I396e52b699874afb8a3cfd324bc9464722bf2d9a
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Original-Commit-Id: 797d740727813c7b600e8ecd9df0bb87b0d39678
    Original-Change-Id: Ia29ee27404d6e9aa615ff59755b59d3f26648e71
    Original-Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Original-Reviewed-on: https://chromium-review.googlesource.com/652327
    Original-Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Original-Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/663919

--- include/config.h
+++ include/config.h
@@ -2005,6 +2005,9 @@
 /* Support computing of other hash sizes (without the VBOOT code) */
 #undef CONFIG_SHA256
 
+/* Unroll some loops in SHA256_transform for better performance. */
+#undef CONFIG_SHA256_UNROLLED
+
 /* Emulate the CLZ (Count Leading Zeros) in software for CPU lacking support */
 #undef CONFIG_SOFTWARE_CLZ
 
