7f0710ffeb9ee48d36c6db0e943bdea3a3ebb983
Furquan Shaikh <furquan@chromium.org>
2017-10-09 22:02:22 -0700

    host_event: Move host events and mask handling into common code
    Instead of duplicating the handling of host events and host event
    masks in chip lpc drivers, add routines in common code to provide
    basic functions like setting/getting of masks, setting/getting of
    events and handling of masks transitions across sysjump.
    
    BUG=None
    BRANCH=None
    TEST=make -j buildall. Verified following:
    1. Event masks are correctly retained across sysjumps.
    2. Wake from S3 works fine.
    3. Wake from S0ix works fine.
    4. SCI generated correctly.
    
    Change-Id: Ie409f91b12788e4b902b2627e31ba5ce40ff1d27
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/707771
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2585065
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>

--- power/intel_x86.c
+++ power/intel_x86.c
@@ -132,7 +132,7 @@ static void s0ix_lpc_disable_wake_mask_for_lid_open(void)
 		lpc_set_host_event_mask(LPC_HOST_EVENT_WAKE, mask);
 
 		/* clear host events */
-		while (lpc_query_host_event_state() != 0)
+		while (lpc_get_next_host_event() != 0)
 			;
 	}
 }
