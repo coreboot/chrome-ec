jackadam1981/ChromeEc

common/sha256: agressive SHA-256 unrolling as an option
Reduces "hash done" time from ~1.30 to ~1.15s on soraka.

BRANCH=none
BUG=b:64196191
TEST=Boot soraka, looks at hash done time.
TEST=make run-sha256 passes with the option set.

Change-Id: Ia29ee27404d6e9aa615ff59755b59d3f26648e71
Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>

include/config.h
@@ -2096,6 +2096,9 @@
 /* Support computing of other hash sizes (without the VBOOT code) */
 #undef CONFIG_SHA256
 
+/* Unroll some loops in SHA256_transform for better performance. */
+#undef CONFIG_SHA256_OPTIMIZED
+
 /* Emulate the CLZ (Count Leading Zeros) in software for CPU lacking support */
 #undef CONFIG_SOFTWARE_CLZ
 

coreboot/chrome-ec

BUG=chromium:702378
TEST=make run-sha256 run-sha256_unrolled passes.
Reviewed-on: https://chromium-review.googlesource.com/652327
Reviewed-by: Furquan Shaikh <furquan@chromium.org>
Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
(cherry picked from commit 797d740727813c7b600e8ecd9df0bb87b0d39678)
Reviewed-on: https://chromium-review.googlesource.com/828661
Tested-by: Vadim Bendebury <vbendeb@chromium.org>
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>

include/config.h
@@ -1836,6 +1836,9 @@
 /* Support computing of other hash sizes (without the VBOOT code) */
 #undef CONFIG_SHA256
 
+/* Unroll some loops in SHA256_transform for better performance. */
+#undef CONFIG_SHA256_UNROLLED
+
 /* Emulate the CLZ (Count Leading Zeros) in software for CPU lacking support */
 #undef CONFIG_SOFTWARE_CLZ
 