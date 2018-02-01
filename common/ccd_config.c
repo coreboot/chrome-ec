jackadam1981/ChromeEc

ccd: 'pp polling' vendor command option
When implementing 'ccd open' and 'ccd unlock' through gsctool, we need
to be able to pass to the host the state of the physical presences
state machine regarding the expected user action (pressing the PP
button).

Two new VENDOR_CC_CCD subcommands are being added: CCDV_PP_POLL_OPEN
and CCDV_PP_UNLOCK. In response to these commands, the Cr50 always
returns VENDOR_RC_SUCCESS return code and a single byte payload
showing the CCD and PP state:

  - CCDPP_CLOSED - PP process is not running, CCD closed. Maybe user
    missed a button press deadline.

  - CCDPP_AWAITING_PRESS (self explanatory)
  - CCDPP_BETWEEN_PRESSES (self explanatory)

  - CCDPP_PP_DONE - CCD is opened/unlocked (as per user request), PP
    process succeeded.

BRANCH=cr50
BUG=b:62537474

TEST=with the upcoming change to gsctool verified that PP states are
     properly conveyed to the user.

Change-Id: I97b1fef4440eea93c5c5ac01b7c60bfce9a4595c
Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

common/ccd_config.c
@@ -1367,6 +1367,67 @@ static enum vendor_cmd_rc ccd_password(void *buf,
 	return VENDOR_RC_SUCCESS;
 }
 
+static enum vendor_cmd_rc ccd_pp_poll(void *buf,
+				      size_t input_size,
+				      size_t *response_size)
+{
+	char *buffer = buf;
+
+	if ((ccd_state == CCD_STATE_OPENED) ||
+	    (ccd_state == CCD_STATE_UNLOCKED)) {
+		buffer[0] = CCDPP_PP_DONE;
+	} else {
+		switch (physical_presense_fsm_state()) {
+		case PP_AWAITING_PRESS:
+			buffer[0] = CCDPP_AWAITING_PRESS;
+			break;
+		case PP_BETWEEN_PRESSES:
+			buffer[0] = CCDPP_BETWEEN_PRESSES;
+			break;
+		default:
+			buffer[0] = CCDPP_CLOSED;
+			break;
+		}
+	}
+	*response_size = 1;
+	return VENDOR_RC_SUCCESS;
+}
+
+static enum vendor_cmd_rc ccd_pp_poll_unlock(void *buf,
+					     size_t input_size,
+					     size_t *response_size)
+{
+	char *buffer;
+
+	if ((ccd_state != CCD_STATE_OPENED) &&
+	    (ccd_state != CCD_STATE_UNLOCKED))
+		return ccd_pp_poll(buf, input_size, response_size);
+
+
+	buffer = buf;
+	*response_size = 1;
+	buffer[0] = CCDPP_PP_DONE;
+
+	return VENDOR_RC_SUCCESS;
+}
+
+static enum vendor_cmd_rc ccd_pp_poll_open(void *buf,
+					   size_t input_size,
+					   size_t *response_size)
+{
+	char *buffer;
+
+	if (ccd_state != CCD_STATE_OPENED)
+		return ccd_pp_poll(buf, input_size, response_size);
+
+
+	buffer = buf;
+	*response_size = 1;
+	buffer[0] = CCDPP_PP_DONE;
+
+	return VENDOR_RC_SUCCESS;
+}
+
 /*
  * Common TPM Vendor command handler used to demultiplex various CCD commands
  * which need to be available both throuh CLI and over /dev/tpm0.
@@ -1403,6 +1464,14 @@ static enum vendor_cmd_rc ccd_vendor(enum vendor_cmd_cc code,
 
 	case CCDV_LOCK:
 		handler = ccd_lock;
+		break;
+
+	case CCDV_PP_POLL_UNLOCK:
+		handler = ccd_pp_poll_unlock;
+		break;
+
+	case CCDV_PP_POLL_OPEN:
+		handler = ccd_pp_poll_open;
 		break;
 
 	default:

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/861001
Reviewed-by: Randall Spangler <rspangler@chromium.org>
(cherry picked from commit 8347907c46df8186d339c10dcca1b1c5f8f641ed)
Reviewed-on: https://chromium-review.googlesource.com/896783

common/ccd_config.c
@@ -1367,6 +1367,67 @@ static enum vendor_cmd_rc ccd_password(void *buf,
 	return VENDOR_RC_SUCCESS;
 }
 
+static enum vendor_cmd_rc ccd_pp_poll(void *buf,
+				      size_t input_size,
+				      size_t *response_size)
+{
+	char *buffer = buf;
+
+	if ((ccd_state == CCD_STATE_OPENED) ||
+	    (ccd_state == CCD_STATE_UNLOCKED)) {
+		buffer[0] = CCD_PP_DONE;
+	} else {
+		switch (physical_presense_fsm_state()) {
+		case PP_AWAITING_PRESS:
+			buffer[0] = CCD_PP_AWAITING_PRESS;
+			break;
+		case PP_BETWEEN_PRESSES:
+			buffer[0] = CCD_PP_BETWEEN_PRESSES;
+			break;
+		default:
+			buffer[0] = CCD_PP_CLOSED;
+			break;
+		}
+	}
+	*response_size = 1;
+	return VENDOR_RC_SUCCESS;
+}
+
+static enum vendor_cmd_rc ccd_pp_poll_unlock(void *buf,
+					     size_t input_size,
+					     size_t *response_size)
+{
+	char *buffer;
+
+	if ((ccd_state != CCD_STATE_OPENED) &&
+	    (ccd_state != CCD_STATE_UNLOCKED))
+		return ccd_pp_poll(buf, input_size, response_size);
+
+
+	buffer = buf;
+	*response_size = 1;
+	buffer[0] = CCD_PP_DONE;
+
+	return VENDOR_RC_SUCCESS;
+}
+
+static enum vendor_cmd_rc ccd_pp_poll_open(void *buf,
+					   size_t input_size,
+					   size_t *response_size)
+{
+	char *buffer;
+
+	if (ccd_state != CCD_STATE_OPENED)
+		return ccd_pp_poll(buf, input_size, response_size);
+
+
+	buffer = buf;
+	*response_size = 1;
+	buffer[0] = CCD_PP_DONE;
+
+	return VENDOR_RC_SUCCESS;
+}
+
 /*
  * Common TPM Vendor command handler used to demultiplex various CCD commands
  * which need to be available both throuh CLI and over /dev/tpm0.
@@ -1403,6 +1464,14 @@ static enum vendor_cmd_rc ccd_vendor(enum vendor_cmd_cc code,
 
 	case CCDV_LOCK:
 		handler = ccd_lock;
+		break;
+
+	case CCDV_PP_POLL_UNLOCK:
+		handler = ccd_pp_poll_unlock;
+		break;
+
+	case CCDV_PP_POLL_OPEN:
+		handler = ccd_pp_poll_open;
 		break;
 
 	default: