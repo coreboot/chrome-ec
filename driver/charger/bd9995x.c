jackadam1981/ChromeEc

charge_ramp: Move ramp allowed / ilim callbacks to common code
The decision on whether to ramp (and how high) depends on the quirks of
charger identification, so move the decision out of board, into the
drivers that implement usb_charger.

BUG=None
TEST=Manual on kevin, verify ramp occurs when port plugged into Z840
workstation.
BRANCH=None

Change-Id: I5b395274133837a18a4f4ac34b59b623287be175
Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>

driver/charger/bd9995x.c
@@ -15,6 +15,7 @@
 #include "ec_commands.h"
 #include "hooks.h"
 #include "i2c.h"
+#include "system.h"
 #include "task.h"
 #include "time.h"
 #include "util.h"
@@ -60,7 +61,31 @@ static enum usb_switch usb_switch_state[BD9995X_CHARGE_PORT_COUNT] = {
 	USB_SWITCH_DISCONNECT,
 	USB_SWITCH_DISCONNECT,
 };
+
+static int bd9995x_get_bc12_ilim(int charge_supplier)
+{
+	switch (charge_supplier) {
+	case CHARGE_SUPPLIER_BC12_CDP:
+		return 1500;
+	case CHARGE_SUPPLIER_BC12_DCP:
+		return 2000;
+	case CHARGE_SUPPLIER_BC12_SDP:
+		return 900;
+	case CHARGE_SUPPLIER_OTHER:
+#ifdef CONFIG_CHARGE_RAMP
+		return 2400;
+#else
+		/*
+		 * Setting the higher limit of current may result in an
+		 * anti-collapse hence limiting the current to 1A.
+		 */
+		return 1000;
 #endif
+	default:
+		return 500;
+	}
+}
+#endif /* HAS_TASK_USB_CHG */
 
 static inline int ch_raw_read16(int cmd, int *param,
 				enum bd9995x_command map_cmd)
@@ -482,6 +507,25 @@ static int usb_charger_process(enum bd9995x_charge_port port)
 	/* No need for the task to schedule a wait event */
 	return 0;
 }
+
+#if defined(CONFIG_CHARGE_RAMP) || defined(CONFIG_CHARGE_RAMP_HW)
+int chg_ramp_allowed(int supplier)
+{
+	/* Don't allow ramping in RO when write protected */
+	if (!system_is_in_rw() && system_is_locked())
+		return 0;
+	else
+		return supplier == CHARGE_SUPPLIER_BC12_DCP ||
+		       supplier == CHARGE_SUPPLIER_BC12_SDP ||
+		       supplier == CHARGE_SUPPLIER_BC12_CDP ||
+		       supplier == CHARGE_SUPPLIER_PROPRIETARY;
+}
+
+int chg_ramp_max(int supplier, int sup_curr)
+{
+	return bd9995x_get_bc12_ilim(supplier);
+}
+#endif /* CONFIG_CHARGE_RAMP_HW */
 #endif /* HAS_TASK_USB_CHG */
 
 /* chip specific interfaces */
@@ -1036,30 +1080,6 @@ int bd9995x_get_battery_voltage(void)
 }
 
 #ifdef HAS_TASK_USB_CHG
-int bd9995x_get_bc12_ilim(int charge_supplier)
-{
-	switch (charge_supplier) {
-	case CHARGE_SUPPLIER_BC12_CDP:
-		return 1500;
-	case CHARGE_SUPPLIER_BC12_DCP:
-		return 2000;
-	case CHARGE_SUPPLIER_BC12_SDP:
-		return 900;
-	case CHARGE_SUPPLIER_OTHER:
-#ifdef CONFIG_CHARGE_RAMP
-		return 2400;
-#else
-		/*
-		 * Setting the higher limit of current may result in an
-		 * anti-collapse hence limiting the current to 1A.
-		 */
-		return 1000;
-#endif
-	default:
-		return 500;
-	}
-}
-
 int bd9995x_bc12_enable_charging(enum bd9995x_charge_port port, int enable)
 {
 	int rv;

coreboot/chrome-ec

Also, rename CONFIG_CHARGE_RAMP to CONFIG_CHARGE_RAMP_SW, to better
contrast with the existing CONFIG_CHARGE_RAMP_HW.
Change-Id: Ia354879652651485183762bda10b42f4467ed542
Signed-off-by: Duncan Laurie <dlaurie@google.com>
Original-Commit-Id: b87fe062eca43fc00909098e716581bf2aeea7eb
Original-Change-Id: I5b395274133837a18a4f4ac34b59b623287be175
Original-Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
Original-Reviewed-on: https://chromium-review.googlesource.com/702681
Original-Commit-Ready: Shawn N <shawnn@chromium.org>
Original-Tested-by: Shawn N <shawnn@chromium.org>
Original-Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/914642

driver/charger/bd9995x.c
@@ -65,7 +65,31 @@ static enum usb_switch usb_switch_state[BD9995X_CHARGE_PORT_COUNT] = {
 	USB_SWITCH_DISCONNECT,
 	USB_SWITCH_DISCONNECT,
 };
+
+static int bd9995x_get_bc12_ilim(int charge_supplier)
+{
+	switch (charge_supplier) {
+	case CHARGE_SUPPLIER_BC12_CDP:
+		return 1500;
+	case CHARGE_SUPPLIER_BC12_DCP:
+		return 2000;
+	case CHARGE_SUPPLIER_BC12_SDP:
+		return 900;
+	case CHARGE_SUPPLIER_OTHER:
+#ifdef CONFIG_CHARGE_RAMP_SW
+		return 2400;
+#else
+		/*
+		 * Setting the higher limit of current may result in an
+		 * anti-collapse hence limiting the current to 1A.
+		 */
+		return 1000;
 #endif
+	default:
+		return 500;
+	}
+}
+#endif /* HAS_TASK_USB_CHG */
 
 static inline int ch_raw_read16(int cmd, int *param,
 				enum bd9995x_command map_cmd)
@@ -487,6 +511,21 @@ static int usb_charger_process(enum bd9995x_charge_port port)
 	/* No need for the task to schedule a wait event */
 	return 0;
 }
+
+#ifdef CONFIG_CHARGE_RAMP_SW
+int usb_charger_ramp_allowed(int supplier)
+{
+	return supplier == CHARGE_SUPPLIER_BC12_DCP ||
+	       supplier == CHARGE_SUPPLIER_BC12_SDP ||
+	       supplier == CHARGE_SUPPLIER_BC12_CDP ||
+	       supplier == CHARGE_SUPPLIER_OTHER;
+}
+
+int usb_charger_ramp_max(int supplier, int sup_curr)
+{
+	return bd9995x_get_bc12_ilim(supplier);
+}
+#endif /* CONFIG_CHARGE_RAMP_SW */
 #endif /* HAS_TASK_USB_CHG */
 
 /* chip specific interfaces */
@@ -1077,30 +1116,6 @@ int bd9995x_get_battery_voltage(void)
 }
 
 #ifdef HAS_TASK_USB_CHG
-int bd9995x_get_bc12_ilim(int charge_supplier)
-{
-	switch (charge_supplier) {
-	case CHARGE_SUPPLIER_BC12_CDP:
-		return 1500;
-	case CHARGE_SUPPLIER_BC12_DCP:
-		return 2000;
-	case CHARGE_SUPPLIER_BC12_SDP:
-		return 900;
-	case CHARGE_SUPPLIER_OTHER:
-#ifdef CONFIG_CHARGE_RAMP
-		return 2400;
-#else
-		/*
-		 * Setting the higher limit of current may result in an
-		 * anti-collapse hence limiting the current to 1A.
-		 */
-		return 1000;
-#endif
-	default:
-		return 500;
-	}
-}
-
 int bd9995x_bc12_enable_charging(enum bd9995x_charge_port port, int enable)
 {
 	int rv;