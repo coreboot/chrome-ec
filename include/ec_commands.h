jackadam1981/ChromeEc

PD_CONTROL: add subcommand to power up a TCPC.
add a subcommand to the PD_CONTROL message to power up a specific
TCPC.  in practice, this typically just takes the TCPC out of sleep
mode for cases where sleep mode is controlled by the EC.

TEST="make buildall" passes; "make tests" passes for reef.

BRANCH=none
BUG=b:35586895

Change-Id: Ib50e265d11eca10c3714049d8cfdf2657eff48c1
Signed-off-by: Caveh Jalali <caveh@google.com>

include/ec_commands.h
@@ -4175,7 +4175,8 @@ enum ec_pd_control_cmd {
 	PD_SUSPEND = 0,      /* Suspend the PD chip (EC: stop talking to PD) */
 	PD_RESUME,           /* Resume the PD chip (EC: start talking to PD) */
 	PD_RESET,            /* Force reset the PD chip */
-	PD_CONTROL_DISABLE   /* Disable further calls to this command */
+	PD_CONTROL_DISABLE,  /* Disable further calls to this command */
+	PD_MCU_ON,           /* Power on the PD chip */
 };
 
 struct __ec_align1 ec_params_pd_control {

coreboot/chrome-ec

under the covers, board_set_tcpc_power_mode() gets a weak function
definition so we don't need to special case this everywhere.
Change-Id: Ic1ed6a70c79f5e7c78099cd0ceb3dfdc2a0489e0
Signed-off-by: Duncan Laurie <dlaurie@google.com>
Original-Commit-Id: 28bfc8037156ba0f9c8189ef6e8b7c8a7d22d4cc
Original-Change-Id: Ib50e265d11eca10c3714049d8cfdf2657eff48c1
Original-Signed-off-by: Caveh Jalali <caveh@google.com>
Original-Reviewed-on: https://chromium-review.googlesource.com/596796
Original-Reviewed-by: Shawn N <shawnn@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/914658

include/ec_commands.h
@@ -4151,7 +4151,8 @@ enum ec_pd_control_cmd {
 	PD_SUSPEND = 0,      /* Suspend the PD chip (EC: stop talking to PD) */
 	PD_RESUME,           /* Resume the PD chip (EC: start talking to PD) */
 	PD_RESET,            /* Force reset the PD chip */
-	PD_CONTROL_DISABLE   /* Disable further calls to this command */
+	PD_CONTROL_DISABLE,  /* Disable further calls to this command */
+	PD_CHIP_ON,          /* Power on the PD chip */
 };
 
 struct __ec_align1 ec_params_pd_control {