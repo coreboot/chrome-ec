jackadam1981/ChromeEc

pd: Add "freeze" dual-role policy
Add a new DRP policy to "freeze" the power role of each port, never
toggling automatically, though manual role swaps may still occur.

BUG=chromium:769895
BRANCH=None
TEST=On servo_v4, verify DUT port stays in SRC role and POWER port
stays in SNK role while disconnected.

Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
Change-Id: Ibff3cd1ffaf0e884b030c231003763a57acbe02e

board/servo_v4/board.h
@@ -104,6 +104,10 @@
 /* Override PD_ROLE_DEFAULT in usb_pd.h */
 #define PD_ROLE_DEFAULT(port) ((port) ? PD_ROLE_SOURCE : PD_ROLE_SINK)
 
+/* Don't automatically change roles */
+#undef CONFIG_USB_PD_DUAL_ROLE_INITIAL_DRP_STATE
+#define CONFIG_USB_PD_DUAL_ROLE_INITIAL_DRP_STATE PD_DRP_FREEZE
+
 /* Variable-current Rp no connect and Ra attach macros */
 #define CC_NC(port, cc, sel)  (pd_tcpc_cc_nc(port, cc, sel))
 #define CC_RA(port, cc, sel)  (pd_tcpc_cc_ra(port, cc, sel))

common/usb_pd_protocol.c
@@ -95,7 +95,7 @@ enum vdm_states {
 
 #ifdef CONFIG_USB_PD_DUAL_ROLE
 /* Port dual-role state */
-enum pd_dual_role_states drp_state = PD_DRP_TOGGLE_OFF;
+enum pd_dual_role_states drp_state = CONFIG_USB_PD_DUAL_ROLE_INITIAL_DRP_STATE;
 
 /* Last received source cap */
 static uint32_t pd_src_caps[CONFIG_USB_PD_PORT_COUNT][PDO_MAX_OBJECTS];
@@ -1842,7 +1842,7 @@ void pd_task(void *u)
 				set_state(port,
 					PD_STATE_SRC_DISCONNECTED_DEBOUNCE);
 			}
-#if defined(CONFIG_USB_PD_DUAL_ROLE) && !defined(CONFIG_USB_PD_DTS)
+#if defined(CONFIG_USB_PD_DUAL_ROLE)
 			/*
 			 * Try.SRC state is embedded here. Wait for SNK
 			 * detect, or if timer expires, transition to
@@ -1855,6 +1855,7 @@ void pd_task(void *u)
 				 get_time().val >= pd[port].try_src_marker) ||
 				 (!(pd[port].flags & PD_FLAGS_TRY_SRC) &&
 				  drp_state != PD_DRP_FORCE_SOURCE &&
+				  drp_state != PD_DRP_FREEZE &&
 				 get_time().val >= next_role_swap)) {
 				pd[port].power_role = PD_ROLE_SINK;
 				set_state(port, PD_STATE_SNK_DISCONNECTED);
@@ -3338,6 +3339,9 @@ static int command_pd(int argc, char **argv)
 			case PD_DRP_TOGGLE_OFF:
 				ccprintf("off\n");
 				break;
+			case PD_DRP_FREEZE:
+				ccprintf("freeze\n");
+				break;
 			case PD_DRP_FORCE_SINK:
 				ccprintf("force sink\n");
 				break;
@@ -3350,6 +3354,8 @@ static int command_pd(int argc, char **argv)
 				pd_set_dual_role(PD_DRP_TOGGLE_ON);
 			else if (!strcasecmp(argv[2], "off"))
 				pd_set_dual_role(PD_DRP_TOGGLE_OFF);
+			else if (!strcasecmp(argv[2], "freeze"))
+				pd_set_dual_role(PD_DRP_FREEZE);
 			else if (!strcasecmp(argv[2], "sink"))
 				pd_set_dual_role(PD_DRP_FORCE_SINK);
 			else if (!strcasecmp(argv[2], "source"))
@@ -3557,6 +3563,7 @@ static const enum pd_dual_role_states dual_role_map[USB_PD_CTRL_ROLE_COUNT] = {
 	[USB_PD_CTRL_ROLE_TOGGLE_OFF]   = PD_DRP_TOGGLE_OFF,
 	[USB_PD_CTRL_ROLE_FORCE_SINK]   = PD_DRP_FORCE_SINK,
 	[USB_PD_CTRL_ROLE_FORCE_SOURCE] = PD_DRP_FORCE_SOURCE,
+	[USB_PD_CTRL_ROLE_FREEZE]       = PD_DRP_FREEZE,
 };
 
 #ifdef CONFIG_USBC_SS_MUX

include/config.h
@@ -2502,6 +2502,9 @@
 /* Define if this board can used TCPC-controlled DRP toggle */
 #undef CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE
 
+/* Initial DRP / toggle policy */
+#define CONFIG_USB_PD_DUAL_ROLE_INITIAL_DRP_STATE PD_DRP_TOGGLE_OFF
+
 /*
  * Define if VBUS source GPIOs (GPIO_USB_C*_5V_EN) are active-low (and named
  * (..._L) rather than default active-high.

coreboot/chrome-ec

BRANCH=servo
Change-Id: Ic4480cb6d241563dee2a848c4b35763ed83cd182
Signed-off-by: Duncan Laurie <dlaurie@google.com>
Original-Commit-Id: 7c2c5a9dc3779587f78a7c602cefeb667d210d41
Original-Signed-off-by: Shawn Nematbakhsh <shawnn@chromium.org>
Original-Change-Id: Ibff3cd1ffaf0e884b030c231003763a57acbe02e
Original-Reviewed-on: https://chromium-review.googlesource.com/715276
Original-Commit-Ready: Shawn N <shawnn@chromium.org>
Original-Tested-by: Shawn N <shawnn@chromium.org>
Original-Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/914645

board/servo_v4/board.h
@@ -99,6 +99,10 @@
 /* Override PD_ROLE_DEFAULT in usb_pd.h */
 #define PD_ROLE_DEFAULT(port) ((port) ? PD_ROLE_SOURCE : PD_ROLE_SINK)
 
+/* Don't automatically change roles */
+#undef CONFIG_USB_PD_INITIAL_DRP_STATE
+#define CONFIG_USB_PD_INITIAL_DRP_STATE PD_DRP_FREEZE
+
 /* Variable-current Rp no connect and Ra attach macros */
 #define CC_NC(port, cc, sel)  (pd_tcpc_cc_nc(port, cc, sel))
 #define CC_RA(port, cc, sel)  (pd_tcpc_cc_ra(port, cc, sel))

common/usb_pd_protocol.c
@@ -93,7 +93,7 @@ enum vdm_states {
 
 #ifdef CONFIG_USB_PD_DUAL_ROLE
 /* Port dual-role state */
-enum pd_dual_role_states drp_state = PD_DRP_TOGGLE_OFF;
+enum pd_dual_role_states drp_state = CONFIG_USB_PD_INITIAL_DRP_STATE;
 
 /* Last received source cap */
 static uint32_t pd_src_caps[CONFIG_USB_PD_PORT_COUNT][PDO_MAX_OBJECTS];
@@ -1824,7 +1824,7 @@ void pd_task(void *u)
 				set_state(port,
 					PD_STATE_SRC_DISCONNECTED_DEBOUNCE);
 			}
-#if defined(CONFIG_USB_PD_DUAL_ROLE) && !defined(CONFIG_USB_PD_DTS)
+#if defined(CONFIG_USB_PD_DUAL_ROLE)
 			/*
 			 * Try.SRC state is embedded here. Wait for SNK
 			 * detect, or if timer expires, transition to
@@ -1837,6 +1837,7 @@ void pd_task(void *u)
 				 get_time().val >= pd[port].try_src_marker) ||
 				 (!(pd[port].flags & PD_FLAGS_TRY_SRC) &&
 				  drp_state != PD_DRP_FORCE_SOURCE &&
+				  drp_state != PD_DRP_FREEZE &&
 				 get_time().val >= next_role_swap)) {
 				pd[port].power_role = PD_ROLE_SINK;
 				set_state(port, PD_STATE_SNK_DISCONNECTED);
@@ -3296,6 +3297,9 @@ static int command_pd(int argc, char **argv)
 			case PD_DRP_TOGGLE_OFF:
 				ccprintf("off\n");
 				break;
+			case PD_DRP_FREEZE:
+				ccprintf("freeze\n");
+				break;
 			case PD_DRP_FORCE_SINK:
 				ccprintf("force sink\n");
 				break;
@@ -3308,6 +3312,8 @@ static int command_pd(int argc, char **argv)
 				pd_set_dual_role(PD_DRP_TOGGLE_ON);
 			else if (!strcasecmp(argv[2], "off"))
 				pd_set_dual_role(PD_DRP_TOGGLE_OFF);
+			else if (!strcasecmp(argv[2], "freeze"))
+				pd_set_dual_role(PD_DRP_FREEZE);
 			else if (!strcasecmp(argv[2], "sink"))
 				pd_set_dual_role(PD_DRP_FORCE_SINK);
 			else if (!strcasecmp(argv[2], "source"))
@@ -3515,6 +3521,7 @@ static const enum pd_dual_role_states dual_role_map[USB_PD_CTRL_ROLE_COUNT] = {
 	[USB_PD_CTRL_ROLE_TOGGLE_OFF]   = PD_DRP_TOGGLE_OFF,
 	[USB_PD_CTRL_ROLE_FORCE_SINK]   = PD_DRP_FORCE_SINK,
 	[USB_PD_CTRL_ROLE_FORCE_SOURCE] = PD_DRP_FORCE_SOURCE,
+	[USB_PD_CTRL_ROLE_FREEZE]       = PD_DRP_FREEZE,
 };
 
 #ifdef CONFIG_USBC_SS_MUX

include/config.h
@@ -2371,6 +2371,9 @@
 /* Define if this board can used TCPC-controlled DRP toggle */
 #undef CONFIG_USB_PD_DUAL_ROLE_AUTO_TOGGLE
 
+/* Initial DRP / toggle policy */
+#define CONFIG_USB_PD_INITIAL_DRP_STATE PD_DRP_TOGGLE_OFF
+
 /*
  * Define if VBUS source GPIOs (GPIO_USB_C*_5V_EN) are active-low (and named
  * (..._L) rather than default active-high.