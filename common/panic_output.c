jackadam1981/ChromeEc

panic: Set EC_HOST_EVENT_PANIC on chipset reset
Add a hook for CHIPSET_RESET to allow the EC to indicate if there is
any new panic info present. This helps coreboot to log EC panic info
in eventlog.

BUG=b:65732924,b:69334392
BRANCH=None
TEST=Verified following:
1. Force panic_set_reason in EC on CHIPSET_RESET
2. reboot on AP console
3. mosys eventlog list shows "EC Event | Panic Reset in previous boot"

Change-Id: I77b49cd0b3bf05b10efc708e3d81af9ed0e3aa49
Signed-off-by: Furquan Shaikh <furquan@chromium.org>

common/panic_output.c
@@ -130,6 +130,7 @@ static void panic_init(void)
 #endif
 }
 DECLARE_HOOK(HOOK_INIT, panic_init, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_CHIPSET_RESET, panic_init, HOOK_PRIO_DEFAULT);
 
 #ifdef CONFIG_CMD_STACKOVERFLOW
 static void stack_overflow_recurse(int n)

coreboot/chrome-ec

Also, update the hook priority for CHIPSET_RESET and HOOK_INIT to
HOOK_PRIO_LAST to allow the EC to first log any software panic before
it is checked.
Change-Id: I314539a724a3f23cd54ebfb8114ff64ee6ff2ce7
Signed-off-by: Duncan Laurie <dlaurie@google.com>
Original-Commit-Id: 9fa6de15cf57e4beaa5726ebbe0e4ac6af004a79
Original-Change-Id: I77b49cd0b3bf05b10efc708e3d81af9ed0e3aa49
Original-Signed-off-by: Furquan Shaikh <furquan@chromium.org>
Original-Reviewed-on: https://chromium-review.googlesource.com/797911
Original-Reviewed-by: Shawn N <shawnn@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/914636

common/panic_output.c
@@ -129,7 +129,8 @@ static void panic_init(void)
 	}
 #endif
 }
-DECLARE_HOOK(HOOK_INIT, panic_init, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, panic_init, HOOK_PRIO_LAST);
+DECLARE_HOOK(HOOK_CHIPSET_RESET, panic_init, HOOK_PRIO_LAST);
 
 #ifdef CONFIG_CMD_STACKOVERFLOW
 static void stack_overflow_recurse(int n)