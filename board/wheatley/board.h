jackadam1981/ChromeEc

power: introducing pwr_avg console command
Keep a running average of power consumption, as seen by the ec
talking to the battery, over a 1 minute span. Samples get collected
every second.
This change allows us to have better power tracking while minimizing
the power impact on the EC, because
- the pwr_avg command only needs to be called once every minute, and is
short, thus less expensive to parse on ECs without a UART buffer
- the recurring update is fairly inexpensive, since it only asks the
battery for current and voltage.

change to chell is due to size constraint
changes to samus/wheatley are due to this functionality
not being necessary on those boards.

usage:
> pwr_avg
mw = -441

BUG=
TEST=make buildall -j

Change-Id: Id1a3479d277aedf90dfa965afb4ee9136654b1cf
Signed-off-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>

board/wheatley/board.h
@@ -148,6 +148,7 @@
 #undef CONFIG_CONSOLE_CMDHELP
 #undef CONFIG_CMD_I2C_SCAN
 #undef CONFIG_CONSOLE_HISTORY
+#undef CONFIG_CMD_PWR
 
 /* Features of eSPI */
 #undef CONFIG_ESPI       /* Use eSPI protocol for host interface of x86 CPU */

coreboot/chrome-ec

pwr_avg provides an average voltage, current, and power over the last
1 minute. It's up to the battery drivers to implement this
functionality.
- the work done to keep the avg is partially done by the batteries
already and it's just a question of retrieving it.
undefined on wheatley since no power debugging planned on that board.
mv = 7153
ma = -605
mw = -4327
BUG=chromium:752320
BRANCH=eve
TEST=make BOARD=eve -j
Reviewed-on: https://chromium-review.googlesource.com/823884
Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
(cherry picked from commit 51e9e69f386366256807e6f4ccdd258821cdcfe0)
 Conflicts:
  board/wheatley/board.h
    undefine some tasks to make buildall -j work
  driver/battery/max17055.c
    did not exist in branch, not needed
Change-Id: I5f37a6f6446fa0e4a205672cb3433827d5da948d
Reviewed-on: https://chromium-review.googlesource.com/966244
Reviewed-by: Duncan Laurie <dlaurie@google.com>

board/wheatley/board.h
@@ -147,6 +147,9 @@
 #undef CONFIG_CMD_TEMP_SENSOR
 #undef CONFIG_CMD_TIMERINFO
 #undef CONFIG_CONSOLE_CMDHELP
+#undef CONFIG_CMD_I2C_SCAN
+#undef CONFIG_CONSOLE_HISTORY
+#undef CONFIG_CMD_PWR_AVG
 
 /* Features of eSPI */
 #undef CONFIG_ESPI       /* Use eSPI protocol for host interface of x86 CPU */