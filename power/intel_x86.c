jackadam1981/ChromeEc

host_events: Bump up host events and masks to 64-bit
With the upcoming change to add a new command to get/set/clear host
events and masks, it seems to be the right time to bump up the host
events and masks to 64-bit. We are already out of available host
events. This change opens up at least 32 bits for new host events.

Old EC commands to operate on host events/masks will still deal with
lower 32-bits of the events/mask. On the other hand, the new command
being added will take care of the entire 64-bit events/masks. This
ensures that old BIOS and kernel versions can still work with the
newer EC versions.

BUG=b:69329196
BRANCH=None
TEST=make -j buildall. Verified:
1. hostevent set 0x4000 ==> Sets correct bit in host events
2. hostevent clear 0x4000 ==> Clears correct bit in host events
3. Kernel is able to query and read correct host event bits from
EC. Verified using evtest.
4. Coreboot is able to read correct wake reason from EC. Verified
using mosys eventlog list.

Change-Id: Idcb24ea364ac6c491efc2f8dd9e29a9df6149e07
Signed-off-by: Furquan Shaikh <furquan@chromium.org>

power/intel_x86.c
@@ -131,7 +131,7 @@ static void s0ix_transition(int check_state, int hook_id)
 static void s0ix_lpc_enable_wake_mask(void)
 {
 	if (chipset_in_state(CHIPSET_STATE_STANDBY | CHIPSET_STATE_ON)) {
-		uint32_t mask;
+		uint64_t mask;
 
 		mask = lpc_get_host_event_mask(LPC_HOST_EVENT_WAKE) |
 			EC_HOST_EVENT_MASK(EC_HOST_EVENT_LID_OPEN) |
@@ -150,7 +150,7 @@ static void s0ix_lpc_enable_wake_mask(void)
 static void s0ix_lpc_disable_wake_mask(void)
 {
 	if (chipset_in_state(CHIPSET_STATE_STANDBY | CHIPSET_STATE_ON)) {
-		uint32_t mask;
+		uint64_t mask;
 
 		mask = lpc_get_host_event_mask(LPC_HOST_EVENT_WAKE) &
 			~EC_HOST_EVENT_MASK(EC_HOST_EVENT_LID_OPEN) &

coreboot/chrome-ec

Change-Id: I286aef0f35cec10754ef7d662f883e999c22d235
Reviewed-on: https://chromium-review.googlesource.com/770925
Reviewed-by: Randall Spangler <rspangler@chromium.org>
(cherry picked from commit c9cd870600b12123dddc88814446327337557369)
Leaving out change under CONFIG_HOSTCMD_RTC #ifdef in
chip/stm32/clock-f.c.
Reviewed-on: https://chromium-review.googlesource.com/989867
Reviewed-by: Joel Kitching <kitching@chromium.org>
Commit-Queue: Joel Kitching <kitching@chromium.org>
Tested-by: Joel Kitching <kitching@chromium.org>
Trybot-Ready: Joel Kitching <kitching@chromium.org>

power/intel_x86.c
@@ -141,7 +141,7 @@ static enum power_state power_wait_s5_rtc_reset(void)
 static void s0ix_lpc_enable_wake_mask(void)
 {
 	if (chipset_in_state(CHIPSET_STATE_STANDBY | CHIPSET_STATE_ON)) {
-		uint32_t mask;
+		host_event_t mask;
 
 		mask = lpc_get_host_event_mask(LPC_HOST_EVENT_WAKE) |
 			EC_HOST_EVENT_MASK(EC_HOST_EVENT_LID_OPEN) |
@@ -161,7 +161,7 @@ static void s0ix_lpc_enable_wake_mask(void)
 static void s0ix_lpc_disable_wake_mask(void)
 {
 	if (chipset_in_state(CHIPSET_STATE_STANDBY | CHIPSET_STATE_ON)) {
-		uint32_t mask;
+		host_event_t mask;
 
 		mask = lpc_get_host_event_mask(LPC_HOST_EVENT_WAKE) &
 			~EC_HOST_EVENT_MASK(EC_HOST_EVENT_LID_OPEN) &