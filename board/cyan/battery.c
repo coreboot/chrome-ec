5f09826819ffb3621bf24118acb0c56e079facf7
DannyChou <Danny.Chou@quantatw.com>
2018-05-09 19:44:12 +0800

    Cyan: Add new battery configuration
    Add Cyan new battery : AC15A8J
    
    BUG=b:69997769
    BRANCH=cyan
    TEST=recognize new battery AP15A8J OK.
    
    Change-Id: Id90d2fd4a5691f18e728ef44a43c775198e2b040
    Signed-off-by: David Huang <David.Huang@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/1051850
    Reviewed-by: Vincent Wang <vwang@chromium.org>
    Reviewed-by: Jerry Parson <jwp@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- board/cyan/battery.c
+++ board/cyan/battery.c
@@ -7,11 +7,12 @@
 
 #include "battery.h"
 #include "battery_smart.h"
+#include "util.h"
 
 #define	SB_SHIP_MODE_ADDR	0x3a
 #define	SB_SHIP_MODE_DATA	0xc574
 
-static const struct battery_info info = {
+static const struct battery_info info_AC15A3J = {
 	.voltage_max = 12600,/* mV */
 	.voltage_normal = 10800,
 	.voltage_min = 8250,
@@ -24,9 +25,30 @@ static const struct battery_info info = {
 	.discharging_max_c = 60,
 };
 
+static const struct battery_info info_AC15A8J= {
+	.voltage_max = 13200,/* mV */
+	.voltage_normal = 11520,
+	.voltage_min = 9000,
+	.precharge_current = 256,/* mA */
+	.start_charging_min_c = 0,
+	.start_charging_max_c = 50,
+	.charging_min_c = 0,
+	.charging_max_c = 60,
+	.discharging_min_c = 0,
+	.discharging_max_c = 60,
+};
+
 const struct battery_info *battery_get_info(void)
 {
-	return &info;
+	char device[10];
+
+	if (!battery_device_name(device, sizeof(device))) {
+		if (!strcasecmp(device, "AC15A3J"))
+			return &info_AC15A3J;
+		else if (!strcasecmp(device, "AC15A8J"))
+			return &info_AC15A8J;
+	}
+	return &info_AC15A8J;
 }
 
 int board_cut_off_battery(void)
