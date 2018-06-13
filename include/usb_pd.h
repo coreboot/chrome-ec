a1fa5af5da8f5209ce64428481d6916ca226a340
Daisuke Nojiri <dnojiri@chromium.org>
2018-06-13 16:26:30 -0700

    usb-pd: Exit DP mode before shutdown
    On start-up, the tcpm tries to discover identity and enter amodes.
    When system reboots, this makes already entered modes re-entered.
    This causes some devices to malfunction.
    
    This patch makes the tcpm exit DP mode if it's already entered.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    BUG=chromium:799535,b:79185392
    BRANCH=none
    TEST=Verify picture is displayed on external monitor after
    suspend/resume, shutdown/power-on via D6000, Hoho, Aplle HDMI adapter,
    StarTech DP adapter, Dingdong with Vayne.
    
    Change-Id: I1fd577e12eaa5a81a0ae0242d38863dc211ac8bc
    Reviewed-on: https://chromium-review.googlesource.com/1185495
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -43,6 +43,7 @@ enum pd_rx_errors {
 #define PD_EVENT_CC               (1<<4) /* CC line change event */
 #define PD_EVENT_TCPC_RESET       (1<<5) /* TCPC has reset */
 #define PD_EVENT_UPDATE_DUAL_ROLE (1<<6) /* DRP state has changed */
+#define PD_EVENT_DP_DISCONNECT    (1<<7) /* DisplayPort disconnect requested */
 
 /* --- PD data message helpers --- */
 #define PDO_MAX_OBJECTS   7
