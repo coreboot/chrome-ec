b22b8446939095a5d19cc099c3ec2bad5c0d8344
Joel Kitching <kitching@chromium.org>
2018-06-25 18:48:07 +0000

    Revert "host_event: Perform lpc_init_mask before keyboard/button init"
    This reverts commit 61cb81aed63dec095af4a7590bcfa23b7bd8c89d.
    
    Reason for revert: Re-using 32-bit host events instead.  See b/110292722
    
    Original change's description:
    > host_event: Perform lpc_init_mask before keyboard/button init
    > 
    > keyboard/button init are responsible for checking if user requested
    > manual recovery. However, by this time, hook init is not run and hence
    > host event set operation for manual recovery is dropped. This change
    > adds a call to lpc_init_mask before keyboard/button init operations
    > are performed.
    > 
    > BUG=b:68189465
    > BRANCH=None
    > TEST=Verified that manual recovery works fine. Recovery using software
    > command works too:
    > reboot ap-off
    > hostevent set 0x4000
    > powerb
    > 
    > Change-Id: I4ba4ac480f78daafb2006a2b77250687b19a838d
    > Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/735799
    > Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    > Reviewed-by: Shawn N <shawnn@chromium.org>
    > (cherry picked from commit 708f6f1f21d4cee970c6bf7dadefee3936af228d)
    > Reviewed-on: https://chromium-review.googlesource.com/989865
    > Reviewed-by: Joel Kitching <kitching@chromium.org>
    > Commit-Queue: Joel Kitching <kitching@chromium.org>
    > Tested-by: Joel Kitching <kitching@chromium.org>
    > Trybot-Ready: Joel Kitching <kitching@chromium.org>
    
    Bug: b:68189465
    Change-Id: I9213094aff40edf03aa5b7e8ffdf3d43e5aea708
    Reviewed-on: https://chromium-review.googlesource.com/1113785
    Reviewed-by: Joel Kitching <kitching@chromium.org>
    Commit-Queue: Joel Kitching <kitching@chromium.org>
    Tested-by: Joel Kitching <kitching@chromium.org>
    Trybot-Ready: Joel Kitching <kitching@chromium.org>

--- common/main.c
+++ common/main.c
@@ -17,7 +17,6 @@
 #include "gpio.h"
 #include "hooks.h"
 #include "jtag.h"
-#include "lpc.h"
 #include "keyboard_scan.h"
 #ifdef CONFIG_MPU
 #include "mpu.h"
@@ -155,16 +154,6 @@ test_mockable __keep int main(void)
 	 */
 #ifdef CONFIG_EEPROM
 	eeprom_init();
-#endif
-
-	/*
-	 * Keyboard scan init/Button init can set recovery events to
-	 * indicate to host entry into recovery mode. Before this is
-	 * done, lpc always report mask needs to be initialized
-	 * correctly.
-	 */
-#ifdef CONFIG_LPC
-	lpc_init_mask();
 #endif
 #ifdef HAS_TASK_KEYSCAN
 	keyboard_scan_init();
