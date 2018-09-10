jackadam1981/ChromeEc

tcpm: add TCPC RX circular buffer in EC
The alert line for TCPC will stay asserted as long as there are RX
messages for the TCPM (i.e. EC) to pull from the TCPC. We should clear
all of the RX messages we know about during a single alert handling
session.

This CL can stand on its own, but it is a part of a CL stack that will
tighten the critical section of time between received messages from the
TCPC and sending follow up message out through the TCPC.

See go/usb-pd-slow-response-time for more details.

BRANCH=none
BUG=b:112088135,b:112344286
TEST=Reduces reset issue in most cases for phaser, bobba. Does not seem to
adversely affect state machine negotiation. Full CL stack consistently
sends a REQUEST at 18ms after a SRC_CAP GoodCRC, which is well below the
24 ms threshold we need to be under for USB PD spec compliance.

Change-Id: I1654b46400e9881f2927a5f6d6ace589edd182de
Signed-off-by: Jett Rink <jettrink@chromium.org>

common/usb_pd_protocol.c
@@ -2586,10 +2586,15 @@ void pd_task(void *u)
 #endif
 
 		/* process any potential incoming message */
-		incoming_packet = evt & PD_EVENT_RX;
+		incoming_packet = tcpm_has_pending_message(port);
 		if (incoming_packet) {
-			if (!tcpm_get_message(port, payload, &head))
-				handle_request(port, head, payload);
+			tcpm_get_message(port, payload, &head);
+			handle_request(port, head, payload);
+
+			/* Check if there are any more messages */
+			if (tcpm_has_pending_message(port))
+				task_set_event(PD_PORT_TO_TASK_ID(port),
+					       TASK_EVENT_WAKE, 0);
 		}
 
 		if (pd[port].req_suspend_state)

coreboot/chrome-ec

BUG=b:112088135,b:112344286,b:111909282,b:112848644,b:113124761
BUG=b:113057273,b:112825261
Also testing pd_suspend scenario manually and EC was responsive after
port 1 suspend because of "bad behavior"
Reviewed-on: https://chromium-review.googlesource.com/1185727
(cherry picked from commit e8070b44b088e99e38ec741ed8091c7e4a033bef)
Reviewed-on: https://chromium-review.googlesource.com/1215451
Reviewed-by: Jonathan Brandmeyer <jbrandmeyer@chromium.org>
Tested-by: Jonathan Brandmeyer <jbrandmeyer@chromium.org>

common/usb_pd_protocol.c
@@ -2608,10 +2608,15 @@ void pd_task(void *u)
 #endif
 
 		/* process any potential incoming message */
-		incoming_packet = evt & PD_EVENT_RX;
+		incoming_packet = tcpm_has_pending_message(port);
 		if (incoming_packet) {
-			if (!tcpm_get_message(port, payload, &head))
-				handle_request(port, head, payload);
+			tcpm_dequeue_message(port, payload, &head);
+			handle_request(port, head, payload);
+
+			/* Check if there are any more messages */
+			if (tcpm_has_pending_message(port))
+				task_set_event(PD_PORT_TO_TASK_ID(port),
+					       TASK_EVENT_WAKE, 0);
 		}
 
 		if (pd[port].req_suspend_state)