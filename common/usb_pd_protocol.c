d25defa1ce18513c8767912a3d8a89352061accc
Jett Rink <jettrink@chromium.org>
2018-10-01 10:34:04 -0600

    pd: prevent incorrect LPM flag use
    Only define PD_FLAGS_LPM_* with the CONFIG_USB_PD_TCPC_LOW_POWER
    option is defined. This prevents us from using the flags when we
    shouldn't.
    
    BRANCH=none
    BUG=b:116269457
    TEST=buildall works for all boards.
    
    Change-Id: I6837819a7ce5d8216e98c92c95275f297d88f911
    Signed-off-by: Jett Rink <jettrink@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1254922
    Reviewed-by: Edward Hill <ecgh@chromium.org>
    Reviewed-by: Yilun Lin <yllin@chromium.org>
    (cherry picked from commit d92cb76f16a1ddd9e1e14276d7d0fae5618264a3)
    Reviewed-on: https://chromium-review.googlesource.com/c/1265072
    Reviewed-by: Martin Roth <martinroth@chromium.org>
    Tested-by: Martin Roth <martinroth@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -3857,6 +3857,7 @@ void pd_task(void *u)
 
 			assert(auto_toggle_supported);
 
+#ifdef CONFIG_USB_PD_TCPC_LOW_POWER
 			/*
 			 * If SW decided we should be in a low power state and
 			 * the CC lines did not change, then don't talk with the
@@ -3865,6 +3866,7 @@ void pd_task(void *u)
 			if (pd[port].flags & PD_FLAGS_LPM_REQUESTED &&
 			    !(evt & PD_EVENT_CC))
 				break;
+#endif
 
 			/* Check for connection */
 			tcpm_get_cc(port, &cc1, &cc2);
