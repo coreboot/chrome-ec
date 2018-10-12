jackadam1981/ChromeEc

cr50: Add commands to get/set serial number bits.
Allocates 64 bits of INFO1 space, in the 'board' section, and
after the current Board ID data, to store the serial number
for use by zero-touch enrollment.

Adds a console command to read / set this data.

Adds a TPM vendor command to set this data.

BUG=b:111195266
TEST=todo
BRANCH=none

Change-Id: I752aefad9654742b7719156202f29d635d2306df
Signed-off-by: Louis Collard <louiscollard@chromium.org>

chip/g/build.mk
@@ -20,6 +20,7 @@ endif
 # Required chip modules
 chip-y = clock.o gpio.o hwtimer.o pre_init.o system.o
 chip-$(CONFIG_BOARD_ID_SUPPORT) += board_id.o
+chip-$(CONFIG_BOARD_ID_SUPPORT) += sn_bits.o
 ifeq ($(CONFIG_POLLING_UART),y)
 chip-y += polling_uart.o
 else

coreboot/chrome-ec

Allocates 16 bytes of INFO1 space, in the 'board' section, and
data for use by zero-touch enrollment.
Adds TPM vendor commands to set initial sn data, and update it
during RMA.
 Conflicts:
	include/config.h
   also had to copy include/common.h
CQ-DEPEND=CL:*657450
TEST=tested locally on soraka
Reviewed-on: https://chromium-review.googlesource.com/1127574
Reviewed-by: Andrey Pronin <apronin@chromium.org>
(cherry picked from commit 98045b7fa94ad11803af540677b2cde5a337be80)
Reviewed-on: https://chromium-review.googlesource.com/c/1278035
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
Tested-by: Vadim Bendebury <vbendeb@chromium.org>

chip/g/build.mk
@@ -22,6 +22,7 @@ endif
 # Required chip modules
 chip-y = clock.o gpio.o hwtimer.o pre_init.o system.o
 chip-$(CONFIG_BOARD_ID_SUPPORT) += board_id.o
+chip-$(CONFIG_SN_BITS_SUPPORT) += sn_bits.o
 ifeq ($(CONFIG_POLLING_UART),y)
 chip-y += polling_uart.o
 else