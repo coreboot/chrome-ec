jackadam1981/ChromeEc

gsctool: add open box RMA option
what happened

BRANCH=none
BUG=chromium:
BUG=chrome-os-partner:
TEST=manual

Change-Id: Ib43cf4eb642d141b7cd7f129ef412e14bd59f30b
Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

extra/usb_updater/gsctool.c
@@ -30,6 +30,7 @@
 #include "tpm_vendor_cmds.h"
 #include "upgrade_fw.h"
 #include "usb_descriptor.h"
+#include "verify_ro.h"
 
 #ifdef DEBUG
 #define debug printf
@@ -193,7 +194,7 @@ struct upgrade_pkt {
 
 static uint32_t protocol_version;
 static char *progname;
-static char *short_opts = "abcd:fhikoPprstUu";
+static char *short_opts = "abcd:fhikO:oPprstUu";
 static const struct option long_opts[] = {
 	/* name    hasarg *flag val */
 	{"any",		0,   NULL, 'a'},
@@ -206,6 +207,7 @@ static const struct option long_opts[] = {
 	{"device",	1,   NULL, 'd'},
 	{"fwver",	0,   NULL, 'f'},
 	{"help",	0,   NULL, 'h'},
+	{"openbox_rma", 1,   NULL, 'O'},
 	{"password",	0,   NULL, 'P'},
 	{"post_reset",	0,   NULL, 'p'},
 	{"rma_auth",	2,   NULL, 'r'},
@@ -511,10 +513,14 @@ static void usage(int errs)
 	       "                           ID could be 32 bit hex or 4 "
 	       "character string.\n"
 	       "  -k,--ccd_lock            Lock CCD\n"
+	       "  -O,--openbox_rma <des_file>\n"
+	       "                           Verify other device's RO integrity\n"
+	       "                           using information provided in "
+	       "<desc file>\n"
 	       "  -o,--ccd_open            Start CCD open sequence\n"
 	       "  -P,--password <password>\n"
 	       "                           Set or clear CCD password. Use\n"
-	       "                           'clear:<cur password>' to clear it.\n"
+	       "                           'clear:<cur password>' to clear it\n"
 	       "  -p,--post_reset          Request post reset after transfer\n"
 	       "  -r,--rma_auth [[auth_code|\"disable\"]\n"
 	       "                           Request RMA challenge, process "
@@ -1815,6 +1821,7 @@ int main(int argc, char *argv[])
 	int try_all_transfer = 0;
 	const char *exclusive_opt_error =
 		"Options -a, -s and -t are mutually exclusive\n";
+	const char *openbox_desc_file = NULL;
 
 	progname = strrchr(argv[0], '/');
 	if (progname)
@@ -1875,6 +1882,9 @@ int main(int argc, char *argv[])
 			break;
 		case 'k':
 			ccd_lock = 1;
+			break;
+		case 'O':
+			openbox_desc_file = optarg;
 			break;
 		case 'o':
 			ccd_open = 1;
@@ -1949,7 +1959,8 @@ int main(int argc, char *argv[])
 	    !corrupt_inactive_rw &&
 	    !password &&
 	    !rma &&
-	    !show_fw_ver) {
+	    !show_fw_ver &&
+	    !openbox_desc_file) {
 		if (optind >= argc) {
 			fprintf(stderr,
 				"\nERROR: Missing required <binary image>\n\n");
@@ -1975,8 +1986,9 @@ int main(int argc, char *argv[])
 	}
 
 	if (((bid_action != bid_none) + !!rma + !!password +
-	     !!ccd_open + !!ccd_unlock + !!ccd_lock) > 2) {
-		fprintf(stderr, "ERROR: options -i, -k, -o, -P, -r, and -u "
+	     !!ccd_open + !!ccd_unlock + !!ccd_lock +
+	     !!openbox_desc_file) > 2) {
+		fprintf(stderr, "ERROR: options -i, -k, -O, -o, -P, -r, and -u "
 			"are mutually exclusive\n");
 		exit(update_error);
 	}
@@ -1993,6 +2005,9 @@ int main(int argc, char *argv[])
 			td.ep_type = ts_xfer;
 		}
 	}
+
+	if (openbox_desc_file)
+		return verify_ro(&td, openbox_desc_file);
 
 	if (ccd_unlock || ccd_open || ccd_lock)
 		process_ccd_state(&td, ccd_unlock, ccd_open, ccd_lock);

coreboot/chrome-ec

This patch enhances the gsctool utility to allow to verify RO sections
of the target AP and EC flash memory.
The only command line parameter required for the new option ('O') is
the file name of the target descriptors database, containing memory
description sections for one or more Chrome OS devices.
Memory description sections are of two types (both types could be
referring AP or EC memory):
 - hash descriptor, this section includes the address range of the
   memory and one or more hash values for the contents of that address
   range. Multiple hashes are needed in case when the same device has
   mnore than one RO firmware releases in circulation.
 - dump descriptor, this is a request for this utility to display on
   the console the contents of the certain area of flash memory on the
   target.
When this utility starts the process, the target might request that
the operator confirms physical presence, in this case the utility
keeps prompting the operator to press the physical presence button
until DUT is satisfied,
BUG=b:73668125
TEST=created a descriptor database for a Robo device feeding it with
     values retrieved on the device by locally running spihash command
     on the device.
     Then ran this utility to verify successful hash and dump
     retrievals, comparing dump values with values obtained through
     Cr50 console directly.
     Created additional dummy hash variants and verified that the
     utility succeeds only if all matches happen at the same variant
     index in different hash sections.
Reviewed-on: https://chromium-review.googlesource.com/933545
Reviewed-by: Mary Ruthven <mruthven@chromium.org>
(cherry picked from commit d57e5eb3128774732e3b3fe40c0f939d9aaeff1a)
Reviewed-on: https://chromium-review.googlesource.com/1141530
Reviewed-by: Nick Sanders <nsanders@chromium.org>
Commit-Queue: Nick Sanders <nsanders@chromium.org>
Tested-by: Nick Sanders <nsanders@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/1221379
Rev

extra/usb_updater/gsctool.c
@@ -30,6 +30,7 @@
 #include "tpm_vendor_cmds.h"
 #include "upgrade_fw.h"
 #include "usb_descriptor.h"
+#include "verify_ro.h"
 
 #ifdef DEBUG
 #define debug printf
@@ -193,7 +194,7 @@ struct upgrade_pkt {
 
 static uint32_t protocol_version;
 static char *progname;
-static char *short_opts = "abcd:fhikoPprstUu";
+static char *short_opts = "abcd:fhikO:oPprstUu";
 static const struct option long_opts[] = {
 	/* name    hasarg *flag val */
 	{"any",		0,   NULL, 'a'},
@@ -206,6 +207,7 @@ static const struct option long_opts[] = {
 	{"device",	1,   NULL, 'd'},
 	{"fwver",	0,   NULL, 'f'},
 	{"help",	0,   NULL, 'h'},
+	{"openbox_rma", 1,   NULL, 'O'},
 	{"password",	0,   NULL, 'P'},
 	{"post_reset",	0,   NULL, 'p'},
 	{"rma_auth",	2,   NULL, 'r'},
@@ -511,10 +513,14 @@ static void usage(int errs)
 	       "                           ID could be 32 bit hex or 4 "
 	       "character string.\n"
 	       "  -k,--ccd_lock            Lock CCD\n"
+	       "  -O,--openbox_rma <desc_file>\n"
+	       "                           Verify other device's RO integrity\n"
+	       "                           using information provided in "
+	       "<desc file>\n"
 	       "  -o,--ccd_open            Start CCD open sequence\n"
 	       "  -P,--password <password>\n"
 	       "                           Set or clear CCD password. Use\n"
-	       "                           'clear:<cur password>' to clear it.\n"
+	       "                           'clear:<cur password>' to clear it\n"
 	       "  -p,--post_reset          Request post reset after transfer\n"
 	       "  -r,--rma_auth [[auth_code|\"disable\"]\n"
 	       "                           Request RMA challenge, process "
@@ -1821,6 +1827,7 @@ int main(int argc, char *argv[])
 	int try_all_transfer = 0;
 	const char *exclusive_opt_error =
 		"Options -a, -s and -t are mutually exclusive\n";
+	const char *openbox_desc_file = NULL;
 
 	progname = strrchr(argv[0], '/');
 	if (progname)
@@ -1881,6 +1888,9 @@ int main(int argc, char *argv[])
 			break;
 		case 'k':
 			ccd_lock = 1;
+			break;
+		case 'O':
+			openbox_desc_file = optarg;
 			break;
 		case 'o':
 			ccd_open = 1;
@@ -1955,7 +1965,8 @@ int main(int argc, char *argv[])
 	    !corrupt_inactive_rw &&
 	    !password &&
 	    !rma &&
-	    !show_fw_ver) {
+	    !show_fw_ver &&
+	    !openbox_desc_file) {
 		if (optind >= argc) {
 			fprintf(stderr,
 				"\nERROR: Missing required <binary image>\n\n");
@@ -1981,8 +1992,9 @@ int main(int argc, char *argv[])
 	}
 
 	if (((bid_action != bid_none) + !!rma + !!password +
-	     !!ccd_open + !!ccd_unlock + !!ccd_lock) > 2) {
-		fprintf(stderr, "ERROR: options -i, -k, -o, -P, -r, and -u "
+	     !!ccd_open + !!ccd_unlock + !!ccd_lock +
+	     !!openbox_desc_file) > 2) {
+		fprintf(stderr, "ERROR: options -i, -k, -O, -o, -P, -r, and -u "
 			"are mutually exclusive\n");
 		exit(update_error);
 	}
@@ -1999,6 +2011,9 @@ int main(int argc, char *argv[])
 			td.ep_type = ts_xfer;
 		}
 	}
+
+	if (openbox_desc_file)
+		return verify_ro(&td, openbox_desc_file);
 
 	if (ccd_unlock || ccd_open || ccd_lock)
 		process_ccd_state(&td, ccd_unlock, ccd_open, ccd_lock);