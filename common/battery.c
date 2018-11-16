91b82d6287986390ce009624e51c584f26e2b384
Daisuke Nojiri <dnojiri@chromium.org>
2018-11-16 18:34:50 +0000

    Revert "Battery: Use host full capacity to compute display percentage"
    This reverts commit 98da1db95df8ebd31e77c7f61e1f3123930bb7f1.
    
    Reason for revert: Replacing it with a different version of the same patch. Patchset6 of CL:1330101
    
    Original change's description:
    > Battery: Use host full capacity to compute display percentage
    > 
    > The full capacity known by the host may slightly differ from the full
    > capacity known by the EC because we notify the host of the new
    > capacity only if the difference is larger than 5 mAh.
    > 
    > This patch makes EC use the host full capacity instead of the local
    > full capacity to compute the display percentage.
    > 
    > Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    > 
    > BUG=b:109954565,b:80270446
    > BRANCH=none
    > TEST=Verify display percentages printed by EC and power_supply_info
    > move up synchronously on charge and the LED and the taskbar icon
    > turn to full at the same time.
    > 
    > Change-Id: Ie695a9937a22fc7a769b82448f4600d4491935b3
    > Reviewed-on: https://chromium-review.googlesource.com/c/1330103
    > Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    > Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    > Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
    
    Bug: b:109954565, b:80270446
    Change-Id: I84ac232d1bfc3195584f5580069b484402a9cbf8
    Reviewed-on: https://chromium-review.googlesource.com/c/1340541
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>

--- common/battery.c
+++ common/battery.c
@@ -573,7 +573,6 @@ void battery_compensate_params(struct batt_params *batt)
 	int numer, denom;
 	int remain = batt->remaining_capacity;
 	int full = batt->full_capacity;
-	int lfcc = *(int *)host_get_memmap(EC_MEMMAP_BATT_LFCC);
 
 	if ((batt->flags & BATT_FLAG_BAD_FULL_CAPACITY) ||
 			(batt->flags & BATT_FLAG_BAD_REMAINING_CAPACITY))
@@ -585,19 +584,15 @@ void battery_compensate_params(struct batt_params *batt)
 	if (batt_host_full_factor == 100) {
 		/* full_factor is effectively disabled in powerd. */
 		batt->full_capacity = full * batt_full_factor / 100;
-		if (lfcc == 0)
-			/* EC just reset. Assume host full is equal. */
-			lfcc = batt->full_capacity;
-		if (remain > lfcc) {
-			batt->remaining_capacity = lfcc;
+		full = batt->full_capacity;
+		if (remain > full) {
+			batt->remaining_capacity = full;
 			remain = batt->remaining_capacity;
 		}
 	} else if (remain * 100 > full * batt_full_factor) {
 		batt->remaining_capacity = full;
 		batt->display_charge = 1000;
 		return;
-	} else if (lfcc == 0) {
-		lfcc = full;
 	}
 
 	/*
@@ -605,8 +600,8 @@ void battery_compensate_params(struct batt_params *batt)
 	 *   charge = 100 * remain/full;
 	 *   100 * (charge - shutdown_pct) / (full_factor - shutdown_pct);
 	 */
-	numer = (100 * remain - lfcc * batt_host_shutdown_pct) * 1000;
-	denom = lfcc * (batt_host_full_factor - batt_host_shutdown_pct);
+	numer = (100 * remain - full * batt_host_shutdown_pct) * 1000;
+	denom = full * (batt_host_full_factor - batt_host_shutdown_pct);
 	/* Rounding (instead of truncating) */
 	batt->display_charge = (numer + denom / 2) / denom;
 }
