a2cb7080f23aa4e6442400227fd14818800502a7
Gwendal Grignou <gwendal@chromium.org>
2018-12-03 12:17:25 -0800

    ec_commands: Add missing definition for LIS2MDL
    BUG=b:110143516,b:115587004
    BRANCH=none
    TEST=Check ectool display the proper chip in ectool motionsense info 3.
    
    Change-Id: I236bd7f50d6edadbc5b2f238eb6882bd6fffc82d
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/1361990
    Tested-by: Karthikeyan Ramasubramanian <kramasub@chromium.org>
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Reviewed-by: Karthikeyan Ramasubramanian <kramasub@chromium.org>
    (cherry picked from commit e84e5b983370cc5349dbd18d3380b09ff8fa3f31)
    Reviewed-on: https://chromium-review.googlesource.com/c/1460282
    Reviewed-by: Justin TerAvest <teravest@chromium.org>
    Commit-Queue: Justin TerAvest <teravest@chromium.org>
    Tested-by: Justin TerAvest <teravest@chromium.org>
    Trybot-Ready: Justin TerAvest <teravest@chromium.org>

--- util/ectool.c
+++ util/ectool.c
@@ -4340,6 +4340,9 @@ static int cmd_motionsense(int argc, char **argv)
 		case MOTIONSENSE_CHIP_LIS2DE:
 			printf("lis2de\n");
 			break;
+		case MOTIONSENSE_CHIP_LIS2MDL:
+			printf("lis2mdl\n");
+			break;
 		default:
 			printf("unknown\n");
 		}
