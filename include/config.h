jackadam1981/ChromeEc

i2c: Add option for calling board-specific functions before and after every i2c transaction
This change adds a new config option CONFIG_I2C_XFER_CALLBACK that
makes i2c_xfer callback into board-provided functions before the start
and after the end of every I2C transaction. This can be used by boards
to implement any I2C device-specific quirks e.g. requiring minimum
bus-free time between every I2C transaction when the slave device
cannot actually do clock stretching.

BUG=b:73147310
BRANCH=None
TEST=make -j buildall

Change-Id: I452de4f22a81ffd97ca4944e1b940a3537637df9
Signed-off-by: Furquan Shaikh <furquan@google.com>

include/config.h
@@ -1729,6 +1729,14 @@
  */
 #undef CONFIG_I2C_XFER_LARGE_READ
 
+/*
+ * If defined, makes i2c_xfer callback into board-provided functions before the
+ * start and after the end of every I2C transaction. This can be used by boards
+ * to implement any I2C device specific quirks e.g. requiring minimum bus-free
+ * time between every I2C transaction with a device.
+ */
+#undef CONFIG_I2C_XFER_CALLBACK
+
 /* EC uses an I2C master interface */
 #undef CONFIG_I2C_MASTER
 

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/956934
Commit-Ready: Furquan Shaikh <furquan@chromium.org>
Tested-by: Furquan Shaikh <furquan@chromium.org>
Reviewed-by: Randall Spangler <rspangler@chromium.org>
(cherry picked from commit 699838c0c1b9222d2d01d1575be3519fa8e3b9ba)
Reviewed-on: https://chromium-review.googlesource.com/c/1277621
Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
Tested-by: Vadim Bendebury <vbendeb@chromium.org>
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
(cherry picked from commit e622c92b10dbcad85ba893f60175bdca204faf33)
Reviewed-on: https://chromium-review.googlesource.com/c/1366962

include/config.h
@@ -1550,6 +1550,14 @@
  */
 #undef CONFIG_I2C_XFER_LARGE_READ
 
+/*
+ * If defined, makes i2c_xfer callback into board-provided functions before the
+ * start and after the end of every I2C transaction. This can be used by boards
+ * to implement any I2C device specific quirks e.g. requiring minimum bus-free
+ * time between every I2C transaction with a device.
+ */
+#undef CONFIG_I2C_XFER_BOARD_CALLBACK
+
 /* EC uses an I2C master interface */
 #undef CONFIG_I2C_MASTER
 