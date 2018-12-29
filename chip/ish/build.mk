jackadam1981/ChromeEc

ISH: IPC: implement generic IPC layer
Introduce new IPC API supporting MNG and HECI protocols.
Currently it supports communication with host(x64)

Change-Id: Iea6d1f96c89228b425861d045618d58f9d146f08

chip/ish/build.mk
@@ -20,6 +20,7 @@ endif
 chip-y+=clock.o gpio.o system.o hwtimer.o uart.o flash.o
 chip-$(CONFIG_I2C)+=i2c.o
 chip-$(CONFIG_HOSTCMD_LPC)+=ipc.o
+chip-$(CONFIG_IPC_HECI)+=ipc_heci.o
 chip-$(CONFIG_WATCHDOG)+=watchdog.o
 
 # location of the scripts and keys used to pack the SPI flash image

coreboot/chrome-ec

BUG=b:79676054
BRANCH=none
TEST=Tested on Atlas board.
Reviewed-on: https://chromium-review.googlesource.com/1279363
Commit-Ready: Hyungwoo Yang <hyungwoo.yang@intel.com>
Tested-by: Hyungwoo Yang <hyungwoo.yang@intel.com>
Reviewed-by: Hyungwoo Yang <hyungwoo.yang@intel.com>
Reviewed-by: Jett Rink <jettrink@chromium.org>
(cherry picked from commit 737317a19e54bb4dfa4d646b11354b4a9d275791)
Reviewed-on: https://chromium-review.googlesource.com/c/1392410
Reviewed-by: Caveh Jalali <caveh@google.com>
Commit-Queue: Caveh Jalali <caveh@google.com>
Tested-by: Caveh Jalali <caveh@google.com>

chip/ish/build.mk
@@ -20,6 +20,7 @@ endif
 chip-y+=clock.o gpio.o system.o hwtimer.o uart.o flash.o
 chip-$(CONFIG_I2C)+=i2c.o
 chip-$(CONFIG_HOSTCMD_LPC)+=ipc.o
+chip-$(CONFIG_ISH_IPC)+=ipc_heci.o
 chip-$(CONFIG_WATCHDOG)+=watchdog.o
 
 # location of the scripts and keys used to pack the SPI flash image