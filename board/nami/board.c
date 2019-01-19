0adaedb30a28caf5d4a421bf82e3e3d6ba8dac2b
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2019-01-19 16:44:21 +0800

    Ekko/Bard: Setting input current limit to 95%
    AC adapter Input current limit exceeds maximum current =3.049A.
    We need to set lower input current limit to make sure safety confirm
    test pass.
    
    BUG=b:123063172
    BRANCH=firmware-nami-10775.B
    TEST=Safety measure AC Adapter Rating PASS
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    
    Change-Id: I4587cfac9ac9e18bcab9f3b42ed5964cb6e3fc8e
    Reviewed-on: https://chromium-review.googlesource.com/c/1424038
    Commit-Queue: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Tested-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

--- board/nami/board.c
+++ board/nami/board.c
@@ -681,7 +681,11 @@ void board_set_charge_limit(int port, int supplier, int charge_ma,
 	 * Limit the input current to 96% negotiated limit,
 	 * to account for the charger chip margin.
 	 */
-	charge_ma = charge_ma * 96 / 100;
+	if (oem == PROJECT_AKALI &&
+		(model == MODEL_EKKO || model == MODEL_BARD))
+		charge_ma = charge_ma * 95 / 100;
+	else
+		charge_ma = charge_ma * 96 / 100;
 	charge_set_input_current_limit(
 			MAX(charge_ma, CONFIG_CHARGER_INPUT_CURRENT),
 			charge_mv);
