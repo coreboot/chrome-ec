jackadam1981/ChromeEc

cr50: add mistral strap config
Mistral has a 1M PU on DIOA9 and a 5k PU on DIOA1. It needs to use
PLT_RST_L and SPI. Add this to the board config table.

BUG=b:124075760
BRANCH=cr50
TEST=Test on a mistral with the new straps. Make sure 0x41 is set in the
board properties.

Change-Id: Ie67610a39239fe2f86ead7e2643cb8bac16ef0d3
Signed-off-by: Mary Ruthven <mruthven@google.com>

board/cr50/board.c
@@ -248,6 +248,12 @@ static struct board_cfg board_cfg_table[] = {
 		.board_properties = BOARD_SLAVE_CONFIG_SPI |
 			BOARD_USE_PLT_RESET,
 	},
+	/* Mistral: DI0A9 = 1M PU, DIOA1 = 5k PU */
+	{
+		.strap_cfg = 0x0E,
+		.board_properties = BOARD_SLAVE_CONFIG_SPI |
+			BOARD_USE_PLT_RESET,
+	},
 	/* I2C Variants: DIOA9 = 1M PD, DIOA1 = 1M PD */
 	/* Reef/Eve: DIOA12 = 5k PD, DIOA6 = 1M PU */
 	{

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/c/1461081
Tested-by: Mary Ruthven <mruthven@chromium.org>
Reviewed-by: Namyoon Woo <namyoon@chromium.org>
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
Commit-Queue: Mary Ruthven <mruthven@chromium.org>
(cherry picked from commit 52569e08985bb8406343835bb7134490f5b86b5c)
Reviewed-on: https://chromium-review.googlesource.com/c/1464383
Tested-by: Vadim Bendebury <vbendeb@chromium.org>
Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
(cherry picked from commit f3db2a2db90afadca05d933dabfef74525a5d784)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1548192

board/cr50/board.c
@@ -248,6 +248,12 @@ static struct board_cfg board_cfg_table[] = {
 		.board_properties = BOARD_SLAVE_CONFIG_SPI |
 			BOARD_USE_PLT_RESET,
 	},
+	/* Mistral: DI0A9 = 1M PU, DIOA1 = 5k PU */
+	{
+		.strap_cfg = 0x0B,
+		.board_properties = BOARD_SLAVE_CONFIG_SPI |
+			BOARD_USE_PLT_RESET,
+	},
 	/* I2C Variants: DIOA9 = 1M PD, DIOA1 = 1M PD */
 	/* Reef/Eve: DIOA12 = 5k PD, DIOA6 = 1M PU */
 	{