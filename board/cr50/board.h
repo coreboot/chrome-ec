01d018775af70ff50908393f61927087d91255f0
Vadim Bendebury <vbendeb@chromium.org>
2019-05-20 17:07:10 -0700

    Revert "cr50: move to use flash event log"
    This reverts commit 3e1e19968b7064f3e5aad721f8bd928bff7163af.
    
    Need to rearrange patches to change order of releasing of Cr50 features.
    
    BUG=b:133187513
    BRANCH=none
    TEST=branch tested after all reverts are completed
    
    Change-Id: I4e4863768c0d31411b707503222e81e8554f41ec
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1625536

--- board/cr50/board.h
+++ board/cr50/board.h
@@ -65,7 +65,6 @@
 /* Size partition in NvMem */
 #define NVMEM_PARTITION_SIZE (CFG_TOP_SIZE - CONFIG_FLASH_NVCTR_SIZE)
 /* Size in bytes of NvMem area */
-#define CONFIG_FLASH_LOG
 #define CONFIG_FLASH_NVMEM_SIZE (NVMEM_PARTITION_SIZE * NVMEM_NUM_PARTITIONS)
 /* Enable <key, value> variable support. */
 #define CONFIG_FLASH_NVMEM_VARS
@@ -108,7 +107,6 @@
 #ifdef CR50_DEV
 /* Enable unsafe dev features for CCD in dev builds */
 #define CONFIG_CASE_CLOSED_DEBUG_V1_UNSAFE
-#define CONFIG_CMD_FLASH_LOG
 #define CONFIG_PHYSICAL_PRESENCE_DEBUG_UNSAFE
 #endif
 
@@ -149,6 +147,8 @@
 
 /* Implement custom udelay, due to usec hwtimer imprecision. */
 #define CONFIG_HW_SPECIFIC_UDELAY
+
+#define CONFIG_TPM_LOGGING
 
 #ifndef __ASSEMBLER__
 
