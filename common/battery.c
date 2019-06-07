27c7dbf75c673d53c31c4513e21cf89f9a8f265e
Evan Green <evgreen@chromium.org>
2019-06-07 13:22:46 -0700

    common/battery: Avoid negative display percentages
    Battery charges less than batt_host_shutdown_pct cause the display
    charge to go negative. This looks silly in EC console prints:
    
    [38.474266 Battery 2% (Display -2.-7 %) / 3h:15 to full]
    
    BUG=b:134586427
    BRANCH=None
    TEST=Run kohaku down to low battery, observe display stays at 0.
    
    Change-Id: If8eb477b8bb21451069bac5c1474b7a408b30582
    Signed-off-by: Evan Green <evgreen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1650136
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3336665
    Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
    Tested-by: Daisuke Nojiri <dnojiri@chromium.org>

--- common/battery.c
+++ common/battery.c
@@ -607,6 +607,8 @@ void battery_compensate_params(struct batt_params *batt)
 	denom = lfcc * (100 - batt_host_shutdown_pct);
 	/* Rounding (instead of truncating) */
 	batt->display_charge = (numer + denom / 2) / denom;
+	if (batt->display_charge < 0)
+		batt->display_charge = 0;
 }
 
 __overridable void board_battery_compensate_params(struct batt_params *batt)
