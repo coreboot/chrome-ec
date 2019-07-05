35b10354362ff7ef0e6927a6e0ad918911fe7e14
David Huang <david.huang@quanta.corp-partner.google.com>
2019-07-05 13:59:21 +0800

    Coral: Add new LGC battery
    Add new battery: LGC AP19A8K for Lava.
    
    BUG=b:136691308
    BRANCH=coral
    TEST=Make sure battery can cutoff by console "cutoff" or "ectool cutoff"
    and resume by plug in adapter.
    
    Change-Id: Ic962bb5723d569bd9c03a78a6451280d62de3c36
    Signed-off-by: David Huang <David.Huang@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1686924
    Tested-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Reviewed-by: Marco Chen <marcochen@chromium.org>
    Commit-Queue: Marco Chen <marcochen@chromium.org>

--- board/coral/battery.c
+++ board/coral/battery.c
@@ -52,6 +52,7 @@ enum battery_type {
 	BATTERY_SMP223_2S1P_COS,
 	BATTERY_SMP403_2S1P_PROTO,
 	BATTERY_SMP403_2S1P,
+	BATTERY_LGC_AP19A8K,
 	BATTERY_TYPE_COUNT,
 };
 
@@ -836,6 +837,34 @@ static const struct board_batt_params info[] = {
 			.charging_max_c		= 60,
 			.discharging_min_c	= 0,
 			.discharging_max_c	= 60,
+		},
+	},
+	[BATTERY_LGC_AP19A8K] = {
+		.fuel_gauge = {
+			.manuf_name = "LGC KTxxxxGxxx",
+			.device_name = "AP19A8K",
+			.ship_mode = {
+				.reg_addr = 0x3A,
+				.reg_data = { 0xC574, 0xC574 },
+			},
+			.fet = {
+				.mfgacc_support = 0,
+				.reg_addr = 0x43,
+				.reg_mask = 0x0001,
+				.disconnect_val = 0x0,
+			},
+		},
+		.batt_info = {
+			.voltage_max		= 13200,
+			.voltage_normal		= 11550,
+			.voltage_min		= 9000,
+			.precharge_current	= 256,
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 50,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 75,
 		},
 	},
 };
