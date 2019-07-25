499af45c1112d09bf7f7feab4af67fd4612f171a
Craig Hesling <hesling@chromium.org>
2019-07-25 14:58:23 -0700

    mock: Add mock structure for fuzzers and tests
    This introduces the common/mock and include/mock directories
    which hold mock implementations of other common libraries.
    
    The general idea it to create mocks which can be used in
    simple scenarios, by providing meaningful default return
    values and functionality, and more complicated scenarios
    like fuzzers, where the return values need to continuously change.
    
    The build system has been adapted to allow the inclusion of
    a new .mocklist file for tests and fuzzers. This file specifies
    exactly which mocks will be pulled into the build for a given
    test/fuzz. In order to maintain cleanliness, this file is optional.
    
    Examples:
    * http://crrev.com/c/1682945/17 makes use of three different mocks,
      one of which is the rollback mock.
    * An upcoming rollback unit test ( http://crrev.com/c/1686460 ),
      needs to pull in mocks that support the rollback interface, but
      explicitly cannot pull in the rollback mock.
    
    BRANCH=none
    BUG=b:116065496
    TEST=make buildall -j
    
    Change-Id: Ib87b1a93b6d73309afaf7115276ead49218598ff
    Signed-off-by: Craig Hesling <hesling@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1719569
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    (cherry picked from commit 09d4632e694ab69c1785fdad910025990a37f15d)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1859830
    Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
    Commit-Queue: Yicheng Li <yichengli@chromium.org>
    Tested-by: Yicheng Li <yichengli@chromium.org>

--- common/build.mk
+++ common/build.mk
@@ -255,3 +255,6 @@ $(out)/$(PROJECT).exe: $(out)/cryptoc/libcryptoc.a
 endif
 
 include $(_common_dir)fpsensor/build.mk
+
+include $(_common_dir)mock/build.mk
+common-y+=$(foreach m,$(mock-y),mock/$(m))
