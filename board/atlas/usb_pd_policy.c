06264af359b352d98f45f1c68d475ca2cd4ab0d3
Caveh Jalali <caveh@chromium.org>
2019-08-20 18:48:11 -0700

    atlas: Wake AP on DP AltMode Entry & Attention
    this is a cherry-pick of the nocturne specific patch
    https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1725249
    applied to atlas.
    
    BUG=chromium:786721
    BRANCH=firmware-atlas-11827.B
    TEST=verified atlas comes out of S0ix and uses external monitor on plugin
    
    Change-Id: Ia678c4438bbb7df271b91caf4786528f1934025b
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1763188
    Reviewed-by: Caveh Jalali <caveh@google.com>

--- board/atlas/usb_pd_policy.c
+++ board/atlas/usb_pd_policy.c
@@ -312,6 +312,13 @@ static int svdm_enter_dp_mode(int port, uint32_t mode_caps)
 	/* Only enter mode if device is DFP_D capable */
 	if (mode_caps & MODE_DP_SNK) {
 		svdm_safe_dp_mode(port);
+
+		if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND))
+			/*
+			 * Wake the system up since we're entering DP AltMode.
+			 */
+			pd_notify_dp_alt_mode_entry();
+
 		return 0;
 	}
 
@@ -381,6 +388,14 @@ static int svdm_dp_attention(int port, uint32_t *payload)
 	const struct usb_mux *mux = &usb_muxes[port];
 
 	dp_status[port] = payload[1];
+
+	if (chipset_in_state(CHIPSET_STATE_ANY_SUSPEND) &&
+	    (irq || lvl))
+		/*
+		 * Wake up the AP.  IRQ or level high indicates a DP sink is now
+		 * present.
+		 */
+		pd_notify_dp_alt_mode_entry();
 
 	/* Its initial DP status message prior to config */
 	if (!(dp_flags[port] & DP_FLAGS_DP_ON)) {
