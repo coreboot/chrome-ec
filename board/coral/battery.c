9392bd1394ed64ac507ea0fa99e990438d1a1b2e
David Huang <david.huang@quanta.corp-partner.google.com>
2019-08-22 09:56:27 +0800

    Coral: Add new LGC battery
    Add new battery: LGC KT0030G023 for Lava.
    
    BUG=b:136691308
    BRANCH=coral
    TEST=Make sure battery can cutoff by console "cutoff" or "ectool cutoff"
    and resume by plug in adapter.
    
    Change-Id: I860c2e05b02f0c2d1fffdf7675e8a283df177f20
    Signed-off-by: David Huang <David.Huang@quantatw.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1763890
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Commit-Queue: David Huang <david.huang@quanta.corp-partner.google.com>

--- board/coral/battery.c
+++ board/coral/battery.c
@@ -54,6 +54,7 @@ enum battery_type {
 	BATTERY_SMP403_2S1P,
 	BATTERY_SIMPLO_BYD,
 	BATTERY_LGC_AP19A8K,
+	BATTERY_LGC_G023,
 	BATTERY_TYPE_COUNT,
 };
 
@@ -873,6 +874,35 @@ static const struct board_batt_params info[] = {
 	[BATTERY_LGC_AP19A8K] = {
 		.fuel_gauge = {
 			.manuf_name = "LGC KTxxxxGxxx",
+			.device_name = "AP19A8K",
+			.ship_mode = {
+				.reg_addr = 0x3A,
+				.reg_data = { 0xC574, 0xC574 },
+			},
+			.fet = {
+				.mfgacc_support = 0,
+				.reg_addr = 0x43,
+				.reg_mask = 0x0001,
+				.disconnect_val = 0x0,
+			},
+		},
+		.batt_info = {
+			.voltage_max		= 13200,
+			.voltage_normal		= 11550,
+			.voltage_min		= 9000,
+			.precharge_current	= 256,
+			.start_charging_min_c	= 0,
+			.start_charging_max_c	= 50,
+			.charging_min_c		= 0,
+			.charging_max_c		= 60,
+			.discharging_min_c	= -20,
+			.discharging_max_c	= 75,
+		},
+	},
+
+	[BATTERY_LGC_G023] = {
+		.fuel_gauge = {
+			.manuf_name = "LGC KT0030G023",
 			.device_name = "AP19A8K",
 			.ship_mode = {
 				.reg_addr = 0x3A,
