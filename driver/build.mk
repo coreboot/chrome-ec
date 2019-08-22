9ef1b5a5434b1195ab87bda38d17438440136a3f
tim <tim2.lin@ite.corp-partner.google.com>
2019-08-22 18:20:55 +0800

    driver/ioexpander_it8801: add I/O expander driver for keyboard
    IT8801 is an I/O expander with a keyboard matrix controller.
    
    BUG=b:133200075
    BRANCH=none
    TEST=Press any key on keyboard and the console command
    "ksstate on" can print keyboard scan state.
    
    Change-Id: I6903aad1eacfdfb6a7c6c04e6954a14e8c8caaac
    Signed-off-by: tim <tim2.lin@ite.corp-partner.google.com>
    Signed-off-by: Alexandru M Stan <amstan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1677976
    (cherry picked from commit 7abc7ca4abbc2f4154e177389924c258ea37ff5d)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1969243
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Ting Shen <phoenixshen@chromium.org>
    Tested-by: Ting Shen <phoenixshen@chromium.org>

--- driver/build.mk
+++ driver/build.mk
@@ -73,6 +73,7 @@ driver-$(CONFIG_CHARGER_RT9467)+=charger/rt946x.o
 driver-$(CONFIG_CHARGER_SY21612)+=charger/sy21612.o
 
 # I/O expander
+driver-$(CONFIG_IO_EXPANDER_IT8801)+=ioexpander_it8801.o
 driver-$(CONFIG_IO_EXPANDER_PCA9534)+=ioexpander_pca9534.o
 driver-$(CONFIG_IO_EXPANDER_NCT38XX)+=ioexpander_nct38xx.o
 
