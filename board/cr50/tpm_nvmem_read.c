jackadam1981/ChromeEc

cr50: use new function to verify index presence
The new TPM API function works even before TPM Startup was executed,
this would allow CCD to read FWMP even if the AP does not boot up.

CQ-DEPEND=CL:1020725
BRANCH=cr50, cr50-mp
BUG=b:67009375
TEST=verified that FWMP can be read both before and after AP is
     booted, and that missing FWMP is also processed properly in both
     cases.

Change-Id: I4e57e6211fdb8fa3166f261aa861dba9bab433a1
Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

board/cr50/tpm_nvmem_read.c
@@ -19,12 +19,12 @@
 enum tpm_read_rv read_tpm_nvmem(uint16_t obj_index,
 				uint16_t obj_size, void *obj_value)
 {
-	TPMI_RH_NV_INDEX object_handle;
+	TPM_HANDLE       object_handle;
 	NV_INDEX         nvIndex;
 
 	object_handle = HR_NV_INDEX + obj_index;
-	if (NvIndexIsAccessible(object_handle,
-				TPM_CC_NV_Read) != TPM_RC_SUCCESS) {
+
+	if (!NvBackdoorFindHandle(object_handle)) {
 		CPRINTF("%s: object at 0x%x not found\n", __func__, obj_index);
 		return tpm_read_not_found;
 	}

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/1020785
Reviewed-by: Andrey Pronin <apronin@chromium.org>
(cherry picked from commit 9439d0e1218e8a910608ac8b6813103699ec06d5)
Reviewed-on: https://chromium-review.googlesource.com/1050520
(cherry picked from commit 95b103867ad7b8a6d730dd8ca25c765d83f24c72)
Reviewed-on: https://chromium-review.googlesource.com/1080318
(cherry picked from commit f92e9c2a62ded9496282cc30d5045d57e0c12eac)

board/cr50/tpm_nvmem_read.c
@@ -19,12 +19,12 @@
 enum tpm_read_rv read_tpm_nvmem(uint16_t obj_index,
 				uint16_t obj_size, void *obj_value)
 {
-	TPMI_RH_NV_INDEX object_handle;
+	TPM_HANDLE       object_handle;
 	NV_INDEX         nvIndex;
 
 	object_handle = HR_NV_INDEX + obj_index;
-	if (NvIndexIsAccessible(object_handle,
-				TPM_CC_NV_Read) != TPM_RC_SUCCESS) {
+
+	if (!NvEarlyStageFindHandle(object_handle)) {
 		CPRINTF("%s: object at 0x%x not found\n", __func__, obj_index);
 		return tpm_read_not_found;
 	}