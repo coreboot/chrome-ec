jackadam1981/ChromeEc

i2c: separate slave and master code
i2c.c includes i2c slave related code but are not compiled due to wrong
build.mk. Separate slave code to i2c_slave.c and fix build.mk
accordingly.

BUG=b:37584134
TEST=compile rose board with CONFIG_I2C and CONFIG_CMD_HOSTCMD. Then
     make sure EC_CMD_GET_PROTOCOL_INFO works.

Change-Id: I1f95ad277713c5e30913e0a010ca03dfd9ec248c
Signed-off-by: Wei-Ning Huang <wnhuang@google.com>

common/build.mk
@@ -57,6 +57,7 @@ common-$(CONFIG_GESTURE_SW_DETECTION)+=gesture.o
 common-$(CONFIG_HOSTCMD_EVENTS)+=host_event_commands.o
 common-$(CONFIG_HOSTCMD_PD)+=host_command_master.o
 common-$(CONFIG_I2C_MASTER)+=i2c.o
+common-$(CONFIG_I2C)+=i2c_slave.o
 common-$(CONFIG_I2C_VIRTUAL_BATTERY)+=virtual_battery.o
 common-$(CONFIG_INDUCTIVE_CHARGING)+=inductive_charging.o
 common-$(CONFIG_KEYBOARD_PROTOCOL_8042)+=keyboard_8042.o \

coreboot/chrome-ec

Split i2c.c code in two (i2c_slave.c and i2c_master.c). i2c_slave.c is
used when EC has no i2c device connected (i2c master code not needed)
but EC is connected to AP via i2c.
BRANCH=none
TEST=1. Compile rose board with CONFIG_I2C and CONFIG_CMD_HOSTCMD. Then
        make sure EC_CMD_GET_PROTOCOL_INFO works.
     2. On Kevin and Elm, cherry-pick to firmware branch respectively,
        build and flash ec, i2c function works.
Reviewed-on: https://chromium-review.googlesource.com/484999
Commit-Ready: Wei-Ning Huang <wnhuang@chromium.org>
Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
(cherry picked from commit 59bd55c41e02eab2ac01cd3adbdda1950f7fb09e)
Reviewed-on: https://chromium-review.googlesource.com/556191
Reviewed-by: Wei-Ning Huang <wnhuang@chromium.org>
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
Tested-by: Vadim Bendebury <vbendeb@chromium.org>
Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
(cherry picked from commit 062aaef6c0be848707febf1520f89f8aab9e6aeb)

common/build.mk
@@ -56,7 +56,8 @@ common-$(CONFIG_FMAP)+=fmap.o
 common-$(CONFIG_GESTURE_SW_DETECTION)+=gesture.o
 common-$(CONFIG_HOSTCMD_EVENTS)+=host_event_commands.o
 common-$(CONFIG_HOSTCMD_PD)+=host_command_master.o
-common-$(CONFIG_I2C_MASTER)+=i2c.o
+common-$(CONFIG_I2C_MASTER)+=i2c_master.o
+common-$(CONFIG_I2C_SLAVE)+=i2c_slave.o
 common-$(CONFIG_I2C_VIRTUAL_BATTERY)+=virtual_battery.o
 common-$(CONFIG_INDUCTIVE_CHARGING)+=inductive_charging.o
 common-$(CONFIG_KEYBOARD_PROTOCOL_8042)+=keyboard_8042.o \