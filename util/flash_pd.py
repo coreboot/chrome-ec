a72597d3bfaed8a602e9db138da23d5fdb7f5e56
Stefan Reinauer <stefan.reinauer@coreboot.org>
2019-09-22 10:39:36 -0700

    Unify #! use in python scripts
    Right now we have several different versions of #! in
    our python scripts. Unify them all and specify that we
    are using python2.
    
    Signed-off-by: Stefan Reinauer <reinauer@chromium.org>
    
    BUG=none
    BRANCH=none
    TEST=make buildall
    
    Change-Id: Iab33a3f5d4b827451a55542bcee8837b00da7867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1817948
    Commit-Queue: Stefan Reinauer <reinauer@chromium.org>
    Tested-by: Stefan Reinauer <reinauer@chromium.org>
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    (cherry picked from commit 68c9a2870ead8a3306a2601c4f5689656d49c6a8)
    
    Change-Id: I55272c289eaeb24bbaa7024ece7beb4cb14ea9ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2314108
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>

--- util/flash_pd.py
+++ util/flash_pd.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+#!/usr/bin/env python2
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Flash PD PSU RW firmware over the USBPD comm channel using console.
