829ef68bf64dc760a76c397b97e8d4a42d5f1d88
Evan Green <evgreen@chromium.org>
2019-09-23 13:07:06 -0700

    printf: Convert %l to %ll
    In order to make our printf more standard, utilize %ll for long long
    arguments, rather than %l. This does cost a little bit in flash space
    for that extra l in a couple of places, but enables us to turn on
    compile-time printf format checking.
    
    For this commit only, the semantics are such that both %l and %ll
    take 64-bit arguments. In the next commit, %l goes to its correct
    behavior of taking a sizeof(long) argument.
    
    BUG=chromium:984041
    TEST=make -j buildall
    BRANCH=none
    
    Change-Id: I18081b55a8dbf5ef8ec15fc499ca75e59d31da58
    Signed-off-by: Evan Green <evgreen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1819652
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    (cherry picked from commit 0c2d7b648e1ea433aad8123538632d4edff6eb85)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1920379
    Tested-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
    (cherry picked from commit 27106f3469e4019c3308bd2d1f5fa250ced1cb66)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1959294
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>

--- core/minute-ia/task.c
+++ core/minute-ia/task.c
@@ -5,6 +5,13 @@
 
 /* Task scheduling / events module for Chrome EC operating system */
 
+/*
+ * Needed before headers to support test builds since minute-ia defines 5
+ * parameters instead of the normal 4. This specifies a flag value of 0 for all
+ * tests tasks.
+ */
+#define TEST_TASK_EXTRA_ARGS 0
+
 #include "atomic.h"
 #include "common.h"
 #include "console.h"
@@ -15,7 +22,6 @@
 #include "util.h"
 #include "task_defs.h"
 #include "interrupts.h"
-#include "ipc.h"
 #include "hpet.h"
 
 /* Console output macros */
@@ -27,7 +33,7 @@
 #define STACK_UNUSED_VALUE 0xdeadd00d
 
 /* declare task routine prototypes */
-#define TASK(n, r, d, s) void r(void *);
+#define TASK(n, r, d, s, f) void r(void *);
 void __idle(void);
 CONFIG_TASK_LIST
 CONFIG_TEST_TASK_LIST
@@ -37,7 +43,7 @@ CONFIG_TEST_TASK_LIST
 extern volatile uint32_t __in_isr;
 
 /* Task names for easier debugging */
-#define TASK(n, r, d, s)  #n,
+#define TASK(n, r, d, s, f)  #n,
 static const char * const task_names[] = {
 	"<< idle >>",
 	CONFIG_TASK_LIST
@@ -47,8 +53,12 @@ static const char * const task_names[] = {
 
 #ifdef CONFIG_TASK_PROFILING
 static uint64_t task_start_time; /* Time task scheduling started */
-static uint64_t exc_start_time;  /* Time of task->exception transition */
-static uint64_t exc_end_time;    /* Time of exception->task transition */
+/*
+ * We only keep 32-bit values for exception start/end time, to avoid
+ * accounting errors when we service interrupt when the timer wraps around.
+ */
+static uint32_t exc_start_time;  /* Time of task->exception transition */
+static uint32_t exc_end_time;    /* Time of exception->task transition */
 static uint64_t exc_total_time;  /* Total time in exceptions */
 static uint32_t svc_calls;	 /* Number of service calls */
 static uint32_t task_switches;	/* Number of times active task changed */
@@ -80,24 +90,26 @@ static void task_exit_trap(void)
 {
 	int i = task_get_current();
 
-	cprints(CC_TASK, "Task %d (%s) exited!", i, task_names[i]);
+	cprints(CC_TASK, "Task %d (%s) exited!", i, task_get_name(i));
 	/* Exited tasks simply sleep forever */
 	while (1)
 		task_wait_event(-1);
 }
 
 /* Startup parameters for all tasks. */
-#define TASK(n, r, d, s)  {	\
+#define TASK(n, r, d, s, f)  {	\
 	.r0 = (uint32_t)d,	\
 	.pc = (uint32_t)r,	\
 	.stack_size = s,	\
+	.flags = f,		\
 },
 static const struct {
 	uint32_t r0;
 	uint32_t pc;
 	uint16_t stack_size;
-} const tasks_init[] = {
-	TASK(IDLE, __idle, 0, IDLE_TASK_STACK_SIZE)
+	uint32_t flags;
+} tasks_init[] = {
+	TASK(IDLE, __idle, 0, IDLE_TASK_STACK_SIZE, 0)
 	CONFIG_TASK_LIST
 	CONFIG_TEST_TASK_LIST
 };
@@ -112,9 +124,9 @@ BUILD_ASSERT(TASK_ID_COUNT < (1 << (sizeof(task_id_t) * 8)));
 
 
 /* Stacks for all tasks */
-#define TASK(n, r, d, s)  + s
+#define TASK(n, r, d, s, f)  + s
 uint8_t task_stacks[0
-		    TASK(IDLE, __idle, 0, IDLE_TASK_STACK_SIZE)
+		    TASK(IDLE, __idle, 0, IDLE_TASK_STACK_SIZE, 0)
 		    CONFIG_TASK_LIST
 		    CONFIG_TEST_TASK_LIST
 ] __aligned(8);
@@ -123,19 +135,6 @@ uint8_t task_stacks[0
 
 
 task_ *current_task, *next_task;
-/*
- * Should IRQs chain to switch_handler()?  This should be set if either of the
- * following is true:
- *
- * 1) Task scheduling has started, and task profiling is enabled.  Task
- * profiling does its tracking in switch_handler().
- *
- * 2) An event was set by an interrupt; this could result in a higher-priority
- * task unblocking.  After checking for a task switch, switch_handler() will
- * clear  the flag (unless profiling is also enabled; then the flag remains
- * set).
- */
-static int need_resched_or_profiling;
 
 /*
  * Bitmap of all tasks ready to be run.
@@ -144,13 +143,13 @@ static int need_resched_or_profiling;
  * can do their init within a task switching context.  The hooks task will then
  * make a call to enable all tasks.
  */
-static uint32_t tasks_ready = (1 << TASK_ID_HOOKS);
+static uint32_t tasks_ready = BIT(TASK_ID_HOOKS);
 /*
  * Initially allow only the HOOKS and IDLE task to run, regardless of ready
  * status, in order for HOOK_INIT to complete before other tasks.
  * task_enable_all_tasks() will open the flood gates.
  */
-static uint32_t tasks_enabled = (1 << TASK_ID_HOOKS) | (1 << TASK_ID_IDLE);
+static uint32_t tasks_enabled = BIT(TASK_ID_HOOKS) | BIT(TASK_ID_IDLE);
 
 static int start_called;  /* Has task swapping started */
 
@@ -166,6 +165,11 @@ void interrupt_disable(void)
 
 void interrupt_enable(void)
 {
+	/*
+	 * allow enbling interrupt only after task switch is ready
+	 */
+	ASSERT(task_start_called() != 1);
+
 	__asm__ __volatile__ ("sti");
 }
 
@@ -174,18 +178,21 @@ inline int in_interrupt_context(void)
 	return !!__in_isr;
 }
 
-inline int get_interrupt_context(void)
-{
-	return 0;
-}
-
 task_id_t task_get_current(void)
 {
-#ifdef CONFIG_DEBUG_BRINGUP
 	/* If we haven't done a context switch then our task ID isn't valid */
-	ASSERT(task_start_called() != 1);
-#endif
+	if (IS_ENABLED(CONFIG_DEBUG_BRINGUP))
+		ASSERT(task_start_called() != 1);
+
 	return current_task - tasks;
+}
+
+const char *task_get_name(task_id_t tskid)
+{
+	if (tskid < ARRAY_SIZE(task_names))
+		return task_names[tskid];
+
+	return "<< unknown >>";
 }
 
 uint32_t *task_get_event_bitmap(task_id_t tskid)
@@ -206,34 +213,18 @@ int task_start_called(void)
 uint32_t switch_handler(int desched, task_id_t resched)
 {
 	task_ *current, *next;
-#ifdef CONFIG_TASK_PROFILING
-	int exc = get_interrupt_context();
-	uint64_t t;
-#endif
-
-
-#ifdef CONFIG_TASK_PROFILING
-	/*
-	 * SVCall isn't triggered via DECLARE_IRQ(), so it needs to track its
-	 * start time explicitly.
-	 */
-	if (exc == 0xb) {
-		exc_start_time = get_time().val;
-		svc_calls++;
-	}
-#endif
 
 	current = current_task;
 
-#ifdef CONFIG_DEBUG_STACK_OVERFLOW
-	if (*current->stack != STACK_UNUSED_VALUE) {
+	if (IS_ENABLED(CONFIG_DEBUG_STACK_OVERFLOW) &&
+	    *current->stack != STACK_UNUSED_VALUE) {
 		panic_printf("\n\nStack overflow in %s task!\n",
-				task_names[current - tasks]);
-#ifdef CONFIG_SOFTWARE_PANIC
-		software_panic(PANIC_SW_STACK_OVERFLOW, current - tasks);
-#endif
+			     task_get_name(current - tasks));
+
+		if (IS_ENABLED(CONFIG_SOFTWARE_PANIC))
+			software_panic(PANIC_SW_STACK_OVERFLOW,
+				       current - tasks);
 	}
-#endif
 
 	if (desched && !current->events) {
 		/*
@@ -247,37 +238,26 @@ uint32_t switch_handler(int desched, task_id_t resched)
 	ASSERT(tasks_ready & tasks_enabled);
 	next = __task_id_to_ptr(__fls(tasks_ready & tasks_enabled));
 
-#ifdef CONFIG_TASK_PROFILING
-	/* Track time in interrupts */
-	t = get_time().val;
-	exc_total_time += (t - exc_start_time);
+	/* Only the first ISR on the (nested IRQ) stack calculates time */
+	if (IS_ENABLED(CONFIG_TASK_PROFILING) &&
+	    __in_isr == 1) {
+		/* Track time in interrupts */
+		uint32_t t = get_time().le.lo;
 
-	/*
-	 * Bill the current task for time between the end of the last interrupt
-	 * and the start of this one.
-	 */
-	current->runtime += (exc_start_time - exc_end_time);
-	exc_end_time = t;
-#else
-	/*
-	 * Don't chain here from interrupts until the next time an interrupt
-	 * sets an event.
-	 */
-	need_resched_or_profiling = 0;
-#endif
+		exc_end_time = t;
+		exc_total_time += (t - exc_start_time);
+	}
 
 	/* Nothing to do */
 	if (next == current)
 		return 0;
 
-#ifdef ISH_DEBUG
-	CPRINTF("[%d -> %d]\n", current - tasks, next - tasks);
-#endif
+	if (IS_ENABLED(ISH_DEBUG))
+		CPRINTF("[%d -> %d]\n", current - tasks, next - tasks);
 
 	/* Switch to new task */
-#ifdef CONFIG_TASK_PROFILING
-	task_switches++;
-#endif
+	if (IS_ENABLED(CONFIG_TASK_PROFILING))
+		task_switches++;
 	next_task = next;
 
 	/* TS required */
@@ -286,53 +266,44 @@ uint32_t switch_handler(int desched, task_id_t resched)
 
 void __schedule(int desched, int resched)
 {
-	__asm__ __volatile__ ("int %0"
-			:
-			: "i" (ISH_TS_VECTOR),
-			  "d" (desched), "c" (resched)
-			);
+	__asm__ __volatile__("int %0"
+			     :
+			     : "i"(ISH_TS_VECTOR), "d"(desched), "c"(resched));
 }
 
 #ifdef CONFIG_TASK_PROFILING
-void __keep task_start_irq_handler(void *excep_return)
+void __keep task_start_irq_handler(void *data)
 {
 	/*
 	 * Get time before checking depth, in case this handler is
 	 * pre-empted.
 	 */
-	uint64_t t = get_time().val;
-	int irq = get_interrupt_context() - 16;
+	uint32_t t = get_time().le.lo;
+	int irq = (uint32_t)data;
 
 	/*
 	 * Track IRQ distribution.  No need for atomic add, because an IRQ
-	 * can't pre-empt itself.
+	 * can't pre-empt itself. If less than 0, then the vector did not map
+	 * to an IRQ but was for a synchronous exception instead (TS_VECTOR)
 	 */
-	if (irq < ARRAY_SIZE(irq_dist))
+	if (irq < CONFIG_IRQ_COUNT)
 		irq_dist[irq]++;
+	else
+		/* Track total number of service calls */
+		atomic_add(&svc_calls, 1);
 
-	/*
-	 * Continue iff a rescheduling event happened or profiling is active,
-	 * and we are not called from another exception (this must match the
-	 * logic for when we chain to svc_handler() below).
-	 */
-	if (!need_resched_or_profiling || (((uint32_t)excep_return & 0xf) == 1))
-		return;
+	/* Only the outer ISR should keep track of the ISR start time */
+	if (__in_isr == 1) {
+		exc_start_time = t;
 
-	exc_start_time = t;
+		/*
+		 * Bill the current task for time between the end of the last
+		 * interrupt and the start of this interrupt (now).
+		 */
+		current_task->runtime += (t - exc_end_time);
+	}
 }
 #endif
-
-void __keep task_resched_if_needed(void *excep_return)
-{
-	/*
-	 * Continue iff a rescheduling event happened or profiling is active,
-	 * and we are not called from another exception.
-	 */
-	if (!need_resched_or_profiling || (((uint32_t)excep_return & 0xf) == 1))
-		return;
-
-	switch_handler(0, 0);
-}
 
 static uint32_t __wait_evt(int timeout_us, task_id_t resched)
 {
@@ -383,10 +354,6 @@ uint32_t task_set_event(task_id_t tskid, uint32_t event, int wait)
 	if (in_interrupt_context()) {
 		/* The receiver might run again */
 		atomic_or(&tasks_ready, 1 << tskid);
-#ifndef CONFIG_TASK_PROFILING
-		if (start_called)
-			need_resched_or_profiling = 1;
-#endif
 	} else {
 		if (wait)
 			return __wait_evt(-1, tskid);
@@ -433,22 +400,23 @@ uint32_t task_wait_event_mask(uint32_t event_mask, int timeout_us)
 void task_enable_all_tasks(void)
 {
 	/* Mark all tasks as ready and table to run. */
-	tasks_ready = tasks_enabled = (1 << TASK_ID_COUNT) - 1;
+	tasks_ready = tasks_enabled = BIT(TASK_ID_COUNT) - 1;
 
-	/* BUG: task_start() was likely already called */
-	start_called = 1;
-
-	/* The host OS driver should wait till the FW completes all hook inits.
-	 * Otherwise, FW may fail to respond to host commands or crash when
-	 * not fully initialized. This MNG (management) type IPC message sent
-	 * asynchronously from the FW indicates completion of initialization.
-	 */
-	CPUTS("*** MNG FW ready ****\n");
-	REG32(IPC_ISH2HOST_DOORBELL) = IPC_BUILD_MNG_MSG(0x8, 1);
-
-	interrupt_enable();
 	/* Reschedule the highest priority task. */
 	__schedule(0, 0);
+}
+
+void task_enable_task(task_id_t tskid)
+{
+	atomic_or(&tasks_enabled, BIT(tskid));
+}
+
+void task_disable_task(task_id_t tskid)
+{
+	atomic_clear(&tasks_enabled, BIT(tskid));
+
+	if (!in_interrupt_context() && tskid == task_get_current())
+		__schedule(0, 0);
 }
 
 void task_enable_irq(int irq)
@@ -467,11 +435,20 @@ void task_clear_pending_irq(int irq)
 
 void task_trigger_irq(int irq)
 {
-	/* Writing to Local APIC Interrupt Command Register (ICR) causes an
-	 * IPI (Inter-processor interrupt) on the APIC bus. Here we direct the
-	 * IPI to originating prccessor to generate self-interrupt
+	/* ISR should not be called before the first task is scheduled */
+	if (!task_start_called())
+		return;
+
+	/* we don't allow nested interrupt */
+	if (in_interrupt_context())
+		return;
+
+	/*
+	 * "int" instruction accepts vector only as immediate value.
+	 * so here, we use one vector(SOFTIRQ_VECTOR) and pass
+	 * the address of ISR of irq in ecx register.
 	 */
-	REG32(LAPIC_ICR_REG) = LAPIC_ICR_BITS | IRQ_TO_VEC(irq);
+	__asm__ __volatile__("int %0\n" : : "i"(SOFTIRQ_VECTOR), "c"(irq));
 }
 
 void mutex_lock(struct mutex *mtx)
@@ -483,8 +460,9 @@ void mutex_lock(struct mutex *mtx)
 	atomic_or(&mtx->waiters, id);
 
 	do {
+		old_val = 0;
 		__asm__ __volatile__(
-				"lock; cmpxchg %1, %2\n"
+				ASM_LOCK_PREFIX "cmpxchg %1, %2\n"
 				: "=a" (old_val)
 				: "r" (value), "m" (mtx->lock), "a" (old_val)
 				: "memory");
@@ -505,7 +483,7 @@ void mutex_unlock(struct mutex *mtx)
 	task_ *tsk = current_task;
 
 	__asm__ __volatile__(
-			"lock; cmpxchg %1, %2\n"
+			ASM_LOCK_PREFIX "cmpxchg %1, %2\n"
 			: "=a" (old_val)
 			: "r" (val), "m" (mtx->lock), "a" (old_val)
 			: "memory");
@@ -515,7 +493,7 @@ void mutex_unlock(struct mutex *mtx)
 	while (waiters) {
 		task_id_t id = __fls(waiters);
 
-		waiters &= ~(1 << id);
+		waiters &= ~BIT(id);
 
 		/* Somebody is waiting on the mutex */
 		task_set_event(id, TASK_EVENT_MUTEX, 0);
@@ -529,7 +507,12 @@ void task_print_list(void)
 {
 	int i;
 
-	ccputs("Task Ready Name         Events      Time (s)  StkUsed\n");
+	if (IS_ENABLED(CONFIG_FPU))
+		ccputs("Task Ready Name         Events      Time (s)  "
+		       "  StkUsed UseFPU\n");
+	else
+		ccputs("Task Ready Name         Events      Time (s)  "
+		       "StkUsed\n");
 
 	for (i = 0; i < TASK_ID_COUNT; i++) {
 		char is_ready = (tasks_ready & (1<<i)) ? 'R' : ' ';
@@ -542,39 +525,50 @@ void task_print_list(void)
 		     sp++)
 			stackused -= sizeof(uint32_t);
 
-		ccprintf("%4d %c %-16s %08x %11.6ld  %3d/%3d\n", i, is_ready,
-			 task_names[i], tasks[i].events, tasks[i].runtime,
-			 stackused, tasks_init[i].stack_size);
+		if (IS_ENABLED(CONFIG_FPU)) {
+			char use_fpu = tasks[i].use_fpu ? 'Y' : 'N';
+
+			ccprintf("%4d %c %-16s %08x %11.6lld  %3d/%3d %c\n",
+				 i, is_ready, task_get_name(i), tasks[i].events,
+				 tasks[i].runtime, stackused,
+				 tasks_init[i].stack_size, use_fpu);
+		} else {
+			ccprintf("%4d %c %-16s %08x %11.6lld  %3d/%3d\n",
+				 i, is_ready, task_get_name(i), tasks[i].events,
+				 tasks[i].runtime, stackused,
+				 tasks_init[i].stack_size);
+		}
+
 		cflush();
 	}
 }
 
 int command_task_info(int argc, char **argv)
 {
-#ifdef CONFIG_TASK_PROFILING
-	int total = 0;
-	int i;
-#endif
-
 	task_print_list();
 
-#ifdef CONFIG_TASK_PROFILING
-	ccputs("IRQ counts by type:\n");
-	cflush();
-	for (i = 0; i < ARRAY_SIZE(irq_dist); i++) {
-		if (irq_dist[i]) {
-			ccprintf("%4d %8d\n", i, irq_dist[i]);
-			total += irq_dist[i];
+	if (IS_ENABLED(CONFIG_TASK_PROFILING)) {
+		int total = 0;
+		int i;
+
+		ccputs("IRQ counts by type:\n");
+		cflush();
+		for (i = 0; i < ARRAY_SIZE(irq_dist); i++) {
+			if (irq_dist[i]) {
+				ccprintf("%4d %8d\n", i, irq_dist[i]);
+				total += irq_dist[i];
+			}
 		}
+		ccprintf("Service calls:          %11d\n", svc_calls);
+		ccprintf("Total exceptions:       %11d\n", total + svc_calls);
+		ccprintf("Task switches:          %11d\n", task_switches);
+		ccprintf("Task switching started: %11.6lld s\n",
+			 task_start_time);
+		ccprintf("Time in tasks:          %11.6lld s\n",
+			 get_time().val - task_start_time);
+		ccprintf("Time in exceptions:     %11.6lld s\n",
+			 exc_total_time);
 	}
-	ccprintf("Service calls:          %11d\n", svc_calls);
-	ccprintf("Total exceptions:       %11d\n", total + svc_calls);
-	ccprintf("Task switches:          %11d\n", task_switches);
-	ccprintf("Task switching started: %11.6ld s\n", task_start_time);
-	ccprintf("Time in tasks:          %11.6ld s\n",
-		 get_time().val - task_start_time);
-	ccprintf("Time in exceptions:     %11.6ld s\n", exc_total_time);
-#endif
 
 	return EC_SUCCESS;
 }
@@ -582,7 +576,7 @@ DECLARE_CONSOLE_COMMAND(taskinfo, command_task_info,
 			NULL,
 			"Print task info");
 
-#ifdef CONFIG_CMD_TASKREADY
+__maybe_unused
 static int command_task_ready(int argc, char **argv)
 {
 	if (argc < 2) {
@@ -595,6 +589,8 @@ static int command_task_ready(int argc, char **argv)
 
 	return EC_SUCCESS;
 }
+
+#ifdef CONFIG_CMD_TASKREADY
 DECLARE_CONSOLE_COMMAND(taskready, command_task_ready,
 			"[setmask]",
 			"Print/set ready tasks");
@@ -604,10 +600,6 @@ void task_pre_init(void)
 {
 	int i, cs;
 	uint32_t *stack_next = (uint32_t *)task_stacks;
-
-	uint8_t default_fp_ctx[] = { /*Initial FP state */
-		0x7f, 0x03, 0xff, 0xff, 0x00, 0x00,
-		0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 
 	__asm__ __volatile__ ("movl %%cs, %0":"=r" (cs));
 
@@ -646,16 +638,29 @@ void task_pre_init(void)
 		sp[10] = INITIAL_EFLAGS;
 
 		sp[11] = (uint32_t) task_exit_trap;
-		sp[12] = 0x00;
+		sp[12] = tasks_init[i].r0;	/* task argument */
 		sp[13] = 0x00;
 		sp[14] = 0x00;
 		sp[15] = 0x00;
 
-#ifdef CONFIG_FPU
-		/* Copy default x86 FPU state for each task */
-		memcpy(tasks[i].fp_ctx, default_fp_ctx,
-			sizeof(default_fp_ctx));
-#endif
+		if (IS_ENABLED(CONFIG_FPU)) {
+			static uint8_t default_fp_ctx[] = {
+				/* Initial FP state */
+				0x7f, 0x00, /* Control[0-15] */
+				0xff, 0xff, /* unused */
+				0x00, 0x00, /* Status[0-15] */
+				0xff, 0xff, /* unused */
+				0xff, 0xff, /* Tag[0-15] */
+				0xff, 0xff};/* unused */
+
+			/* Copy default x86 FPU state for each task */
+			memcpy(tasks[i].fp_ctx, default_fp_ctx,
+			       sizeof(default_fp_ctx));
+
+			if (tasks_init[i].flags & MIA_TASK_FLAG_USE_FPU)
+				tasks[i].use_fpu = 1;
+		}
+
 		/* Fill unused stack; also used to detect stack overflow. */
 		for (sp = stack_next; sp < (uint32_t *)tasks[i].sp; sp++)
 			*sp = STACK_UNUSED_VALUE;
@@ -667,7 +672,6 @@ void task_pre_init(void)
 
 	/* Initialize IRQs */
 	init_interrupts();
-
 }
 
 void task_clear_fp_used(void)
@@ -676,8 +680,8 @@ void task_clear_fp_used(void)
 
 int task_start(void)
 {
-#ifdef CONFIG_TASK_PROFILING
-	task_start_time = exc_end_time = get_time().val;
-#endif
-	return __task_start(&need_resched_or_profiling);
+	if (IS_ENABLED(CONFIG_TASK_PROFILING))
+		task_start_time = exc_end_time = get_time().val;
+
+	return __task_start(&start_called);
 }
