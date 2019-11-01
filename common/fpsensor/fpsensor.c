jackadam1981/ChromeEc

fpsensor: Refactor fp_command_mode
This refactoring allows us to call fp_command_mode from the debug
console commands and ensure that we're testing the same underlying code
path that the host commands use.

BRANCH=none
BUG=b:124773209
TEST="fpenroll" in hatch FP console
     "fpmatch" in hatch FP console
     "fpclear" in hatch FP console
TEST=On nocturne:
     flash_fp_mcu ec.bin
     Enroll fingerprint via UI, lock/unlock, Remove fingerprint via UI

Change-Id: I5e1e314c7f1d67dc663795cafe751545516e9f89
Signed-off-by: Tom Hughes <tomhughes@chromium.org>

common/fpsensor/fpsensor.c
@@ -724,12 +724,23 @@ static void upload_pgm_image(uint8_t *frame)
 	CPRINTF("\x04"); /* End Of Transmission */
 }
 
-static int fp_console_action(uint32_t mode)
+static enum ec_error_list fp_console_action(uint32_t mode)
 {
 	int tries = 200;
-	ccprintf("Waiting for finger ...\n");
-	sensor_mode = mode;
-	task_set_event(TASK_ID_FPSENSOR, TASK_EVENT_UPDATE_CONFIG, 0);
+	uint32_t mode_output = 0;
+	int rc = 0;
+
+	CPRINTS("Waiting for finger ...");
+
+	rc = fp_command_mode(mode, &mode_output);
+
+	if (rc != EC_RES_SUCCESS) {
+		/*
+		 * EC host command errors do not directly map to console command
+		 * errors.
+		 */
+		return EC_ERROR_UNKNOWN;
+	}
 
 	while (tries--) {
 		if (!(sensor_mode & FP_MODE_ANY_CAPTURE)) {
@@ -770,7 +781,7 @@ DECLARE_CONSOLE_COMMAND(fpcapture, command_fpcapture, "", "");
 
 int command_fpenroll(int argc, char **argv)
 {
-	int rc;
+	enum ec_error_list rc;
 	int percent = 0;
 	uint32_t event;
 	static const char * const enroll_str[] = {"OK", "Low Quality",

coreboot/chrome-ec

BRANCH=nocturne
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1652285
Reviewed-by: Nicolas Norvez <norvez@chromium.org>
(cherry picked from commit 4f7f1b7fc6eec40094000e77e87f22f512f8321b)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1863532
Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
Commit-Queue: Yicheng Li <yichengli@chromium.org>
Tested-by: Yicheng Li <yichengli@chromium.org>

common/fpsensor/fpsensor.c
@@ -579,12 +579,23 @@ static void upload_pgm_image(uint8_t *frame)
 	ccprintf("\x04"); /* End Of Transmission */
 }
 
-static int fp_console_action(uint32_t mode)
+static enum ec_error_list fp_console_action(uint32_t mode)
 {
 	int tries = 200;
-	ccprintf("Waiting for finger ...\n");
-	sensor_mode = mode;
-	task_set_event(TASK_ID_FPSENSOR, TASK_EVENT_UPDATE_CONFIG, 0);
+	uint32_t mode_output = 0;
+	int rc = 0;
+
+	CPRINTS("Waiting for finger ...");
+
+	rc = fp_set_sensor_mode(mode, &mode_output);
+
+	if (rc != EC_RES_SUCCESS) {
+		/*
+		 * EC host command errors do not directly map to console command
+		 * errors.
+		 */
+		return EC_ERROR_UNKNOWN;
+	}
 
 	while (tries--) {
 		if (!(sensor_mode & FP_MODE_ANY_CAPTURE)) {
@@ -600,7 +611,7 @@ int command_fpcapture(int argc, char **argv)
 {
 	int capture_type = FP_CAPTURE_SIMPLE_IMAGE;
 	uint32_t mode;
-	int rc;
+	enum ec_error_list rc;
 
 	if (system_is_locked())
 		return EC_RES_ACCESS_DENIED;
@@ -625,7 +636,7 @@ DECLARE_CONSOLE_COMMAND(fpcapture, command_fpcapture, "", "");
 
 int command_fpenroll(int argc, char **argv)
 {
-	int rc;
+	enum ec_error_list rc;
 	int percent = 0;
 	uint32_t event;
 	static const char * const enroll_str[] = {"OK", "Low Quality",
@@ -661,7 +672,7 @@ DECLARE_CONSOLE_COMMAND(fpenroll, command_fpenroll, "", "");
 
 int command_fpmatch(int argc, char **argv)
 {
-	int rc = fp_console_action(FP_MODE_MATCH);
+	enum ec_error_list rc = fp_console_action(FP_MODE_MATCH);
 	uint32_t event = atomic_read_clear(&fp_events);
 
 	if (rc == EC_SUCCESS && event & EC_MKBP_FP_MATCH) {