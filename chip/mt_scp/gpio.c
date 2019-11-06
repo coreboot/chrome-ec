jackadam1981/ChromeEc

Cleanup: Correct GPIO alternate function parameter
Added code to correct the GPIO alternate function parameter at Chipset
level. Optionally board level functions can cleanup the code in additional
change lists.

BUG=b:139427854
BRANCH=none
TEST=make buildall -j

Change-Id: I1171ca36a703291070fc89f972f84414adcf04fc
Signed-off-by: Vijay Hiremath <vijay.p.hiremath@intel.com>

chip/mt_scp/gpio.c
@@ -12,13 +12,14 @@
 #include "task.h"
 #include "util.h"
 
-void gpio_set_alternate_function(uint32_t port, uint32_t mask, int func)
+void gpio_set_alternate_function(uint32_t port, uint32_t mask,
+				enum gpio_alternate_func func)
 {
 	int bit, mode_reg_index, shift;
 	uint32_t mode_bits, mode_mask;
 
 	/* Up to 8 alt functions per port */
-	if (func > 7)
+	if (func > GPIO_ALT_FUNC_6)
 		return;
 
 	while (mask) {

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1880974
Reviewed-by: Keith Short <keithshort@chromium.org>
(cherry picked from commit 3b390264a415ce121a8c6f8db9fa9c42c647aaec)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1901086
Tested-by: Hung-Te Lin <hungte@chromium.org>
Auto-Submit: Hung-Te Lin <hungte@chromium.org>
Reviewed-by: Hung-Te Lin <hungte@chromium.org>
Commit-Queue: Hung-Te Lin <hungte@chromium.org>

chip/mt_scp/gpio.c
@@ -12,13 +12,14 @@
 #include "task.h"
 #include "util.h"
 
-void gpio_set_alternate_function(uint32_t port, uint32_t mask, int func)
+void gpio_set_alternate_function(uint32_t port, uint32_t mask,
+				enum gpio_alternate_func func)
 {
 	int bit, mode_reg_index, shift;
 	uint32_t mode_bits, mode_mask;
 
 	/* Up to 8 alt functions per port */
-	if (func > 7)
+	if (func > GPIO_ALT_FUNC_7)
 		return;
 
 	if (func == GPIO_ALT_FUNC_NONE)