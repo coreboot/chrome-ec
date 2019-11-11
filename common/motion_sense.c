jackadam1981/ChromeEc

Add double tap and make motion sense wake up ap
Fix skip_interrupt when CONFIG_MKBP_EVENT_WAKEUP_MASK is defined.
Change to use CONFIG_GESTURE_DETECTION_MASK since
CONFIG_GESTURE_SENSOR_BATTERY_TAP and CONFIG_GESTURE_SENSOR_DOUBLE_TAP
both define it.

BUG=b:135575671
BRANCH=none
TEST=build pass. EC can receive double tap interrupt.

Change-Id: I6eec40ef7405ec0653ff62dbce98f975cb19e332
Signed-off-by: Heng-Ruey Hsu <henryhsu@chromium.org>

common/motion_sense.c
@@ -729,7 +729,7 @@ static void check_and_queue_gestures(uint32_t *event)
 	/* Run gesture recognition engine */
 	gesture_calc(event);
 #endif
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_DETECTION_MASK
 	if (*event & TASK_EVENT_MOTION_ACTIVITY_INTERRUPT(
 				MOTIONSENSE_ACTIVITY_DOUBLE_TAP)) {
 #ifdef CONFIG_GESTURE_HOST_DETECTION
@@ -740,7 +740,11 @@ static void check_and_queue_gestures(uint32_t *event)
 		 * AP is ignoring double tap event, do no wake up and no
 		 * automatic disable.
 		 */
+#ifndef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+		vector.flags = MOTIONSENSE_SENSOR_FLAG_WAKEUP;
+#else
 		vector.flags = 0;
+#endif
 		vector.activity = MOTIONSENSE_ACTIVITY_DOUBLE_TAP;
 		vector.state = 1; /* triggered */
 		vector.sensor_num = MOTION_SENSE_ACTIVITY_SENSOR_ID;

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1710210
Reviewed-by: Jett Rink <jettrink@chromium.org>
(cherry picked from commit d8abaaa35932d7b0e7a0aa99e52a9b81b7d74584)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1906403
Reviewed-by: Hung-Te Lin <hungte@chromium.org>
Commit-Queue: Hung-Te Lin <hungte@chromium.org>
Tested-by: Hung-Te Lin <hungte@chromium.org>
Auto-Submit: Hung-Te Lin <hungte@chromium.org>

common/motion_sense.c
@@ -727,7 +727,7 @@ static void check_and_queue_gestures(uint32_t *event)
 	/* Run gesture recognition engine */
 	gesture_calc(event);
 #endif
-#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
+#ifdef CONFIG_GESTURE_DETECTION_MASK
 	if (*event & TASK_EVENT_MOTION_ACTIVITY_INTERRUPT(
 				MOTIONSENSE_ACTIVITY_DOUBLE_TAP)) {
 #ifdef CONFIG_GESTURE_HOST_DETECTION
@@ -738,7 +738,11 @@ static void check_and_queue_gestures(uint32_t *event)
 		 * AP is ignoring double tap event, do no wake up and no
 		 * automatic disable.
 		 */
+#ifdef CONFIG_GESTURE_SENSOR_BATTERY_TAP
 		vector.flags = 0;
+#else
+		vector.flags = MOTIONSENSE_SENSOR_FLAG_WAKEUP;
+#endif
 		vector.activity = MOTIONSENSE_ACTIVITY_DOUBLE_TAP;
 		vector.state = 1; /* triggered */
 		vector.sensor_num = MOTION_SENSE_ACTIVITY_SENSOR_ID;