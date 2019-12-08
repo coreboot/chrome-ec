9b0c98b29744d5785197c2adc1b2a06a46f827c6
Denis Brockus <dbrockus@chromium.org>
2019-12-08 13:44:40 -0700

    Reland "tcpci: tcpci_tcpm_set_polarity should not clobber"
    This is a reland of 03a4a86192612f92cac85a9313d327a62aeb6ef1
    
    Original change's description:
    > tcpci: tcpci_tcpm_set_polarity should not clobber
    > 
    > Bits 1-7 were getting clobbered when we updated Bit 0
    > 
    > BUG=none
    > BRANCH=none
    > TEST=verify tcpci still functions
    > 
    > Change-Id: Ida60a08aa9e250e1dfb640caff44efc75c8ca1a9
    > Signed-off-by: Denis Brockus <dbrockus@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1956438
    > Reviewed-by: Jett Rink <jettrink@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1979567
    > Tested-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    > Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    > Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>
    
    Bug: none
    Change-Id: I5fa2d9db17089228755745777aae5e540353d7df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1993561
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- driver/tcpm/tcpci.c
+++ driver/tcpm/tcpci.c
@@ -414,8 +414,10 @@ int tcpci_enter_low_power_mode(int port)
 
 int tcpci_tcpm_set_polarity(int port, enum tcpc_cc_polarity polarity)
 {
-	return tcpc_write(port, TCPC_REG_TCPC_CTRL,
-			  TCPC_REG_TCPC_CTRL_SET(polarity));
+	return tcpc_update8(port,
+			    TCPC_REG_TCPC_CTRL,
+			    TCPC_REG_TCPC_CTRL_SET(1),
+			    (polarity) ? MASK_SET : MASK_CLR);
 }
 
 #ifdef CONFIG_USBC_PPC
