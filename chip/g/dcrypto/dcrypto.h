jackadam1981/ChromeEc

g: Add support for 192 and 256 bit AES-GCM in DCRYPTO_gcm_init
DCRYPTO_gcm_init hardcoded key length to 128 bit causing preventing
testing of 192 and 256 bit functionality for AES-GCM.

BUG=b:135623371
BRANCH=cr50
TEST=compile, specific test for issue as described in bug

Change-Id: I4fc41f6155661709115c57aa944c8976e17bffac
Signed-off-by: Vadim Sukhomlinov <sukhomlinov@chromium.org>

chip/g/dcrypto/dcrypto.h
@@ -70,8 +70,8 @@ struct GCM_CTX {
 };
 
 /* Initialize the GCM context structure. */
-void DCRYPTO_gcm_init(struct GCM_CTX *ctx, const uint8_t *key,
-		const uint8_t *iv, size_t iv_len);
+void DCRYPTO_gcm_init(struct GCM_CTX *ctx, uint32_t key_len,
+		const uint8_t *key, const uint8_t *iv, size_t iv_len);
 /* Additional authentication data to include in the tag calculation. */
 void DCRYPTO_gcm_aad(struct GCM_CTX *ctx, const uint8_t *aad_data, size_t len);
 /* Encrypt & decrypt return the number of bytes written to out

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1766098
Reviewed-by: Andrey Pronin <apronin@chromium.org>
(cherry picked from commit 24f7511e41c1f8140b19d69d9440a3ea6f91bd89)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1954339
Tested-by: Vadim Bendebury <vbendeb@chromium.org>
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
(cherry picked from commit 47fb09677c6b52618349773730db122f1089f97f)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2013457
Tested-by: Mary Ruthven <mruthven@chromium.org>
Reviewed-by: Mary Ruthven <mruthven@chromium.org>
Commit-Queue: Mary Ruthven <mruthven@chromium.org>
(cherry picked from commit 6b20a7a7fe28a69560ef3ec7d417c62c960482bc)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2024351

chip/g/dcrypto/dcrypto.h
@@ -80,8 +80,8 @@ struct GCM_CTX {
 };
 
 /* Initialize the GCM context structure. */
-void DCRYPTO_gcm_init(struct GCM_CTX *ctx, const uint8_t *key,
-		const uint8_t *iv, size_t iv_len);
+void DCRYPTO_gcm_init(struct GCM_CTX *ctx, uint32_t key_bits,
+		const uint8_t *key, const uint8_t *iv, size_t iv_len);
 /* Additional authentication data to include in the tag calculation. */
 void DCRYPTO_gcm_aad(struct GCM_CTX *ctx, const uint8_t *aad_data, size_t len);
 /* Encrypt & decrypt return the number of bytes written to out