jackadam1981/ChromeEc

anx7447.c: anx7447/anx3447 has an additional condition(0xAA:bit6) for Look4connection command
0xAA:bit6 defalut value is 1. When writing Look4connection command
to toggle CC, this bit shall be set to "0"; when TCPM found CC is connected,
this bit shall be set to "1" again

BRANCH=none
BUG=b:146318582
TEST=none

Change-Id: Ic75ebc5b126cb464a999f195fd6d2089d820e5e1
Signed-off-by: Xin Ji <xji@analogixsemi.com>

driver/tcpm/anx7447.h
@@ -39,6 +39,7 @@
 #define ANX7447_REG_VCONN_OCP_440mA	0x0C
 
 #define ANX7447_REG_ANALOG_CTRL_10	0xAA
+#define CABLE_DET_DIG 			0x40
 #define ANX7447_REG_R_VCONN_PWR_PRT_INRUSH_TIME_MASK	0x38
 #define ANX7447_REG_R_VCONN_PWR_PRT_INRUSH_TIME_19US	0x00
 #define ANX7447_REG_R_VCONN_PWR_PRT_INRUSH_TIME_38US	0x08

coreboot/chrome-ec

0xAA:bit6 default value is 1. When writing Look4connection command
TEST=Manually testing as following:
Attached C to hdmi dongle that works as expected.
Attached charger, make sure DUT can be charged as expected.
Attached with HP USB-C/A Universal Dock G2, make sure
DUT can be charged and can't see tcpc timeout.
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2065783
Tested-by: Devin Lu <Devin.Lu@quantatw.com>
Reviewed-by: Diana Z <dzigterman@chromium.org>
Reviewed-by: S Wang <swang@analogix.corp-partner.google.com>
Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>
(cherry picked from commit 20188601a1cd3b4be8a89f4e3b3ef812fb619545)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2106973
Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
Tested-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

driver/tcpm/anx7447.h
@@ -39,6 +39,8 @@
 #define ANX7447_REG_VCONN_OCP_440mA	0x0C
 
 #define ANX7447_REG_ANALOG_CTRL_10	0xAA
+#define ANX7447_REG_CABLE_DET_DIG	0x40
+
 #define ANX7447_REG_R_VCONN_PWR_PRT_INRUSH_TIME_MASK	0x38
 #define ANX7447_REG_R_VCONN_PWR_PRT_INRUSH_TIME_19US	0x00
 #define ANX7447_REG_R_VCONN_PWR_PRT_INRUSH_TIME_38US	0x08