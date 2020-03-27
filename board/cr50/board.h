jackadam1981/ChromeEc

make DIOB3 interruptible and wakable for EC-EFS2
If the board supports EC-CR50 communication, Cr50 enables both
rising/falling-edge triggered interrupt on DIOB3 pin and makes
it wakable as well.Cr50 connects GPIO_AP_FLASH_SELECT to DIOB4.

If the board does not support EC-CR50 communication, Cr50 connects
GPIO_AP_FLASH_SELECT to DIOB3.

If EC puts high on DIOB3 to activate EC-CR50 communication, CR50
enables UART_EC RX and TX. Per hardware design for this feature,
H1 is supposed to dominate UART EC port against servo.

BUG=chromium:1035706
BRANCH=cr50
TEST=None

Change-Id: I1221a1a19219274622ab710568ce7c66ab2f1da7
Signed-off-by: Namyoon Woo <namyoon@chromium.org>

board/cr50/board.h
@@ -380,6 +380,18 @@ void board_unwedge_i2cs(void);
 
 int board_in_prod_mode(void);
 
+/* GPIO Interrupt handler for GPIO_EC_PACKET_MODE_EN rising edge */
+void ec_comm_packet_mode_en(enum gpio_signal signal);
+
+/* GPIO Interrupt handler for GPIO_EC_PACKET_MODE_DIS falling edge */
+void ec_comm_packet_mode_dis(enum gpio_signal signal);
+
+/*
+ * Return True if the given UART is in packet mode, in which EC-CR50
+ * communication is on-going.
+ */
+int ec_comm_is_uart_in_packet_mode(int uart);
+
 #endif /* !__ASSEMBLER__ */
 
 /* USB interface indexes (use define rather than enum to expand them) */
@@ -405,6 +417,7 @@ int board_in_prod_mode(void);
 #define UART_CR50	0
 #define UART_AP		1
 #define UART_EC		2
+#define UART_NULL	0xff
 
 #define UARTN UART_CR50
 

coreboot/chrome-ec

enables UART_EC RX and TX.
TEST=none
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1989581
Reviewed-by: Mary Ruthven <mruthven@chromium.org>
(cherry picked from commit 1411a256089ad45a99a2e327972bc6f732f5a6e9)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2122633
Tested-by: Mary Ruthven <mruthven@chromium.org>
Commit-Queue: Mary Ruthven <mruthven@chromium.org>

board/cr50/board.h
@@ -377,6 +377,18 @@ void board_unwedge_i2cs(void);
 
 int board_in_prod_mode(void);
 
+/* GPIO Interrupt handler for GPIO_EC_PACKET_MODE_EN rising edge */
+void ec_comm_packet_mode_en(enum gpio_signal unsed);
+
+/* GPIO Interrupt handler for GPIO_EC_PACKET_MODE_DIS falling edge */
+void ec_comm_packet_mode_dis(enum gpio_signal unsed);
+
+/*
+ * Return True if the given UART is in packet mode, in which EC-CR50
+ * communication is on-going.
+ */
+int ec_comm_is_uart_in_packet_mode(int uart);
+
 #endif /* !__ASSEMBLER__ */
 
 /* USB interface indexes (use define rather than enum to expand them) */
@@ -402,6 +414,7 @@ int board_in_prod_mode(void);
 #define UART_CR50	0
 #define UART_AP		1
 #define UART_EC		2
+#define UART_NULL	0xff
 
 #define UARTN UART_CR50
 