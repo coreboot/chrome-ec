jackadam1981/ChromeEc

move ec_comm implementation to common directory
This patch moves ec_comm.c and ec_efs.c from board/cr50 to common/,
so that they can be shared with other board configuration (like host).
This is to build unittest for those files.

BUG=none
BRANCH=cr50
TEST=make buildall -j

Signed-off-by: Namyoon Woo <namyoon@chromium.org>
Change-Id: I67ac313054ebe4604848a176f0a42e3483957e74

common/build.mk
@@ -65,6 +65,7 @@ common-$(CONFIG_DEDICATED_RECOVERY_BUTTON)+=button.o
 common-$(CONFIG_DEVICE_EVENT)+=device_event.o
 common-$(CONFIG_DEVICE_STATE)+=device_state.o
 common-$(CONFIG_DPTF)+=dptf.o
+common-$(CONFIG_EC_CR50_COMM)+=ec_comm.o ec_efs.o
 common-$(CONFIG_EC_EC_COMM_MASTER)+=ec_ec_comm_master.o
 common-$(CONFIG_EC_EC_COMM_SLAVE)+=ec_ec_comm_slave.o
 common-$(CONFIG_HOSTCMD_ESPI)+=espi.o

include/config.h
@@ -1411,6 +1411,11 @@
 /* Enable verbose output to UART console and extra timestamp print precision. */
 #define CONFIG_CONSOLE_VERBOSE
 
+/*
+ * Enable EC-CR50 communication (a.k.a. EC-EFS2). This is for CR50 config only.
+ */
+#undef CONFIG_EC_CR50_COMM
+
 /*****************************************************************************/
 /* Support for EC-EC communication */
 

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2094076
Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
(cherry picked from commit 72e5fc940ab60c5b3ea659a27033f74786c45323)
Change-Id: I1490cf8e107ed276ca67142e4512a39b9ddb79ed
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2122645
Tested-by: Mary Ruthven <mruthven@chromium.org>
Reviewed-by: Mary Ruthven <mruthven@chromium.org>
Commit-Queue: Mary Ruthven <mruthven@chromium.org>

common/build.mk
@@ -51,6 +51,7 @@ common-$(CONFIG_CURVE25519)+=curve25519-generic.o
 endif
 common-$(CONFIG_DEVICE_STATE)+=device_state.o
 common-$(CONFIG_DPTF)+=dptf.o
+common-$(CONFIG_EC_EFS_SUPPORT)+=ec_comm.o ec_efs.o
 common-$(CONFIG_EXTENSION_COMMAND)+=extension.o
 common-$(CONFIG_EXTPOWER_GPIO)+=extpower_gpio.o
 common-$(CONFIG_FANS)+=fan.o pwm.o

include/config.h
@@ -861,6 +861,11 @@
 #define CONFIG_CONSOLE_VERBOSE
 
 /*
+ * Enable EC-CR50 communication (a.k.a. EC-EFS2). This is for CR50 config only.
+ */
+#undef CONFIG_EC_EFS_SUPPORT
+
+/*
  * Enable the experimental console.
  *
  * NOTE: If you enable this experimental console, you will need to run the