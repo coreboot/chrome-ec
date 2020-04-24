jackadam1981/ChromeEc

make DIOB3 interruptible and wakable for EC-EFS2
If the board supports EC-CR50 communication, Cr50 enables both
rising/falling-edge triggered interrupt on DIOB3 pin and makes
it wakable as well.Cr50 connects GPIO_AP_FLASH_SELECT to DIOB4.

If the board does not support EC-CR50 communication, Cr50 connects
GPIO_AP_FLASH_SELECT to DIOB3.

If EC puts high on DIOB3 to activate EC-CR50 communication, CR50
enables UART_EC RX and TX. Per hardware design for this feature,
H1 is supposed to dominate UART EC port against servo.

BUG=chromium:1035706
BRANCH=cr50
TEST=None

Change-Id: I1221a1a19219274622ab710568ce7c66ab2f1da7
Signed-off-by: Namyoon Woo <namyoon@chromium.org>

board/cr50/board.c
@@ -447,12 +447,18 @@ static void init_ac_detect(void)
 /*****************************************************************************/
 
 /*
- * There's no way to trigger on both rising and falling edges, so force a
- * compiler error if we try. The workaround is to use the pinmux to connect
- * two GPIOs to the same input and configure each one for a separate edge.
+ * There's no way to have more than one trigger condition on a GPIO,
+ * so force a compiler error if we try. The workaround is to use the pinmux to
+ * connect as many GPIOs as the number of required trigger conditions to the
+ * same input and configure each one for a separate condition.
+ */
+#define GPIO_INT_COND(x) ((x) & GPIO_INT_ANY & ~GPIO_INPUT)
+/*
+ * Checkes flags has only one bit set among GPIO_INT_F_RISING,
+ * GPIO_INT_F_FALLING, GPIO_INT_F_LOW, GPIO_INT_F_HIGH.
  */
 #define GPIO_INT(name, pin, flags, signal)	\
-	BUILD_ASSERT(((flags) & GPIO_INT_BOTH) != GPIO_INT_BOTH);
+	BUILD_ASSERT((GPIO_INT_COND(flags) & (GPIO_INT_COND(flags) - 1)) == 0);
 #include "gpio.wrap"
 
 /**
@@ -594,6 +600,15 @@ void board_configure_deep_sleep_wakepins(void)
 		/* enable powerdown exit */
 		GWRITE_FIELD(PINMUX, EXITEN0, DIOM0, 1);
 	}
+
+	if (board_has_ec_cr50_comm_support()) {
+		/* disable powerdown exit */
+		GWRITE_FIELD(PINMUX, EXITEN0,   DIOB3, 0);
+		GWRITE_FIELD(PINMUX, EXITEDGE0, DIOB3, 1); /* edge triggered */
+		GWRITE_FIELD(PINMUX, EXITINV0,  DIOB3, 0); /* wake on rising */
+		 /* enable powerdown exit */
+		GWRITE_FIELD(PINMUX, EXITEN0,   DIOB3, 1);
+	}
 }
 
 static void deferred_tpm_rst_isr(void);
@@ -670,16 +685,21 @@ static void configure_board_specific_gpios(void)
 		GWRITE(PINMUX, DIOB4_SEL, GC_PINMUX_GPIO0_GPIO2_SEL);
 		GWRITE(PINMUX, GPIO0_GPIO2_SEL, GC_PINMUX_DIOB4_SEL);
 
-		/* Enable the input */
+		/* Enable the input for DIOB4 */
 		GWRITE_FIELD(PINMUX, DIOB4_CTL, IE, 1);
+
+		/* Connect GPIO_EC_PACKET_MODE_EN to DIOB3 as input. */
+		GWRITE(PINMUX, GPIO1_GPIO7_SEL, GC_PINMUX_DIOB3_SEL);
+		/* Connect GPIO_EC_PACKET_MODE_DIS to DIOB3 as input. */
+		GWRITE(PINMUX, GPIO1_GPIO8_SEL, GC_PINMUX_DIOB3_SEL);
 	} else {
 		/* Connect GPIO_AP_FLASH_SELECT to DIOB3. */
 		GWRITE(PINMUX, DIOB3_SEL, GC_PINMUX_GPIO0_GPIO2_SEL);
 		GWRITE(PINMUX, GPIO0_GPIO2_SEL, GC_PINMUX_DIOB3_SEL);
-
-		/* Enable the input */
-		GWRITE_FIELD(PINMUX, DIOB3_CTL, IE, 1);
 	}
+	/* Enable the input for DIOB3 */
+	GWRITE_FIELD(PINMUX, DIOB3_CTL, IE, 1);
+
 }
 
 static uint8_t mismatched_board_id;

board/cr50/board.h
@@ -380,6 +380,18 @@ void board_unwedge_i2cs(void);
 
 int board_in_prod_mode(void);
 
+/* GPIO Interrupt handler for GPIO_EC_PACKET_MODE_EN rising edge */
+void ec_comm_packet_mode_en(enum gpio_signal signal);
+
+/* GPIO Interrupt handler for GPIO_EC_PACKET_MODE_DIS falling edge */
+void ec_comm_packet_mode_dis(enum gpio_signal signal);
+
+/*
+ * Return True if the given UART is in packet mode, in which EC-CR50
+ * communication is on-going.
+ */
+int ec_comm_is_uart_in_packet_mode(int uart);
+
 #endif /* !__ASSEMBLER__ */
 
 /* USB interface indexes (use define rather than enum to expand them) */
@@ -405,6 +417,7 @@ int board_in_prod_mode(void);
 #define UART_CR50	0
 #define UART_AP		1
 #define UART_EC		2
+#define UART_NULL	0xff
 
 #define UARTN UART_CR50
 

coreboot/chrome-ec

enables UART_EC RX and TX.
TEST=none
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1989581
Reviewed-by: Mary Ruthven <mruthven@chromium.org>
(cherry picked from commit 1411a256089ad45a99a2e327972bc6f732f5a6e9)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2122633
Tested-by: Mary Ruthven <mruthven@chromium.org>
Commit-Queue: Mary Ruthven <mruthven@chromium.org>
(cherry picked from commit 4af2954d6a9cb5e556ace4b2c7a4555d87ea911f)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2165774

board/cr50/board.c
@@ -460,12 +460,18 @@ static void init_ac_detect(void)
 /*****************************************************************************/
 
 /*
- * There's no way to trigger on both rising and falling edges, so force a
- * compiler error if we try. The workaround is to use the pinmux to connect
- * two GPIOs to the same input and configure each one for a separate edge.
+ * There's no way to have more than one trigger condition on a GPIO,
+ * so force a compiler error if we try. The workaround is to use the pinmux to
+ * connect as many GPIOs as the number of required trigger conditions to the
+ * same input and configure each one for a separate condition.
+ */
+#define GPIO_INT_COND(x) ((x) & GPIO_INT_ANY & ~GPIO_INPUT)
+/*
+ * Checks flags has only one bit set among GPIO_INT_F_RISING,
+ * GPIO_INT_F_FALLING, GPIO_INT_F_LOW, GPIO_INT_F_HIGH.
  */
 #define GPIO_INT(name, pin, flags, signal)	\
-	BUILD_ASSERT(((flags) & GPIO_INT_BOTH) != GPIO_INT_BOTH);
+	BUILD_ASSERT((GPIO_INT_COND(flags) & (GPIO_INT_COND(flags) - 1)) == 0);
 #include "gpio.wrap"
 
 /**
@@ -607,6 +613,15 @@ void board_configure_deep_sleep_wakepins(void)
 		/* enable powerdown exit */
 		GWRITE_FIELD(PINMUX, EXITEN0, DIOM0, 1);
 	}
+
+	if (board_has_ec_cr50_comm_support()) {
+		/* disable powerdown exit */
+		GWRITE_FIELD(PINMUX, EXITEN0,   DIOB3, 0);
+		GWRITE_FIELD(PINMUX, EXITEDGE0, DIOB3, 0); /* level sensitive */
+		GWRITE_FIELD(PINMUX, EXITINV0,  DIOB3, 0); /* wake on high */
+		 /* enable powerdown exit */
+		GWRITE_FIELD(PINMUX, EXITEN0,   DIOB3, 1);
+	}
 }
 
 static void deferred_tpm_rst_isr(void);
@@ -704,16 +719,21 @@ static void configure_board_specific_gpios(void)
 		GWRITE(PINMUX, DIOB4_SEL, GC_PINMUX_GPIO0_GPIO2_SEL);
 		GWRITE(PINMUX, GPIO0_GPIO2_SEL, GC_PINMUX_DIOB4_SEL);
 
-		/* Enable the input */
+		/* Enable the input for DIOB4 */
 		GWRITE_FIELD(PINMUX, DIOB4_CTL, IE, 1);
+
+		/* Connect GPIO_EC_PACKET_MODE_EN to DIOB3 as input. */
+		GWRITE(PINMUX, GPIO1_GPIO7_SEL, GC_PINMUX_DIOB3_SEL);
+		/* Connect GPIO_EC_PACKET_MODE_DIS to DIOB3 as input. */
+		GWRITE(PINMUX, GPIO1_GPIO8_SEL, GC_PINMUX_DIOB3_SEL);
 	} else {
 		/* Connect GPIO_AP_FLASH_SELECT to DIOB3. */
 		GWRITE(PINMUX, DIOB3_SEL, GC_PINMUX_GPIO0_GPIO2_SEL);
 		GWRITE(PINMUX, GPIO0_GPIO2_SEL, GC_PINMUX_DIOB3_SEL);
-
-		/* Enable the input */
-		GWRITE_FIELD(PINMUX, DIOB3_CTL, IE, 1);
 	}
+	/* Enable the input for DIOB3 */
+	GWRITE_FIELD(PINMUX, DIOB3_CTL, IE, 1);
+
 }
 
 static uint8_t mismatched_board_id;

board/cr50/board.h
@@ -375,6 +375,18 @@ void board_unwedge_i2cs(void);
 
 int board_in_prod_mode(void);
 
+/* GPIO Interrupt handler for GPIO_EC_PACKET_MODE_EN rising edge */
+void ec_comm_packet_mode_en(enum gpio_signal unsed);
+
+/* GPIO Interrupt handler for GPIO_EC_PACKET_MODE_DIS falling edge */
+void ec_comm_packet_mode_dis(enum gpio_signal unsed);
+
+/*
+ * Return True if the given UART is in packet mode, in which EC-CR50
+ * communication is on-going.
+ */
+int ec_comm_is_uart_in_packet_mode(int uart);
+
 #endif /* !__ASSEMBLER__ */
 
 /* USB interface indexes (use define rather than enum to expand them) */
@@ -400,6 +412,7 @@ int board_in_prod_mode(void);
 #define UART_CR50	0
 #define UART_AP		1
 #define UART_EC		2
+#define UART_NULL	0xff
 
 #define UARTN UART_CR50
 