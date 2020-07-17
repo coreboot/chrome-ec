6f7b1ea2dddd9809dbb4c5602e2ef0b37eb81307
Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
2020-07-17 19:29:54 +0200

    driver: add ICM-426xx driver support
    Add ICM-426xx accel/gyro driver code.
    
    Conflicts:
     - SPI connection not supported. Remove i2c_spi_addr_flags and
       associated code.
     - Remove set/get_scale, not supported in branch.
    
    BUG=chromium:1117541, b:192817920
    BRANCH=rammus
    TEST=ectool motionsense fifo_read && tast run hardware.SensorRing
    
    Signed-off-by: Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
    Change-Id: I83fe48abc6aa9cde86576a777ac4272d90fac597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2317888
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3099546
    Reviewed-by: JuHyun Kim <jkim@invensense.com>
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>
    Tested-by: Michael5 Chen <michael5_chen1@pegatron.corp-partner.google.com>
    Tested-by: JuHyun Kim <jkim@invensense.com>
    Commit-Queue: Zhuohao Lee <zhuohao@chromium.org>

--- driver/build.mk
+++ driver/build.mk
@@ -18,6 +18,7 @@ driver-$(CONFIG_ACCEL_LIS2D_COMMON)+=accel_lis2dh.o stm_mems_common.o
 driver-$(CONFIG_MAG_LIS2MDL)+=mag_lis2mdl.o
 driver-$(CONFIG_SENSORHUB_LSM6DSM)+=sensorhub_lsm6dsm.o
 driver-$(CONFIG_SYNC)+=sync.o
+driver-$(CONFIG_ACCELGYRO_ICM426XX)+=accelgyro_icm426xx.o accelgyro_icm_common.o
 
 # BC1.2 Charger Detection Devices
 driver-$(CONFIG_BC12_DETECT_MAX14637)+=bc12/max14637.o
