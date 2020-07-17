8f49f9313358785f0f49010cb7ab91f49b40e236
Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
2020-07-17 19:29:54 +0200

    driver: add ICM-426xx driver support
    Add ICM-426xx accel/gyro driver code.
    
    BUG=chromium:1117541
    BRANCH=None
    TEST=ectool motionsense fifo_read && tast run hardware.SensorRing
    
    Signed-off-by: Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
    Change-Id: I83fe48abc6aa9cde86576a777ac4272d90fac597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2317888
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2584545
    Tested-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>
    Commit-Queue: Zhuohao Lee <zhuohao@chromium.org>
    Auto-Submit: David Huang <david.huang@quanta.corp-partner.google.com>

--- include/accelgyro.h
+++ include/accelgyro.h
@@ -144,6 +144,9 @@ struct accelgyro_saved_data_t {
 	uint16_t scale[3];
 };
 
+#define SENSOR_APPLY_DIV_SCALE(_input, _scale) \
+	(((_input) * (uint64_t)MOTION_SENSE_DEFAULT_SCALE) / (_scale))
+
 #define SENSOR_APPLY_SCALE(_input, _scale) \
 	(((_input) * (_scale)) / MOTION_SENSE_DEFAULT_SCALE)
 
