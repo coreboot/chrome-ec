d25840b3c217dddc12c8cdfdcac641840415421e
Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
2020-08-25 09:56:58 +0000

    driver: add icm426xx chip type define
    Add new enum motionsensor_chip and update ectool motionsense.
    
    BUG=chromium:1117541
    BRANCH=None
    TEST=ectool motionsense info
    
    Cq-Depend: chromium:2317888
    Signed-off-by: Jean-Baptiste Maneyrol <jmaneyrol@invensense.com>
    Change-Id: I07736d61bdb7332bfdc44c8f7294233e43a6e00d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2374647
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2598311
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Tested-by: David Huang <david.huang@quanta.corp-partner.google.com>
    Commit-Queue: YH Lin <yueherngl@chromium.org>

--- util/ectool.c
+++ util/ectool.c
@@ -4035,6 +4035,9 @@ static int cmd_motionsense(int argc, char **argv)
 		case MOTIONSENSE_CHIP_LIS2DE:
 			printf("lis2de\n");
 			break;
+		case MOTIONSENSE_CHIP_ICM426XX:
+			printf("icm426xx\n");
+			break;
 		default:
 			printf("unknown\n");
 		}
