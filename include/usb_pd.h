8a6dc23c99d7f9604c1cb3d53eed7f2ec4a6c422
Edward Hill <ecgh@chromium.org>
2020-11-06 16:12:53 -0700

    Revert "zork: Wait 500ms before allowing DP event to cause resume."
    This reverts commit 9230877e50547a3e5b164e919f271a8414550884.
    
    Revert CL:2506425 from firmware-zork-13434.B-master to allow clean
    cherry-pick of final version of CL:2506424 from master.
    
    Signed-off-by: Edward Hill <ecgh@chromium.org>
    Change-Id: I30f59b5fe447f3dcfea458384579b9cc3e2b7f4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2523835

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -2841,9 +2841,9 @@ void pd_set_polarity(int port, enum tcpc_cc_polarity polarity);
 
 /*
  * Notify the AP that we have entered into DisplayPort Alternate Mode.  This
- * sets a host event which may wake the AP.
+ * sets a DP_ALT_MODE_ENTERED MKBP event which may wake the AP.
  */
-__override_proto void pd_notify_dp_alt_mode_entry(void);
+void pd_notify_dp_alt_mode_entry(void);
 
 /*
  * Determines the PD state of the port partner according to Table 4-10 in USB PD
