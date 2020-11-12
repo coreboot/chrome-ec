fe89729c1115090a1c7f7217171ea4e1f68d0488
Diana Z <dzigterman@chromium.org>
2020-11-12 11:27:16 -0700

    TCPMv2: Replace charging allow list
    Replace the charging allow list with a check to verify whether the
    partner can provide at least 27W to the DUT.  This should cover existing
    members of the allow list, and deprecate the need to continue adding to
    this list.
    
    BRANCH=None
    BUG=b:173070679
    TEST=on drawcia, verify DUT can charge from power bank, Apple 3-in-1,
    and servo_v4
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: Id0b516782809b6912602e9b7ab0546938e0b85ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2535217
    Tested-by: Devin Lu <Devin.Lu@quantatw.com>
    Reviewed-by: Denis Brockus <dbrockus@chromium.org>
    Commit-Queue: Denis Brockus <dbrockus@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2565632
    Commit-Queue: Edward Hill <ecgh@chromium.org>
    Tested-by: Edward Hill <ecgh@chromium.org>

--- include/usb_pd.h
+++ include/usb_pd.h
@@ -254,6 +254,12 @@ enum pd_rx_errors {
 #define PD_V_SAFE0V_MAX		800
 #define PD_V_SAFE5V_MIN		4750
 
+/* Maximum voltage in mV offered by PD 3.0 Version 2.0 Spec */
+#define PD_REV3_MAX_VOLTAGE	20000
+
+/* Power in mW at which we will automatically charge from a DRP partner */
+#define PD_DRP_CHARGE_POWER_MIN	27000
+
 /* function table for entered mode */
 struct amode_fx {
 	int (*status)(int port, uint32_t *payload);
