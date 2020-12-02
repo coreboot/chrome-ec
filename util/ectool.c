jackadam1981/ChromeEc

Add EC_CMD_BATTERY_GET_STATIC v1 for zork
Some zork variants have battery model names that differ only beyond
the 7th character, which cannot be differentiated with the current
limitation of 8 characters per battery string. Introduce a new hostcmd
version that allows longer battery strings and enable it on Zork.

Because allowing longer strings through the host memory map is more
difficult and not required (because getting the full longer string
is mostly only useful for servicing), the host memory map is unchanged.
ectool is updated to use hostcmd (rather than memory map) if the new
command version is available, in order to take advantage of it.

BUG=b:171854783
TEST=ectool battery prints longer strings when supported by the EC;
     a hacked EC on morphius can return 11 characters of text.
     An EC running older firmware still works with a new ectool.
BRANCH=zork

Change-Id: I63d20d4f690b6945cb1d423aafaf55dafc039211
Signed-off-by: Peter Marheine <pmarheine@chromium.org>

util/ectool.c
@@ -7707,7 +7707,7 @@ void print_battery_flags(int flags)
 int get_battery_command(int index)
 {
 	struct ec_params_battery_static_info static_p;
-	struct ec_response_battery_static_info static_r;
+	struct ec_response_battery_static_info_v2 static_r;
 	struct ec_params_battery_dynamic_info dynamic_p;
 	struct ec_response_battery_dynamic_info dynamic_r;
 	int rv;
@@ -7715,7 +7715,7 @@ int get_battery_command(int index)
 	printf("Battery %d info:\n", index);
 
 	static_p.index = index;
-	rv = ec_command(EC_CMD_BATTERY_GET_STATIC, 0,
+	rv = ec_command(EC_CMD_BATTERY_GET_STATIC, 1,
 			&static_p, sizeof(static_p),
 			&static_r, sizeof(static_r));
 	if (rv < 0)
@@ -7733,21 +7733,21 @@ int get_battery_command(int index)
 		return -1;
 	}
 
-	if (!is_string_printable(static_r.manufacturer))
+	if (!is_string_printable(static_r.manufacturer_ext))
 		goto cmd_error;
-	printf("  OEM name:               %s\n", static_r.manufacturer);
+	printf("  OEM name:               %s\n", static_r.manufacturer_ext);
 
-	if (!is_string_printable(static_r.model))
+	if (!is_string_printable(static_r.model_ext))
 		goto cmd_error;
-	printf("  Model number:           %s\n", static_r.model);
+	printf("  Model number:           %s\n", static_r.model_ext);
 
-	if (!is_string_printable(static_r.type))
+	if (!is_string_printable(static_r.type_ext))
 		goto cmd_error;
-	printf("  Chemistry   :           %s\n", static_r.type);
+	printf("  Chemistry   :           %s\n", static_r.type_ext);
 
-	if (!is_string_printable(static_r.serial))
+	if (!is_string_printable(static_r.serial_ext))
 		goto cmd_error;
-	printf("  Serial number:          %s\n", static_r.serial);
+	printf("  Serial number:          %s\n", static_r.serial_ext);
 
 	if (!is_battery_range(static_r.design_capacity))
 		goto cmd_error;
@@ -7809,15 +7809,15 @@ int cmd_battery(int argc, char *argv[])
 			fprintf(stderr, "Bad battery index.\n");
 			return -1;
 		}
-
-		if (index > 0)
-			return get_battery_command(index);
 	}
 
 	/*
-	 * TODO(b:65697620): When supported/required, read battery 0 information
-	 * through EC commands as well.
+	 * Read non-primary batteries through hostcmd, and all batteries
+	 * if longer strings are supported for static info.
 	 */
+	if (index > 0 ||
+	    ec_cmd_version_supported(EC_CMD_BATTERY_GET_STATIC, 1))
+		return get_battery_command(index);
 
 	val = read_mapped_mem8(EC_MEMMAP_BATTERY_VERSION);
 	if (val < 1) {

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2519243
Reviewed-by: Edward Hill <ecgh@chromium.org>
(cherry picked from commit 6bc9bb622a31845277d5513fa80fb6766ee68f6c)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2530813
Commit-Queue: Edward Hill <ecgh@chromium.org>
Tested-by: Edward Hill <ecgh@chromium.org>

util/ectool.c
@@ -7694,7 +7694,7 @@ void print_battery_flags(int flags)
 int get_battery_command(int index)
 {
 	struct ec_params_battery_static_info static_p;
-	struct ec_response_battery_static_info static_r;
+	struct ec_response_battery_static_info_v1 static_r;
 	struct ec_params_battery_dynamic_info dynamic_p;
 	struct ec_response_battery_dynamic_info dynamic_r;
 	int rv;
@@ -7702,7 +7702,7 @@ int get_battery_command(int index)
 	printf("Battery %d info:\n", index);
 
 	static_p.index = index;
-	rv = ec_command(EC_CMD_BATTERY_GET_STATIC, 0,
+	rv = ec_command(EC_CMD_BATTERY_GET_STATIC, 1,
 			&static_p, sizeof(static_p),
 			&static_r, sizeof(static_r));
 	if (rv < 0)
@@ -7720,21 +7720,21 @@ int get_battery_command(int index)
 		return -1;
 	}
 
-	if (!is_string_printable(static_r.manufacturer))
+	if (!is_string_printable(static_r.manufacturer_ext))
 		goto cmd_error;
-	printf("  OEM name:               %s\n", static_r.manufacturer);
+	printf("  OEM name:               %s\n", static_r.manufacturer_ext);
 
-	if (!is_string_printable(static_r.model))
+	if (!is_string_printable(static_r.model_ext))
 		goto cmd_error;
-	printf("  Model number:           %s\n", static_r.model);
+	printf("  Model number:           %s\n", static_r.model_ext);
 
-	if (!is_string_printable(static_r.type))
+	if (!is_string_printable(static_r.type_ext))
 		goto cmd_error;
-	printf("  Chemistry   :           %s\n", static_r.type);
+	printf("  Chemistry   :           %s\n", static_r.type_ext);
 
-	if (!is_string_printable(static_r.serial))
+	if (!is_string_printable(static_r.serial_ext))
 		goto cmd_error;
-	printf("  Serial number:          %s\n", static_r.serial);
+	printf("  Serial number:          %s\n", static_r.serial_ext);
 
 	if (!is_battery_range(static_r.design_capacity))
 		goto cmd_error;
@@ -7796,15 +7796,15 @@ int cmd_battery(int argc, char *argv[])
 			fprintf(stderr, "Bad battery index.\n");
 			return -1;
 		}
-
-		if (index > 0)
-			return get_battery_command(index);
 	}
 
 	/*
-	 * TODO(b:65697620): When supported/required, read battery 0 information
-	 * through EC commands as well.
+	 * Read non-primary batteries through hostcmd, and all batteries
+	 * if longer strings are supported for static info.
 	 */
+	if (index > 0 ||
+	    ec_cmd_version_supported(EC_CMD_BATTERY_GET_STATIC, 1))
+		return get_battery_command(index);
 
 	val = read_mapped_mem8(EC_MEMMAP_BATTERY_VERSION);
 	if (val < 1) {