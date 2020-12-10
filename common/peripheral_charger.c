jackadam1981/ChromeEc

PCHG: Add host command
This patch adds a host command to get and set the peripheral charge
manager info and status.

$ ectool pchg
1

$ ectool pchg 0
State: 4
Battery: 50%
Flags: 0x0

$ ectool pchg 0 reset
Usage3 not implemented

$ ectool pchg 0 reset foo
Invalid parameter count
  Usage1: pchg
  Usage2: pchg <port>
  Usage3: pchg <port> reset/init/enable/disable
    <port> is a peripheral port number.
  Usage1 prints the number of ports.
  Usage2 prints the status of a port.
  Usage3 resets, initializes, enables, or disables a port.

$ ectool pchg 100
Bad port index

BUG=b:173235954
BRANCH=Trogdor
TEST=See the description above.

Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
Change-Id: I33f261e48b16d5933b6f3ca9f3c12fec476edda3

common/peripheral_charger.c
@@ -6,6 +6,7 @@
 #include "atomic.h"
 #include "common.h"
 #include "hooks.h"
+#include "host_command.h"
 #include "peripheral_charger.h"
 #include "queue.h"
 #include "stdbool.h"
@@ -328,6 +329,39 @@ void pchg_task(void *u)
 		task_wait_event(-1);
 	}
 }
+
+static enum ec_status hc_pchg_count(struct host_cmd_handler_args *args)
+{
+	struct ec_response_pchg_count *r = args->response;
+
+	r->port_count = pchg_count;
+	args->response_size = sizeof(*r);
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_PCHG_COUNT, hc_pchg_count, EC_VER_MASK(0));
+
+static enum ec_status hc_pchg_status(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_pchg_status *p = args->params;
+	struct ec_response_pchg_status *r = args->response;
+	int port = p->port;
+	struct pchg *ctx;
+
+	if (port >= pchg_count)
+		return EC_RES_INVALID_PARAM;
+
+	ctx = &pchgs[port];
+
+	r->state = ctx->state;
+	r->battery_percentage = ctx->battery_percent;
+	r->error = ctx->error;
+
+	args->response_size = sizeof(*r);
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_PCHG_STATUS, hc_pchg_status, EC_VER_MASK(0));
 
 static int cc_pchg(int argc, char **argv)
 {

coreboot/chrome-ec

This patch adds a host command to get the peripheral charge port
count and status.
State: CHARGING (4)
$ ectool pchg 0 foo
TEST=Done on CoachZ. See the description above.
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2555628
Reviewed-by: Vincent Palatin <vpalatin@chromium.org>
(cherry picked from commit d9b7ea8ff23a744e7a5e0f28fe7ca0e36ef11a9d)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2585267
Reviewed-by: Wai-Hong Tam <waihong@google.com>
Commit-Queue: Wai-Hong Tam <waihong@google.com>
Tested-by: Wai-Hong Tam <waihong@google.com>

common/peripheral_charger.c
@@ -6,6 +6,7 @@
 #include "atomic.h"
 #include "common.h"
 #include "hooks.h"
+#include "host_command.h"
 #include "peripheral_charger.h"
 #include "queue.h"
 #include "stdbool.h"
@@ -32,13 +33,7 @@ static void pchg_queue_event(struct pchg *ctx, enum pchg_event event)
 static const char *_text_state(enum pchg_state state)
 {
 	/* TODO: Use "S%d" for normal build. */
-	static const char * const state_names[] = {
-		[PCHG_STATE_RESET] = "RESET",
-		[PCHG_STATE_INITIALIZED] = "INITIALIZED",
-		[PCHG_STATE_ENABLED] = "ENABLED",
-		[PCHG_STATE_DETECTED] = "DETECTED",
-		[PCHG_STATE_CHARGING] = "CHARGING",
-	};
+	static const char * const state_names[] = EC_PCHG_STATE_TEXT;
 
 	if (state >= sizeof(state_names))
 		return "UNDEF";
@@ -328,6 +323,39 @@ void pchg_task(void *u)
 		task_wait_event(-1);
 	}
 }
+
+static enum ec_status hc_pchg_count(struct host_cmd_handler_args *args)
+{
+	struct ec_response_pchg_count *r = args->response;
+
+	r->port_count = pchg_count;
+	args->response_size = sizeof(*r);
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_PCHG_COUNT, hc_pchg_count, EC_VER_MASK(0));
+
+static enum ec_status hc_pchg(struct host_cmd_handler_args *args)
+{
+	const struct ec_params_pchg *p = args->params;
+	struct ec_response_pchg *r = args->response;
+	int port = p->port;
+	struct pchg *ctx;
+
+	if (port >= pchg_count)
+		return EC_RES_INVALID_PARAM;
+
+	ctx = &pchgs[port];
+
+	r->state = ctx->state;
+	r->battery_percentage = ctx->battery_percent;
+	r->error = ctx->error;
+
+	args->response_size = sizeof(*r);
+
+	return EC_RES_SUCCESS;
+}
+DECLARE_HOST_COMMAND(EC_CMD_PCHG, hc_pchg, EC_VER_MASK(0));
 
 static int cc_pchg(int argc, char **argv)
 {