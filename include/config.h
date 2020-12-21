f79acb1871d99ef9cc64825c6d578001bf7b7a1c
Mary Ruthven <mruthven@chromium.org>
2020-12-22 07:14:58 +1100

    coil: remove unused btle
    Some of the btle files use words we are removing from coil. They're not
    used by cr50, so this change removes the files
    
    BUG=b:175244613
    TEST=make buildall -j ; grep -ri btle ; find -name btle*
    
    Change-Id: If746eaa34e4fa8fefeb2230a6114ee248d38d542
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2600298
    Reviewed-by: Namyoon Woo <namyoon@chromium.org>
    (cherry picked from commit 5e1a54f157554153973bc9422b3fe1d90aa1c8a5)
    
    Change-Id: I90d977b4d508091f2a20b321351ba1155eba6bc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3003718
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>
    (cherry picked from commit f412fc26c24f0e5233aec52eb4bc9ad7a19c57a8)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3059133
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>

--- include/config.h
+++ include/config.h
@@ -301,15 +301,6 @@
  */
 #undef CONFIG_BATTERY_LEVEL_NEAR_FULL
 
-/* Include support for Bluetooth LE */
-#undef CONFIG_BLUETOOTH_LE
-
-/* Include support for testing the radio for Bluetooth LE */
-#undef CONFIG_BLUETOOTH_LE_RADIO_TEST
-
-/* Include support for the HCI and link layers for Bluetooth LE */
-#undef CONFIG_BLUETOOTH_LE_STACK
-
 /* Include debugging support for the Bluetooth link layer */
 #undef CONFIG_BLUETOOTH_LL_DEBUG
 
