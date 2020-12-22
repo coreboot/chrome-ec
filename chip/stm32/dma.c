7c0c0ebafc07e46f7ddb8672712d885597102378
Eric Yilun Lin <yllin@chromium.org>
2020-12-22 10:01:59 +0800

    stm32/dma: disable TX DMA to clear DMA TX BUFFER
    BUG=b:156027159, b:173732816
    TEST=run reboot test on devices and pass for over 3000 runs
    BRANCH=kukui
    
    Change-Id: Ifb67d023efeb2f5db2de125fc2f2e90d31e1a99e
    Signed-off-by: Eric Yilun Lin <yllin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2581135
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

--- chip/stm32/dma.c
+++ chip/stm32/dma.c
@@ -79,8 +79,7 @@ void dma_disable(enum dma_channel channel)
 {
 	stm32_dma_chan_t *chan = dma_get_channel(channel);
 
-	if (chan->ccr & STM32_DMA_CCR_EN)
-		chan->ccr &= ~STM32_DMA_CCR_EN;
+	chan->ccr &= ~(STM32_DMA_CCR_EN | STM32_DMA_CCR_TCIE);
 }
 
 void dma_disable_all(void)
@@ -89,7 +88,7 @@ void dma_disable_all(void)
 
 	for (ch = 0; ch < STM32_DMAC_COUNT; ch++) {
 		stm32_dma_chan_t *chan = dma_get_channel(ch);
-		chan->ccr &= ~STM32_DMA_CCR_EN;
+		chan->ccr &= ~(STM32_DMA_CCR_EN | STM32_DMA_CCR_TCIE);
 	}
 }
 
