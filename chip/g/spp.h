e696023dd456309f57066329e08789fe1029864f
Mary Ruthven <mruthven@chromium.org>
2021-01-05 18:47:34 -0800

    coil: sps->spp
    BUG=b:175244613
    TEST=make buildall -j
    
    Change-Id: Ia34cccffdd6a82c25b479bb8d2e6370bbf00baf0
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2615121
    Reviewed-by: Namyoon Woo <namyoon@chromium.org>
    (cherry picked from commit 88bce43426c9d97f321ca96d8cf55250acc07963)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3003819
    Reviewed-by: Andrey Pronin <apronin@chromium.org>

--- chip/g/spp.h
+++ chip/g/spp.h
@@ -4,39 +4,39 @@
  * found in the LICENSE file.
  */
 
-#ifndef __CROS_EC_INCLUDE_SPS_H
-#define __CROS_EC_INCLUDE_SPS_H
+#ifndef __CROS_EC_INCLUDE_SPP_H
+#define __CROS_EC_INCLUDE_SPP_H
 
 #include "spi.h"
 #include "util.h"
 
-/* SPS Control Mode */
-enum sps_mode {
-	SPS_GENERIC_MODE = 0,
-	SPS_SWETLAND_MODE = 1,
-	SPS_ROM_MODE = 2,
-	SPS_UNDEF_MODE = 3,
+/* SPP Control Mode */
+enum spp_mode {
+	SPP_GENERIC_MODE = 0,
+	SPP_SWETLAND_MODE = 1,
+	SPP_ROM_MODE = 2,
+	SPP_UNDEF_MODE = 3,
 };
 
 /* Receive and transmit FIFO size and mask. */
-#define SPS_FIFO_SIZE		BIT(10)
-#define SPS_FIFO_MASK		(SPS_FIFO_SIZE - 1)
+#define SPP_FIFO_SIZE		BIT(10)
+#define SPP_FIFO_MASK		(SPP_FIFO_SIZE - 1)
 
 /*
  * Tx interrupt callback function prototype. This function returns a portion
  * of the received SPI data and current status of the CS line. When CS is
  * deasserted, this function is called with data_size of zero and a non-zero
- * cs_status. This allows the recipient to delineate the SPS frames.
+ * cs_status. This allows the recipient to delineate the SPP frames.
  */
 typedef void (*rx_handler_f)(uint8_t *data, size_t data_size, int cs_disabled);
 
 /*
- * Push data to the SPS TX FIFO
+ * Push data to the SPP TX FIFO
  * @param data Pointer to 8-bit data
  * @param data_size Number of bytes to transmit
  * @return : actual number of bytes placed into tx fifo
  */
-int sps_transmit(uint8_t *data, size_t data_size);
+int spp_transmit(uint8_t *data, size_t data_size);
 
 /*
  * These functions return zero on success or non-zero on failure (attempt to
@@ -45,10 +45,10 @@ int sps_transmit(uint8_t *data, size_t data_size);
  *
  * rx_fifo_threshold value of zero means 'default'.
  */
-int sps_register_rx_handler(enum sps_mode mode,
+int spp_register_rx_handler(enum spp_mode mode,
 			    rx_handler_f rx_handler,
 			    unsigned rx_fifo_threshold);
-int sps_unregister_rx_handler(void);
-void sps_tx_status(uint8_t byte);
+int spp_unregister_rx_handler(void);
+void spp_tx_status(uint8_t byte);
 
 #endif
