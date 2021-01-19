9fd1f538da4f33dde6ec923b5bf52daac9148091
Hyungwoo Yang <hyungwoo.yang@intel.corp-partner.google.com>
2021-01-19 09:24:34 -0800

    Zephyr: Create public headers for bmi260, bma2x2, and tcs3400
    Separate out the public part of sensor headers so
    it can be included from Zephyr.
    
    BUG=b:175434113
    BRANCH=none
    TEST=make buildall -j8
    build volteer on zephyr
    
     Conflicts:
    	board/volteer/sensors.c: sensors are not enabled in branch
    	driver/accel_bma2x2.h: waddledoo add BMA253 sensor support
    	driver/accelgyro_bmi_common.h: Makefile include path does not include
    		include/driver
    
    Signed-off-by: Hyungwoo Yang <hyungwoo.yang@intel.corp-partner.google.com>
    Change-Id: I899e9aea99d6c7d56a1b6735538b280fe0735313
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2637251
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    (cherry picked from commit 6a87ebfedddef0a3d0438bcbb894e45dc16fe1e1)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3214012
    Commit-Queue: Rong Chang <rongchang@chromium.org>
    Tested-by: Rong Chang <rongchang@chromium.org>
    Auto-Submit: Rong Chang <rongchang@chromium.org>

--- driver/accelgyro_bmi260.h
+++ driver/accelgyro_bmi260.h
@@ -10,19 +10,8 @@
 
 #include "accelgyro.h"
 #include "common.h"
-#include "driver/mag_bmm150.h"
-
-/*
- * The addr field of motion_sensor support both SPI and I2C:
- * This is defined in include/i2c.h and is no longer an 8bit
- * address. The 7/10 bit address starts at bit 0 and leaves
- * room for a 10 bit address, although we don't currently
- * have any 10 bit slaves.  I2C or SPI is indicated by a
- * more significant bit
- */
-
-/* I2C addresses */
-#define BMI260_ADDR0_FLAGS	0x68
+#include "mag_bmm150.h"
+#include "driver/accelgyro_bmi260_public.h"
 
 #define BMI260_CHIP_ID      0x00
 #define BMI260_CHIP_ID_MAJOR    0x27
@@ -329,13 +318,5 @@
 #define BMI260_FF_DATA_LEN_GYR          6
 
 #define BMI260_FF_DATA_LEN_MAG          8
-
-extern const struct accelgyro_drv bmi260_drv;
-
-void bmi260_interrupt(enum gpio_signal signal);
-
-#ifdef CONFIG_CMD_I2C_STRESS_TEST_ACCEL
-extern struct i2c_stress_test_dev bmi260_i2c_stress_test_dev;
-#endif
 
 #endif /* __CROS_EC_ACCELGYRO_BMI260_H */
