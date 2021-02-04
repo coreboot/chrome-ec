e5fa7fc82639d619b7a53db6b8b94117c857014e
Edward Hill <ecgh@chromium.org>
2021-02-04 01:28:23 +0000

    Revert "aleena: Add detect accel/gyro sensor in HOOK_INIT"
    This reverts commit 103e253e60bb6e37278eb13ac880f172d6941358.
    
    Reason for revert: build fails on firmware-grunt-11031.B branch:
    board/aleena/board.c:163:204: error: 'HOOK_PRIO_INIT_ADC' undeclared here (not in a function)
    
    Original change's description:
    > aleena: Add detect accel/gyro sensor in HOOK_INIT
    >
    > Add detect accel/gyro sensor in HOOK_INIT for sysjump rw.
    > Use "HOOK_PRIO_INIT_ADC + 2" For sku_id checking.
    > And make sure detect sensor when chipset power on.
    >
    > BUG=none
    > BRANCH=grunt
    > TEST=sysjump ro/rw to check motionsense data.
    >
    > Signed-off-by: David Huang <david.huang@quanta.corp-partner.google.com>
    >
    > Change-Id: I143cde2ab3028ec5658acd16fdb6ec6aae710ef6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2655305
    > Tested-by: David Huang <david.huang@quanta.corp-partner.google.com>
    > Auto-Submit: David Huang <david.huang@quanta.corp-partner.google.com>
    > Reviewed-by: YH Lin <yueherngl@chromium.org>
    > Reviewed-by: Keith Short <keithshort@chromium.org>
    > Commit-Queue: David Huang <david.huang@quanta.corp-partner.google.com>
    > (cherry picked from commit 8801fd7768c8b8436bd0282af71cdd449257c08a)
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2662223
    > Commit-Queue: Keith Short <keithshort@chromium.org>
    
    Bug: none
    Change-Id: I2cf8d24b2a41f715293b4a08ef11833e2320ce73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2674380
    Reviewed-by: Edward Hill <ecgh@chromium.org>
    Commit-Queue: Edward Hill <ecgh@chromium.org>
    Tested-by: Edward Hill <ecgh@chromium.org>

--- board/aleena/board.c
+++ board/aleena/board.c
@@ -137,8 +137,6 @@ static void board_detect_motionsensor(void)
 	int ret;
 	int val;
 
-	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
-		return;
 	if (base_accelgyro_config != BASE_GYRO_NONE)
 		return;
 
@@ -160,7 +158,6 @@ static void board_detect_motionsensor(void)
 }
 DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_detect_motionsensor,
 	     HOOK_PRIO_DEFAULT);
-DECLARE_HOOK(HOOK_INIT, board_detect_motionsensor, HOOK_PRIO_INIT_ADC + 2);
 
 void board_update_sensor_config_from_sku(void)
 {
