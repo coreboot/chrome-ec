jackadam1981/ChromeEc

PCHG: Count number of dropped events per port
Currently, dropped events are counted for all ports combined. This
patch makes them counted for each port separately.

BUG=b:173235954
BRANCH=trogdor
TEST=Verify 'pchg 0' prints dropped event count.

Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
Change-Id: I09380927613defe34bef0fece022fc9d47ebb8a1

include/peripheral_charger.h
@@ -140,6 +140,8 @@ struct pchg {
 	uint8_t battery_percent;
 	/* FW version */
 	uint32_t fw_version;
+	/* Number of dropped events (due to queue overflow) */
+	uint8_t dropped_event_count;
 };
 
 /**

coreboot/chrome-ec

BUG=b:173235954,b:183151376
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2770502
(cherry picked from commit 4126a065489fe40668186c5f5b4dee4797388b25)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2787474

include/peripheral_charger.h
@@ -138,6 +138,8 @@ struct pchg {
 	uint32_t error;
 	/* Battery percentage (0% ~ 100%) of the connected peripheral device */
 	uint8_t battery_percent;
+	/* Number of dropped events (due to queue overflow) */
+	uint32_t dropped_event_count;
 };
 
 /**