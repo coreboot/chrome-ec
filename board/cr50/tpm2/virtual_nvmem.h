85e0ad191f07ac9a5af0c95c293b5b606d70f626
Leo Lai <cylai@chromium.org>
2021-05-13 22:36:09 +0800

    add reserved `VIRTUAL_NV_INDEX_RMA_BYTES` in comment.
    The index is added to cr50 repository while the usage is only for
    generic TPM2.0 case. In the future we don't want GSC to re-use the index
    number.
    
    BUG=b:187821960
    BUG=b:191163997
    TEST=make buildall -j
    Signed-off-by: cylai@chromium.org
    
    Change-Id: I6cfb12cf4558dd0bf2c99e045ef6e0b89698bbc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2892151
    Tested-by: Leo Lai <cylai@google.com>
    Commit-Queue: Andrey Pronin <apronin@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    (cherry picked from commit 98e4da99997a020f8aba11b2cfc7edaf67376677)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3004077
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>

--- board/cr50/tpm2/virtual_nvmem.h
+++ board/cr50/tpm2/virtual_nvmem.h
@@ -25,6 +25,9 @@ enum virtual_nv_index {
 	VIRTUAL_NV_INDEX_SN_DATA,
 	VIRTUAL_NV_INDEX_G2F_CERT,
 	VIRTUAL_NV_INDEX_RSU_DEV_ID,
+	/* Reserved for generic TPM2.0 stand-alone counter. */
+	/* TODO(b/191163997): support reading RMA bytes via this index. */
+	/* VIRTUAL_NV_INDEX_RMA_BYTES = 0x013fff04, */
 	VIRTUAL_NV_INDEX_END,
 };
 /* Reserved space for future virtual indexes; this is the last valid index. */
