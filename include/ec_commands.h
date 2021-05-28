b55081fd7232797ac734f347bdfbf87a3c9c107f
Daisuke Nojiri <dnojiri@chromium.org>
2021-05-28 10:48:14 -0700

    chgstv2: Refactor charger_discharge_on_ac
    This patch makes charger_discharge_on_ac call board_discharge_on_ac.
    It also makes set_chg_ctrl_mode call charger_discharge_on_ac. This
    makes sense since when the charge control mode changes,
    discharge-on-ac also needs to be enabled or disabled.
    
    BUG=b:188457962
    BRANCH=none
    TEST=make runhosttests
    
    Change-Id: I65ec09f580afc987cc86f4c60c15c1f90ead6c3c
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2986848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3088963
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Auto-Submit: Eric Yilun Lin <yllin@google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -4254,6 +4254,8 @@ enum ec_charge_control_mode {
 	CHARGE_CONTROL_NORMAL = 0,
 	CHARGE_CONTROL_IDLE,
 	CHARGE_CONTROL_DISCHARGE,
+	/* Add no more entry below. */
+	CHARGE_CONTROL_COUNT,
 };
 
 #define EC_CHARGE_MODE_TEXT { \
