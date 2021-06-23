c0549a8269f948abf8825d2b7e1c73dab71e2927
Diana Z <dzigterman@chromium.org>
2021-06-23 14:46:19 -0600

    Typec: Add disconnected status event
    Add an event bit to represent that the port has been disconnected.
    Solved the problem that when two DUTS are connected via Type-C cable,
    unplug Type-C cable "Power Source Option" and it will not disappear
    
    BRANCH=None
    BUG=b:188330043, b:188855371
    TEST=make -j buildall
    
    Signed-off-by: Diana Z <dzigterman@chromium.org>
    Change-Id: Ia0975cae66d81a3ef066e62632d8cd0b453b9a3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2983991
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Reviewed-by: Prashant Malani <pmalani@chromium.org>
    (cherry picked from commit 0e6fdce59219098876872041f25b8cdde553a2f6)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3005175
    Reviewed-by: Philip Chen <philipchen@chromium.org>
    Reviewed-by: Bob Moragues <moragues@chromium.org>
    Commit-Queue: Philip Chen <philipchen@chromium.org>
    Tested-by: Jian Tong <tongjian@huaqin.corp-partner.google.com>

--- include/ec_commands.h
+++ include/ec_commands.h
@@ -6514,6 +6514,7 @@ enum tcpc_cc_polarity {
 
 #define PD_STATUS_EVENT_SOP_DISC_DONE		BIT(0)
 #define PD_STATUS_EVENT_SOP_PRIME_DISC_DONE	BIT(1)
+#define PD_STATUS_EVENT_DISCONNECTED		BIT(3)
 
 /*
  * Encode and decode for BCD revision response
