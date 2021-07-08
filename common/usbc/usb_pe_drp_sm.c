jackadam1981/ChromeEc

TCPMv2: Notify of disconnect on TC detach
Send a notification that the port has been disconnected when the TC
triggers HOOK_USB_PD_DISCONNECT.

BRANCH=None
BUG=b:188330043
TEST=on storo, verify that disconnect quickly registers in the UI power
status menu

Signed-off-by: Diana Z <dzigterman@chromium.org>
Change-Id: I758ca918ccd199e3ca2f69aec67323065b48add7

common/usbc/usb_pe_drp_sm.c
@@ -1352,6 +1352,9 @@ static void pe_handle_detach(void)
 	/* Reset port events */
 	pd_clear_events(port, GENMASK(31, 0));
 
+	/* But then set disconnected event */
+	pd_notify_event(port, PD_STATUS_EVENT_DISCONNECTED);
+
 	/* Tell Policy Engine to invalidate the explicit contract */
 	pe_invalidate_explicit_contract(port);
 

coreboot/chrome-ec

BUG=b:188330043,b:188855371
TEST=on storo,homestar, verify that disconnect quickly registers in the UI power
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2983992
Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
(cherry picked from commit f14e31aaf661b9ed25df3117dd30dc75e0ed3bf5)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3005179
Tested-by: Jian Tong <tongjian@huaqin.corp-partner.google.com>
Reviewed-by: Philip Chen <philipchen@chromium.org>
Commit-Queue: Jian Tong <tongjian@huaqin.corp-partner.google.com>

common/usbc/usb_pe_drp_sm.c
@@ -1267,6 +1267,9 @@ static void pe_handle_detach(void)
 	/* Reset port events */
 	pd_clear_events(port, GENMASK(31, 0));
 
+	/* But then set disconnected event */
+	pe_notify_event(port, PD_STATUS_EVENT_DISCONNECTED);
+
 	/* Tell Policy Engine to invalidate the explicit contract */
 	pe_invalidate_explicit_contract(port);
 