jackadam1981/ChromeEc

garg: Add 2nd base accelerometer
This patch adds a 2nd base accelerometer icm-40608.

BUG=b:194632295
BRANCH=firmware-octopus-11297.B
TEST=watch ectool motionsense

Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>
Change-Id: I99716c34b8519447db7fe9903904046d216ae7e0

board/garg/board.c
@@ -9,12 +9,15 @@
 #include "adc_chip.h"
 #include "battery.h"
 #include "button.h"
+#include "cbi_ssfc.h"
 #include "charge_manager.h"
 #include "charge_state.h"
 #include "common.h"
 #include "cros_board_info.h"
 #include "driver/accel_kionix.h"
 #include "driver/accelgyro_bmi_common.h"
+#include "driver/accelgyro_icm426xx.h"
+#include "driver/accelgyro_icm_common.h"
 #include "driver/charger/bd9995x.h"
 #include "driver/ppc/nx20p348x.h"
 #include "driver/tcpm/anx7447.h"
@@ -127,9 +130,16 @@ const mat33_fp_t base_standard_ref = {
 	{ 0, 0,  FLOAT_TO_FP(1)}
 };
 
+const mat33_fp_t base_icm_ref = {
+	{ FLOAT_TO_FP(-1), 0,  0},
+	{ 0, FLOAT_TO_FP(-1), 0},
+	{ 0, 0,  FLOAT_TO_FP(1)}
+};
+
 /* sensor private data */
 static struct kionix_accel_data g_kx022_data;
 static struct bmi_drv_data_t g_bmi160_data;
+static struct icm_drv_data_t g_icm426xx_data;
 
 /* Drivers */
 struct motion_sensor_t motion_sensors[] = {
@@ -207,6 +217,52 @@ struct motion_sensor_t motion_sensors[] = {
 
 unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 
+struct motion_sensor_t icm426xx_base_accel = {
+	 .name = "Base Accel",
+	 .active_mask = SENSOR_ACTIVE_S0_S3,
+	 .chip = MOTIONSENSE_CHIP_ICM426XX,
+	 .type = MOTIONSENSE_TYPE_ACCEL,
+	 .location = MOTIONSENSE_LOC_BASE,
+	 .drv = &icm426xx_drv,
+	 .mutex = &g_base_mutex,
+	 .drv_data = &g_icm426xx_data,
+	 .port = I2C_PORT_SENSOR,
+	 .i2c_spi_addr_flags = ICM426XX_ADDR0_FLAGS,
+	 .rot_standard_ref = &base_icm_ref,
+	 .default_range = 4,  /* g */
+	 .min_frequency = ICM426XX_ACCEL_MIN_FREQ,
+	 .max_frequency = ICM426XX_ACCEL_MAX_FREQ,
+	 .config = {
+		 /* EC use accel for angle detection */
+		 [SENSOR_CONFIG_EC_S0] = {
+			.odr = 13000 | ROUND_UP_FLAG,
+			.ec_rate = 100 * MSEC,
+		 },
+		 /* Sensor on for angle detection */
+		 [SENSOR_CONFIG_EC_S3] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 100 * MSEC,
+		 },
+	 },
+};
+
+struct motion_sensor_t icm426xx_base_gyro = {
+	 .name = "Base Gyro",
+	 .active_mask = SENSOR_ACTIVE_S0_S3,
+	 .chip = MOTIONSENSE_CHIP_ICM426XX,
+	 .type = MOTIONSENSE_TYPE_GYRO,
+	 .location = MOTIONSENSE_LOC_BASE,
+	 .drv = &icm426xx_drv,
+	 .mutex = &g_base_mutex,
+	 .drv_data = &g_icm426xx_data,
+	 .port = I2C_PORT_SENSOR,
+	 .i2c_spi_addr_flags = ICM426XX_ADDR0_FLAGS,
+	 .default_range = 1000, /* dps */
+	 .rot_standard_ref = &base_icm_ref,
+	 .min_frequency = ICM426XX_GYRO_MIN_FREQ,
+	 .max_frequency = ICM426XX_GYRO_MAX_FREQ,
+};
+
 static int board_is_convertible(void)
 {
 	/*
@@ -219,6 +275,12 @@ static int board_is_convertible(void)
 static void board_update_sensor_config_from_sku(void)
 {
 	if (board_is_convertible()) {
+		if (get_cbi_ssfc_sensor() == SSFC_SENSOR_ICM426XX) {
+			motion_sensors[BASE_ACCEL] = icm426xx_base_accel;
+			motion_sensors[BASE_GYRO] = icm426xx_base_gyro;
+			ccprints("BASE GYRO is ICM426XX");
+		} else
+			ccprints("BASE GYRO is BMI160");
 		motion_sensor_count = ARRAY_SIZE(motion_sensors);
 		/* Enable Base Accel interrupt */
 		gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
@@ -228,6 +290,19 @@ static void board_update_sensor_config_from_sku(void)
 		/* Base accel is not stuffed, don't allow line to float */
 		gpio_set_flags(GPIO_BASE_SIXAXIS_INT_L,
 			       GPIO_INPUT | GPIO_PULL_DOWN);
+	}
+}
+
+void sensor_interrupt(enum gpio_signal signal)
+{
+	switch (motion_sensors[BASE_ACCEL].chip) {
+	case MOTIONSENSE_CHIP_ICM426XX:
+		icm426xx_interrupt(signal);
+		break;
+	case MOTIONSENSE_CHIP_BMI160:
+	default:
+		bmi160_interrupt(signal);
+		break;
 	}
 }
 

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3067144
Reviewed-by: Diana Z <dzigterman@chromium.org>
(cherry picked from commit 9b35963509b0d6ea3e5fd1530f4b6dffcb301c66)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3070939
Reviewed-by: Marco Chen <marcochen@chromium.org>
Reviewed-by: Henry Sun <henrysun@google.com>
Commit-Queue: Henry Sun <henrysun@google.com>

board/garg/board.c
@@ -9,12 +9,15 @@
 #include "adc_chip.h"
 #include "battery.h"
 #include "button.h"
+#include "cbi_ssfc.h"
 #include "charge_manager.h"
 #include "charge_state.h"
 #include "common.h"
 #include "cros_board_info.h"
 #include "driver/accel_kionix.h"
 #include "driver/accelgyro_bmi160.h"
+#include "driver/accelgyro_icm426xx.h"
+#include "driver/accelgyro_icm_common.h"
 #include "driver/charger/bd9995x.h"
 #include "driver/ppc/nx20p348x.h"
 #include "driver/tcpm/anx7447.h"
@@ -130,9 +133,16 @@ const mat33_fp_t base_standard_ref = {
 	{ 0, 0,  FLOAT_TO_FP(1)}
 };
 
+const mat33_fp_t base_icm_ref = {
+	{ FLOAT_TO_FP(-1), 0,  0},
+	{ 0, FLOAT_TO_FP(-1), 0},
+	{ 0, 0,  FLOAT_TO_FP(1)}
+};
+
 /* sensor private data */
 static struct kionix_accel_data g_kx022_data;
 static struct bmi160_drv_data_t g_bmi160_data;
+static struct icm_drv_data_t g_icm426xx_data;
 
 /* Drivers */
 struct motion_sensor_t motion_sensors[] = {
@@ -210,6 +220,52 @@ struct motion_sensor_t motion_sensors[] = {
 
 unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 
+struct motion_sensor_t icm426xx_base_accel = {
+	 .name = "Base Accel",
+	 .active_mask = SENSOR_ACTIVE_S0_S3,
+	 .chip = MOTIONSENSE_CHIP_ICM426XX,
+	 .type = MOTIONSENSE_TYPE_ACCEL,
+	 .location = MOTIONSENSE_LOC_BASE,
+	 .drv = &icm426xx_drv,
+	 .mutex = &g_base_mutex,
+	 .drv_data = &g_icm426xx_data,
+	 .port = I2C_PORT_SENSOR,
+	 .addr = ICM426XX_ADDR0_FLAGS,
+	 .rot_standard_ref = &base_icm_ref,
+	 .default_range = 4,  /* g */
+	 .min_frequency = ICM426XX_ACCEL_MIN_FREQ,
+	 .max_frequency = ICM426XX_ACCEL_MAX_FREQ,
+	 .config = {
+		 /* EC use accel for angle detection */
+		 [SENSOR_CONFIG_EC_S0] = {
+			.odr = 13000 | ROUND_UP_FLAG,
+			.ec_rate = 100 * MSEC,
+		 },
+		 /* Sensor on for angle detection */
+		 [SENSOR_CONFIG_EC_S3] = {
+			.odr = 10000 | ROUND_UP_FLAG,
+			.ec_rate = 100 * MSEC,
+		 },
+	 },
+};
+
+struct motion_sensor_t icm426xx_base_gyro = {
+	 .name = "Base Gyro",
+	 .active_mask = SENSOR_ACTIVE_S0_S3,
+	 .chip = MOTIONSENSE_CHIP_ICM426XX,
+	 .type = MOTIONSENSE_TYPE_GYRO,
+	 .location = MOTIONSENSE_LOC_BASE,
+	 .drv = &icm426xx_drv,
+	 .mutex = &g_base_mutex,
+	 .drv_data = &g_icm426xx_data,
+	 .port = I2C_PORT_SENSOR,
+	 .addr = ICM426XX_ADDR0_FLAGS,
+	 .default_range = 1000, /* dps */
+	 .rot_standard_ref = &base_icm_ref,
+	 .min_frequency = ICM426XX_GYRO_MIN_FREQ,
+	 .max_frequency = ICM426XX_GYRO_MAX_FREQ,
+};
+
 static int board_is_convertible(void)
 {
 	/*
@@ -222,6 +278,12 @@ static int board_is_convertible(void)
 static void board_update_sensor_config_from_sku(void)
 {
 	if (board_is_convertible()) {
+		if (get_cbi_ssfc_sensor() == SSFC_SENSOR_ICM426XX) {
+			motion_sensors[BASE_ACCEL] = icm426xx_base_accel;
+			motion_sensors[BASE_GYRO] = icm426xx_base_gyro;
+			ccprints("BASE GYRO is ICM426XX");
+		} else
+			ccprints("BASE GYRO is BMI160");
 		motion_sensor_count = ARRAY_SIZE(motion_sensors);
 		/* Enable Base Accel interrupt */
 		gpio_enable_interrupt(GPIO_BASE_SIXAXIS_INT_L);
@@ -231,6 +293,19 @@ static void board_update_sensor_config_from_sku(void)
 		/* Base accel is not stuffed, don't allow line to float */
 		gpio_set_flags(GPIO_BASE_SIXAXIS_INT_L,
 			       GPIO_INPUT | GPIO_PULL_DOWN);
+	}
+}
+
+void sensor_interrupt(enum gpio_signal signal)
+{
+	switch (motion_sensors[BASE_ACCEL].chip) {
+	case MOTIONSENSE_CHIP_ICM426XX:
+		icm426xx_interrupt(signal);
+		break;
+	case MOTIONSENSE_CHIP_BMI160:
+	default:
+		bmi160_interrupt(signal);
+		break;
 	}
 }
 