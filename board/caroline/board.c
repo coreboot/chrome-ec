jackadam1981/ChromeEc

kohaku: Set base light sensor information correctly
BH1370 light sensor only support 10Hz frequency.
Set parameters properly. Sensor only used on kohaku.

BUG=b:172343923
BRANCH=hatch
TEST=Check on Kohaku the light parameters are correct.

Change-Id: Ibb9b2699e414cf80730d290aba981dcd4898f84f
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

board/kohaku/board.c
@@ -313,12 +313,11 @@ struct motion_sensor_t motion_sensors[] = {
 		.i2c_spi_addr_flags = BH1730_I2C_ADDR_FLAGS,
 		.rot_standard_ref = NULL,
 		.default_range = 65535,
-		.min_frequency = 10,
-		.max_frequency = 10,
+		.min_frequency = BH1730_10000_MHZ,
+		.max_frequency = BH1730_10000_MHZ,
 		.config = {
 			[SENSOR_CONFIG_EC_S0] = {
-				.odr = 100000,
-				.ec_rate = 0,
+				.odr = BH1730_10000_MHZ,
 			},
 		},
 	},

coreboot/chrome-ec

Conflicts:
	board/kohaku/board.c: Make change in caroline board.
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2586069
Reviewed-by: Scott Collyer <scollyer@chromium.org>
(cherry picked from commit e2882f20fa28810e735e1a8f0750674e7940eb2d)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3200806
Reviewed-by: Stefan Reinauer <reinauer@google.com>

board/caroline/board.c
@@ -589,11 +589,11 @@ struct motion_sensor_t motion_sensors[] = {
 		.addr = BH1730_I2C_ADDR,
 		.rot_standard_ref = NULL,
 		.default_range = 65535,
-		.min_frequency = 10,
-		.max_frequency = 10,
+		.min_frequency = BH1730_MIN_FREQ,
+		.max_frequency = BH1730_MAX_FREQ,
 		.config = {
 			[SENSOR_CONFIG_EC_S0] = {
-				.odr = 100000,
+				.odr = BH1730_10000_MHZ,
 				.ec_rate = 0,
 			},
 		},