jackadam1981/ChromeEc

driver: Add support for BMI220 sensor
Based on BMI260 sensor, we add support for BMI220 sensor.

BRANCH=dedede
BUG=b:188373185
TEST=Base sensor implementation tested on storo360, test sensor
function is OK, and factory test is normal.

Signed-off-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
Change-Id: Ifa88c7c58203ba405fde757b85570d53a27221a0

include/driver/accelgyro_bmi260.h
@@ -14,7 +14,10 @@
 #include "driver/accelgyro_bmi260_public.h"
 
 #define BMI260_CHIP_ID      0x00
+/* BMI260 chip identifier */
 #define BMI260_CHIP_ID_MAJOR    0x27
+/* BMI220 chip identifier */
+#define BMI220_CHIP_ID          0x26
 
 #define BMI260_ERR_REG          0x02
 

coreboot/chrome-ec

Based on BMI260 sensor, add support for BMI220 sensor.
BUG=b:188373185,b:193945779
BRANCH=dedede,kukui
TEST=run factory test on storo360
Conflicts:
	driver/accelgyro_bmi_common.c:
	include/ec_commands.h:
	util/ectool.c: no BMI3XX/BMA4XX support
	util/config_allowed.txt: no zephyr support
	third_party/bmi220/accelgyro_bmi220_config_tbin.h: branch does
		not have e0bf946ced: rom resident sections
Signed-off-by: Rong Chang <rongchang@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3077595
Reviewed-by: Eric Yilun Lin <yllin@google.com>
Reviewed-by: Chen-Tsung Hsieh <chentsung@chromium.org>
(cherry picked from commit 499d3f70351e7ab11d3b7252f1f9b0ff69aaf902)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3214013

driver/accelgyro_bmi260.h
@@ -14,7 +14,10 @@
 #include "driver/accelgyro_bmi260_public.h"
 
 #define BMI260_CHIP_ID      0x00
+/* BMI260 chip identifier */
 #define BMI260_CHIP_ID_MAJOR    0x27
+/* BMI220 chip identifier */
+#define BMI220_CHIP_ID_MAJOR    0x26
 
 #define BMI260_ERR_REG          0x02
 