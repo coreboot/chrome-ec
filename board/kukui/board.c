jackadam1981/ChromeEc

krane: Add motion sense config for BMI220
Add runtime BMI220 detection using CHIP_ID register. Fallback to BMI160
sensor driver.

BUG=b:193945779
BRANCH=kukui
TEST='ectool motionsense' and verify lid angle.

Signed-off-by: Rong Chang <rongchang@chromium.org>
Change-Id: Ia284cdf115e5d60bdd63a36ad82751b23eb438b3

board/kukui/board.c
@@ -56,6 +56,10 @@ static void gauge_interrupt(enum gpio_signal signal)
 	task_wake(TASK_ID_CHARGER);
 }
 
+#ifdef SECTION_IS_RW
+static void motion_interrupt(enum gpio_signal signal);
+#endif /* SECTION_IS_RW */
+
 #include "gpio_list.h"
 
 /******************************************************************************/
@@ -559,7 +563,64 @@ const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 const struct motion_sensor_t *motion_als_sensors[] = {
 	&motion_sensors[CLEAR_ALS],
 };
+
+#ifdef BOARD_KRANE
+
+static bool is_bmi220_present;
+
+static void board_detect_bmi220(void)
+{
+	int id = -1;
+	struct motion_sensor_t *s;
+
+	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		return;
+
+	/* Detect accelgyro chip */
+	bmi_read8(I2C_PORT_ACCEL, BMI260_ADDR0_FLAGS, BMI260_CHIP_ID, &id);
+	if (id == BMI220_CHIP_ID) {
+		/* Lid Accel*/
+		s = &motion_sensors[LID_ACCEL];
+		CPRINTS("BMI220 - %s", s->name);
+		s->chip = MOTIONSENSE_CHIP_BMI220;
+		s->drv = &bmi260_drv;
+		s->i2c_spi_addr_flags = BMI260_ADDR0_FLAGS;
+		/* Lid Gyro */
+		s = &motion_sensors[LID_GYRO];
+		CPRINTS("BMI220 - %s", s->name);
+		s->chip = MOTIONSENSE_CHIP_BMI220;
+		s->drv = &bmi260_drv;
+		s->i2c_spi_addr_flags = BMI260_ADDR0_FLAGS;
+#ifdef CONFIG_MAG_BMI_BMM150
+		/* Lid Mag */
+		s = &motion_sensors[LID_MAG];
+		CPRINTS("BMI220 - %s", s->name);
+		s->chip = MOTIONSENSE_CHIP_BMI220;
+		s->drv = &bmi260_drv;
+		s->i2c_spi_addr_flags = BMI260_ADDR0_FLAGS;
+#endif /* CONFIG_MAG_BMI_BMM150 */
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_detect_bmi220, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, board_detect_bmi220, HOOK_PRIO_DEFAULT + 1);
+#endif /* BOARD_KRANE */
+
 #endif /* VARIANT_KUKUI_NO_SENSORS */
+
+#ifdef SECTION_IS_RW
+static void motion_interrupt(enum gpio_signal signal)
+{
+#if defined(BOARD_KRANE)
+	if (is_bmi220_present)
+		bmi260_interrupt(signal);
+	else
+		bmi160_interrupt(signal);
+#elif !defined(VARIANT_KUKUI_NO_SENSORS)
+	bmi160_interrupt(signal);
+#endif /* BOARD_KRANE, !VARIANT_KUKUI_NO_SENSORS */
+
+}
+#endif /* SECTION_IS_RW */
 
 /*
  * Return if VBUS is sagging too low

coreboot/chrome-ec

Conflicts:
	board/kukui/board.h: not support CONFIG_I2C_XFER_LARGE_TRANSFER
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3156315
Reviewed-by: Eric Yilun Lin <yllin@google.com>
Reviewed-by: Ting Shen <phoenixshen@chromium.org>
(cherry picked from commit 70a4a8505a33b2ced5b9932ea4da15edbe3b396e)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3214015

board/kukui/board.c
@@ -56,6 +56,10 @@ static void gauge_interrupt(enum gpio_signal signal)
 	task_wake(TASK_ID_CHARGER);
 }
 
+#ifdef SECTION_IS_RW
+static void motion_interrupt(enum gpio_signal signal);
+#endif /* SECTION_IS_RW */
+
 #include "gpio_list.h"
 
 /******************************************************************************/
@@ -544,7 +548,62 @@ const unsigned int motion_sensor_count = ARRAY_SIZE(motion_sensors);
 const struct motion_sensor_t *motion_als_sensors[] = {
 	&motion_sensors[CLEAR_ALS],
 };
+
+#ifdef BOARD_KRANE
+
+static bool is_bmi220_present;
+
+static void board_detect_bmi220(void)
+{
+	int id = -1;
+	struct motion_sensor_t *s;
+
+	if (chipset_in_state(CHIPSET_STATE_ANY_OFF))
+		return;
+
+	/* Detect accelgyro chip */
+	bmi_read8(I2C_PORT_ACCEL, BMI260_ADDR0_FLAGS, BMI260_CHIP_ID, &id);
+	if (id == BMI220_CHIP_ID_MAJOR) {
+		is_bmi220_present = true;
+		/* Lid Accel*/
+		s = &motion_sensors[LID_ACCEL];
+		s->chip = MOTIONSENSE_CHIP_BMI220;
+		s->drv = &bmi260_drv;
+		s->i2c_spi_addr_flags = BMI260_ADDR0_FLAGS;
+		/* Lid Gyro */
+		s = &motion_sensors[LID_GYRO];
+		s->chip = MOTIONSENSE_CHIP_BMI220;
+		s->drv = &bmi260_drv;
+		s->i2c_spi_addr_flags = BMI260_ADDR0_FLAGS;
+#ifdef CONFIG_MAG_BMI_BMM150
+		/* Lid Mag */
+		s = &motion_sensors[LID_MAG];
+		s->chip = MOTIONSENSE_CHIP_BMI220;
+		s->drv = &bmi260_drv;
+		s->i2c_spi_addr_flags = BMI260_ADDR0_FLAGS;
+#endif /* CONFIG_MAG_BMI_BMM150 */
+	}
+}
+DECLARE_HOOK(HOOK_CHIPSET_STARTUP, board_detect_bmi220, HOOK_PRIO_DEFAULT);
+DECLARE_HOOK(HOOK_INIT, board_detect_bmi220, HOOK_PRIO_DEFAULT + 1);
+#endif /* BOARD_KRANE */
+
 #endif /* VARIANT_KUKUI_NO_SENSORS */
+
+#ifdef SECTION_IS_RW
+static void motion_interrupt(enum gpio_signal signal)
+{
+#if defined(BOARD_KRANE)
+	if (is_bmi220_present)
+		bmi260_interrupt(signal);
+	else
+		bmi160_interrupt(signal);
+#elif !defined(VARIANT_KUKUI_NO_SENSORS)
+	bmi160_interrupt(signal);
+#endif /* BOARD_KRANE, !VARIANT_KUKUI_NO_SENSORS */
+
+}
+#endif /* SECTION_IS_RW */
 
 /*
  * Return if VBUS is sagging too low