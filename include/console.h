a4be6742259d541dd8f037487cde676733094117
Mary Ruthven <mruthven@chromium.org>
2021-11-09 14:20:36 -0600

    Revert "console: improve command name length error message"
    This reverts commit 0ddb4813fcb6a0caf8d9248de15c2bd3d797e277.
    
    BUG=b:200823466
    TEST=make buildall -j
    
    Change-Id: I836c78c229768c2d377da90d757cadd35ad62570
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3273379
    Reviewed-by: Vadim Sukhomlinov <sukhomlinov@chromium.org>
    (cherry picked from commit aa07ab0df55dc179abaf21ba591355a02cf99026)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3296905

--- include/console.h
+++ include/console.h
@@ -164,9 +164,8 @@ void console_enable_output(void);
  * Register a console command handler.
  *
  * @param name          Command name; must not be the beginning of another
- *                      existing command name.  Must be less than 15 characters
- *                      long (excluding null terminator).  Note this is NOT in
- *                      quotes so it can be concatenated to form a struct name.
+ *                      existing command name.  Note this is NOT in quotes
+ *                      so it can be concatenated to form a struct name.
  * @param routine       Command handling routine, of the form
  *                      int handler(int argc, char **argv)
  * @param argdesc       String describing arguments to command; NULL if none.
@@ -202,8 +201,8 @@ void console_enable_output(void);
 /* This macro takes all possible args and discards the ones we don't use */
 #define _DCL_CON_CMD_ALL(NAME, ROUTINE, ARGDESC, HELP, FLAGS)		\
 	static const char __con_cmd_label_##NAME[] = #NAME;		\
-	_Static_assert(sizeof(__con_cmd_label_##NAME) < 16,		\
-		       "command name '" #NAME "' is too long");		\
+	struct size_check##NAME {					\
+		int field[2 * (sizeof(__con_cmd_label_##NAME) < 16) - 1]; }; \
 	const struct console_command __keep __no_sanitize_address	\
 	__con_cmd_##NAME						\
 	__attribute__((section(".rodata.cmds." #NAME))) =		\
