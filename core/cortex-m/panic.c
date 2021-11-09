deb7b9dbaf9c54b5c34dbc6269f178861c8e5992
Mary Ruthven <mruthven@chromium.org>
2021-11-09 17:34:35 -0600

    Revert "chip/npcx: Add support for pad-switching UART"
    This reverts commit ab9084fd2a70a505e0e43b39e0ad40876e4853fc.
    
    BUG=b:200823466
    TEST=make buildall -j
    
    Change-Id: I2bc23607e4658260bd83c8f9c85d24fe1f9131a2
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3273444
    Reviewed-by: Vadim Sukhomlinov <sukhomlinov@chromium.org>

--- core/cortex-m/panic.c
+++ core/cortex-m/panic.c
@@ -13,7 +13,6 @@
 #include "system.h"
 #include "task.h"
 #include "timer.h"
-#include "uart.h"
 #include "util.h"
 #include "watchdog.h"
 
@@ -337,9 +336,6 @@ void __keep report_panic(void)
 	pdata->cm.hfsr = CPU_NVIC_HFSR;
 	pdata->cm.dfsr = CPU_NVIC_DFSR;
 
-#ifdef CONFIG_UART_PAD_SWITCH
-	uart_reset_default_pad_panic();
-#endif
 	panic_data_print(pdata);
 #ifdef CONFIG_DEBUG_EXCEPTIONS
 	panic_show_process_stack(pdata);
