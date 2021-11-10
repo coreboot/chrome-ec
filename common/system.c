11d494211523b2e29ab7a49aa0478edfdbc859d0
Mary Ruthven <mruthven@chromium.org>
2021-11-09 19:43:05 -0600

    Revert "EFS: Allow EC to switch slot on reboot"
    This reverts commit 220c75bd1626c656234d6121af8068c42ff182f8.
    
    BUG=b:200823466
    TEST=make buildall -j
    
    Change-Id: I0f71ee2478cf4900b87e62101e7fb2ed2adba6cc
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3273447
    Reviewed-by: Vadim Sukhomlinov <sukhomlinov@chromium.org>
    (cherry picked from commit 7bba0b1edb635806250fe617043f93d2f8b0541c)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3296970

--- common/system.c
+++ common/system.c
@@ -1377,17 +1377,7 @@ enum ec_status host_command_reboot(struct host_cmd_handler_args *args)
 		/* Cancel pending reboot */
 		reboot_at_shutdown = EC_REBOOT_CANCEL;
 		return EC_RES_SUCCESS;
-	}
-
-	if (p.flags & EC_REBOOT_FLAG_SWITCH_RW_SLOT) {
-#ifdef CONFIG_VBOOT_EFS
-		if (system_set_active_copy(system_get_update_copy()))
-			CPRINTS("Failed to set active slot");
-#else
-		return EC_RES_INVALID_PARAM;
-#endif
-	}
-	if (p.flags & EC_REBOOT_FLAG_ON_AP_SHUTDOWN) {
+	} else if (p.flags & EC_REBOOT_FLAG_ON_AP_SHUTDOWN) {
 		/* Store request for processing at chipset shutdown */
 		reboot_at_shutdown = p.cmd;
 		return EC_RES_SUCCESS;
