3b0257411665461b0aadbaf5516a515d428b7ffe
Mary Ruthven <mruthven@chromium.org>
2021-11-12 13:22:18 -0600

    Revert "GPIO/IOEX/eSPI: Give different IO signals unique values"
    This reverts commit 9f392b0d616f6fec17d213736e6bf9f4217392e4.
    
    BUG=b:200823466
    TEST=make buildall -j
    
    Change-Id: If593116d7a0f09929a03652c500ab482b68dfc74
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3285754
    Reviewed-by: Vadim Sukhomlinov <sukhomlinov@chromium.org>
    (cherry picked from commit 6feec999556cc9d6622a3e05f896a4db5f5ceb24)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3297000

--- include/gpio_signal.h
+++ include/gpio_signal.h
@@ -6,28 +6,13 @@
 #ifndef __CROS_EC_GPIO_SIGNAL_H
 #define __CROS_EC_GPIO_SIGNAL_H
 
-#include "compile_time_macros.h"
-
-/*
- * There are 3 different IO signal types used by the EC.
- * Ensure they each use a unique range of values so we can tell them apart.
- * 1) Local GPIO => 0 to 0x0FFF
- * 2) IO expander GPIO => 0x1000 to 0x1FFF
- * 3) eSPI virtual wire signals (defined in include/espi.h) => 0x2000 to 0x2FFF
- */
-
 #define GPIO(name, pin, flags) GPIO_##name,
 #define UNIMPLEMENTED(name) GPIO_##name,
 #define GPIO_INT(name, pin, flags, signal) GPIO_##name,
 
-#define GPIO_SIGNAL_START 0 /* The first valid GPIO signal is 0 */
-
 enum gpio_signal {
 	#include "gpio.wrap"
-	GPIO_COUNT,
-	/* Ensure that sizeof gpio_signal is large enough for ioex_signal */
-	GPIO_LIMIT = 0x0FFF
+	GPIO_COUNT
 };
-BUILD_ASSERT(GPIO_COUNT < GPIO_LIMIT);
 
 #endif /* __CROS_EC_GPIO_SIGNAL_H */
