jackadam1981/ChromeEc

moonbuggy: supports ADS control handler function
Add ADS control handlers 15v/5v services.

BUG=b:211546881
BRANCH=cros/main
TEST=build BOARD PASS and EE check is PASS.

Change-Id: I8bd5db4572423a2f272789a67538e62a5bb08f85
Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

board/moonbuggy/board.h
@@ -169,10 +169,13 @@ enum temp_sensor_id {
 	TEMP_SENSOR_COUNT
 };
 
-
 /* Board specific handlers */
 void led_alert(int enable);
 void show_critical_error(void);
+
+/* Board ADS control handlers */
+void ads_5v_interrupt(enum gpio_signal signal);
+void ads_15v_interrupt(enum gpio_signal signal);
 
 /*
  * firmware config fields

coreboot/chrome-ec

Add ADS control handlers 12v/5v services.
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3369165
Reviewed-by: Joe Tessler <jrt@chromium.org>
Reviewed-by: Keith Short <keithshort@chromium.org>
Commit-Queue: Keith Short <keithshort@chromium.org>
(cherry picked from commit 83b28157895642cb6925e6354dbfbc3851379999)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3378990

board/moonbuggy/board.h
@@ -172,10 +172,13 @@ enum temp_sensor_id {
 	TEMP_SENSOR_COUNT
 };
 
-
 /* Board specific handlers */
 void led_alert(int enable);
 void show_critical_error(void);
+
+/* Board ADS control handlers */
+void ads_5v_interrupt(enum gpio_signal signal);
+void ads_12v_interrupt(enum gpio_signal signal);
 
 /*
  * firmware config fields