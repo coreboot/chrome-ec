05a101c4d6985e16166f57728d2667f6ec3adf0d
Scott Collyer <scollyer@google.com>
2022-03-16 12:12:08 -0700

    panqueque: Modify PSTATE to lock RO firmware
    This CL modifies the two config options required to lock the RO
    firmware partition.
    
    BUG=b:221112537
    BRANCH=quiche
    TEST=make BOARD=panqueque
    
    Signed-off-by: Scott Collyer <scollyer@google.com>
    Change-Id: I0ca6907bcb61cc056bc8ebd2dc2448e6faf937b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3530952
    Tested-by: Scott Collyer <scollyer@chromium.org>
    Reviewed-by: Diana Z <dzigterman@chromium.org>
    Commit-Queue: Scott Collyer <scollyer@chromium.org>

--- board/panqueque/board.h
+++ board/panqueque/board.h
@@ -17,8 +17,8 @@
  * CONFIG_FLASH_PSTATE_LOCKED must be defined in order to enable write protect
  * using option bytes WRP registers.
  */
-#define CONFIG_SYSTEM_UNLOCKED /* Allow dangerous commands while in dev. */
-#undef CONFIG_FLASH_PSTATE_LOCKED
+#undef CONFIG_SYSTEM_UNLOCKED /* Allow dangerous commands while in dev. */
+#define CONFIG_FLASH_PSTATE_LOCKED
 
 /* USB Type C and USB PD defines */
 #define USB_PD_PORT_HOST   0
