056c644eb371e9ab1b1e8757069d977f4e0fa3e7
Brian Granaghan <granaghan@google.com>
2022-03-31 01:31:52 +0000

    gsctool: Support flog on H1D3C.
    BUG=b:219038720
    TEST=gsctool -aL
    
    Change-Id: I1c0b417e61dcb6460345c2fcf8d43952ebbf65d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3573469
    Commit-Queue: Brian Granaghan <granaghan@google.com>
    Tested-by: Brian Granaghan <granaghan@google.com>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    (cherry picked from commit 6244dfa30c03811409e726931b6bffe98c58f29e)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3625962
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>
    (cherry picked from commit 81026987f32fbd9795af98fae6b0583d1a520402)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3632624

---
+++ extra/usb_updater/dauntless_event.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2022 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#ifndef __EXTRA_USB_UPDATER_DAUNTLESS_EVENT_H
+#define __EXTRA_USB_UPDATER_DAUNTLESS_EVENT_H
+
+#include <stdint.h>
+
+typedef struct {
+	uint64_t time;
+	uint32_t size;
+	uint32_t event_type;
+	uint8_t payload[0];
+} dt_event_t;
+
+union dt_entry_u {
+	uint8_t raw[256];
+	dt_event_t evt;
+};
+
+
+#endif // __EXTRA_USB_UPDATER_DAUNTLESS_EVENT_H
