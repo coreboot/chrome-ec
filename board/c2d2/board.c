d771c18ba9989eed5eca134d27e1973a63bded74
Mary Ruthven <mruthven@chromium.org>
2022-08-02 06:58:16 -0700

    c2d2: increase the usart1_to_usb size
    Set the usart1_to_usb size to 1024 to match the other consoles.
    
    BUG=b:240718978
    BRANCH=servo
    TEST=none
    
    Change-Id: I0b57677365eca98684a883482562d5e9ad2f2ced
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3805279
    Commit-Queue: Ziting Shen <zitingshen@google.com>
    Reviewed-by: Ziting Shen <zitingshen@google.com>
    (cherry picked from commit e55cc58e28694bb4c01189466355b79c0744e434)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3807128

--- board/c2d2/board.c
+++ board/c2d2/board.c
@@ -219,7 +219,7 @@ int usb_i2c_board_is_enabled(void) { return 1; }
 static struct usart_config const usart1;
 struct usb_stream_config const usart1_usb;
 
-static struct queue const usart1_to_usb = QUEUE_DIRECT(128, uint8_t,
+static struct queue const usart1_to_usb = QUEUE_DIRECT(1024, uint8_t,
 	usart1.producer, usart1_usb.consumer);
 static struct queue const usb_to_usart1 = QUEUE_DIRECT(64, uint8_t,
 	usart1_usb.producer, usart1.consumer);
