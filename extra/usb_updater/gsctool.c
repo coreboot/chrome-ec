caf0666a392c3601ec79e324d52187e17abbae61
Judy Hsiao <judyhsiao@google.com>
2022-08-12 02:06:44 +0000

    Revert "apro: add new return codes"
    This reverts commit c1f5a5481f1121e2f408055f04906205b779dc91.
    
    Reason for revert: b:242249503
    ```
    gsctool.c:2382:2: error: duplicate case value
     2382 |  case AP_RO_UNSUPPORTED_NOT_TRIGGERED:
          |  ^~~~
    gsctool.c:2373:2: note: previously used here
     2373 |  case AP_RO_UNSUPPORTED_NOT_TRIGGERED:
          |  ^~~~
    ```
    
    Original change's description:
    > apro: add new return codes
    >
    > Add new ap_ro_integrity_check return codes. The existing AP_RO_PASS (2)
    > return code doesn't verify the GBB. Rename it to
    > AP_RO_PASS_UNVERIFIED_GBB. Shimless RMA should only treat
    > AP_RO_PASS (7) as a pass. Nothing returns this right now.
    > This CL also adds AP_RO_FAIL_CLEARED(8) and AP_RO_IN_PROGRESS(9).
    > AP_RO_IN_PROGRESS is used if AP RO verification is ongoing.
    > AP_RO_FAIL_CLEARED will be used in a followup CL.
    >
    > old:
    > 2 - AP_RO_PASS
    >
    > new:
    > 2 - AP_RO_PASS_UNVERIFIED_GBB
    > ...
    > 7 - AP_RO_PASS
    > 8 - AP_RO_FAIL_CLEARED
    > 9 - AP_RO_IN_PROGRESS
    >
    > This saves 8 bytes since it also shortens a print message. The remaining
    > space changes from 5804 to 5812 bytes.
    >
    > BUG=b:234497234
    > TEST=make buildall -j
    >
    > Change-Id: I8d19a411c2534236c9defa82291872420c19a15b
    > Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3805819
    > Reviewed-by: Andrey Pronin <apronin@chromium.org>
    > Commit-Queue: Andrey Pronin <apronin@chromium.org>
    
    Bug=b:234497234, b:242249503
    Change-Id: I63ebc6a1343410e3b2a5ab0684a8a533553ec1ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/3826713
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Auto-Submit: Judy Hsiao <judyhsiao@google.com>
    Tested-by: Judy Hsiao <judyhsiao@google.com>
    Owners-Override: Judy Hsiao <judyhsiao@google.com>
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Andrey Pronin <apronin@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -2358,9 +2358,6 @@ static int process_get_apro_boot_status(struct transfer_descriptor *td)
 	case AP_RO_PASS:
 		printf("pass\n");
 		break;
-	case AP_RO_PASS_UNVERIFIED_GBB:
-		printf("pass - unverified gbb!\n");
-		break;
 	case AP_RO_FAIL:
 		printf("FAIL\n");
 		break;
@@ -2369,15 +2366,6 @@ static int process_get_apro_boot_status(struct transfer_descriptor *td)
 		break;
 	case AP_RO_UNSUPPORTED_UNKNOWN:
 		printf("not supported\ntriggered: unknown\n");
-		break;
-	case AP_RO_UNSUPPORTED_NOT_TRIGGERED:
-		printf("not supported\ntriggered: no\n");
-		break;
-	case AP_RO_IN_PROGRESS:
-		printf("in progress.");
-		break;
-	case AP_RO_FAIL_CLEARED:
-		printf("fail CLEARED!");
 		break;
 	case AP_RO_UNSUPPORTED_NOT_TRIGGERED:
 		printf("not supported\ntriggered: no\n");
