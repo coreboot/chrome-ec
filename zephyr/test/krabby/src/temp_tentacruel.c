8cbadbcec140cb01845b83f5d2711cb75a9f25a1
Eric Yilun Lin <yllin@chromium.org>
2022-11-10 10:17:21 +0800

    Revert "tentacruel : Setup thermal Policy"
    This reverts commit 06b578f084504f721a4ebf05d8c682bd824e88c2.
    
    Reason for revert: To apply fast-forwarded commits.
    
    Change-Id: Ie5867de8f493678e1608e14832364e49e2ea7a07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4019022
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>

--- zephyr/test/krabby/src/temp_tentacruel.c
+++ zephyr/test/krabby/src/temp_tentacruel.c
@@ -21,6 +21,7 @@
 #define ADC_DEVICE_NODE DT_NODELABEL(adc0)
 #define CHARGER_TEMP TEMP_SENSOR_ID(DT_NODELABEL(temp_charger))
 #define ORIGINAL_CURRENT 5000
+#define TEMP_THRESHOLD 55
 
 struct charge_state_data curr;
 static int fake_voltage;
@@ -33,7 +34,7 @@ static uint16_t current_table[] = {
 	3600,
 	3000,
 	2400,
-	1600,
+	1800,
 };
 
 int setup_faketemp(int fake_voltage)
@@ -55,7 +56,7 @@ static void ignore_first_minute(void)
 
 ZTEST(temp_tentacruel, test_decrease_current)
 {
-	fake_voltage = 411;
+	fake_voltage = 376;
 	curr.batt.flags |= BATT_FLAG_RESPONSIVE;
 	count = 0;
 
@@ -79,7 +80,7 @@ ZTEST(temp_tentacruel, test_decrease_current)
 
 ZTEST(temp_tentacruel, test_increase_current)
 {
-	fake_voltage = 446;
+	fake_voltage = 380;
 	curr.batt.flags |= BATT_FLAG_RESPONSIVE;
 	count = 3;
 
