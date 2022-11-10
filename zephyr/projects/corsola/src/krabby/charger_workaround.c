429ee43557ab572902841aa125b9dacf1a16b274
Eric Yilun Lin <yllin@chromium.org>
2022-11-10 10:18:09 +0800

    Revert "rt9490: disable the force B-FET on experimental feature"
    This reverts commit d7dd2d73f0757fe97296fb9dadf792bd539659bc.
    
    Reason for revert: To apply fast-forwarded commits.
    
    Change-Id: Ib5fef0d8e6902fad26dedd30870ea196e43af61a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4019694
    Reviewed-by: Sung-Chi Li <lschyi@chromium.org>
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>

--- zephyr/projects/corsola/src/krabby/charger_workaround.c
+++ zephyr/projects/corsola/src/krabby/charger_workaround.c
@@ -19,6 +19,14 @@
 BUILD_ASSERT(IS_ENABLED(CONFIG_BOARD_KRABBY) ||
 	     IS_ENABLED(CONFIG_BOARD_TENTACRUEL) || IS_ENABLED(CONFIG_TEST));
 
+static void enter_hidden_mode(void)
+{
+	i2c_write8(chg_chips[CHARGER_SOLO].i2c_port,
+		   chg_chips[CHARGER_SOLO].i2c_addr_flags, 0xF1, 0x69);
+	i2c_write8(chg_chips[CHARGER_SOLO].i2c_port,
+		   chg_chips[CHARGER_SOLO].i2c_addr_flags, 0xF2, 0x96);
+}
+
 /* b/194967754#comment5: work around for IBUS ADC unstable issue */
 static void ibus_adc_workaround(void)
 {
@@ -30,7 +38,8 @@ static void ibus_adc_workaround(void)
 		    chg_chips[CHARGER_SOLO].i2c_addr_flags,
 		    RT9490_REG_ADC_CHANNEL0, RT9490_VSYS_ADC_DIS, MASK_SET);
 
-	rt9490_enable_hidden_mode(CHARGER_SOLO, true);
+	enter_hidden_mode();
+
 	/* undocumented registers... */
 	i2c_write8(chg_chips[CHARGER_SOLO].i2c_port,
 		   chg_chips[CHARGER_SOLO].i2c_addr_flags, 0x52, 0xC4);
@@ -38,7 +47,6 @@ static void ibus_adc_workaround(void)
 	i2c_update8(chg_chips[CHARGER_SOLO].i2c_port,
 		    chg_chips[CHARGER_SOLO].i2c_addr_flags,
 		    RT9490_REG_ADC_CHANNEL0, RT9490_VSYS_ADC_DIS, MASK_CLR);
-	rt9490_enable_hidden_mode(CHARGER_SOLO, false);
 }
 
 /* b/214880220#comment44: lock i2c at 400khz */
@@ -48,14 +56,13 @@ static void i2c_speed_workaround(void)
 		return;
 	}
 
-	rt9490_enable_hidden_mode(CHARGER_SOLO, true);
+	enter_hidden_mode();
 	/* Set to Auto mode, default run at 400kHz */
 	i2c_write8(chg_chips[CHARGER_SOLO].i2c_port,
 		   chg_chips[CHARGER_SOLO].i2c_addr_flags, 0x71, 0x22);
 	/* Manually select for 400kHz, valid only when 0x71[7] == 1 */
 	i2c_write8(chg_chips[CHARGER_SOLO].i2c_port,
 		   chg_chips[CHARGER_SOLO].i2c_addr_flags, 0xF7, 0x14);
-	rt9490_enable_hidden_mode(CHARGER_SOLO, false);
 }
 
 static void eoc_deglitch_workaround(void)
