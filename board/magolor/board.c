jackadam1981/ChromeEc

magolor: delay one second when charger enter low power mode
add delay one second waiting for charger enter low power mode

BUG=b:258301166
BRANCH=none
TEST=checking ec console don't power on until enter hibernate

Change-Id: I56b5e1f84f32dc4dea69b0c53b45980cb9b741e7
Signed-off-by: Ben Chen <ben.chen2@quanta.corp-partner.google.com>

board/magolor/board.c
@@ -415,6 +415,8 @@ void board_hibernate(void)
 	if (board_get_charger_chip_count() > 1)
 		raa489000_hibernate(1, true);
 	raa489000_hibernate(0, true);
+
+	msleep(1000); /* Wait for charger to entering low power mode */
 }
 
 void board_reset_pd_mcu(void)

coreboot/chrome-ec

BUG=b:259155418
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4050016
Reviewed-by: Aseda Aboagye <aaboagye@chromium.org>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
(cherry picked from commit bd967de4a3322c2539ea86287fcb8e68e3ba9cff)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4127240
Reviewed-by: Ivan Chen <yulunchen@google.com>
Auto-Submit: Shou-Chieh Hsu <shouchieh@chromium.org>
Tested-by: Shou-Chieh Hsu <shouchieh@chromium.org>
Commit-Queue: Ivan Chen <yulunchen@google.com>

board/magolor/board.c
@@ -419,6 +419,8 @@ void board_hibernate(void)
 	if (board_get_charger_chip_count() > 1)
 		raa489000_hibernate(1, true);
 	raa489000_hibernate(0, true);
+
+	msleep(1000); /* Wait for charger to enter low power mode */
 }
 
 void board_reset_pd_mcu(void)