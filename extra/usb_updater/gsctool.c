be345deb21a9ba4d756e78e211d6a9848a986674
Dennis Kempin <denniskempin@google.com>
2023-01-06 22:59:33 +0000

    Revert "gsctool: Add timestamp unreliable flag for dauntless."
    This reverts commit 658729d9e338f6a970e46e69dc4c638d8ac7ccae.
    
    Reason for revert: Breaks CQ builds. See http://b/264704727
    
    Original change's description:
    > gsctool: Add timestamp unreliable flag for dauntless.
    >
    > This flag is set for logs that occur between a cold boot and AP
    > settings the base time. These logs will start with previous_timestamp+1
    > and have 1<<64 set to denote that the actual base time was unknown.
    >
    > BUG=b:260779816
    > TEST=gsctool -a -L --dauntless with and without the unreliable timestamp
    > change in firmware
    >
    > Signed-off-by: Brian Granaghan <granaghan@google.com>
    > Change-Id: Iac279a948c7f34ede711c3c334ec1410b34f040c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4112943
    > Reviewed-by: Edward Hill <ecgh@chromium.org>
    > Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    
    Bug: b:260779816
    Change-Id: I9a497cc9e0f996182f7f18f93646b963d9cf5e31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4144680
    Tested-by: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Brian Granaghan <granaghan@google.com>
    Owners-Override: Dennis Kempin <denniskempin@google.com>
    Commit-Queue: Edward Hill <ecgh@chromium.org>
    Commit-Queue: Brian Granaghan <granaghan@google.com>
    Auto-Submit: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Edward Hill <ecgh@chromium.org>

--- extra/usb_updater/gsctool.c
+++ extra/usb_updater/gsctool.c
@@ -3549,7 +3549,6 @@ struct parsed_flog_entry {
 	uint64_t raw_timestamp;
 	time_t timestamp;
 	uint32_t event_type;
-	bool timestamp_reliable;
 };
 
 static int pop_flog_dt(struct transfer_descriptor *td,
@@ -3574,10 +3573,7 @@ static int pop_flog_dt(struct transfer_descriptor *td,
 	memcpy(parsed_entry->payload, entry.evt.payload,
 	       parsed_entry->payload_size);
 	parsed_entry->raw_timestamp = entry.evt.time;
-	parsed_entry->timestamp =
-	    parsed_entry->raw_timestamp & ~(1ULL << 63)) / 1000;
-	parsed_entry->timestamp_reliable =
-	    (parsed_entry->raw_timestamp >> 63) == 0;
+	parsed_entry->timestamp = parsed_entry->raw_timestamp / 1000;
 	return rv;
 
 }
@@ -3604,7 +3600,6 @@ static int pop_flog(struct transfer_descriptor *td,
 	       parsed_entry->payload_size);
 	parsed_entry->raw_timestamp = entry.r.timestamp;
 	parsed_entry->timestamp = entry.r.timestamp;
-	parsed_entry->timestamp_reliable = true;
 	return rv;
 
 }
@@ -3669,8 +3664,6 @@ static int process_get_flog(struct transfer_descriptor *td, uint64_t prev_stamp,
 		}
 		for (i = 0; i < entry.payload_size; i++)
 			printf(" %02x", entry.payload[i]);
-		if (entry.timestamp_reliable == false)
-			printf(" -- TIMESTAMP UNRELIABLE!");
 		printf("\n");
 		retries = max_retries;
 	}
