3f725a93eb6a78c9dc4935a98a3e4d0bef959cdd
Andrey Pronin <apronin@google.com>
2023-02-10 18:41:59 -0800

    cr50: add get_tpm_pcr_value method
    This CL adds get_tpm_pcr_value() for reading current PCR values.
    
    BUG=b:267674073
    TEST=build
    
    Cq-Depend: chromium:4242409
    Change-Id: I632cb13a3f44130f29b72cc4c22ea97a692ffad8
    Signed-off-by: Andrey Pronin <apronin@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4252001
    Tested-by: Andrey Pronin <apronin@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>

--- board/cr50/tpm_nvmem_ops.h
+++ board/cr50/tpm_nvmem_ops.h
@@ -7,6 +7,10 @@
 #ifndef __EC_BOARD_CR50_TPM_NVMEM_OPS_H
 #define __EC_BOARD_CR50_TPM_NVMEM_OPS_H
 
+#ifndef SHA256_DIGEST_SIZE
+#define SHA256_DIGEST_SIZE 32
+#endif /* SHA256_DIGEST_SIZE */
+
 #define TPM_ORDERLY_STATE_SIZE 512
 
 enum tpm_read_rv {
@@ -51,5 +55,11 @@ size_t read_tpm_nvmem_size(uint16_t obj_index);
 void tpm_orderly_state_capture(char copy[TPM_ORDERLY_STATE_SIZE]);
 
 void tpm_orderly_state_restore(const char copy[TPM_ORDERLY_STATE_SIZE]);
+
+/*
+ * Read value of the specified PCR in SHA-256 bank into a buffer,
+ * or return false if no such PCR
+ */
+bool get_tpm_pcr_value(uint32_t pcr_num, uint8_t value[SHA256_DIGEST_SIZE]);
 
 #endif  /* ! __EC_BOARD_CR50_TPM_NVMEM_OPS_H */
