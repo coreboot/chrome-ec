5cb22745dc1dc583b84d873631096dc061fb09b9
Mary Ruthven <mruthven@chromium.org>
2023-03-03 08:16:44 -0800

    cr50:DBG: remove rarely used console commands
    DBG images are running low on space. This change removes some of the
    more rarely used console commands. It saves 2352 bytes.
    
    This disables sleepmask, timerinfo, i2cxfer, history, and i2cscan in
    DBG images. If someone needs to reenable them for a build, they can
    comment out the undefs.
    
    BUG=none
    TEST=make buildall -j
    
    Change-Id: I40115d6e2769fae4e489eb926778c38c94b66cd3
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4307437
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    (cherry picked from commit 84267aafea80423ed40024006e0a05a7acc03fc9)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4481586
    Reviewed-by: Jett Rink <jettrink@chromium.org>

--- board/cr50/board.h
+++ board/cr50/board.h
@@ -127,6 +127,12 @@
 #define CONFIG_CCD_OPEN_PREPVT
 
 #ifdef CR50_DEV
+/* Remove console commands to save space. */
+#undef CONFIG_CMD_SLEEPMASK
+#undef CONFIG_CMD_TIMERINFO
+#undef CONFIG_CONSOLE_HISTORY
+#undef CONFIG_CMD_I2C_SCAN
+#undef CONFIG_CMD_I2C_XFER
 /* Enable unsafe dev features for CCD in dev builds */
 #define CONFIG_CASE_CLOSED_DEBUG_V1_UNSAFE
 #define CONFIG_CMD_FLASH_LOG
