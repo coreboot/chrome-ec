jackadam1981/ChromeEc

zephyr: Support PLATFORM_EC_CMD_POWERINDEBUG flag
Support PLATFORM_EC_CMD_POWERINDEBUG flag to disable `powerindebug` on
zephyr.

BUG=b:272185086
BRANCH=none
TEST=zmake build geralt
TEST=disable flag and check powerindebug

Signed-off-by: Knox Chiou <knoxchiou@google.com>
Change-Id: I89af4161b6bcc351e750d2716e0cc594cfe74814

zephyr/shim/include/config_chip.h
@@ -2024,6 +2024,11 @@ extern char mock_jump_data[CONFIG_PLATFORM_EC_PRESERVED_END_OF_RAM_SIZE];
 #define CONFIG_CMD_HCDEBUG
 #endif
 
+#undef CONFIG_CMD_POWERINDEBUG
+#ifdef PLATFORM_EC_CMD_POWERINDEBUG
+#define CONFIG_CMD_POWERINDEBUG
+#endif
+
 #undef CONFIG_CMD_USB_PD_PE
 #ifdef CONFIG_PLATFORM_EC_CONSOLE_CMD_USB_PD_PE
 #define CONFIG_CMD_USB_PD_PE

coreboot/chrome-ec

Support PLATFORM_EC_CMD_POWERINDEBUG flag for `powerindebug` on
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4383291
Tested-by: Knox Chiou <knoxchiou@chromium.org>
Commit-Queue: Knox Chiou <knoxchiou@chromium.org>
Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>
Reviewed-by: Keith Short <keithshort@chromium.org>
Code-Coverage: Knox Chiou <knoxchiou@chromium.org>
(cherry picked from commit 9a77bb3d4aa0135669a1746552b03042bcc3b065)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4576375
Commit-Queue: Chao Gui <chaogui@google.com>
Reviewed-by: Matt Wang <matt_wang@compal.corp-partner.google.com>
Reviewed-by: Logan Liao <logan_liao@compal.corp-partner.google.com>
Reviewed-by: Chao Gui <chaogui@google.com>
Tested-by: Logan Liao <logan_liao@compal.corp-partner.google.com>
Reviewed-by: Diana Z <dzigterman@chromium.org>

zephyr/shim/include/config_chip.h
@@ -2024,6 +2024,11 @@ extern char mock_jump_data[CONFIG_PLATFORM_EC_PRESERVED_END_OF_RAM_SIZE];
 #define CONFIG_CMD_HCDEBUG
 #endif
 
+#undef CONFIG_CMD_POWERINDEBUG
+#ifdef CONFIG_PLATFORM_EC_CMD_POWERINDEBUG
+#define CONFIG_CMD_POWERINDEBUG
+#endif
+
 #undef CONFIG_CMD_USB_PD_PE
 #ifdef CONFIG_PLATFORM_EC_CONSOLE_CMD_USB_PD_PE
 #define CONFIG_CMD_USB_PD_PE