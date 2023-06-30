8decb286a272a998155bdc1c23e9a1af95e01867
Vadim Bendebury <vbendeb@chromium.org>
2023-06-30 13:43:37 -0700

    Add fimrware_builder.py necessary for CQ
    This is a modified copy of firmware_builder.py from the cr50_stab
    branch.
    
    Note that there is no output generated by running with 'build' option
    and no test is provided, the goal of CQ runs is to ensure that gsctool
    builds properly.
    
    BUG=b:289551698
    TEST=running './firmware_builder.py --metrics /tmp/metrics build'
         succeeds and results in generating of /tmp/metrics containing an
         empty JSON object.
    
    Change-Id: I5b3837536fcc2872bfb1b45a2541f26dad4d8c60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4661791
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    Tested-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-by: Chris Gerber <gerb@google.com>
    Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>

---
+++ firmware_builder.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Build, bundle, or test all of the EC boards.
+
+This is the entry point for the custom firmware builder workflow recipe.  It
+gets invoked by chromite/api/controller/firmware.py.
+"""
+
+import argparse
+import multiprocessing
+import os
+import subprocess
+import sys
+
+# pylint: disable=import-error
+from google.protobuf import json_format
+# TODO(crbug/1181505): Code outside of chromite should not be importing from
+# chromite.api.gen.  Import json_format after that so we get the matching one.
+from chromite.api.gen.chromite.api import firmware_pb2
+
+
+def build(opts):
+    """Builds all targets in extra/usb_updater
+
+    Note that when we are building unit tests for code coverage, we don't
+    need this step. It builds EC **firmware** targets, but unit tests with
+    code coverage are all host-based. So if the --code-coverage flag is set,
+    we don't need to build the firmware targets and we can return without
+    doing anything but creating the metrics file and giving an informational
+    message.
+    """
+    # TODO(b/169178847): Add appropriate metric information
+    metrics = firmware_pb2.FwBuildMetricList()
+    with open(opts.metrics, 'w') as f:
+        f.write(json_format.MessageToJson(metrics))
+
+    cmd = ['make', 'BOARD=cr50', '-j{}'.format(opts.cpus), '-C',
+           'extra/usb_updater']
+    print(f'# Running {" ".join(cmd)}.')
+    subprocess.run(cmd,
+                   cwd=os.path.dirname(__file__),
+                   check=True)
+
+
+def bundle(opts):
+    pass # Nothing to bundle.
+
+
+def test(opts):
+    pass # Nothing to test
+
+
+def main(args):
+    """Builds, bundles, or tests all of the EC targets.
+
+    Additionally, the tool reports build metrics.
+    """
+    opts = parse_args(args)
+
+    if not hasattr(opts, 'func'):
+        print('Must select a valid sub command!')
+        return -1
+
+    # Run selected sub command function
+    try:
+        opts.func(opts)
+    except subprocess.CalledProcessError:
+        return 1
+    else:
+        return 0
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '--cpus',
+        default=multiprocessing.cpu_count(),
+        help='The number of cores to use.',
+    )
+
+    parser.add_argument(
+        '--metrics',
+        dest='metrics',
+        required=True,
+        help='File to write the json-encoded MetricsList proto message.',
+    )
+
+    parser.add_argument(
+        '--metadata',
+        required=False,
+        help='Full pathname for the file in which to write build artifact '
+        'metadata.',
+    )
+
+    parser.add_argument(
+        '--output-dir',
+        required=False,
+        help='Full pathanme for the directory in which to bundle build '
+        'artifacts.',
+    )
+
+    parser.add_argument(
+        '--code-coverage',
+        required=False,
+        action='store_true',
+        help='Build host-based unit tests for code coverage.',
+    )
+
+    parser.add_argument(
+        '--bcs-version',
+        dest='bcs_version',
+        default='',
+        required=False,
+        # TODO(b/180008931): make this required=True.
+        help='BCS version to include in metadata.',
+    )
+
+    # Would make this required=True, but not available until 3.7
+    sub_cmds = parser.add_subparsers()
+
+    build_cmd = sub_cmds.add_parser('build',
+                                    help='Builds all firmware targets')
+    build_cmd.set_defaults(func=build)
+
+    build_cmd = sub_cmds.add_parser('bundle',
+                                    help='Does nothing, kept for compatibility')
+    build_cmd.set_defaults(func=bundle)
+
+    test_cmd = sub_cmds.add_parser('test',
+                                   help='Does nothing, kept for compatibility')
+    test_cmd.set_defaults(func=test)
+
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
