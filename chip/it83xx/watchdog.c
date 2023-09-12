b5b9c8b794ef82fba5511c1209c501fb7f16dad0
Rob Barnes <robbarnes@google.com>
2023-09-12 16:40:07 -0600

    nds32/riscv: Set WATCHDOG_WARN panic reason on watchdog warning
    When a watchdog warning occurs, fully populate the panic data using the
    PANIC_SW_WATCHDOG_WARN reason. Include the program counter and current
    task. Remove special case for preserving the program counter since
    this can result in stale program counter on hard hangs.
    
    The program counter field in the panic info will no longer be populated
    on watchdogs. Instead the program counter will be stored int the `info`
    register. Also the task id will be stored in the `exception` register.
    This aligns with how cortex-m watchdogs are handled on CrOS EC.
    
    BUG=b:300164756
    TEST=Tested watchdog and hang on kracko(nds32) and tomato(riscv)
    
    Change-Id: I0b67032bb12564fb636eafdb47f537256fcf6040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4859537
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Tested-by: Rob Barnes <robbarnes@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Rob Barnes <robbarnes@google.com>
    (cherry picked from commit 74739ffbe3b113f5187d860eea7139fc13db3326)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5014634
    Tested-by: Eric Yilun Lin <yllin@google.com>
    Commit-Queue: Eric Yilun Lin <yllin@google.com>
    Reviewed-by: Rob Barnes <robbarnes@google.com>
    Reviewed-by: Ting Shen <phoenixshen@chromium.org>

--- chip/it83xx/watchdog.c
+++ chip/it83xx/watchdog.c
@@ -39,12 +39,27 @@ static void watchdog_set_warning_timer(int32_t ms, int init)
 void watchdog_warning_irq(void)
 {
 #ifdef CONFIG_SOFTWARE_PANIC
-	struct panic_data * const pdata_ptr = get_panic_data_write();
-
+	/*
+	 * Why we fill ipc/mepc here in the watchdog bark/warning interrupt:
+	 *
+	 * In ITE, a full watchdog bite results in an EC reset that bypasses all
+	 * exception handlers. We save the program counter and current task now
+	 * (during a warning) before a full watchdog bite occurs so it is
+	 * accessible after the bite.
+	 *
+	 * Why we set set PANIC_SW_WATCHDOG_WARN reason:
+	 *
+	 * The PANIC_SW_WATCHDOG_WARN reason will be changed to a regular
+	 * PANIC_SW_WATCHDOG in system_common_pre_init if a watchdog reset
+	 * actually occurs. If no watchdog reset occurs, this watchdog warning
+	 * panic may still be collected by the kernel and handled as a
+	 * non-fatal EC panic.
+	 */
 #if defined(CHIP_CORE_NDS32)
-	pdata_ptr->nds_n8.ipc = get_ipc();
+	panic_set_reason(PANIC_SW_WATCHDOG_WARN, get_ipc(), task_get_current());
 #elif defined(CHIP_CORE_RISCV)
-	pdata_ptr->riscv.mepc = get_mepc();
+	panic_set_reason(PANIC_SW_WATCHDOG_WARN, get_mepc(),
+			 task_get_current());
 #endif
 #endif
 	/* clear interrupt status */
