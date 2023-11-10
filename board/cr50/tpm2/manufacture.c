474397c0388573bc108511a594727da58043b1ac
Vadim Sukhomlinov <sukhomlinov@google.com>
2023-11-10 15:58:44 -0800

    cr50: add debugging for EPS value before/after command execution
    Report zero-length EPS value either in GP or NV state.
    
    Minor refactoring to isolate access to NV cached EPS in a separate
    function and provide access to gp.EPSeed.t.size in tpm_registers.c
    This also simplifies code in new_nvmem.c a bit.
    
    BUG=b:262324344
    TEST=make; tpm_test.py -p ;  manually tested that EPS size is printed
    when turned on unconditionally
    
    Change-Id: Ib6fd13eb096331e75d68a0a19d5eef8778b54071
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5020931
    Auto-Submit: Vadim Sukhomlinov <sukhomlinov@chromium.org>
    Tested-by: Vadim Sukhomlinov <sukhomlinov@chromium.org>
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Vadim Sukhomlinov <sukhomlinov@chromium.org>
    Reviewed-by: Vadim Sukhomlinov <sukhomlinov@chromium.org>
    Code-Coverage: Vadim Sukhomlinov <sukhomlinov@chromium.org>

--- board/cr50/tpm2/manufacture.c
+++ board/cr50/tpm2/manufacture.c
@@ -5,6 +5,7 @@
 
 #include "console.h"
 #include "tpm_manufacture.h"
+#include "tpm_nvmem_ops.h"
 
 #include "Global.h"
 #include "NV_fp.h"
@@ -22,16 +23,8 @@ int tpm_manufactured(void)
 	uint32_t nv_ram_index;
 	const uint32_t rsa_ek_nv_index = EK_CERT_NV_START_INDEX;
 	const uint32_t ecc_ek_nv_index = EK_CERT_NV_START_INDEX + 1;
-	NV_RESERVED_ITEM ri;
-	/*
-	 * Make sure we read length of TPM2B struct properly. Note, it is
-	 * stored in machine format, so no ending conversion is needed.
-	 */
-	uint16_t eps_seed_len; /* Shall match TPM2B size type. */
+	uint16_t eps_seed_len;
 
-	BUILD_ASSERT(sizeof(gp.EPSeed.t.size) == sizeof(eps_seed_len));
-
-	NvGetReserved(NV_EP_SEED, &ri);
 	/*
 	 * If nvram_index (value written at NV RAM offset of zero) is all
 	 * ones, or either endorsement certificate is not installed or EPS seed
@@ -40,10 +33,9 @@ int tpm_manufactured(void)
 	 * Thus, wiping flash NV ram allows to re-manufacture the chip.
 	 */
 	_plat__NvMemoryRead(0, sizeof(nv_ram_index), &nv_ram_index);
-	_plat__NvMemoryRead(ri.offset, sizeof(eps_seed_len), &eps_seed_len);
+	eps_seed_len = tpm_nv_eps_len();
 
 	if ((nv_ram_index == ~0) || (eps_seed_len < PRIMARY_SEED_SIZE) ||
-	    (eps_seed_len > ri.size - sizeof(gp.EPSeed.t.size)) ||
 	    (NvIsUndefinedIndex(rsa_ek_nv_index) == TPM_RC_SUCCESS) ||
 	    (NvIsUndefinedIndex(ecc_ek_nv_index) == TPM_RC_SUCCESS)) {
 		CPRINTF("%s: NOT manufactured\n", __func__);
