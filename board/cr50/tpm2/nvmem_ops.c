63cb86315b608c2f27dabcb93d1177a9ebb734b3
Vadim Sukhomlinov <sukhomlinov@google.com>
2023-11-10 16:20:21 -0800

    cr50: wipe secrets with randoms, don't touch EPS
    EPS seed is same for different users, it persists in NV and there is
    no reason to wipe it.
    
    Destroy over secrets by overwriting with randoms if possible, and don't
    touch length. This may help to avoid corner cases when call to
    nvmem_wipe_cache() somehow clashes with TPM2_Clear or something else
    which would dump these NV spaces to NVmem, so make it closer to what
    TPM2_Clear() does.
    
    These spaces are only recreated on manufacturing in call to
    HierarchyPreInstall_Init().
    
    BUG=b:262324344
    TEST=make; check that image works
    
    Change-Id: I9f32d776a9362befbbecf9ade3f88e4f28535ee7
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5021018
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    (cherry picked from commit a6ef7bc05eb5691a240dd9a82caf3d2350dfed27)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5241365
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    (cherry picked from commit 3e6f69f6447d9394cd6078e16d130cbe66f15241)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5368994
    Auto-Submit: Mary Ruthven <mruthven@chromium.org>

--- board/cr50/tpm2/nvmem_ops.c
+++ board/cr50/tpm2/nvmem_ops.c
@@ -3,9 +3,17 @@
  * found in the LICENSE file.
  */
 
+#include "dcrypto.h"
+
 #include "Global.h"
 #include "NV_fp.h"
 #include "util.h"
+
+static void fill_rand_or_clear(void *buf, size_t size)
+{
+	if (!fips_rand_bytes(buf, size))
+		memset(buf, 0, size);
+}
 
 void nvmem_wipe_cache(void)
 {
@@ -18,15 +26,20 @@ void nvmem_wipe_cache(void)
 	NvSelectivelyInvalidateCache(allowlist_range);
 
 	/*
-	 * Wipe some confidential persistent data
+	 * Wipe some confidential persistent data, same as TPM2_Clear().
 	 */
 	memset(&gp.ownerAuth, 0, sizeof(gp.ownerAuth));
 	memset(&gp.endorsementAuth, 0, sizeof(gp.endorsementAuth));
 	memset(&gp.lockoutAuth, 0, sizeof(gp.lockoutAuth));
-	memset(&gp.EPSeed, 0, sizeof(gp.EPSeed));
-	memset(&gp.SPSeed, 0, sizeof(gp.SPSeed));
-	memset(&gp.PPSeed, 0, sizeof(gp.PPSeed));
-	memset(&gp.phProof, 0, sizeof(gp.phProof));
-	memset(&gp.shProof, 0, sizeof(gp.shProof));
-	memset(&gp.ehProof, 0, sizeof(gp.ehProof));
+
+	gp.SPSeed.t.size = PRIMARY_SEED_SIZE;
+	fill_rand_or_clear(gp.SPSeed.t.buffer, sizeof(gp.SPSeed.t.buffer));
+	gp.PPSeed.t.size = PRIMARY_SEED_SIZE;
+	fill_rand_or_clear(gp.PPSeed.t.buffer, sizeof(gp.PPSeed.t.buffer));
+	gp.phProof.t.size = PROOF_SIZE;
+	fill_rand_or_clear(gp.phProof.t.buffer, sizeof(gp.phProof.t.buffer));
+	gp.shProof.t.size = PROOF_SIZE;
+	fill_rand_or_clear(gp.shProof.t.buffer, sizeof(gp.shProof.t.buffer));
+	gp.ehProof.t.size = PROOF_SIZE;
+	fill_rand_or_clear(gp.ehProof.t.buffer, sizeof(gp.ehProof.t.buffer));
 }
