jackadam1981/ChromeEc

cr50: debug EPS state before / after NV compaction
BUG=b:262324344
TEST=make ; verify that compaction works and that EPS size is properly
retrieved.

Change-Id: I6fb54cf96b4e51171962fc3d601d3214d80ddbd4
Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>

common/new_nvmem.c
@@ -921,7 +921,16 @@ test_export_static enum ec_error_list compact_nvmem(void)
 	struct access_tracker at = {};
 	int saved_object_count;
 	int final_delimiter_needed = 1;
+#ifdef BOARD_CR50
+	NV_RESERVED_ITEM ri;
+	uint16_t eps_seed_len = 0; /* Shall match TPM2B size type. */
 
+	/* (b/262324344): debugging EPS status. */
+	NvGetReserved(NV_EP_SEED, &ri);
+	_plat__NvMemoryRead(ri.offset, sizeof(eps_seed_len), &eps_seed_len);
+	if (eps_seed_len != ri.size - sizeof(gp.EPSeed.t.size))
+		CPRINTS("%s: EPS before is zero", __func__);
+#endif
 	/* How much space was used before compaction. */
 	before = total_used_size();
 
@@ -1021,6 +1030,13 @@ test_export_static enum ec_error_list compact_nvmem(void)
 
 	CPRINTS("Compaction done, went from %zd to %zd bytes", before,
 		total_used_size());
+
+	/* (b/262324344): debugging EPS status. */
+#ifdef BOARD_CR50
+	_plat__NvMemoryRead(ri.offset, sizeof(eps_seed_len), &eps_seed_len);
+	if (eps_seed_len != ri.size - sizeof(gp.EPSeed.t.size))
+		CPRINTS("%s: EPS after is zero, rv is %d", __func__, rv);
+#endif
 	return rv;
 }
 

coreboot/chrome-ec

Also added CONFIG_NVMEM_DEBUG_EPS to control EPS debugging once issue
is resolved.
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5023050
Code-Coverage: Vadim Sukhomlinov <sukhomlinov@chromium.org>
Reviewed-by: Vadim Sukhomlinov <sukhomlinov@chromium.org>
Reviewed-by: Mary Ruthven <mruthven@chromium.org>
Auto-Submit: Vadim Sukhomlinov <sukhomlinov@chromium.org>
Commit-Queue: Mary Ruthven <mruthven@chromium.org>
Tested-by: Vadim Sukhomlinov <sukhomlinov@chromium.org>
(cherry picked from commit 26fc47a88bf64500f098d34a8bf1cefacd85af59)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5045752
Tested-by: Mary Ruthven <mruthven@chromium.org>
Reviewed-by: Jett Rink <jettrink@chromium.org>
Auto-Submit: Mary Ruthven <mruthven@chromium.org>
(cherry picked from commit 696fdcdc85de8cc9d4f8af37f856d56fd7a6292e)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5126956

common/new_nvmem.c
@@ -921,7 +921,16 @@ test_export_static enum ec_error_list compact_nvmem(void)
 	struct access_tracker at = {};
 	int saved_object_count;
 	int final_delimiter_needed = 1;
+#ifdef CONFIG_NVMEM_DEBUG_EPS
+	NV_RESERVED_ITEM ri;
+	uint16_t eps_seed_len = 0; /* Shall match TPM2B size type. */
 
+	/* (b/262324344): debugging EPS status. */
+	NvGetReserved(NV_EP_SEED, &ri);
+	_plat__NvMemoryRead(ri.offset, sizeof(eps_seed_len), &eps_seed_len);
+	if (eps_seed_len != ri.size - sizeof(gp.EPSeed.t.size))
+		CPRINTS("%s: EPS before is zero", __func__);
+#endif
 	/* How much space was used before compaction. */
 	before = total_used_size();
 
@@ -1021,6 +1030,13 @@ test_export_static enum ec_error_list compact_nvmem(void)
 
 	CPRINTS("Compaction done, went from %zd to %zd bytes", before,
 		total_used_size());
+
+	/* (b/262324344): debugging EPS status. */
+#ifdef CONFIG_NVMEM_DEBUG_EPS
+	_plat__NvMemoryRead(ri.offset, sizeof(eps_seed_len), &eps_seed_len);
+	if (eps_seed_len != ri.size - sizeof(gp.EPSeed.t.size))
+		CPRINTS("%s: EPS after is zero, rv is %d", __func__, rv);
+#endif
 	return rv;
 }
 
@@ -1948,10 +1964,12 @@ static enum ec_error_list verify_reserved(uint8_t *reserved_bitmap,
 		container_body[0] = i;
 
 		switch (i) {
+#ifdef CONFIG_NVMEM_DEBUG_EPS
 		case NV_EP_SEED:
 			CPRINTS("%s: EPS not loaded", __func__);
 			cflush();
 			break;
+#endif
 			/*
 			 * No need to save these on initialization from
 			 * scratch, unmarshaling code will properly expand