ed6fb938a9cddedbf63874e40ba99e68299a7ab5
Vadim Sukhomlinov <sukhomlinov@google.com>
2024-01-02 12:32:26 -0800

    cr50: add overflow checking of input parameters for nvmem
    Improve nvmem overflow check to handle overflows/wrapping.
    
    BUG=b:287674488
    TEST=make buildall -j; check that image works on chromebook.
    
    Change-Id: Ie4f24228a77d509c211bb0b890a733d6a6df8096
    Signed-off-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5161147
    Reviewed-by: Vadim Bendebury <vbendeb@google.com>
    Commit-Queue: Vadim Bendebury <vbendeb@google.com>
    (cherry picked from commit bc1715c348dffa97cbbb940107cd4378f694da26)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5187277
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>

--- common/nvmem.c
+++ common/nvmem.c
@@ -281,7 +281,8 @@ static enum ec_error_list nvmem_get_partition_off(enum nvmem_users user,
 	 * Ensure that read/write operation that is calling this function
 	 * doesn't exceed the end of its buffer.
 	 */
-	if (offset + len > nvmem_user_sizes[user])
+	if (offset >= nvmem_user_sizes[user] || len >= nvmem_user_sizes[user] ||
+	    offset + len >= nvmem_user_sizes[user])
 		return EC_ERROR_OVERFLOW;
 	/* Compute offset within the partition for the rd/wr operation */
 	*p_buf_offset = start_offset + offset;
