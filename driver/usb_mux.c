jackadam1981/ChromeEc

usb_mux: add board_set callback
The callback exists on ToT, it allows to use board-specific callback
from usb_mux_set

BUG=b:183123775
TEST=manual test on dratini/dragonair

Change-Id: I76828f4739f317ae3cb7afa4b7edf16f75031a2e
Signed-off-by: Boris Mittelberg <bmbm@google.com>

driver/usb_mux/usb_mux.c
@@ -121,6 +121,10 @@ void usb_mux_set(int port, enum typec_mux mux_mode,
 		     "usb/dp mux: port(%d) typec_mux(%d) usb2(%d) polarity(%d)",
 		     port, mux_mode, usb_mode, polarity);
 
+	/* Apply board specific setting */
+	if (mux->board_set)
+		mux->board_set(mux, mux_state);
+
 	/*
 	 * If we are completely disconnecting the mux, then we should put it in
 	 * its lowest power state.

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4133689
Reviewed-by: Shelley Chen <shchen@chromium.org>
Tested-by: Shelley Chen <shchen@chromium.org>
Reviewed-by: Mary Ruthven <mruthven@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/4475136
Commit-Queue: Daisuke Nojiri <dnojiri@chromium.org>
Tested-by: Daisuke Nojiri <dnojiri@chromium.org>
Auto-Submit: Daisuke Nojiri <dnojiri@chromium.org>
(cherry picked from commit 31fa7019c684ca60e57996eb1d4425704bd28208)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5171329

driver/usb_mux.c
@@ -59,6 +59,10 @@ void usb_mux_set(int port, enum typec_mux mux_mode,
 		CPRINTS(
 		     "usb/dp mux: port(%d) typec_mux(%d) usb2(%d) polarity(%d)",
 		     port, mux_mode, usb_mode, polarity);
+
+	/* Apply board specific setting */
+	if (mux->board_set)
+		mux->board_set(port, mux_state);
 }
 
 int usb_mux_get(int port, const char **dp_str, const char **usb_str)