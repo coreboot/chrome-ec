6633d82fd153dc30fae7a710d081000188a575f5
Sean Carpenter <seancarpenter@google.com>
2024-08-19 14:06:30 -0700

    Creates an Android.bp for gsctool
    BUG=b:348470531
    TEST=manual - compiled and installed on a Brya Anahera and ensured core
    commands like gsctool -s -o function.
    
    Change-Id: I0017dc203eff17e4de8ea04e9ee826e4f03fec0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5792099
    Reviewed-by: Tim Van Patten <timvp@google.com>
    Commit-Queue: Sean Carpenter <seancarpenter@google.com>
    Reviewed-by: Jett Rink <jettrink@google.com>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Tested-by: Sean Carpenter <seancarpenter@google.com>

---
+++ android/android_generated_version.h
@@ -0,0 +1,11 @@
+// Copyright 2024 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO: b/360918766 - Add support for real version and build info
+#define CROS_EC_VERSION32 "UNKNOWN_EC_VERSION32"
+#define CROS_ECTOOL_VERSION "UNKNOWN_ECTOOL_VERSION"
+#define CROS_STM32MON_VERSION "UNKNOWN_STM32MON_VERSION"
+#define VERSION "UNKNOWN_VERSION"
+#define BUILDER "UNKNOWN_USER@UNKNOWN_HOSTNAME"
+#define DATE "UNKNOWN_DATE"
