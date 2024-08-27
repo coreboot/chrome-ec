jackadam1981/ChromeEc

squirtle: Add TCPC gpio config in board_reset_pd_mcu
Add tcpc gpio settings in board_reset_pd_mcu.
Prevent tcpc gpio from being uncontrollable when
the battery is no longer present after EC REBOOT.

BUG=b:361715142
TEST=zmake build squirtle

Change-Id: I65f7a4753b53aedf227f9ab9f32e82cf99c7b13d

zephyr/program/corsola/src/npcx_usbc.c
@@ -182,6 +182,13 @@ void board_reset_pd_mcu(void)
 #if CONFIG_USB_PD_PORT_MAX_COUNT > 1
 	/* reset C1 RT1718s */
 	rt1718s_sw_reset(USBC_PORT_C1);
+
+#ifdef CONFIG_BATTERY_PRESENT_CUSTOM
+	/* set GPIO 1~3 as push pull, as output, output low. */
+	rt1718s_gpio_set_flags(port, RT1718S_GPIO1, GPIO_OUT_LOW);
+	rt1718s_gpio_set_flags(port, RT1718S_GPIO2, GPIO_OUT_LOW);
+	rt1718s_gpio_set_flags(port, RT1718S_GPIO3, GPIO_OUT_LOW);
+#endif
 #endif /* CONFIG_USB_PD_PORT_MAX_COUNT > 1 */
 }
 

coreboot/chrome-ec

the battery is no longer present after EC reboot.
LOW_COVERAGE_REASON=Tests will be added in b:361715142
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5810398
Reviewed-by: Ting Shen <phoenixshen@chromium.org>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Commit-Queue: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
Reviewed-by: Siyu Qin <qinsiyu@huaqin.corp-partner.google.com>
Tested-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
(cherry picked from commit cf8a53e5d5b0d8941564932374a5dc2787f0a7ed)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5815669

zephyr/program/corsola/src/npcx_usbc.c
@@ -182,6 +182,13 @@ void board_reset_pd_mcu(void)
 #if CONFIG_USB_PD_PORT_MAX_COUNT > 1
 	/* reset C1 RT1718s */
 	rt1718s_sw_reset(USBC_PORT_C1);
+
+#ifdef CONFIG_BOARD_SQUIRTLE
+	/* set GPIO 1~3 as push pull, as output, output low. */
+	rt1718s_gpio_set_flags(USBC_PORT_C1, RT1718S_GPIO1, GPIO_OUT_LOW);
+	rt1718s_gpio_set_flags(USBC_PORT_C1, RT1718S_GPIO2, GPIO_OUT_LOW);
+	rt1718s_gpio_set_flags(USBC_PORT_C1, RT1718S_GPIO3, GPIO_OUT_LOW);
+#endif
 #endif /* CONFIG_USB_PD_PORT_MAX_COUNT > 1 */
 }
 