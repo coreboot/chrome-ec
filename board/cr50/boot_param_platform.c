3c41cfee6ed40890dd94e894404f47f0b4b173c8
Andrey Pronin <apronin@google.com>
2024-10-01 02:55:09 +0000

    cr50: implement boot_param_platform crypto methods
    This CL implements crypto and utility methods in boot_param_platform.c
    (all methods except __platform_get_dice_config and
    __platform_get_gsc_boot_param).
    
    BUG=b:365780447
    TEST=make BOARD=cr50
    
    Change-Id: Ic7bfb6770fb88f125d9cdc006ff927c30f923ce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5900845
    Tested-by: Andrey Pronin <apronin@chromium.org>
    Reviewed-by: Vadim Sukhomlinov <sukhomlinov@google.com>
    Auto-Submit: Andrey Pronin <apronin@chromium.org>
    Commit-Queue: Vadim Sukhomlinov <sukhomlinov@google.com>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>

--- board/cr50/boot_param_platform.c
+++ board/cr50/boot_param_platform.c
@@ -16,6 +16,18 @@
 #define verbose_log(s)
 #endif
 
+#define MAX_ECDSA_KEYGEN_ATTEMPTS 16
+
+/* Static ECDSA key seed. Only one key is allowed to be used at any time. */
+static uint8_t g_key_seed[P256_NBYTES] = { 0 };
+static int g_key_seed_valid = { 0 };
+
+static inline void invalidate_g_key_seed(void)
+{
+	memset(g_key_seed, 0, P256_NBYTES);
+	g_key_seed_valid = 0;
+}
+
 /* Perform HKDF-SHA256(ikm, salt, info) */
 bool __platform_hkdf_sha256(
 	/* [IN] input key material */
@@ -30,10 +42,14 @@ bool __platform_hkdf_sha256(
 	const struct slice_mut_s result
 )
 {
-	return false;
+	int res = DCRYPTO_hkdf(result.data, result.size,
+		salt.data, salt.size,
+		ikm.data, ikm.size,
+		info.data, info.size);
+	return res != 0;
 }
 
-/* Calculate SH256 for the provided buffer */
+/* Calculate SHA256 for the provided buffer */
 bool __platform_sha256(
 	/* [IN] data to hash */
 	const struct slice_ref_s data,
@@ -41,7 +57,10 @@ bool __platform_sha256(
 	uint8_t digest[DIGEST_BYTES]
 )
 {
-	return false;
+	/* SHA256_hw_hash produces big-endian hash */
+	SHA256_hw_hash(data.data, data.size, (struct sha256_digest *)digest);
+
+	return true;
 }
 
 /* Get DICE config */
@@ -50,8 +69,71 @@ bool __platform_get_dice_config(
 	struct dice_config_s *cfg
 )
 {
+	/* TODO: get all configuration variables */
 	return false;
 }
+
+/* Get GSC boot parameters */
+bool __platform_get_gsc_boot_param(
+	/* [OUT] early entropy */
+	uint8_t early_entropy[EARLY_ENTROPY_BYTES],
+	/* [OUT] SessionKeySeed */
+	uint8_t session_key_seed[KEY_SEED_BYTES],
+	/* [OUT] AuthTokenKeySeed */
+	uint8_t auth_token_key_seed[KEY_SEED_BYTES]
+)
+{
+	/* TODO: get all configuration variables */
+	return false;
+}
+
+/*
+ * This function is used Generate ECDSA P-256 key from the DRBG output.
+ *
+ * We can't just call DCRYPTO_p256_key_from_bytes() on the unadjusted
+ * DRBG bytes because it adds 1 to the provided seed. And we need to
+ * follow an exact keygen sequence (which doesn't add 1) so that AP FW
+ * can generate the same CDI key from CDI.
+ *
+ * We can't change DCRYPTO_p256_key_from_bytes() - it is used for EK,
+ * and we can't add a new function to dcrypto - it would change the u2f
+ * certified binary. So, we have to create a custom function here.
+ *
+ * Instead of recreating the steps of DCRYPTO_p256_key_from_bytes() locally,
+ * this custom function subtracts 1 from the seed before passing it to
+ * DCRYPTO_p256_key_from_bytes(). Adding a big-endian subtraction cycle
+ * should result in smaller code than copy-pasting the entire logic.
+ *
+ * We don't need to check that the original value is not 0 because
+ * DCRYPTO_p256_key_from_bytes() will fail if it sees all 0xFF.
+ *
+ * Having an adjust_drbg_bytes() step after hmac_drbg_generate()
+ * is chosen over wrapping DCRYPTO_p256_key_from_bytes() in a
+ * custom function because DCRYPTO_p256_key_from_bytes() is called
+ * from multiple places: __platform_ecdsa_p256_keygen_hmac_drbg,
+ * __platform_ecdsa_p256_sign, and __platform_ecdsa_p256_get_pub_key.
+ * Doing the sub-1 correction in all of them would require making a local
+ * copy of g_key_seed (since otherwise we'd be decrementing it on every
+ * use). Having an adjust_drbg_bytes step in one place allows us to
+ * avoid this extra copy.
+ */
+static inline void adjust_drbg_bytes(
+	/* [IN/OUT] bytes from DRBG, used as a big-endian */
+	uint8_t key_bytes[P256_NBYTES]
+)
+{
+	int i;
+
+	for (i = P256_NBYTES - 1; i >= 0; i--) {
+		if (key_bytes[i] == 0) {
+			key_bytes[i] = 0xFF;
+		} else {
+			key_bytes[i]--;
+			return;
+		}
+	}
+}
+
 
 /* Generate ECDSA P-256 key using HMAC-DRBG initialized by the seed */
 bool __platform_ecdsa_p256_keygen_hmac_drbg(
@@ -61,7 +143,70 @@ bool __platform_ecdsa_p256_keygen_hmac_drbg(
 	const void **key
 )
 {
-	return false;
+	p256_int d;
+	struct drbg_ctx drbg;
+	enum dcrypto_result result = DCRYPTO_FAIL;
+	size_t attempt = 0;
+
+	*key = NULL;
+
+	hmac_drbg_init(&drbg, seed, DIGEST_BYTES,
+		NULL, 0, NULL, 0,
+		HMAC_DRBG_DO_NOT_AUTO_RESEED);
+
+	g_key_seed_valid = 0;
+	do {
+		result = hmac_drbg_generate(&drbg,
+			g_key_seed, sizeof(g_key_seed),
+			NULL, 0);
+		if (result != DCRYPTO_OK) {
+			verbose_log("hmac_drbg_generate failed");
+			drbg_exit(&drbg);
+			invalidate_g_key_seed();
+			return false;
+		}
+		/* See the description of adjust_drbg_bytes() above */
+		adjust_drbg_bytes(g_key_seed);
+		result = DCRYPTO_p256_key_from_bytes(NULL, NULL,
+			&d, g_key_seed);
+		attempt++;
+	} while (result == DCRYPTO_RETRY &&
+		 attempt < MAX_ECDSA_KEYGEN_ATTEMPTS);
+	drbg_exit(&drbg);
+	if (result != DCRYPTO_OK) {
+		verbose_log("DCRYPTO_p256_key_from_bytes failed");
+		invalidate_g_key_seed();
+		return false;
+	}
+
+	g_key_seed_valid = 1;
+	*key = g_key_seed;
+	return true;
+}
+
+static bool get_ecdsa_points(
+	/* [IN] ECDSA key handle */
+	const void *key,
+	/* [OUT] x as p256_int, can pass NULL if not required */
+	p256_int *x,
+	/* [OUT] y as p256_int, can pass NULL if not required */
+	p256_int *y,
+	/* [OUT] d as p256_int, must be non-NULL */
+	p256_int *d
+)
+{
+	enum dcrypto_result result = DCRYPTO_FAIL;
+
+	if (key != g_key_seed || !g_key_seed_valid) {
+		verbose_log("__platform_ecdsa_p256_get_pub_key: invalid key");
+		return false;
+	}
+	result = DCRYPTO_p256_key_from_bytes(x, y, d, g_key_seed);
+	if (result != DCRYPTO_OK) {
+		verbose_log("DCRYPTO_p256_key_from_bytes failed");
+		return false;
+	}
+	return true;
 }
 
 /* Generate ECDSA P-256 signature: 64 bytes (R | S) */
@@ -74,6 +219,30 @@ bool __platform_ecdsa_p256_sign(
 	uint8_t signature[ECDSA_SIG_BYTES]
 )
 {
+	struct sha256_digest digest;
+	p256_int d, h, r, s;
+	struct drbg_ctx ctx;
+	enum dcrypto_result result = DCRYPTO_FAIL;
+
+	if (!get_ecdsa_points(key, NULL, NULL, &d))
+		return false;
+
+	/* SHA256_hw_hash produces big-endian hash */
+	SHA256_hw_hash(data.data, data.size, &digest);
+	p256_from_bin(digest.b8, &h);
+
+	hmac_drbg_init_rfc6979(&ctx, &d, &h);
+	result = dcrypto_p256_ecdsa_sign(&ctx, &d, &h, &r, &s);
+	drbg_exit(&ctx);
+
+	if (result != DCRYPTO_OK) {
+		verbose_log("dcrypto_p256_ecdsa_sign failed");
+		return false;
+	}
+
+	p256_to_bin(&r, signature);
+	p256_to_bin(&s, signature + ECDSA_POINT_BYTES);
+
 	return false;
 }
 
@@ -85,7 +254,15 @@ bool __platform_ecdsa_p256_get_pub_key(
 	struct ecdsa_public_s *pub_key
 )
 {
-	return false;
+	p256_int x, y, d;
+
+	if (!get_ecdsa_points(key, &x, &y, &d))
+		return false;
+
+	p256_to_bin(&x, pub_key->x);
+	p256_to_bin(&y, pub_key->y);
+
+	return true;
 }
 
 /* Free ECDSA key handle */
@@ -94,6 +271,7 @@ void __platform_ecdsa_p256_free(
 	const void *key
 )
 {
+	invalidate_g_key_seed();
 }
 
 /* Check if APROV status allows making 'normal' boot mode decision */
@@ -102,7 +280,11 @@ bool __platform_aprov_status_allows_normal(
 	uint32_t aprov_status
 )
 {
-	return false;
+	/*
+	 * For Cr50 ARPOV status is ignored,
+	 * so this method always returns true.
+	 */
+	return true;
 }
 
 /* Print error string to log */
@@ -111,6 +293,8 @@ void __platform_log_str(
 	const char *str
 )
 {
+	ccprintf("%s\n", str);
+	cflush();
 }
 
 /* memcpy */
