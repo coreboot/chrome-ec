ce6f85ab6273d2e9b52c4df4a117c967cf21d03f
Firas Sammoura <fsammoura@google.com>
2024-10-14 21:05:35 +0000

    driver/fingerprint: Change return to assert in PLAT_FREE
    Change the return to assert in PLAT_FREE to catch the free of the
    pointer to a NULL.
    
    BUG=b:373446652
    TEST=CQ
    
    Change-Id: I2762fc71a8f2b438d39babda5b23b54397519ac4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5931965
    Reviewed-by: Jacob Kung <jacob.kung@egistec.corp-partner.google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Firas Sammoura <fsammoura@google.com>
    Commit-Queue: Firas Sammoura <fsammoura@google.com>

--- driver/fingerprint/egis/platform/inc/plat_mem.h
+++ driver/fingerprint/egis/platform/inc/plat_mem.h
@@ -6,6 +6,7 @@
 #ifndef __CROS_EC_DRIVER_FINGERPRINT_EGIS_PLATFORM_INC_PLAT_MEM_H_
 #define __CROS_EC_DRIVER_FINGERPRINT_EGIS_PLATFORM_INC_PLAT_MEM_H_
 
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -22,14 +23,10 @@ static inline void plat_free(void *x)
 	sys_free(x);
 }
 
-// TODO (b/373446652): Change the return to an ASSERT to catch the free of a
-// pointer to NULL.
 // TODO (b/373435445): Combine PLAT_FREE and plat_free.
 static inline void PLAT_FREE(void **x)
 {
-	if (x == NULL || *x == NULL) {
-		return;
-	}
+	assert(x != NULL && *x != NULL);
 	plat_free(*x);
 	*x = NULL;
 }
