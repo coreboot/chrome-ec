6534eabd77d6edfb2880e89f78d0237580b47e3c
wangganxiang <wangganxiang@huaqin.corp-partner.google.com>
2024-10-22 19:12:27 +0800

    Rull/Roric/Ruke: Add straus keyboard
    Added support for strauss keyboard and backlight configuration.
    
    BUG=b:372211281
    TEST=1.zmake build rull
    LOW_COVERAGE_REASON=Tests will be added in b:372211281
    
    Change-Id: I7ddb7c2494cfc4958532179ccad2a50481d53e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5952665
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Reviewed-by: Peter Marheine <pmarheine@chromium.org>
    Tested-by: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Commit-Queue: Ganxiang Wang <wangganxiang@huaqin.corp-partner.google.com>
    Reviewed-by: Jiahong Wei <weijiahong@huaqin.corp-partner.google.com>

--- zephyr/program/nissa/rull/src/board.c
+++ zephyr/program/nissa/rull/src/board.c
@@ -11,11 +11,13 @@
 #include "fan.h"
 #include "gpio/gpio_int.h"
 #include "hooks.h"
+#include "keyboard_backlight.h"
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(board_init, LOG_LEVEL_INF);
+#define CPRINTS(format, args...) cprints(CC_SYSTEM, format, ##args)
 
 enum battery_present battery_hw_present(void)
 {
@@ -45,3 +47,39 @@ test_export_static void fan_init(void)
 	}
 }
 DECLARE_HOOK(HOOK_INIT, fan_init, HOOK_PRIO_POST_FIRST);
+
+static bool key_bl = FW_KB_BL_NOT_PRESENT;
+
+int8_t board_vivaldi_keybd_idx(void)
+{
+	CPRINTS("idx:This is %s kb backlight.", key_bl ? "with" : "without");
+	if (key_bl == FW_KB_BL_NOT_PRESENT) {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_0));
+	} else {
+		return DT_NODE_CHILD_IDX(DT_NODELABEL(kbd_config_1));
+	}
+}
+
+/*
+ * Keyboard function decided by FW config.
+ */
+test_export_static void kb_init(void)
+{
+	int ret;
+	uint32_t val;
+
+	ret = cros_cbi_get_fw_config(FW_KB_BL, &val);
+	if (ret != 0) {
+		LOG_ERR("Error retrieving CBI FW_CONFIG field %d", FW_KB_BL);
+		return;
+	}
+
+	CPRINTS("This is %s kb backlight.", key_bl ? "with" : "without");
+	if (val == FW_KB_BL_PRESENT) {
+		key_bl = FW_KB_BL_PRESENT;
+	} else {
+		key_bl = FW_KB_BL_NOT_PRESENT;
+		kblight_enable(0);
+	}
+}
+DECLARE_HOOK(HOOK_INIT, kb_init, HOOK_PRIO_POST_FIRST);
