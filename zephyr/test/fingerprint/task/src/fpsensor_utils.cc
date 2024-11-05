e10b7b2dd7e695d11b765cd0a24222f1ede05048
Craig Hesling <hesling@chromium.org>
2024-11-05 15:52:18 -0800

    test: Change default/normal input for is_raw_capture test
    The normal usage of is_raw_capture is without the FP_MODE_CAPTURE
    bit set. This has been corrected in the https://crrev.com/c/5995276
    revert.
    
    BUG=b:375982132
    TEST=Run host tests
    
    Change-Id: I4a484fe64ba4fca01cd90c1e89bc9659d3ebe09f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/5996691
    Reviewed-by: Firas Sammoura <fsammoura@google.com>
    Reviewed-by: Tom Hughes <tomhughes@chromium.org>
    Tested-by: Craig Hesling <hesling@chromium.org>
    Commit-Queue: Craig Hesling <hesling@chromium.org>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>

--- zephyr/test/fingerprint/task/src/fpsensor_utils.cc
+++ zephyr/test/fingerprint/task/src/fpsensor_utils.cc
@@ -69,22 +69,21 @@ ZTEST(fpsensor_utils, test_is_test_capture)
 
 ZTEST(fpsensor_utils, test_is_raw_capture)
 {
-	zassert_true(is_raw_capture(FP_MODE_CAPTURE |
-				    FP_CAPTURE_VENDOR_FORMAT
-					    << FP_MODE_CAPTURE_TYPE_SHIFT));
-	zassert_false(is_raw_capture(FP_MODE_CAPTURE |
-				     FP_CAPTURE_SIMPLE_IMAGE
-					     << FP_MODE_CAPTURE_TYPE_SHIFT));
-	zassert_false(is_raw_capture(FP_MODE_CAPTURE |
-				     FP_CAPTURE_PATTERN0
-					     << FP_MODE_CAPTURE_TYPE_SHIFT));
-	zassert_false(is_raw_capture(FP_MODE_CAPTURE |
-				     FP_CAPTURE_PATTERN1
-					     << FP_MODE_CAPTURE_TYPE_SHIFT));
+	zassert_true(is_raw_capture(FP_CAPTURE_VENDOR_FORMAT
+				    << FP_MODE_CAPTURE_TYPE_SHIFT));
+	zassert_false(is_raw_capture(FP_CAPTURE_SIMPLE_IMAGE
+				     << FP_MODE_CAPTURE_TYPE_SHIFT));
+	zassert_false(is_raw_capture(FP_CAPTURE_PATTERN0
+				     << FP_MODE_CAPTURE_TYPE_SHIFT));
+	zassert_false(is_raw_capture(FP_CAPTURE_PATTERN1
+				     << FP_MODE_CAPTURE_TYPE_SHIFT));
+	zassert_true(is_raw_capture(FP_CAPTURE_QUALITY_TEST
+				    << FP_MODE_CAPTURE_TYPE_SHIFT));
+	zassert_false(is_raw_capture(FP_CAPTURE_RESET_TEST
+				     << FP_MODE_CAPTURE_TYPE_SHIFT));
+
+	/* Check the case when FP_MODE_CAPTURE is set. */
 	zassert_true(is_raw_capture(FP_MODE_CAPTURE |
 				    FP_CAPTURE_QUALITY_TEST
 					    << FP_MODE_CAPTURE_TYPE_SHIFT));
-	zassert_false(is_raw_capture(FP_MODE_CAPTURE |
-				     FP_CAPTURE_RESET_TEST
-					     << FP_MODE_CAPTURE_TYPE_SHIFT));
 }
