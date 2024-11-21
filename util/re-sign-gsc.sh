0fa1b4117824a172f0b0707ba84c5cef00c51a9d
Mary Ruthven <mruthven@chromium.org>
2024-11-20 19:21:13 -0600

    re-sign-gsc: support signing from any directory
    Use paths relative to the script dir, so re-sign-gsc can run from any
    directory.
    
    BUG=b:380145576
    TEST=cd /tmp/test_sign and run the re-sign-gsc script. Verify an image
    is created in the tmp directory.
    
    Change-Id: Ide3a47645876407308aac85f6f4b5a8148a2c784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6042063
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@google.com>

--- util/re-sign-gsc.sh
+++ util/re-sign-gsc.sh
@@ -12,9 +12,10 @@
 # The image to re-sign can be supplied as an optional third param.
 #
 # The generated binary is saved in {cr,ti}50.<sha>.<devid0>.<devid1>.bin where
-# <sha> is the git sha of the current source tree. The suffix '-dirty' is
-# added to the <sha> component in case there are changes in any of the files
-# under git control.
+# <sha> is the git sha of the original image. The suffix '-dirty' is added to
+# the <sha> component if "+" is in the {cr,ti}50 version string. "+" means that
+# there were changes in some of the files under git control when the original
+# image was built.
 
 set -ue
 
@@ -133,12 +134,14 @@ main () {
   local dev_id1
   local flash_base
   local full_bin
+  local gsc_dir
   local manifest
   local output
   local prefix
   local rw_a_base
   local rw_b_base
   local rw_key
+  local rw_ver
   local sha
   local tmp_file
   local xml
@@ -191,17 +194,18 @@ main () {
   case "${bin_size}" in
     (524288) rw_a_base=16384 # RO area size is fixed at 16K
              rw_b_base=$(( bin_size / 2 + rw_a_base ))
+             gsc_dir="${SCRIPT_DIR}/../../cr50"
 	     key_name="cr50-hsm-backed-node-locked-key"
-             rw_key="util/signer/${key_name}.pem.pub"
-             manifest="util/signer/ec_RW-manifest-dev.json"
-             xml="util/signer/fuses.xml"
+             rw_key="${gsc_dir}/util/signer/${key_name}.pem.pub"
+             manifest="${gsc_dir}/util/signer/ec_RW-manifest-dev.json"
+             xml="${gsc_dir}/util/signer/fuses.xml"
              codesigner_params+=(
                --b
                --pkcs11_engine="${PKCS11_MODULE_PATH}:0:${KMS_PROJECT_PATH}/${key_name}/cryptoKeyVersions/1"
              )
              flash_base=262144
              prefix="cr50"
-             KMS_PKCS11_CONFIG="$(readlink -f chip/g/config.yaml)"
+             KMS_PKCS11_CONFIG="$(readlink -f "${gsc_dir}/chip/g/config.yaml")"
              ;;
     (1048576) local rw_bases
               # Third and sixths lines showing signed header magic are base
@@ -215,27 +219,31 @@ main () {
                    }')
               rw_a_base="${rw_bases[0]}"
               rw_b_base="${rw_bases[1]}"
-              rw_key="ports/dauntless/signing/ti50_dev.key"
-              manifest="ports/dauntless/signing/manifest.TOT.json"
-              xml="ports/dauntless/signing/fuses.xml"
+              gsc_dir="${SCRIPT_DIR}/../../ti50/common"
+              rw_key="${gsc_dir}/ports/dauntless/signing/ti50_dev.key"
+              manifest="${gsc_dir}/ports/dauntless/signing/manifest.TOT.json"
+              xml="${gsc_dir}/ports/dauntless/signing/fuses.xml"
               codesigner_params+=(
                 --dauntless
                 --pkcs11_engine="${PKCS11_MODULE_PATH}:0:${KMS_PROJECT_PATH}/ti50-node-locked-key/cryptoKeyVersions/1"
               )
               flash_base=524288
               prefix="ti50"
-              KMS_PKCS11_CONFIG="$(readlink -f ports/dauntless/config.yaml)"
-
+              KMS_PKCS11_CONFIG="$(readlink -f "${gsc_dir}/ports/dauntless/config.yaml")"
               ;;
     (*) echo "What is ${full_bin}?" >&2
         exit 1
         ;;
   esac
 
-  # Determine the current git tree state. This would match the binary image's
-  # version string if it was built from this tree.
-  sha="$(git rev-parse --short HEAD)"
-  if git status --porcelain 2>&1 | grep -qv '^ *?'; then
+  # Extract the sha from the original image version string. Find the version
+  # string from the image. Use the sha from the ti50 or cr50 repo.
+  rw_ver="$(strings "${full_bin}" | grep -m 1 "${prefix}_.*tpm" | \
+	  sed -E "s/.*(${prefix}\S*).*/\1/")"
+  sha="${rw_ver/*[-+]/}"
+  # If the rw version contains a "+" then the repo was not clean when the image
+  # was built. Always re-sign these images.
+  if [[ "${rw_ver}" =~ \+ ]] ; then
     sha="${sha}-dirty"
   fi
 
@@ -278,6 +286,7 @@ main () {
              "${codesigner_params[@]}"
 
   cp "${tmp_file}" "${output}"
+  echo "signed image at ${output}"
 }
 
 main "$@"
