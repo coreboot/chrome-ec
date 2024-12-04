1751d4f8410ab7f65c247f523a02caa3f95e22f1
guojun <guojun@huaqin.corp-partner.google.com>
2024-12-04 10:43:34 +0800

    Rull/Roric/Ruke: Modify temp_sensor parameters with 6w and 15w
    Due the system thermal policy and fan table requirements, modify the
    temp_sensor parameters compatible with 6w and 15w thermal protection mechanism.
    
    BUG=b:383032918
    TEST=1.zmake build rull
         2.test verification pass
    LOW_COVERAGE_REASON=Tests will be added in b:383032918
    
    Change-Id: Id0f0e570a5ae5317fdeef54b336064355a87e532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6069426
    Reviewed-by: Mike Lee <mike5@huaqin.corp-partner.google.com>
    Reviewed-by: Kyle Lin <kylelinck@google.com>
    Tested-by: guojun guojun <guojun@huaqin.corp-partner.google.com>
    Reviewed-by: Jeremy Lin <poseng@google.com>
    Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
    Commit-Queue: Kyle Lin <kylelinck@google.com>

--- zephyr/program/nissa/rull/src/board.c
+++ zephyr/program/nissa/rull/src/board.c
@@ -15,6 +15,8 @@
 #include "hooks.h"
 #include "keyboard_backlight.h"
 #include "keyboard_customization.h"
+#include "temp_sensor/temp_sensor.h"
+#include "thermal.h"
 #include "util.h"
 
 #include <zephyr/drivers/gpio.h>
@@ -88,6 +90,72 @@ enum ec_status charger_profile_override_set_param(uint32_t param,
 	return EC_RES_INVALID_PARAM;
 }
 
+/* thermal tabel control for 15W CPU */
+#define TEMP_VR TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_vr))
+#define TEMP_CPU TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_cpu))
+#define TEMP_AMBIENT TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_ambient))
+#define TEMP_CHARGER TEMP_SENSOR_ID(DT_NODELABEL(temp_sensor_charger))
+
+const static struct ec_thermal_config thermal_vr = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(93),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(88),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(51),
+	.temp_fan_max = C_TO_K(67),
+};
+
+const static struct ec_thermal_config thermal_cpu = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(93),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(88),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(50),
+	.temp_fan_max = C_TO_K(70),
+};
+
+const static struct ec_thermal_config thermal_ambient = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(77),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(80),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(75),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(40),
+	.temp_fan_max = C_TO_K(50),
+};
+
+const static struct ec_thermal_config thermal_charger = {
+	.temp_host = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(93),
+		[EC_TEMP_THRESH_HALT] = C_TO_K(95),
+	},
+	.temp_host_release = {
+		[EC_TEMP_THRESH_WARN] = 0,
+		[EC_TEMP_THRESH_HIGH] = C_TO_K(90),
+		[EC_TEMP_THRESH_HALT] = 0,
+	},
+	.temp_fan_off = C_TO_K(55),
+	.temp_fan_max = C_TO_K(75),
+};
+
 test_export_static void fan_init(void)
 {
 	int ret;
@@ -103,6 +171,12 @@ test_export_static void fan_init(void)
 	if (val != FW_FAN_PRESENT) {
 		/* Disable the fan */
 		fan_set_count(0);
+	} else {
+		thermal_params[TEMP_VR] = thermal_vr;
+		thermal_params[TEMP_CPU] = thermal_cpu;
+		thermal_params[TEMP_AMBIENT] = thermal_ambient;
+		thermal_params[TEMP_CHARGER] = thermal_charger;
+		CPRINTS("Thermal: 15W thermal control");
 	}
 }
 DECLARE_HOOK(HOOK_INIT, fan_init, HOOK_PRIO_POST_FIRST);
