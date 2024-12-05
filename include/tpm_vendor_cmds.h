3d7558f8408143e129c04e9cc6513fc6031cbd38
Jett Rink <jettrink@google.com>
2024-12-05 12:16:18 -0700

    gsctool: Rename variable to tpm_did_vid
    Variable contains the TPM DID_VID not USB VID:PID
    
    BUG=none
    TEST=none
    
    Change-Id: Ied37701ec607a0d255604e982c8012d7f956fa2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6075365
    Auto-Submit: Jett Rink <jettrink@google.com>
    Tested-by: Jett Rink <jettrink@google.com>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Jett Rink <jettrink@google.com>
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    (cherry picked from commit cdc04ca8e6a03d1e37984e12f8b4dd74427e618a)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6102434
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Jett Rink <jettrink@google.com>
    Auto-Submit: Mary Ruthven <mruthven@chromium.org>

--- include/tpm_vendor_cmds.h
+++ include/tpm_vendor_cmds.h
@@ -334,7 +334,7 @@ enum ap_ro_check_vc_errors {
 
 /* Returns info to identify the specific GSC chip type. */
 struct get_chip_id_response {
-	uint32_t tpm_vid_pid;
+	uint32_t tpm_did_vid;
 	uint32_t chip_id;
 };
 
