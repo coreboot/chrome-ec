jackadam1981/ChromeEc

trulo: Initial zephyr config for uldrenite
Initial EC Zephyr config for Trulo/Uldrenite

BUG=b:380789023
TEST=zmake build uldrenite success
LOW_COVERAGE_REASON=b:380789023 initial bring-up

Change-Id: I2b164077d3cdb0c4303d62cc04b38b36327bffb1

zephyr/program/trulo/BUILD.py
@@ -55,7 +55,19 @@ register_trulo_project(
     modules=["cmsis", "picolibc", "ec", "pigweed"],
 )
 
+register_trulo_project(
+    project_name="uldrenite",
+    kconfig_files=[
+        # Common to all projects.
+        here / "program.conf",
+        # Parent project's config
+        here / "trulo" / "project.conf",
+    ],
+    modules=["cmsis", "picolibc", "ec", "pigweed"],
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="trulo", addr=0x40144)
 assert_rw_fwid_DO_NOT_EDIT(project_name="trulo-ti", addr=0x40144)
+assert_rw_fwid_DO_NOT_EDIT(project_name="uldrenite", addr=0x40144)

coreboot/chrome-ec

Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6051623
Tested-by: Logan Liao <logan_liao@compal.corp-partner.google.com>
Reviewed-by: Logan Liao <logan_liao@compal.corp-partner.google.com>
Code-Coverage: Zoss <zoss-cl-coverage@prod.google.com>
Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>
Reviewed-by: Ivan Chen <yulunchen@google.com>
Commit-Queue: Ivan Chen <yulunchen@google.com>
(cherry picked from commit 87751553c86fcacec33db036b6202a3b1ec9af80)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/6099794
Commit-Queue: Logan Liao <logan_liao@compal.corp-partner.google.com>

zephyr/program/trulo/BUILD.py
@@ -33,6 +33,17 @@ register_trulo_project(
     project_name="trulo",
 )
 
+register_trulo_project(
+    project_name="uldrenite",
+    kconfig_files=[
+        # Common to all projects.
+        here / "program.conf",
+        # Parent project's config
+        here / "uldrenite" / "project.conf",
+    ],
+)
+
 # Note for reviews, do not let anyone edit these assertions, the addresses
 # must not change after the first RO release.
 assert_rw_fwid_DO_NOT_EDIT(project_name="trulo", addr=0x40144)
+assert_rw_fwid_DO_NOT_EDIT(project_name="uldrenite", addr=0x40144)